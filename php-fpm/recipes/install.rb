2e2c14477fec7055800a572cebe41a39aab51e84
Till Klampaeckel <till@php.net>
2010-08-17 17:02:49 +0200

    * attempt to fix sed commands  * added missing "

--- php-fpm/recipes/install.rb
+++ php-fpm/recipes/install.rb
@@ -24,38 +24,39 @@ execute "PHP: ./configure" do
   not_if &php_already_installed
 end
 
-execute "PHP: make && make install" do
-  cwd "/tmp/php-#{node["php-fpm][:version]}"
+execute "PHP: make, make install" do
+  cwd "/tmp/php-#{node["php-fpm"][:version]}"
   environment "HOME" => "/root"
   command "make && make install"
   not_if &php_already_installed
 end
 
+:phpini = "/etc/php5/cgi/php.ini"
 execute "PHP: post install configuration" do
-  # turn on error logging, turn off display of errors
 
+  # turn on error logging, turn off display of errors
   if node["php-fpm"][:logerrors]
-    command "sed -i "s,log_errors = Off,log_errors = On,g" /etc/php5/cgi/php.ini"
-    command "sed -i "s,;error_log = filename,error_log = #{node["php-fpm"][:logfile]},g" $php_ini"
+    command %Q{ sed -i "s,log_errors = Off,log_errors = On,g" #{phpini} }
+    command %Q{ sed -i "s,;error_log = filename,error_log = #{node["php-fpm"][:logfile]},g" #{phpini} }
   end
 
   unless node["php-fpm"][:displayerrors]
-    command "sed -i "s,display_errors = On,display_errors = Off,g" $php_ini"
+    command %Q{ sed -i "s,display_errors = On,display_errors = Off,g" #{phpini} }
   end
 
   # hide PHP
-  command "sed -i "s,expose_php = On,expose_php = Off,g" $php_ini"
+  command %Q{ sed -i "s,expose_php = On,expose_php = Off,g" #{phpini} }
 
   # realpath cache
-  command "sed -i "s,; realpath_cache_size=16k,realpath_cache_size=128k,g" $php_ini"
-  command "sed -i "s,; realpath_cache_ttl=120,realpath_cache_ttl=3600,g" $php_ini"
+  command %Q{ sed -i "s,; realpath_cache_size=16k,realpath_cache_size=128k,g" #{phpini} }
+  command %Q{ sed -i "s,; realpath_cache_ttl=120,realpath_cache_ttl=3600,g" #{phpini} }
 
   # up the memory_limit and max_execution_time
-  command "sed -i "s,memory_limit = 16M,memory_limit = #{node["php-fpm"][:memorylimit]},g" $php_ini"
-  command "sed -i "s,max_execution_time = 30,max_execution_time = 60,g" $php_ini"
+  command %Q{ sed -i "s,memory_limit = 16M,memory_limit = #{node["php-fpm"][:memorylimit]},g" #{phpini} }
+  command %Q{ sed -i "s,max_execution_time = 30,max_execution_time = #{node["php-fpm"][:maxexecutiontime]},g" #{phpini} }
 
   # fix ubuntu fuck ups
-  command "sed -i "s,magic_quotes_gpc = On,magic_quotes_gpc = Off,g" $php_ini"
+  command %Q{ sed -i "s,magic_quotes_gpc = On,magic_quotes_gpc = Off,g" #{phpini} }
 end
 
 execute "PHP: install pear packages" do
@@ -67,6 +68,5 @@ end
 service "php-fpm" do
   service_name "php-fpm"
   supports [:start, :status, :restart]
-  action :start
-  not_if "which couchdb > /dev/null && couchdb -s"
+  action :restart
 end
