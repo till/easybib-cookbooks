37749fc79a2db85eae59b485140978ee26001e1a
Mathias Meyer <meyer@paperplanes.de>
2010-08-18 21:20:34 +0800

    Fixed user and group for deployment.

--- nginx-app/recipes/configure.rb
+++ nginx-app/recipes/configure.rb
@@ -4,8 +4,8 @@ node[:deploy].each do |application, deploy|
   template "/etc/nginx/sites-available/easybib.com.conf" do
     source "easybib.com.conf.erb"
     mode "0755"
-    owner node["nginx-app"][:user]
-    group node["nginx-app"][:group]
+    owner deploy[:user]
+    group deploy[:group]
     variables :deploy => deploy, :application => application
     notifies :restart, resources(:service => "nginx"), :delayed
   end
