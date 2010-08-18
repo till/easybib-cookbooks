cceb9bebed57ab96d5001b68b601c5f05e646ee4
Till Klampaeckel <till@php.net>
2010-08-18 17:44:38 +0200

    * removed sed calls for php.ini template

--- php-fpm/recipes/install.rb
+++ php-fpm/recipes/install.rb
@@ -31,30 +31,11 @@ execute "PHP: make, make install" do
   not_if &php_already_installed
 end
 
-phpini = "/etc/php5/cgi/php.ini"
-execute "PHP: post install configuration" do
-
-  # turn on error logging, turn off display of errors
-  if node["php-fpm"][:logerrors]
-    command %Q{ sed -i "s,log_errors = Off,log_errors = On,g" #{phpini} }
-    command %Q{ sed -i "s,;error_log = filename,error_log = #{node["php-fpm"][:logfile]},g" #{phpini} }
-  end
-
-  unless node["php-fpm"][:displayerrors]
-    command %Q{ sed -i "s,display_errors = On,display_errors = Off,g" #{phpini} }
-  end
-
-  # hide PHP
-  command %Q{ sed -i "s,expose_php = On,expose_php = Off,g" #{phpini} }
-
-  # realpath cache
-  command %Q{ sed -i "s,; realpath_cache_size=16k,realpath_cache_size=128k,g" #{phpini} }
-  command %Q{ sed -i "s,; realpath_cache_ttl=120,realpath_cache_ttl=3600,g" #{phpini} }
-
-  # up the memory_limit and max_execution_time
-  command %Q{ sed -i "s,memory_limit = 16M,memory_limit = #{node["php-fpm"][:memorylimit]},g" #{phpini} }
-  command %Q{ sed -i "s,max_execution_time = 30,max_execution_time = #{node["php-fpm"][:maxexecutiontime]},g" #{phpini} }
-
+template "/usr/local/lib/php.ini" do
+  mode "0755"
+  source "php.ini.erb"
+  owner "www-data"
+  group "www-data"
 end
 
 service "php-fpm" do
