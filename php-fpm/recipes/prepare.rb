a8968ec3f4d431332aaa66454015e8077c721b8e
Till Klampaeckel <till@php.net>
2010-08-18 16:49:33 +0200

    * turned shell commands into chef resources  * small bugfixes

--- php-fpm/recipes/prepare.rb
+++ php-fpm/recipes/prepare.rb
@@ -12,12 +12,14 @@ execute "create PHP's logfile" do
   command "touch #{node["php-fpm"][:logfile]}"
 end
 
-execute "create standard temporary directory" do
-  command "mkdir -p #{node["php-fpm"][:tmpdir]}"
-  command "chown #{node["php-fpm"][:user]}:#{node["php-fpm"][:group]} #{node["php-fpm"][:tmpdir]}"
+directory node["php-fpm"][:tmpdir] do
+  mode "0755"
+  owner node["php-fpm"][:user]
+  group node["php-fpm"][:group]
 end
-  
 
-execute "ensure correct permissions on PHP's log file" do
-  command "chown -R #{node["php-fpm"][:user]}:#{node["php-fpm"][:group]} #{node["php-fpm"][:logfile]}"
+file node["php-fpm"][:logfile] do
+  mode "0755"
+  owner node["php-fpm"][:user]
+  group node["php-fpm"][:group]
 end
