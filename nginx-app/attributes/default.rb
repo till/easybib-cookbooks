7d978211c627b9f791cbd6f73cd6d266ad8e9006
Till Klampaeckel <till@php.net>
2010-08-18 15:21:04 +0200

    remove what scalarium offers

--- nginx-app/attributes/default.rb
+++ nginx-app/attributes/default.rb
@@ -1,5 +1,1 @@
 default["nginx-app"] = {}
-default["nginx-app"][:user] = "www-data"
-default["nginx-app"][:group] = "www-data"
-default["nginx-app"][:domain] = "scalarium.easybib.com"
-default["nginx-app"][:root] = "/var/www/easybib.com/current/www"
