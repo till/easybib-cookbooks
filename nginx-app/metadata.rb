07a09e8e60c76d6581b4b7e7de08d84af6ddeb31
Till Klampaeckel <till@php.net>
2010-08-18 16:23:45 +0200

    * removed files i don't override  * custom haproxy.cfg.erb

--- nginx-app/metadata.rb
+++ nginx-app/metadata.rb
@@ -4,6 +4,6 @@ license           "BSD License"
 description       "Installs and configures an nginx for our appservers."
 version           "0.1"
 recipe            "nginx::server", "Installs Nginx"
-recipe            "nginx::prepare", "Creates prequesites like user and directories"
+recipe            "nginx::configure", "Configures virtualhost, etc."
 
 supports 'ubuntu'
