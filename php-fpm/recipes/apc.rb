9cf2c0f058c6153837a992b3e343c5cf780d825b
Till Klampaeckel <till@php.net>
2010-08-19 18:52:38 +0200

    * make version an attribute (to please roidis)

--- php-fpm/recipes/apc.rb
+++ php-fpm/recipes/apc.rb
@@ -1,22 +1,19 @@
-apc_version = "3.1.4"
-
-remote_file "/tmp/APC-#{apc_version}.tgz" do
-  source "http://pecl.php.net/get/APC-#{apc_version}.tgz"
+remote_file "/tmp/APC-#{node["php-fpm"][:apc_version]}.tgz" do
+  source "http://pecl.php.net/get/APC-#{node["php-fpm"][:apc_version]}.tgz"
   checksum "53d8442e8b7e3804537d14e5776962cfdc5ae4d8"
 end
 
 execute "APC: unpack" do
-  command "cd /tmp && tar -xzf APC-#{apc_version}.tgz"
+  command "cd /tmp && tar -xzf APC-#{node["php-fpm"][:apc_version]}.tgz"
 end
 
 execute "APC: ./configure" do
-  cwd "/tmp/APC-#{apc_version}"
-  environment "HOME" => "/root"
+  cwd "/tmp/APC-#{node["php-fpm"][:apc_version]}"
   command "./configure"
 end
 
 execute "APC: make, make install" do
-  cwd "/tmp/APC-#{apc_version}"
+  cwd "/tmp/APC-#{node["php-fpm"][:apc_version]}"
   command "make && make install"
 end
 
