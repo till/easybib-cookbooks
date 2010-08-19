c983ed43732fc6fed4b0d5293f3e4825129dd47f
Till Klampaeckel <till@php.net>
2010-08-19 17:53:09 +0200

    * new cookbook to fix an unfuddle subversion f-up

---
+++ unfuddle-ssl-fix/recipes/install.rb
@@ -0,0 +1,8 @@
+template "/etc/ssl/certs/Equifax_Secure_Certificate_Authority.cer" do
+  mode "0777"
+  source "Equifax_Secure_Certificate_Authority.cer.erb"
+end
+
+execute "make installed certificate known to subversion" do
+  command "echo 'ssl-authority-files = /etc/ssl/certs/Equifax_Secure_Certificate_Authority.cer' >> /etc/subversion/servers"
+end
