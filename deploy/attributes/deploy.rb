5de6efc9f1007d245621d75f62d786c6e6ee726f
Till Klampaeckel <till@php.net>
2010-08-19 23:15:59 +0200

    try to fix up restart command

--- deploy/attributes/deploy.rb
+++ deploy/attributes/deploy.rb
@@ -1,4 +1,4 @@
 deploy.each do |application, deploy|
   node[:deploy][application][:user] = 'www-data'
-  node[:deploy][application][:restart_command] = '/etc/init.d/php-fpm restart'
+  node[:deploy][application][:restart_command] = '/etc/init.d/php-fpm stop && /etc/init.d/php-fpm start'
 end
