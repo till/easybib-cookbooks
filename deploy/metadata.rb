fec72b13e865455c3422c2d0ec535bf98bfe99a2
Till Klampaeckel <till@php.net>
2010-08-19 17:04:50 +0200

    * OH: "empfiehlt sich, ist aber nicht notwendig"

---
+++ deploy/metadata.rb
@@ -0,0 +1,8 @@
+maintainer        "Till Klampaeckel"
+maintainer_email  "till@php.net"
+license           "BSD License"
+description       "Deploys easybib"
+version           "0.1"
+recipe            "deploy::easybib", "Deploys c0dez!!!1"
+
+supports 'ubuntu'
