cf57e3705a17a78a588a43cf47d4c4ee5376664c
Till Klampaeckel <till@php.net>
2010-08-19 19:58:36 +0200

    * base cookbook

---
+++ easybib-base/metadata.rb
@@ -0,0 +1,8 @@
+maintainer        "Till Klampaeckel"
+maintainer_email  "till@php.net"
+license           "PHP License"
+description       "Tools we'd like on all servers."
+version           "0.1"
+recipe            "easybib-base::setup", "Installs them."
+
+supports 'ubuntu'
