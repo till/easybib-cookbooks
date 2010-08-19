8610522cbfb02e3e43c60f5c6aaba0e5341d9b4f
Till Klampaeckel <till@php.net>
2010-08-19 14:51:21 +0200

    * more logfiles

--- php-fpm/attributes/default.rb
+++ php-fpm/attributes/default.rb
@@ -1,7 +1,9 @@
 default["php-fpm"] = {}
 default["php-fpm"][:version] = '5.3.3'
 default["php-fpm"][:checksum] = "5adf1a537895c2ec933fddd48e78d8a2"
-default["php-fpm"][:logfile] = "/var/log/php.log"
+default["php-fpm"][:logfile] = "/var/log/php/error.log"
+default["php-fpm"][:slowlog] = "/var/log/php/slow.log"
+default["php-fpm"][:fpmlog] = "/var/log/php/fpm.log"
 default["php-fpm"][:displayerrors] = false
 default["php-fpm"][:logerrors] = true
 default["php-fpm"][:maxexecutiontime] = 60
