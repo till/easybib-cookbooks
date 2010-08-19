ff8835f0f675f861e5f39a53298340666d59985a
Till Klampaeckel <till@php.net>
2010-08-19 14:50:21 +0200

    * create log dir  * create various log files

--- php-fpm/recipes/prepare.rb
+++ php-fpm/recipes/prepare.rb
@@ -17,7 +17,25 @@ directory node["php-fpm"][:tmpdir] do
   group node["php-fpm"][:group]
 end
 
+directory File.dirname(node["php-fpm"][:logfile]) do
+  mode "0755"
+  owner node["php-fpm"][:user]
+  group node["php-fpm"][:group]
+end
+
 file node["php-fpm"][:logfile] do
+  mode "0755"
+  owner node["php-fpm"][:user]
+  group node["php-fpm"][:group]
+end
+
+file node["php-fpm"][:slowlog] do
+  mode "0755"
+  owner node["php-fpm"][:user]
+  group node["php-fpm"][:group]
+end
+
+file node["php-fpm"][:fpmlog] do
   mode "0755"
   owner node["php-fpm"][:user]
   group node["php-fpm"][:group]
