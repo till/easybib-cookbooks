98f5209e6bd14250ceaff3f1333d5cdd9c967f10
Till Klampaeckel <till@php.net>
2010-08-20 13:48:19 +0200

    * fix license

--- easybib-base/metadata.rb
+++ easybib-base/metadata.rb
@@ -1,6 +1,6 @@
 maintainer        "Till Klampaeckel"
 maintainer_email  "till@php.net"
-license           "PHP License"
+license           "BSD License"
 description       "Tools we'd like on all servers."
 version           "0.1"
 recipe            "easybib-base::setup", "Installs them."
