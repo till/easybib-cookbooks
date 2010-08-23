363d2a333da558337a67b208ca20e348a1549022
Till Klampaeckel <till@php.net>
2010-08-23 22:44:57 +0200

    * use prefix with pear install  * add soap support

--- php-fpm/recipes/install.rb
+++ php-fpm/recipes/install.rb
@@ -20,7 +20,7 @@ end
 execute "PHP: ./configure" do
   cwd "/tmp/php-#{node["php-fpm"][:version]}"
   environment "HOME" => "/root"
-  command "./configure --prefix=#{node["php-fpm"][:prefix]} --with-openssl --disable-posix --without-sqlite --without-sqlite3 --with-mysqli=mysqlnd --disable-posix --disable-phar --disable-pdo --enable-fpm --with-fpm-user=#{node["php-fpm"][:user]} --with-fpm-group=#{node["php-fpm"][:group]} --with-pear=/usr/local/pear"
+  command "./configure --prefix=#{node["php-fpm"][:prefix]} --enable-soap --with-openssl --disable-posix --without-sqlite --without-sqlite3 --with-mysqli=mysqlnd --disable-posix --disable-phar --disable-pdo --enable-fpm --with-fpm-user=#{node["php-fpm"][:user]} --with-fpm-group=#{node["php-fpm"][:group]} --with-pear=#{node["php-fpm"][:prefix]}/pear"
   not_if &php_already_installed
 end
 
