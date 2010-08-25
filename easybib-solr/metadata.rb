80aa88ea88a31d289ff24d4a5dcc01bab4cbc216
Till Klampaeckel <till@php.net>
2010-08-26 00:22:01 +0200

    * add prepare

--- easybib-solr/metadata.rb
+++ easybib-solr/metadata.rb
@@ -4,6 +4,7 @@ license           "BSD License"
 description       "Recipes to setup an EC2 instance for our solr setup."
 version           "0.1"
 recipe            "easybib-solr::solr", "Checks out all apps and does some config for solr."
+recipe            "easybib-solr::prepare", "Installs dependencies for Solr, creates directories."
 recipe            "easybib-solr::raid", "Setup a raid of EBS volumes."
 
 supports 'ubuntu'
