29c8072e8ebe86606998b5f8a41b86526c8f5d3a
Till Klampaeckel <till@php.net>
2010-08-25 18:12:08 +0200

    * add metadata

---
+++ easybib-solr/metadata.rb
@@ -0,0 +1,9 @@
+maintainer        "Till Klampaeckel"
+maintainer_email  "till@php.net"
+license           "BSD License"
+description       "Recipes to setup an EC2 instance for our solr setup."
+version           "0.1"
+recipe            "easybib-solr::solr", "Checks out all apps and does some config for solr."
+recipe            "easybib-solr::raid", "Setup a raid of EBS volumes."
+
+supports 'ubuntu'
