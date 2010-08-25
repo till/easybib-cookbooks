3213cb3d1ddc054f515a88f53c526503c58a1f47
Till Klampaeckel <till@php.net>
2010-08-26 00:20:59 +0200

    * a couple new attributes

--- easybib-solr/attributes/default.rb
+++ easybib-solr/attributes/default.rb
@@ -1,2 +1,5 @@
 default[:easybib_solr] = {}
 default[:easybib_solr][:working_directory] = "/solr"
+default[:easybib_solr][:ebs] = {}
+default[:easybib_solr][:ebs][:size] = 200
+default[:easybib_solr][:ebs][:raid] = 1
