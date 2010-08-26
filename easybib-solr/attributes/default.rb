ea262538af4fb532094b2906afd83adce5051b9d
Till Klampaeckel <till@php.net>
2010-08-26 14:53:32 +0200

    * do raid 0  * make zone configurable

--- easybib-solr/attributes/default.rb
+++ easybib-solr/attributes/default.rb
@@ -2,4 +2,5 @@ default[:easybib_solr] = {}
 default[:easybib_solr][:working_directory] = "/solr"
 default[:easybib_solr][:ebs] = {}
 default[:easybib_solr][:ebs][:size] = 200
-default[:easybib_solr][:ebs][:raid] = 1
+default[:easybib_solr][:ebs][:raid] = 0
+default[:easybib_solr][:ebs][:zone] = "us-east-1b"
