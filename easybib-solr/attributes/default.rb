0ff3e4bb3f07bb05f8bd7bcf745b71ec82be26e9
Till Klampaeckel <till@php.net>
2010-08-26 20:40:59 +0200

    * add log_dir

--- easybib-solr/attributes/default.rb
+++ easybib-solr/attributes/default.rb
@@ -1,5 +1,6 @@
 default[:easybib_solr] = {}
 default[:easybib_solr][:working_directory] = "/solr"
+default[:easybib_solr][:log_dir] = "/var/log/solr"
 default[:easybib_solr][:ebs] = {}
 default[:easybib_solr][:ebs][:size] = 200
 default[:easybib_solr][:ebs][:raid] = 0
