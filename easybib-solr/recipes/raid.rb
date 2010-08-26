d256ec0e716a47612e6f3c22de59a6355cd0bb91
Till Klampaeckel <till@php.net>
2010-08-26 14:54:02 +0200

    * finished raid creation   * configure raid   * format raid (xfs)   * add it to fstab   * mount it

--- easybib-solr/recipes/raid.rb
+++ easybib-solr/recipes/raid.rb
@@ -1,4 +1,5 @@
 package "mdadm"
+package "xfsprogs"
 
 gem_package "fog" do
   action :install
@@ -12,22 +13,39 @@ cookbook_file "/usr/local/bin/build-ebs-raid" do
 end
 
 size     = node[:easybib_solr][:ebs][:size]
-instance = node[:scalarium]?
+instance = node[:ec2][:instance_id]
+raid     = "/dev/md0"
+level    = node[:easybib_solr][:ebs][:raid]
+mount    = node[:easybib_solr][:working_directory]
+zone     = node[:easybib_solr][:ebs][:zone]
 
 execute "Create first volume: /dev/sdf" do
-  command "/usr/local/bin/build-ebs-raid --zone=us-east-1b --size=#{size} --instance=#{instance} --device=/dev/sdf"
+  command "/usr/local/bin/build-ebs-raid --zone=#{zone} --size=#{size} --instance=#{instance} --device=/dev/sdf"
 end
 
 execute "Create first volume: /dev/sdg" do
-  command "/usr/local/bin/build-ebs-raid --zone=us-east-1b --size=#{size} --instance=#{instance} --device=/dev/sdg"
+  command "/usr/local/bin/build-ebs-raid --zone=#{zone} --size=#{size} --instance=#{instance} --device=/dev/sdg"
 end
 
-# Todo
-# raid 1 or 0
-# install mdadm
-# find instance id
-# make.ext3
-# create raid
-# write /etc/mdadm/mdadm.conf
+
+mdadm "#{raid}" do
+  devices [ "/dev/sdf", "/dev/sdg" ]
+  level level.to_i
+  chunk 256
+  action [ :create, :assemble ]
+end
+
+execute "Format #{mount}" do
+  command "mkfs.xfs #{raid}"
+end
+
+execute "Add #{mount} to /etc/fstab" do
+  command "echo '#{raid} #{mount} xfs noatime 0 0' | sudo tee -a /etc/fstab"
+end
+
+mount "{mount}" do
+  device raid
+  fstype "xfs"
+end
 
 supports "ubuntu"
