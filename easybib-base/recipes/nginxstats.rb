6ba2dc3120c9f9d613f409518def9c90b47fc1c4
Till Klampaeckel <till@php.net>
2010-08-27 16:18:22 +0200

    * add recipe for nginxstats script

---
+++ easybib-base/recipes/nginxstats.rb
@@ -0,0 +1,6 @@
+remote_file "/usr/local/bin/nginxstats" do
+  source "nginxstats.py"
+  mode 0755
+  owner "root"
+  group "root"
+end
--- easybib-base/recipes/setup.rb
+++ easybib-base/recipes/setup.rb
@@ -3,3 +3,4 @@ package "jwhois"
 package "multitail"
 
 include_recipe "awscommand"
+include_recipe "nginxstats"
