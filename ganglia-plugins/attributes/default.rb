cf38b87e3fb75e349c5eee745a7247a151251b4f
Till Klampaeckel <till@php.net>
2010-08-31 18:17:11 +0200

    add attributes for redis

--- ganglia-plugins/attributes/default.rb
+++ ganglia-plugins/attributes/default.rb
@@ -1,4 +1,8 @@
 default[:ganglia_plugins] = {}
 default[:ganglia_plugins][:conf_dir] = '/etc/ganglia/conf.d'
 default[:ganglia_plugins][:plugin_dir] = '/etc/ganglia/python_modules'
+default[:ganglia_plugins][:nginx] = {}
 default[:ganglia_plugins][:nginx][:status] = '/nginx_status'
+default[:ganglia_plugins][:redis] = {}
+default[:ganglia_plugins][:redis][:host] = "127.0.0.1"
+default[:ganglia_plugins][:redis][:port] = 6379
