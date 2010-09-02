cbb1d47f5c8f290bbc257921080e09b24bf26a1c
Till Klampaeckel <till@php.net>
2010-09-02 17:53:38 +0200

    * always install strace

--- easybib-base/recipes/setup.rb
+++ easybib-base/recipes/setup.rb
@@ -2,6 +2,7 @@ package "htop"
 package "jwhois"
 package "multitail"
 package "apache2-utils"
+package "strace"
 
 include_recipe "easybib-base::awscommand"
 include_recipe "easybib-base::nginxstats"
