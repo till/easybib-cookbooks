c3e66fbdb4ee267964aa3946d34b1fd7904331eb
Till Klampaeckel <till@php.net>
2010-09-05 01:20:54 +0200

    * write a custom method to run svn manually (svn info chokes otherwise)

--- easybib-solr/recipes/server.rb
+++ easybib-solr/recipes/server.rb
@@ -2,24 +2,24 @@ ebs_vol=node[:easybib_solr][:working_directory]
 
 # node["solr"] is provided by scalarium
 
-svn_args="--non-interactive --no-auth-cache"
-
-subversion "Checkout: Research Importers" do
-  repository "#{node["solr"]["deploy_svn"]}/research_importers/#{node["solr"]["research_version"]}/"
-  destination "#{ebs_vol}/research_importers"
-  svn_username node["deploy"]["easybib"]["scm"]["user"]
-  svn_password node["deploy"]["easybib"]["scm"]["password"]
-  svn_arguments svn_args
-  action :checkout
+def till_run_svn(repository, destination, scm_data)
+  svn_args="--non-interactive --no-auth-cache"
+  svn_auth="--username #{scm_data["user"]} --password #{scm_data["password"]}"
+  run "svn checkout #{svn_args} #{svn_auth} #{respository} #{destination}"
 end
 
-subversion "Checkout: Solr" do
-  repository "#{node["solr"]["deploy_svn"]}/solr/#{node["solr"]["solr_svn_version"]}"
-  destination "#{ebs_vol}/apache-solr-#{node["solr"]["solr_version"]}-compiled"
-  svn_username node["deploy"]["easybib"]["scm"]["user"]
-  svn_password node["deploy"]["easybib"]["scm"]["password"]
-  svn_arguments svn_args
-  action :checkout
+execute "Checkout: Research Importers" do
+  repository  = "#{node["solr"]["deploy_svn"]}/research_importers/#{node["solr"]["research_version"]}/"
+  destination = "#{ebs_vol}/research_importers"
+
+  till_run_svn(repository, destination, node["deploy"]["easybib"]["scm"])
+end
+
+execute "Checkout: Solr" do
+  repository  = "#{node["solr"]["deploy_svn"]}/solr/#{node["solr"]["solr_svn_version"]}"
+  destination = "#{ebs_vol}/apache-solr-#{node["solr"]["solr_version"]}-compiled"
+
+  till_run_svn(repository, destination, node["deploy"]["easybib"]["scm"])
 end
 
 link "#{ebs_vol}/research_importers/etc/solr.conf" do
