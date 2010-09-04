33cc51f075cc1d2308b12461cde278df5448d483
Till Klampaeckel <till@php.net>
2010-09-05 00:45:34 +0200

    * added not_if and ignore_failure to link commands  * start the solr service

--- easybib-solr/recipes/solr.rb
+++ easybib-solr/recipes/solr.rb
@@ -11,19 +11,22 @@ end
 subversion "Checkout: Solr" do
   repository "#{node["solr"]["deploy_svn"]}/solr/#{node["solr"]["solr_svn_version"]}"
   destination "#{ebs_vol}/apache-solr-#{node["solr"]["solr_version"]}-compiled"
-  action :sync    
+  action :sync
 end
 
 link "#{ebs_vol}/research_importers/etc/solr.conf" do
   to "/etc/solr.conf"
+  not_if test -h /etc/solr.conf
 end
 
 link "#{ebs_vol}/research_importers/scripts/solr.sh" do
   to "/etc/init.d/solr"
+  not_if test -h /etc/init.d/solr
 end
 
 link "#{node[:easybib_solr][:log_dir]}" do
   to "#{ebs_vol}/apache-solr-#{node["solr"]["solr_version"]}-compiled/logs"
+  ignore_failure true
 end
 
 remote_file "/etc/logrotate.d/solr" do
@@ -36,5 +39,5 @@ end
 service "solr" do
   service_name "solr"
   supports [:start, :status, :restart, :stop]
-  action :enable
+  action [ :enable, :start ]
 end
