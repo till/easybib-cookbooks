b1bd760dc5988da8a2ebaa1c0c6d4ba5ebe292ea
Till Klampaeckel <till@php.net>
2010-09-05 01:02:30 +0200

    * add username, password and args to svn command [provided by scalarium]

--- easybib-solr/recipes/server.rb
+++ easybib-solr/recipes/server.rb
@@ -5,12 +5,18 @@ ebs_vol=node[:easybib_solr][:working_directory]
 subversion "Checkout: Research Importers" do
   repository "#{node["solr"]["deploy_svn"]}/research_importers/#{node["solr"]["research_version"]}/"
   destination "#{ebs_vol}/research_importers"
+  svn_username node["deploy"]["easybib"]["scm"]["user"]
+  svn_password node["deploy"]["easybib"]["scm"]["password"]
+  svn_arguments "--no-auth-cache"
   action :sync
 end
 
 subversion "Checkout: Solr" do
   repository "#{node["solr"]["deploy_svn"]}/solr/#{node["solr"]["solr_svn_version"]}"
   destination "#{ebs_vol}/apache-solr-#{node["solr"]["solr_version"]}-compiled"
+  svn_username node["deploy"]["easybib"]["scm"]["user"]
+  svn_password node["deploy"]["easybib"]["scm"]["password"]
+  svn_arguments "--no-auth-cache"
   action :sync
 end
 
