b8bfb3dab98d999cc34a2efbc486923407ee2e3b
Till Klampaeckel <till@php.net>
2010-09-06 17:06:08 +0200

    * merged additions from jweiss (thanks)

--- deploy/recipes/easybib.rb
+++ deploy/recipes/easybib.rb
@@ -1,70 +1,45 @@
-include_recipe "deploy::source"
 
+# already checks out the code for every app
+# do you really want this?
+#include_recipe "deploy::source"
 
-if node[:scalarium][:instance][:roles].include?('nginxphpapp')
-
-  deploy = node[:deploy][:easybib]
-
-elsif node[:scalarium][:instance][:roles].include?('bibapi')
-
-  deploy = node[:deploy][:easybib_api]
-
-elsif node[:scalarium][:instance][:roles].include?('easybibsolr')
-
-  node[:deploy].each do |application, deployInfo|
-
-    # -server and -research importers
-    if application == "easybib_solr_research_importers"
-
-      deploy                   = deployInfo
-      deploy[:deploy_to]       = "/solr/research_importers"
-      deploy[:restart_command] = ""
-
-      break
-
-    end
-
-    if application == "easybib_solr_server"
-
-      deploy                   = deployInfo
-      deploy[:deploy_to]       = "/solr/apache-solr-1.4-compiled"
-      deploy[:restart_command] = "/etc/init.d/solr restart"
-
-      break
-
-    end
-
-    Chef::Log.debug("Skipping deploy::easybib because application #{application} doesn't. run on #{node[:scalarium][:instance][:roles]}")
-
+node[:deploy].each do |application, deploy|
+  case application
+  when 'easybib'
+    next unless node[:scalarium][:instance][:roles].include?('nginxphpapp')
+  when 'easybib_api'
+    next unless node[:scalarium][:instance][:roles].include?('bibapi')
+  when 'easybib_solr_research_importers'
+    # not sure on which roles you want to have this app
+    next unless node[:scalarium][:instance][:roles].include?('solr')
+    
+    deploy[:deploy_to]       = "/solr/research_importers"
+    deploy[:restart_command] = ""
+  when 'easybib_solr_server'
+    # not sure on which roles you want to have this app
+    next unless node[:scalarium][:instance][:roles].include?('solr')
+    
+    deploy[:deploy_to]       = "/solr/apache-solr-1.4-compiled"
+    deploy[:restart_command] = "/etc/init.d/solr restart"
   end
-
-else
-
-  deploy = nil
-
-end
-
-# This fixes a Scalarium bug:
-# UI asks for branch or revision but is only able to deal with revisions.
-matches = deploy[:scm][:revision].match(/(r[0-9]{1,})|([0-9]{1,})/)
-if matches.nil? 
-
-  deploy[:scm][:repository] = "#{deploy[:scm][:repository]}/#{deploy[:scm][:revision]}"
-  deploy[:scm][:revision]   = nil
-
-end
-
-if !deploy.nil?
-
+  
+  # we survived until here - so we are good to actually checkout and deploy
+  # done for every app
+  
+  # fix SVN url - have to look into that
+  unless deploy[:scm][:revision].match(/(r[0-9]{1,})|([0-9]{1,})/)
+    deploy[:scm][:repository] = "#{deploy[:scm][:repository]}/#{deploy[:scm][:revision]}"
+    deploy[:scm][:revision]   = nil
+  end
+  
   # chef bug
   directory "#{deploy[:deploy_to]}/shared/cached-copy" do
     recursive true
     action :delete
   end
-
+  
   # setup deployment & checkout
   deploy deploy[:deploy_to] do
-
     repository deploy[:scm][:repository]
     user "www-data"
 
@@ -86,7 +61,6 @@ if !deploy.nil?
     svn_username deploy[:scm][:user]
     svn_password deploy[:scm][:password]
     svn_arguments "--no-auth-cache"
-
   end
-
+  
 end
