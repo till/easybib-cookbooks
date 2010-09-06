8489e59bcfd7b40d9469f8f791e521069015f121
Till Klampaeckel <till@php.net>
2010-09-06 14:29:50 +0200

    * remove custom svn code (we'll deploy using scalarium apps)

--- easybib-solr/recipes/server.rb
+++ easybib-solr/recipes/server.rb
@@ -2,26 +2,6 @@ ebs_vol=node[:easybib_solr][:working_directory]
 
 # node["solr"] is provided by scalarium
 
-def till_run_svn(repository, destination, scm_data)
-  svn_args="--non-interactive --no-auth-cache"
-  svn_auth="--username #{scm_data["user"]} --password #{scm_data["password"]}"
-  run "svn checkout #{svn_args} #{svn_auth} #{respository} #{destination}"
-end
-
-execute "Checkout: Research Importers" do
-  repository  = "#{node["solr"]["deploy_svn"]}/research_importers/#{node["solr"]["research_version"]}/"
-  destination = "#{ebs_vol}/research_importers"
-
-  till_run_svn(repository, destination, node["deploy"]["easybib"]["scm"])
-end
-
-execute "Checkout: Solr" do
-  repository  = "#{node["solr"]["deploy_svn"]}/solr/#{node["solr"]["solr_svn_version"]}"
-  destination = "#{ebs_vol}/apache-solr-#{node["solr"]["solr_version"]}-compiled"
-
-  till_run_svn(repository, destination, node["deploy"]["easybib"]["scm"])
-end
-
 link "#{ebs_vol}/research_importers/etc/solr.conf" do
   to "/etc/solr.conf"
   not_if "test -h /etc/solr.conf"
