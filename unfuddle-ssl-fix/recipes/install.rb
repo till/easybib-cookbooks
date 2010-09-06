0a8a6295eafef7eb2d43671525d13b574aa48583
Till Klampaeckel <till@php.net>
2010-09-06 18:55:55 +0200

    * use create_if_missing to not re-run these actions all the time

--- unfuddle-ssl-fix/recipes/install.rb
+++ unfuddle-ssl-fix/recipes/install.rb
@@ -3,8 +3,13 @@ package "subversion"
 template "/etc/ssl/certs/Equifax_Secure_Certificate_Authority.cer" do
   mode "0777"
   source "Equifax_Secure_Certificate_Authority.cer.erb"
+  create_if_missing :true
 end
 
-execute "make installed certificate known to subversion" do
-  command "echo 'ssl-authority-files = /etc/ssl/certs/Equifax_Secure_Certificate_Authority.cer' >> /etc/subversion/servers"
+remote_file "/root/.subversion/servers" do
+  source "servers"
+  mode 0755
+  owner "root"
+  group "root"
+  create_if_missing :true
 end
