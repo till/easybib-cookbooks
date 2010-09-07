cf6d7b115b627fba5d5945f5334b008544bb0767
Till Klampaeckel <till@php.net>
2010-09-07 18:36:50 +0200

    * accept the license

--- easybib-solr/recipes/prepare.rb
+++ easybib-solr/recipes/prepare.rb
@@ -1,3 +1,7 @@
+execute "Accept sun's license" do
+  command "echo 'sun-java6-jdk shared/accepted-sun-dlj-v1-1 boolean true' | sudo debconf-set-selections"
+end
+
 package "sun-java6-jre"
 package "subversion"
 
