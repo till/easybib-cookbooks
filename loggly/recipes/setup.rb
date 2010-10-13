721ec30c47689a70e9d9eee5f7cfce4307f96e8e
till <till@php.net>
2010-10-13 15:01:34 +0200

    * need to restart rsyslog after the the config has been added

--- loggly/recipes/setup.rb
+++ loggly/recipes/setup.rb
@@ -2,3 +2,8 @@ template "/etc/rsyslog.d/10-loggly.conf" do
   source "10-loggly.conf.erb"
   mode "0644"
 end
+
+service "rsyslog" do
+  supports "status => true, :restart => true, :reload => true
+  action [ :restart ]
+end
