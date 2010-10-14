0a473531505312f89d4c97fd43d4eaa465161d32
till <till@php.net>
2010-10-14 17:23:13 +0200

    * try to fix deploy_to path by connecting to role

--- nginx-app/recipes/configure.rb
+++ nginx-app/recipes/configure.rb
@@ -2,6 +2,14 @@ include_recipe "deploy"
 include_recipe "nginx-app::server"
 
 node[:deploy].each do |application, deploy|
+
+  case application
+  when 'easybib'
+    next unless node[:scalarium][:instance][:roles].include?('nginxphpapp')
+  when 'easybib_api'
+    next unless node[:scalarium][:instance][:roles].include?('bibapi')
+  end
+  
   template "/etc/nginx/sites-enabled/easybib.com.conf" do
     source "easybib.com.conf.erb"
     mode "0755"
