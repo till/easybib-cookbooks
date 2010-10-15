54192766388008f28c1996530703243da8b3b4eb
till <till@php.net>
2010-10-15 16:10:55 +0200

    * install subversion crap into various users

--- unfuddle-ssl-fix/recipes/install.rb
+++ unfuddle-ssl-fix/recipes/install.rb
@@ -6,16 +6,29 @@ template "/etc/ssl/certs/Equifax_Secure_Certificate_Authority.cer" do
   not_if "test -f /etc/ssl/certs/Equifax_Secure_Certificate_Authority.cer"
 end
 
-directory "/root/.subversion" do
-  owner "root"
-  group "root"
-  mode "0755"
-  action :create
-end
+users = %w[ root www-data ]
 
-remote_file "/root/.subversion/servers" do
-  source "servers"
-  mode 0755
-  owner "root"
-  group "root"
+users.each do |user|
+
+  case user
+  when 'root'
+    home_dir = "/root"
+  when 'www-data'
+    home_dir = "/var/www"
+  else
+    home_dir = "/home/#{user}"
+  end
+
+  directory "#{home_dir}/.subversion" do
+    owner user
+    mode "0755"
+    action :create
+  end
+
+  remote_file "#{home_dir}/.subversion/servers" do
+    source "servers"
+    mode 0755
+    owner user
+  end
+
 end
