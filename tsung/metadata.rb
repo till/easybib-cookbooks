ac59b13aa82e6764f460fd089b37496c25d7f3b8
till <till@php.net>
2010-10-28 19:30:05 +0200

    add metadata

---
+++ tsung/metadata.rb
@@ -0,0 +1,8 @@
+maintainer        "Till Klampaeckel"
+maintainer_email  "till@php.net"
+license           "BSD License"
+description       "Setup tsung and nginx to view its stats."
+version           "0.1"
+recipe            "tsung::setup", "Install and configure tsung and nginx."
+
+supports 'ubuntu'
