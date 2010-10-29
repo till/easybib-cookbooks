2d682d2d155b7a67e865fe7afbd5723aa595b04c
till <till@php.net>
2010-10-30 01:23:16 +0200

    * install Net_CheckIP2

--- php-fpm/recipes/pear.rb
+++ php-fpm/recipes/pear.rb
@@ -5,4 +5,5 @@ execute "PHP: install pear packages" do
   command "pear install -f Crypt_HMAC2-beta"
   command "pear install -f Net_Gearman-alpha"
   command "pear install -f Services_Amazon_S3-alpha"
+  command "pear install -f Net_CheckIP2-1.0.0RC3"
 end
