f4847f081d5119384dcb5a08be3f55b01fa26a01
till <till@php.net>
2010-11-26 21:12:37 +0100

    * remove empty password (= no password)  * bugfix: chmod >> mode

--- teracode-dev/recipes/users.rb
+++ teracode-dev/recipes/users.rb
@@ -7,18 +7,17 @@ users.each { |user|
     home "/home/#{user}"
     gid "sudo"
     shell "/bin/zsh"
-    password ""
   end
 
   directory "/home/#{user}/.ssh" do
-    chmod "0700"
+    mode "0700"
     owner "#{user}"
     group "#{user}"
   end
 
   remote_file "/home/#{user}/.ssh/authorized_keys" do
     source "#{user}-pub.key"
-    chmod "0600"
+    mode "0600"
     owner "#{user}"
     group "#{user}"
     action :create
