8a86de32a6147cabad56d0cd6b821ed8cb534229
till <till@php.net>
2010-11-27 16:49:20 +0100

    * install various dotfiles for zsh usage [to avoid being prompted on first login]

---
+++ teracode-dev/recipes/zsh.rb
@@ -0,0 +1,31 @@
+node[:dev][:users].each do |user|
+
+  remote_file "/home/#{user}/.zshrc" do
+    source "zshrc"
+    mode 0644
+    owner "#{user}"
+    group "#{user}"
+  end
+
+  remote_file "/home/#{user}/.zshfunctions" do
+    source "zshfunctions"
+    mode 0644
+    owner "#{user}"
+    group "#{user}"
+  end
+
+  remote_file "/home/#{user}/.zshaliases" do
+    source "zshaliases"
+    mode 0644
+    owner "#{user}"
+    group "#{user}"
+  end
+
+  directory "/home/#{user}/.zsh" do
+    mode 0755
+    owner "#{user}"
+    group "#{user}"
+    action :create    
+  end
+
+end
