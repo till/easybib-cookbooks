57d183aa111ce3eee2c25d436be6bafeacaca152
till <till@php.net>
2011-01-06 15:59:03 +0100

    setup variables depending on "source" in config

--- php-fpm/recipes/configure.rb
+++ php-fpm/recipes/configure.rb
@@ -1,4 +1,19 @@
-template "#{php_prefix}/etc/php.ini" do
+if node["php-fpm"][:source] == "source"
+  etc_cli_dir = "#{php_prefix}/etc"
+  etc_fpm_dir = "#{php_prefix}/etc"
+  conf_cli = "php-cli.ini"
+  conf_fpm = "php.ini"
+elsif node["php-fpm"][:source] == "ubuntu" 
+  etc_cli_dir = "/etc/php5/cli"
+  etc_fpm_dir = "/etc/php5/fpm"
+  conf_cli = "php.ini"
+  conf_fpm = "php.ini"
+else 
+  Chef::Log.error("Unknown source: #{node["php-fpm"][:source]}. Bailed.")
+  return
+end
+
+template "#{etc_fpm_dir}/#{conf_fpm}" do
   mode "0755"
   source "php.ini.erb"
   variables(
@@ -8,7 +23,7 @@ template "#{php_prefix}/etc/php.ini" do
   group node["php-fpm"][:group]
 end
 
-template "#{php_prefix}/etc/php-cli.ini" do
+template "#{etc_cli_dir}/#{conf_cli}" do
   mode "0755"
   source "php.ini.erb"
   variables(
@@ -18,18 +33,20 @@ template "#{php_prefix}/etc/php-cli.ini" do
   group node["php-fpm"][:group]
 end
 
-template "/usr/local/etc/php-fpm.conf" do
+template "#{etc_fpm_dir}/php-fpm.conf" do
   mode "0755"
   source "php-fpm.conf.erb"
   owner node["php-fpm"][:user]
   group node["php-fpm"][:group]
 end
 
-directory "#{php_prefix}/etc/php" do
-  owner "root"
-  group "root"
-  mode "0755"
-  action :create
+if node["php-fpm"][:source] == "source"
+  directory "#{php_prefix}/etc/php" do
+    owner "root"
+    group "root"
+    mode "0755"
+    action :create
+  end
 end
 
 
