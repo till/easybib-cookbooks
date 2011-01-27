090989a6c7ae9101ea65ecef3430ad34118bd4c9
till <till@php.net>
2011-01-27 18:44:57 +0100

    * download the plugins from muninexchange  * symlink them

--- munin-node/recipes/nginx.rb
+++ munin-node/recipes/nginx.rb
@@ -1,7 +1,27 @@
-munin_plugins = ["nginx_status", "nginx_requests"]
+# prior to 10.04 we have to download them ourselves
+munin_plugins = ["nginx_status", "nginx_requests", "nginx_memory"]
+
+directory "/opt/munin-nginx" do
+  mode "0755"
+end
+
+remote_file "/opt/munin-nginx/nginx_memory" do
+  source "http://muninexchange.projects.linpro.no/download.php?phid=626"
+  mode "0755"
+end
+
+remote_file "/opt/munin-nginx/nginx_status" do
+  source "http://muninexchange.projects.linpro.no/download.php?phid=65"
+  mode "0755"
+end
+
+remote_file "/opt/munin-nginx/nginx_requests" do
+  source "http://muninexchange.projects.linpro.no/download.php?phid=64"
+  mode "0755"
+end
 
 munin_plugins.each do |plugin|
   link "/etc/munin/plugins/#{plugin}" do
-    to "/usr/share/munin/plugins/#{plugin}"
+    to "/opt/munin-nginx/#{plugin}"
   end
 end
