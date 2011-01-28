c99e976804d4a6933098055196961327c9db3afd
till <till@php.net>
2011-01-28 19:34:10 +0100

    * add metadata to the recipe

---
+++ nodejs/metadata.rb
@@ -0,0 +1,7 @@
+maintainer        "Till Klampaeckel"
+maintainer_email  "till@php.net"
+license           "PHP License"
+description       "Basic node.js install"
+version           "0.1"
+recipe            "nodejs::setup", "Install and configure node.js"
+supports 'ubuntu'
