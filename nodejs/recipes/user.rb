35fc67c7ee8eedc4a828b3c9cc34c9226b18ab42
till <till@php.net>
2011-02-04 23:22:24 +0100

    * create group along with user  * only do this when it's not root

--- nodejs/recipes/user.rb
+++ nodejs/recipes/user.rb
@@ -1,6 +1,14 @@
-user "#{node[:nodejs][:user]}" do
-  comment "user to run node"
-  home "/var/www"
-  shell "/bin/false"
-  action :create
+if node[:nodejs][:user] != "root" 
+
+  group "#{node[:nodejs][:user]}" do
+    gid 31337
+  end
+
+  user "#{node[:nodejs][:user]}" do
+    comment "user to run node"
+    gid 31337
+    home "/srv/www/#{node[:nodejs][:application]}"
+    shell "/bin/false"
+    action :create
+  end
 end
