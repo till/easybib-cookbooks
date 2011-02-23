f1d9fafa96bd45d06eed28451868d3957a90be17
till <till@php.net>
2011-02-23 12:39:29 +0100

    * update ppa to use and package name

--- php-fpm/recipes/install-apt.rb
+++ php-fpm/recipes/install-apt.rb
@@ -1,6 +1,6 @@
 package "python-software-properties"
 
-ppa="fholzhauer/ppa"
+ppa="easybib/ppa"
 execute "add #{ppa}" do
   command "add-apt-repository ppa:#{ppa}"
 end
@@ -9,6 +9,6 @@ execute "update sources" do
   command "aptitude update"
 end
 
-package "php"
+package "php5-easybib"
 
 include_recipe "php-fpm::configure"
