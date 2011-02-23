9737e9bfbaa83123ad6cb15c18562f1e18b9f8d0
till <till@php.net>
2011-02-23 13:15:18 +0100

    * only setup munin-node when a server with role monitoring-master is in the cluster

--- munin-node/recipes/setup.rb
+++ munin-node/recipes/setup.rb
@@ -3,16 +3,18 @@ require 'resolv'
 package "munin-node"
 include_recipe "munin-node::service"
 
-ip_munin = Resolv.getaddress(node[:scalarium][:roles]["monitoring-master"][:instances]["darth-vader"]["private_dns_name"])
+if node[:scalarium][:roles].include?('monitoring-master')
+  ip_munin = Resolv.getaddress(node[:scalarium][:roles]["monitoring-master"][:instances]["darth-vader"]["private_dns_name"])
 
-template "/etc/munin/munin-node.conf" do
-  source "munin-node.erb"
-  variables({
-    :ip_munin => ip_munin
-  })
-end
+  template "/etc/munin/munin-node.conf" do
+    source "munin-node.erb"
+    variables({
+      :ip_munin => ip_munin
+    })
+  end
 
-if node[:scalarium][:instance][:roles].include?('nginxphpapp')
-  include_recipe "munin-node::nginx"
-  include_recipe "munin-node::phpfpm"
+  if node[:scalarium][:instance][:roles].include?('nginxphpapp')
+    include_recipe "munin-node::nginx"
+    include_recipe "munin-node::phpfpm"
+  end
 end
