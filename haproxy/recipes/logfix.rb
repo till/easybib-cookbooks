96d0ed75c391000e3e147dcc0d31f010f07bd2aa
till <till@php.net>
2011-03-24 17:50:23 +0100

    * fix path

--- haproxy/recipes/logfix.rb
+++ haproxy/recipes/logfix.rb
@@ -1,10 +1,10 @@
 # move /var/logs to mount
-execute "Move /var/logs" do
-  command "mv /var/logs /mnt"
-  not_if "test -h /var/logs"
+execute "Move /var/log" do
+  command "mv /var/log /mnt"
+  not_if "test -h /var/log"
 end
 
 # symlink so we don't break any services
-link "/var/logs" do
-  to "/mnt/logs"
+link "/var/log" do
+  to "/mnt/log"
 end
