6afba3584dadf305178e8c97466436237acb975d
till <till@php.net>
2011-03-24 17:32:16 +0100

    * wrong recipe name (apache >> apache2)

--- munin/recipes/server.rb
+++ munin/recipes/server.rb
@@ -4,7 +4,7 @@ execute "enable mod_rewrite" do
   command "a2enmod rewrite"
 end
 
-include_recipe "apache::service"
+include_recipe "apache2::service"
 template "/etc/apache2/sites-enabled/munin" do
   mode "0644"
   source "apache2.erb"
