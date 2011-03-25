7ed8b464e9951114eef2cab299b1b1e5449e777d
till <till@php.net>
2011-03-25 17:10:43 +0100

    * add attributes for standard variables  * add configuration for haproxy

---
+++ loggly/attributes/default.rb
@@ -0,0 +1,4 @@
+default[:syslog]                     = {}
+default[:syslog][:host]              = "logs.loggly.com"
+default[:syslog][:haproxy]           = {}
+default[:syslog][:haproxy][:log_dir] = "/mnt/logs/haproxy"
