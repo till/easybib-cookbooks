dfa9b51f213dfe8d7ed36eec6f9cde137c8f7b49
till <till@php.net>
2011-03-25 17:30:45 +0100

    * refactoring:    * logfiles in attributes    * generate config with a loop    * remove haproxy related

--- loggly/attributes/default.rb
+++ loggly/attributes/default.rb
@@ -1,4 +1,6 @@
 default[:syslog]                     = {}
+default[:syslog][:logfiles]          = { "/var/log/nginx/error.log" => "error", "/var/log/php/slow.log" => "notice", "/var/log/php/fpm.log" => "error" }
 default[:syslog][:host]              = "logs.loggly.com"
 default[:syslog][:haproxy]           = {}
 default[:syslog][:haproxy][:log_dir] = "/mnt/logs/haproxy"
+default[:syslog][:poll]              = 10
