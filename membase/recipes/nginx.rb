892aca51cee263b6b1ce1681e38d2cf0d409295f
till <till@php.net>
2011-03-28 15:22:11 +0200

    * restart nginx when template is installed

--- membase/recipes/nginx.rb
+++ membase/recipes/nginx.rb
@@ -1,5 +1,10 @@
 package "nginx"
 
+service "nginx" do
+  supports :status => true, :restart => true, :reload => true
+  action [ :enable, :start ]
+end
+
 sites = "/etc/nginx/sites-enabled"
 
 execute "remove default vhost" do
@@ -12,4 +17,6 @@ template "#{sites}/membase.conf" do
   variables({
     :ip => node[:scalarium][:instance][:ip]
   })
+  notifies :restart, resources(:service => "nginx"), :delayed
 end
+
