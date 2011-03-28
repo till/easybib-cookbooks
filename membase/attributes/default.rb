8d5e1a331403b46da6e94acc02b5e20612055d8e
till <till@php.net>
2011-03-28 15:18:59 +0200

    * test 4000 MB

--- membase/attributes/default.rb
+++ membase/attributes/default.rb
@@ -1,5 +1,5 @@
-set_unless[:membase][:clustersize]   = 300
-set_unless[:membase][:bucketsize]    = 300
+set_unless[:membase][:clustersize]   = 4000
+set_unless[:membase][:bucketsize]    = 4000
 set_unless[:membase][:bucketname]    = "easybib_sessions"
 set_unless[:membase][:adminuser]     = "admin"
 set_unless[:membase][:adminpassword] = "password"
