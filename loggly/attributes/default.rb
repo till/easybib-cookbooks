b6827dca740abaaa382eeaf977f526a5b2e659ac
till <till@php.net>
2011-03-31 19:27:50 +0200

    * prep to make this recipe standalone

--- loggly/attributes/default.rb
+++ loggly/attributes/default.rb
@@ -4,3 +4,8 @@ default[:syslog][:host]              = "logs.loggly.com"
 default[:syslog][:haproxy]           = {}
 default[:syslog][:haproxy][:log_dir] = "/mnt/logs/haproxy"
 default[:syslog][:poll]              = 10
+
+#set_unless[:loggly][:domain] = "example"
+#set_unless[:loggly][:input]  = 1
+#set_unless[:loggly][:user]   = "account"
+#set_unless[:loggly][:pass]   = "password"
