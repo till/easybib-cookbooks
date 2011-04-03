c7e9e60af58a7203d67665b776434818263c5ee5
till <till@php.net>
2011-04-03 19:46:42 +0200

    * fix attributes (make them vagrant compatible)

--- haproxy/attributes/default.rb
+++ haproxy/attributes/default.rb
@@ -1,3 +1,5 @@
-set[:scalarium_initial_setup][:bind_mounts][:mounts] = scalarium_initial_setup[:bind_mounts][:mounts].update({
-  "/var/log/haproxy" => "/mnt/var/log/haproxy"
-})
+if attribute?(:scalarium_initial_setup)
+  set[:scalarium_initial_setup][:bind_mounts][:mounts] = scalarium_initial_setup[:bind_mounts][:mounts].update({
+    "/var/log/haproxy" => "/mnt/var/log/haproxy"
+  })
+end
