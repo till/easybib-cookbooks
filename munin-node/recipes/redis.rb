05ece1b25fbfe1f36633eb9dc786ecb1ceeb40f7
till <till@php.net>
2011-04-10 02:57:00 +0200

    * added dependency on perl package (just to make sure)  * fixed symlinks: 3 different names point to the same script

--- munin-node/recipes/redis.rb
+++ munin-node/recipes/redis.rb
@@ -1,8 +1,14 @@
+package "libio-all-perl"
+
 cookbook_file "/opt/redis-munin.pl" do
   source "redis-munin.pl"
   mode   "0755"
 end
 
-link "/etc/munin/plugins/redis-munin" do
-  to "/opt/redis-munin.pl"
+munin_plugins = ["redis_connected_clients", "redis_per_sec", "redis_used_memory"]
+
+munin_plugins.each do |plugin|
+  link "/etc/munin/plugins/#{plugin}" do
+    to "/opt/redis-munin.pl"
+  end
 end
