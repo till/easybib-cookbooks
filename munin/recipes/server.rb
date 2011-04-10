41ebd24562449daa7d66e60a482b2c30a2d5420a
till <till@php.net>
2011-04-10 19:42:03 +0200

    * fix typo apache >> apache2

--- munin/recipes/server.rb
+++ munin/recipes/server.rb
@@ -8,5 +8,5 @@ include_recipe "apache2::service"
 template "/etc/apache2/sites-enabled/munin" do
   mode "0644"
   source "apache2.erb"
-  notifies :restart, "service[apache]"
+  notifies :restart, "service[apache2]"
 end
