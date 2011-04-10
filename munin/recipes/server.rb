977fdf90ebc1a8ca7103c207c77a73f820ed98ec
till <till@php.net>
2011-04-10 19:58:58 +0200

    * use my own service definition instead of scalarium's

--- munin/recipes/server.rb
+++ munin/recipes/server.rb
@@ -4,9 +4,15 @@ execute "enable mod_rewrite" do
   command "a2enmod rewrite"
 end
 
-include_recipe "apache2::service"
+#include_recipe "apache2::service"
+
 template "/etc/apache2/sites-enabled/munin" do
-  mode "0644"
+  mode   "0644"
   source "apache2.erb"
-  notifies :restart, "service[apache2]"
+end
+
+service "apache2" do
+  supports        :restart => true
+  restart_command "/etc/init.d/apache2 restart && sleep 1"
+  action          :restart
 end
