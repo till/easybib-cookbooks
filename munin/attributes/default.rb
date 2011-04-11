40c7c6980f01115e873839589a117fd0a7b35bda
till <till@php.net>
2011-04-11 18:39:54 +0200

    * add bindmounts

--- munin/attributes/default.rb
+++ munin/attributes/default.rb
@@ -1,3 +1,11 @@
 default[:munin] = {}
 
 set_unless[:sysop_email] = 'foo@example.org'
+
+# munin's db dir and log
+if attribute?(:scalarium_initial_setup)
+  set[:scalarium_initial_setup][:bind_mounts][:mounts] = scalarium_initial_setup[:bind_mounts][:mounts].update({
+    "/var/lib/munin" => "/vol/ganglia/munin/lib",
+    "/var/log/munin" => "/mnt/var/log/munin"
+  })
+end
