cbab0bda57a985320b020b4060b1405117981140
till <till@php.net>
2011-04-11 17:49:51 +0200

    * add recipe

--- munin/metadata.rb
+++ munin/metadata.rb
@@ -5,4 +5,5 @@ description       "Munin setup and configuration"
 version           "0.1"
 recipe            "munin::server", "Install munin"
 recipe            "munin::configure", "Configure nodes, etc."
+recipe            "munin::apache2", "Apache2 related configuration setup to display munin stats"
 supports 'ubuntu'
