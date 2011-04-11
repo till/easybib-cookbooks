a441af12f4c63a38b76ba449dc9a030b8abcc204
till <till@php.net>
2011-04-11 17:48:41 +0200

    * move apache2 related config into munin::apache2

--- munin/recipes/server.rb
+++ munin/recipes/server.rb
@@ -1,18 +1,5 @@
 package "munin"
 
-execute "enable mod_rewrite" do
-  command "a2enmod rewrite"
-end
-
-#include_recipe "apache2::service"
-
-template "/etc/apache2/sites-enabled/munin" do
-  mode   "0644"
-  source "apache2.erb"
-end
-
-service "apache2" do
-  supports        :restart => true
-  restart_command "/etc/init.d/apache2 restart && sleep 1"
-  action          :restart
+if File.directory?("/etc/apache2") 
+  include_recipe "munin::apache2"
 end
