535ae5d361aa3d75e313f4a8e7bd7db1b60ed6c8
till <till@php.net>
2011-04-12 02:02:18 +0200

    * refactor template    * use attributes to define 'passthroughs'    * remove duplicate configuration in favour of loop

--- nginx-app/attributes/default.rb
+++ nginx-app/attributes/default.rb
@@ -1,3 +1,4 @@
-default["nginx-app"] = {}
-default["nginx-app"][:user] = "www-data"
-default["nginx-app"][:group] = "www-data"
+default["nginx-app"]                      = {}
+default["nginx-app"][:user]               = "www-data"
+default["nginx-app"][:group]              = "www-data"
+default["nginx-app"][:static_directories] = ["js", "css", "images", "raw"]
