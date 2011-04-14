3ad72e4ef9f725cb9682f4bc774ab140e83e4d85
till <till@php.net>
2011-04-14 17:25:26 +0200

    * add attribute node[:munin][:libdir]  * make sure directory exists

--- munin/attributes/default.rb
+++ munin/attributes/default.rb
@@ -1,7 +1,7 @@
-default[:munin] = {}
+set_unless[:munin]          = {}
+set_unless[:munin][:libdir] = '/vol/ganglia/munin/lib'
 
 set_unless[:sysop_email] = 'foo@example.org'
-
 # munin's db dir and log
 if attribute?(:scalarium_initial_setup)
   set[:scalarium_initial_setup][:bind_mounts][:mounts] = scalarium_initial_setup[:bind_mounts][:mounts].update({
--- munin/recipes/server.rb
+++ munin/recipes/server.rb
@@ -1,5 +1,13 @@
 package "munin"
 
+directory "#{node[:munin][:libdir]}" do
+  owner     'munin'
+  group     'munin'
+  mode      '0755'
+  action    :create
+  recursive true
+end
+
 if File.directory?("/etc/apache2") 
   include_recipe "munin::apache2"
 else
