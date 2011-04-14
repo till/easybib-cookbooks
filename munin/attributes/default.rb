7d959db2bb5d746aa56e77eb5a142610a71c11e6
till <till@php.net>
2011-04-14 21:17:02 +0200

    * didn't work, fixed this with the previous commit

--- munin/attributes/default.rb
+++ munin/attributes/default.rb
@@ -2,10 +2,3 @@ set_unless[:munin]          = {}
 set_unless[:munin][:libdir] = '/vol/ganglia/munin/lib'
 
 set_unless[:sysop_email] = 'foo@example.org'
-# munin's db dir and log
-if attribute?(:scalarium_initial_setup)
-  set[:scalarium_initial_setup][:bind_mounts][:mounts] = scalarium_initial_setup[:bind_mounts][:mounts].update({
-    "/var/lib/munin" => "/vol/ganglia/munin/lib",
-    "/var/log/munin" => "/mnt/var/log/munin"
-  })
-end
