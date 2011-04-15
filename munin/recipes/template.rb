bf2745bf5c1df066b30522df98253b62c8332feb
till <till@php.net>
2011-04-15 19:12:59 +0200

    * recipe to install template customizations

---
+++ munin/recipes/template.rb
@@ -0,0 +1,20 @@
+tmpl   = ["munin-domainview.tmpl", "munin-nodeview.tmpl", "munin-overview.tmpl", "munin-serviceview.tmpl"]
+static = ["some.js", "style.css"]
+
+tmpl.each do |template|
+  cookbook_file "/etc/munin/templates/#{template}" do
+    source "#{template}"
+    owner  "munin"
+    group  "munin"
+    mode   "0644"
+  end
+end
+
+static.each do |asset|
+  cookbook_file "/var/www/munin/#{asset}" do
+    source "#{asset}"
+    owner  "munin"
+    group  "munin"
+    mode   "0644"
+  end
+end
