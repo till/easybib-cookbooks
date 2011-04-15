dfd132f0510b0b0204f1a40aabacfd2a02f9bbc0
till <till@php.net>
2011-04-15 13:52:52 +0200

    * include munin-plugins for nginx  * use cookbook_file (chef 0.9)  * small refactoring  * closes #2

--- munin-node/recipes/nginx.rb
+++ munin-node/recipes/nginx.rb
@@ -1,26 +1,16 @@
-# prior to 10.04 we have to download them ourselves
 munin_plugins = ["nginx_status", "nginx_request", "nginx_memory"]
 
 directory "/opt/munin-nginx" do
   mode "0755"
 end
 
-remote_file "/opt/munin-nginx/nginx_memory" do
-  source "http://exchange.munin-monitoring.org/plugins/nginx_memory/version/1/download"
-  mode "0755"
-end
-
-remote_file "/opt/munin-nginx/nginx_status" do
-  source "http://exchange.munin-monitoring.org/plugins/nginx_status/version/3/download"
-  mode "0755"
-end
-
-remote_file "/opt/munin-nginx/nginx_request" do
-  source "http://exchange.munin-monitoring.org/plugins/nginx_request/version/2/download"
-  mode "0755"
-end
-
 munin_plugins.each do |plugin|
+
+  cookbook_file "/opt/munin-nginx/#{plugin}" do
+    source "#{plugin}"
+    mode "0755"
+  end
+
   link "/etc/munin/plugins/#{plugin}" do
     to "/opt/munin-nginx/#{plugin}"
   end
