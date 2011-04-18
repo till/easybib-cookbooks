6a4916a578ae1e1595f690e54a6f94c6cf7829bd
till <till@php.net>
2011-04-18 16:41:40 +0200

    * make recipe compatible to source install  * refactor to use settings attributes

--- munin/recipes/template.rb
+++ munin/recipes/template.rb
@@ -1,21 +1,27 @@
 tmpl   = ["munin-domainview.tmpl", "munin-nodeview.tmpl", "munin-overview.tmpl", "munin-serviceview.tmpl"]
 static = ["some.js", "style.css"]
 
+if File.directory('/etc/opt/munin/templates') 
+  tmplDir = '/etc/opt/munin/templates'
+else
+  tmplDir = '/etc/munin/templates'
+end
+
 tmpl.each do |template|
-  cookbook_file "/etc/munin/templates/#{template}" do
+  cookbook_file "#{tmplDir}/#{template}" do
     source "#{template}"
-    owner  "munin"
-    group  "munin"
+    owner  node[:munin][:user]
+    group  node[:munin][:group]
     mode   "0644"
     backup false
   end
 end
 
 static.each do |asset|
-  cookbook_file "/var/www/munin/#{asset}" do
+  cookbook_file "#{node[:munin][:www_dir]}/#{asset}" do
     source "#{asset}"
-    owner  "munin"
-    group  "munin"
+    owner  node[:munin][:user]
+    group  node[:munin][:group]
     mode   "0644"
     backup false
   end
