69f0f76b28680a8b590c0a65c9125575f40ff632
till <till@php.net>
2011-04-27 00:58:23 +0200

    * change path to /etc/haproxy  * added haproxy::down to initially copy all files on setup event

---
+++ haproxy/recipes/down.rb
@@ -0,0 +1,8 @@
+default[:haproxy][:errorloc].each do |code,file|
+  cookbook_file "/etc/haproxy/#{file}" do
+    source "#{file}"
+    owner "haproxy"
+    group "haproxy"
+    mode 0644
+  end
+end
