d7d214db2b81e6caf5e7ec4824f9a33e38342afc
till <till@php.net>
2011-05-03 15:11:00 +0200

    * add deploy_to default setting  * add recipe for analysis-specific setup

--- nginx-app/attributes/default.rb
+++ nginx-app/attributes/default.rb
@@ -2,3 +2,6 @@ default["nginx-app"]                      = {}
 default["nginx-app"][:user]               = "www-data"
 default["nginx-app"][:group]              = "www-data"
 default["nginx-app"][:static_directories] = ["js", "css", "images", "raw"]
+
+# for localhost
+set_unless[:deploy][:deploy_to] = "/var/www"
---
+++ nginx-app/recipes/analysis.rb
@@ -0,0 +1,8 @@
+template "/etc/nginx/sites-enabled/easybib.com.conf" do
+  source "easybib.com.conf.erb"
+  mode "0755"
+  owner node["nginx-app"][:user]
+  group node["nginx-app"][:group]
+  variables :deploy => deploy, :application => "easybib"
+  notifies :restart, resources(:service => "nginx"), :delayed
+end
