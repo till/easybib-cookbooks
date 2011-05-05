318cacecb198549b9eb24b554d449b8ebd2678c6
till <till@php.net>
2011-05-05 20:36:53 +0200

    owner/group, restart

--- elasticsearch/recipes/nginx.rb
+++ elasticsearch/recipes/nginx.rb
@@ -1,5 +1,11 @@
-# assume nginx-app was run before
+# assume nginx-app::server was run before
 
 template "/etc/nginx/sites-enabled/elasticsearch.conf" do
   source "elasticsearch.conf.erb"
+  owner  "www-data"
+  group  "www-data"
+end
+
+service "nginx" do
+  action :restart
 end
