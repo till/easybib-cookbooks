0fb9622de542f6e1ad6f17f0af95dbcc2dfff255
till <till@php.net>
2011-05-20 16:37:30 +0200

    * add metadata

---
+++ vagrant-fix/metadata.rb
@@ -0,0 +1,8 @@
+maintainer        "Till Klampaeckel"
+maintainer_email  "till@php.net"
+license           "New BSD License"
+description       "Runs aptitude update and gem update prior to all other recipes."
+version           "0.1"
+recipe            "vagrant-fix::default", "See description!"
+
+supports 'ubuntu'
