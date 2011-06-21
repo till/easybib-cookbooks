194854314499567952c35f64d1c84eb77997461b
till <till@php.net>
2011-06-21 22:17:01 +0200

    * update to 0.16.2

--- elasticsearch/attributes/default.rb
+++ elasticsearch/attributes/default.rb
@@ -1,2 +1,2 @@
 set_unless[:elasticsearch][:download] = "https://github.com/downloads/elasticsearch/elasticsearch"
-set_unless[:elasticsearch][:version]  = "elasticsearch-0.16.1.tar.gz"
+set_unless[:elasticsearch][:version]  = "elasticsearch-0.16.2.tar.gz"
