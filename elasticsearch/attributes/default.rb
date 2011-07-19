6b1277e8a317f73aaeac4961f262270f8a3cb84d
till <till@php.net>
2011-07-19 14:41:52 +0200

    update to 0.17.0

--- elasticsearch/attributes/default.rb
+++ elasticsearch/attributes/default.rb
@@ -1,4 +1,4 @@
 set_unless[:elasticsearch][:download] = "https://github.com/downloads/elasticsearch/elasticsearch"
-set_unless[:elasticsearch][:version]  = "elasticsearch-0.16.4.tar.gz"
+set_unless[:elasticsearch][:version]  = "elasticsearch-0.17.0.tar.gz"
 set_unless[:elasticsearch][:basedir]  = "/mnt/opt"
 set_unless[:elasticsearch][:cluster]  = "citationanalytics"
