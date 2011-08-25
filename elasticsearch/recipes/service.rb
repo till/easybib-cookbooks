7d78101e2eea4fe779179dd0dbc43ac1733e3ab6
till <till@php.net>
2011-08-25 16:29:35 +0200

    * different memory settings for ec2 (by instance type)  * use local variables in template

--- elasticsearch/recipes/service.rb
+++ elasticsearch/recipes/service.rb
@@ -52,11 +52,54 @@ execute "register elasticsearch as a service" do
   not_if  do File.symlink?("/etc/init.d/elasticsearch") end
 end
 
+if node[:ec2]
+  case node[:ec2][:instance_type]
+    when "t1.micro"
+      mem_min = 256
+      mem_max = 256
+    when "m1.small"
+      mem_min = 1024
+      mem_max = 1024
+    when "m1.large"
+      mem_min = 6000
+      mem_max = 6000
+    when "m1.xlarge"
+      mem_min = node[:elasticsearch][:memory][:max]
+      mem_max = node[:elasticsearch][:memory][:max]
+    when "m2.xlarge"
+      mem_min = 15000
+      mem_max = 15000
+    when "m2.2xlarge"
+      mem_min = 30000
+      mem_max = 30000
+    when "m2.4xlarge"
+      mem_min = 60000
+      mem_max = 60000
+    when "c1.medium"
+      mem_min = 900
+      mem_max = 900
+    when "c1.xlarge"
+      mem_min = 6000
+      mem_max = 6000
+    else
+      raise Chef::Exceptions::Env, "Unknown instance identifier: #{node[:ec2][:instance_type]}"
+  end
+else
+  # assume developer VM
+  mem_min = 256
+  mem_max = 256
+end
+
 template "#{service_dir}/service/elasticsearch.conf" do
   source "service-elasticsearch.conf.erb"
   owner  node[:elasticsearch][:user]
   group  node[:elasticsearch][:group]
   mode   "0644"
+  variables(
+    :mem_min => mem_min,
+    :mem_max => mem_max,
+    :es_home => node[:elasticsearch][:home]
+  )
 end
 
 service "elasticsearch" do
