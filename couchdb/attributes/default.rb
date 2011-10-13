7f9cfcea84800c70003438a6944fe550638f228a
till <till@php.net>
2011-10-13 19:32:02 +0200

    download for couchbase

--- couchdb/attributes/default.rb
+++ couchdb/attributes/default.rb
@@ -1,13 +1,16 @@
-default[:couchdb] = {}
-default[:couchdb][:version] = '1.0.2'
-default[:couchdb][:datadir] = '/couchdb/'
-default[:couchdb][:logdir] = '/mnt/couchdb/log'
-default[:couchdb][:viewdir] = '/views/'
-default[:couchdb][:backup] = true
-default[:couchdb][:backupdir] = '/backup'
-default[:couchdb][:gid] = 2000
-default[:couchdb][:replica] = false
+default[:couchdb]                  = {}
+default[:couchdb][:version]        = '1.0.2'
+default[:couchdb][:datadir]        = '/couchdb/'
+default[:couchdb][:logdir]         = '/mnt/couchdb/log'
+default[:couchdb][:viewdir]        = '/views/'
+default[:couchdb][:backup]         = true
+default[:couchdb][:backupdir]      = '/backup'
+default[:couchdb][:gid]            = 2000
+default[:couchdb][:replica]        = false
 default[:couchdb][:backup_backlog] = 10
-default[:couchdb][:port] = 5984
+default[:couchdb][:port]           = 5984
+
+default[:couchbase]      = {}
+default[:couchbase][:dl] = "http://packages.couchbase.com/releases/couch/1.1.2/couchbase-single-server-community_x86_64_1.1.2.deb"
 
 set_unless[:couchdb_admins] = {}
