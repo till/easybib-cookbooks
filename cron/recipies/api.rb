f8bf68872bbc1ffbac4e8626199f774311f95067
till <till@php.net>
2011-11-19 21:14:10 +0100

    blah SPELLING

--- cron/recipies/api.rb
+++
@@ -1,9 +0,0 @@
-# install a cronjob to run every 5 minutes
-
-if node[:scalarium][:instance][:roles].include?('bibapi')
-  cron "stats from redis to mysql" do
-    mailto node[:sysop_email]
-    minute "*/5"
-    command "/srv/www/easybib_api/current/app/modules/default/scripts/cron_tracking_into_mysql.php"
-  end
-end
