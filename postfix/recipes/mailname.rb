d7fe7ad655afd9b6ab7d57b4fc1c9bf14499f31f
till <till@php.net>
2011-11-28 17:44:48 +0100

    * use node.json value when available

--- postfix/recipes/mailname.rb
+++ postfix/recipes/mailname.rb
@@ -2,7 +2,13 @@
 if node[:scalarium]
   my_hostname = node[:scalarium][:instance][:hostname]
 else
-  my_hostname = node[:hostname]
+  # node.json
+  if node[:server_name]
+    my_hostanem = node[:server_name]
+  # from 'ohai'
+  else
+    my_hostname = node[:hostname]
+  end
 end
 
 template "/etc/mailname" do
