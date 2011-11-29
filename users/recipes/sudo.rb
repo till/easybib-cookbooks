5856226629c0264c560dda91db49cbe081112cb7
till <till@php.net>
2011-11-29 22:43:53 +0100

    * some things are too simple :)

--- users/recipes/sudo.rb
+++ users/recipes/sudo.rb
@@ -1,9 +1,3 @@
-# install standard /etc/sudoers with includeDir directive
-template "/etc/sudoers" do
-  source "sudoers.erb"
-  mode   "0440"
-end
-
 # install a config for easybib
 if node[:users]
   template "/etc/sudoers.d/easybib" do
