1d57c316ec45004467099cc05ea6be4b34fc2165
till <till@php.net>
2011-11-29 18:47:39 +0100

    recipe to install PHPUnit 3.4

---
+++ phpunit/recipes/default.rb
@@ -0,0 +1,15 @@
+# this cookbook sets up PHPUnit 3.4
+
+phpunit_location = "/usr/local/phpunit34"
+
+execute "enable auto_discover" do
+  command "pear config-set auto_discover 1"
+end
+
+execute "install PHPUnit 3.4" do
+  command "pear install --installroot #{phpunit_location} pear.phpunit.de/PHPUnit-3.4.15"
+end
+
+link "/usr/bin/phpunit34" do
+  to "#{phpunit_location}/usr/bin/phpunit"
+end
