2e7aa1cc9c50b6203e48b9ae17581ff414526bd2
till <till@php.net>
2011-11-29 18:59:26 +0100

    ignore failure

--- phpunit/recipes/default.rb
+++ phpunit/recipes/default.rb
@@ -8,8 +8,10 @@ execute "enable auto_discover" do
   command "pear config-set auto_discover 1"
 end
 
+# ignore failure to make multiple runs painless
 execute "install PHPUnit 3.4" do
-  command "pear install --installroot #{phpunit_location} pear.phpunit.de/PHPUnit-3.4.15"
+  command        "pear install --installroot #{phpunit_location} pear.phpunit.de/PHPUnit-3.4.15"
+  ignore_failure true
 end
 
 link "#{pear_bin}/phpunit34" do
