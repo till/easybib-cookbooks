1a7179b684b100c3fb986931c32ddd4237597b54
till <till@php.net>
2011-11-29 19:24:58 +0100

    * install phpunit with fixed up include_path

--- phpunit/recipes/default.rb
+++ phpunit/recipes/default.rb
@@ -3,6 +3,7 @@
 phpunit_location = "/usr/local/phpunit34"
 
 pear_bin = `pear config-get bin_dir`.strip
+php_dir  = `pear config-get php_dir`.strip
 
 execute "enable auto_discover" do
   command "pear config-set auto_discover 1"
@@ -14,6 +15,14 @@ execute "install PHPUnit 3.4" do
   not_if  do
     File.exist?("#{phpunit_location}#{pear_bin}/phpunit")
   end
+end
+
+template "#{phpunit_location}#{pear_bin}/phpunit" do
+  mode   "0755"
+  source "phpunit.erb"
+  variables({
+    :complete_path => "#{phpunit_location}#{php_dir}"
+  })
 end
 
 link "#{pear_bin}/phpunit34" do
