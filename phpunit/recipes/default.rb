1c8b64072ac91a7f00dcffef8c2e1e1549a0602b
till <till@php.net>
2011-11-29 18:54:58 +0100

    fix paths

--- phpunit/recipes/default.rb
+++ phpunit/recipes/default.rb
@@ -2,6 +2,8 @@
 
 phpunit_location = "/usr/local/phpunit34"
 
+pear_bin = `pear config-get bin_dir`
+
 execute "enable auto_discover" do
   command "pear config-set auto_discover 1"
 end
@@ -10,6 +12,6 @@ execute "install PHPUnit 3.4" do
   command "pear install --installroot #{phpunit_location} pear.phpunit.de/PHPUnit-3.4.15"
 end
 
-link "/usr/bin/phpunit34" do
-  to "#{phpunit_location}/usr/bin/phpunit"
+link "#{pear_bin}/phpunit34" do
+  to "#{phpunit_location}#{pear_bin}/phpunit"
 end
