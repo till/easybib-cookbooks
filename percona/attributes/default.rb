d7a8d02093793ccf529601ce3dc72aab70b587c0
till <till@php.net>
2011-12-10 23:18:11 +0100

    more attributes for the cronjob

--- percona/attributes/default.rb
+++ percona/attributes/default.rb
@@ -6,5 +6,8 @@ default["percona-server"][:key]     = "1C4CBDCDCD2EFD2A"
 set_unless[:mysql]                        = {}
 set_unless[:mysql][:server_root_password] = "plzhax0rme"
 
-set_unless[:xtrabackup]       = {}
-set_unless[:xtrabackup][:dir] = "/var/backups"
+set_unless[:xtrabackup]           = {}
+set_unless[:xtrabackup][:dir]     = "/var/backups"
+set_unless[:xtrabackup][:weekday] = 6
+set_unless[:xtrabackup][:hour]    = 8
+set_unless[:xtrabackup][:minute]  = 0
