6303cd0aa896ac57823fff35c1bca4f9a464c7ef
till <till@php.net>
2011-12-10 23:11:45 +0100

    defaults

--- percona/attributes/default.rb
+++ percona/attributes/default.rb
@@ -1,3 +1,8 @@
 default["percona-server"] = {}
 default["percona-server"][:version] = "5.1"
 default["percona-server"][:key]     = "1C4CBDCDCD2EFD2A"
+
+# set if scalarium is not available
+set_unless[:mysql]                        = {}
+set_unless[:mysql][:server_root_password] = "plzhax0rme"
+
