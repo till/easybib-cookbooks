502cc5da3f1053712b884a03cdca2bb117f81b66
till <till@php.net>
2011-12-11 02:22:22 +0100

    add http port

--- gearman/attributes/default.rb
+++ gearman/attributes/default.rb
@@ -1,4 +1,5 @@
-default[:gearman] = {}
-default[:gearman][:port] = '31337'
+default[:gearman]         = {}
+default[:gearman][:port]  = '31337'
 default[:gearman][:debug] = "1"
 default[:gearman][:pecl]  = '0.8.0'
+default[:gearman][:http]  = 80
