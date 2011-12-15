382e319eefed7358100d140f152c9afdb0c4f2f7
till <till@php.net>
2011-12-15 22:28:17 +0100

    configurable script names

--- percona/recipes/daily-backup.rb
+++ percona/recipes/daily-backup.rb
@@ -2,14 +2,14 @@ include_recipe "percona::xtrabackup"
 
 # install scripts required for daily backup
 
-# compress the backup
-template "/usr/bin/easybib-backthisup" do
+# tar & compress the backup
+template node[:xtrabacku][:processor] do
   source "backthisup.sh.rb"
   mod    "0755"
 end
 
 # script to store backups in s3
-template "/usr/bin/easybib-s3upload" do
+template node[:xtrabackup][:uploader] do
   source "s3upload.sh.erb"
   mode   "0755"
 end
