dc4f6b9cbc55090a6fbf37e72848ef859203adee
till <till@php.net>
2011-12-29 17:20:00 +0100

    recipe to install and configure suhosin

---
+++ php-suhosin/attributes/default.rb
@@ -0,0 +1,6 @@
+default[:suhosin]                      = {}
+default[:suhosin][:version]            = "0.9.32.1"
+default[:suhosin][:post]               = {}
+default[:suhosin][:post][:max_vars]    = 1000
+default[:suhosin][:request]            = {}
+default[:suhosin][:request][:max_vars] = 2000
---
+++ php-suhosin/recipes/default.rb
@@ -0,0 +1,31 @@
+php_ext_dir = `php -r 'ini_get("extension_dir");'`.strip
+suhosin_ext = "#{php_ext_dir}/suhosin.so"
+suhosin_dl  = "suhosin-#{node[:suhosin][:version]}.tar.gz"
+
+remote_file "/tmp/#{suhosin_dl}" do
+  source "http://download.suhosin.org/#{suhosin_dl}"
+  mode   "0644"
+  not_if do
+    File.exists?(suhosin_ext)
+  end
+end
+
+execute "extract" do
+  command "cd /tmp && tar zxf #{suhosin_dl}"
+  not_if do
+    File.exists?(suhosin_ext)
+  end
+end
+
+execute "phpize, configure, make install" do
+  command "cd /tmp/suhosin-#{node[:suhosin][:version]} && phpize && ./configure && make install"
+  not_if do
+    File.exists?(suhosin_ext)
+  end
+end
+
+template "/usr/local/etc/php/suhosin.ini" do
+  source "suhosin.ini.erb"
+  mode   "0644"
+end
+
