a5029024b378530e32402e3a9165939928e9467b
till <till@php.net>
2012-01-06 19:40:11 +0100

    hash vs array

--- statsd/attributes/default.rb
+++ statsd/attributes/default.rb
@@ -1,7 +1,7 @@
-set_unless[:librato]                     = {}
-set_unless[:librato][:node]              = {}
+set_unless[:librato]                     = []
+set_unless[:librato][:node]              = []
 set_unless[:librato][:node][:user]       = 'root'
 set_unless[:librato][:node][:group]      = 'root'
-set_unless[:librato][:metrics]           = {}
+set_unless[:librato][:metrics]           = []
 set_unless[:librato][:metrics][:email]   = 'foo@example.org'
 set_unless[:librato][:metrics][:api_key] = '123'
