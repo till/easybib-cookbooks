d12fa995521cb23664c5ba7f5d34fcac7bbf2fce
till <till@php.net>
2012-01-06 18:43:39 +0100

    create config

---
+++ statsd/recipes/configure.rb
@@ -0,0 +1,14 @@
+base = "/etc/statsd"
+
+directory base do
+  mode  "0755"
+  owner node[:librato][:node][:user]
+  group node[:librato][:node][:group]
+end
+
+template "#{base}/config.js"
+  source "easybib-config.js.erb"
+  mode   "0600"
+  owner  node[:librato][:node][:user]
+  group  node[:librato][:node][:group]
+end
