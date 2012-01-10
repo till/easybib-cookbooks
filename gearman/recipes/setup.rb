175108d12a0320be99be5e1b9f3beb35b785bdc7
Lee Azzarello <lee@rockingtiger.com>
2012-01-10 17:16:02 -0500

    add drizzle ppa to provide libgearman4, which fixes a bug in the release version

--- gearman/recipes/setup.rb
+++ gearman/recipes/setup.rb
@@ -1,3 +1,9 @@
+execute "add-drizzle-ppa" do
+  command "add-apt-repository ppa:drizzle-developers/ppa"
+  creates "/etc/apt/sources.list.d/drizzle-developers-ppa-lucid.list"
+end
+
+package "libgearman4"
 package "gearman-job-server"
 
 include_recipe "gearman::configure"
