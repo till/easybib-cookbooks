9e7ab94ac6a8f0209dbbc9172fb0e6ed31abe55e
till <till@php.net>
2012-01-11 18:57:51 +0100

    quickfix: provide path to php

--- cron/recipes/api.rb
+++ cron/recipes/api.rb
@@ -4,6 +4,6 @@ if node[:scalarium][:instance][:roles].include?('bibapi')
   cron "stats from redis to mysql" do
     mailto node[:sysop_email]
     minute "*/5"
-    command "/srv/www/easybib_api/current/app/modules/default/scripts/cron_tracking_into_mysql.php"
+    command "/usr/local/bin/php /srv/www/easybib_api/current/app/modules/default/scripts/cron_tracking_into_mysql.php"
   end
 end
