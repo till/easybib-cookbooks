6015c4cc7a08e08ffd3ff5f80a125ef2da2e42c2
Lee Azzarello <lee@rockingtiger.com>
2012-01-23 16:39:27 -0500

    more dash replacement for notifies resource

--- silverline/recipes/addrepo.rb
+++ silverline/recipes/addrepo.rb
@@ -1,11 +1,11 @@
 include_recipe "apt::ppa"
 
-execute "import-silverline-key" do
+execute "import_silverline_key" do
   command "curl http://apt.librato.com/packages.librato.key | apt-key add -"
   action :nothing
 end
 
 template "/etc/apt/sources.list.d/silverline.list" do
   source "silverline.list.erb"
-  notifies :run, "execute[import-silverline-key]", :immediately
+  notifies :run, "execute[import_silverline_key]", :immediately
 end
