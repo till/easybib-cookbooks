58810b56e4658c9653822d5f4d61657a0e3d99ba
Lee Azzarello <lee@rockingtiger.com>
2012-01-23 13:52:02 -0500

    add service to setup process

--- gearman/recipes/setup.rb
+++ gearman/recipes/setup.rb
@@ -3,6 +3,13 @@ execute "add-drizzle-ppa" do
   creates "/etc/apt/sources.list.d/drizzle-developers-ppa-lucid.list"
 end
 
+service "gearman-job-server" do
+  service_name "gearman-job-server"
+
+  supports :status => false, :restart => true, :reload => false, "force-reload" => true
+  action :enable
+end
+
 package "libgearman4"
 package "gearman-job-server"
 
