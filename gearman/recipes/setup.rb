b7bdd0be8826fa66cebd50f3bfa7b1b248fd5081
Lee Azzarello <lee@rockingtiger.com>
2012-01-23 14:05:29 -0500

    chef on scalarium requires services be defined after the package is installed

--- gearman/recipes/setup.rb
+++ gearman/recipes/setup.rb
@@ -3,14 +3,14 @@ execute "add-drizzle-ppa" do
   creates "/etc/apt/sources.list.d/drizzle-developers-ppa-lucid.list"
 end
 
+package "libgearman4"
+package "gearman-job-server"
+
 service "gearman-job-server" do
   service_name "gearman-job-server"
 
   supports :status => false, :restart => true, :reload => false, "force-reload" => true
   action :enable
 end
-
-package "libgearman4"
-package "gearman-job-server"
 
 include_recipe "gearman::configure"
