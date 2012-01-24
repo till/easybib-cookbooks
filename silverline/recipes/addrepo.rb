fe76246e77c695f616357eb3c8c705e15bdbd563
Lee Azzarello <lee@rockingtiger.com>
2012-01-23 19:22:31 -0500

    change the name of things to test that syntax error

--- silverline/recipes/addrepo.rb
+++ silverline/recipes/addrepo.rb
@@ -1,11 +1,11 @@
 include_recipe "apt::ppa"
 
-execute "import_silverline_key" do
+execute "foo_bar" do
   command "curl http://apt.librato.com/packages.librato.key | apt-key add -"
   action :nothing
 end
 
 template "/etc/apt/sources.list.d/silverline.list" do
   source "silverline.list.erb"
-  notifies :run, "execute[import_silverline_key]", :immediately
+  notifies :run, "execute[foo_bar]", :immediately
 end
