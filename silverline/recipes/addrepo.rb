c15b83682f7eb141250f3c22ca5db00e54dcde0b
Lee Azzarello <lee@rockingtiger.com>
2012-01-23 19:28:23 -0500

    the notify source has some bizarre reserved words

--- silverline/recipes/addrepo.rb
+++ silverline/recipes/addrepo.rb
@@ -1,11 +1,11 @@
 include_recipe "apt::ppa"
 
-execute "foo_bar" do
+execute "packages_librato_key" do
   command "curl http://apt.librato.com/packages.librato.key | apt-key add -"
   action :nothing
 end
 
 template "/etc/apt/sources.list.d/silverline.list" do
   source "silverline.list.erb"
-  notifies :run, "execute[foo_bar]", :immediately
+  notifies :run, "execute[packages_librato_key]", :immediately
 end
