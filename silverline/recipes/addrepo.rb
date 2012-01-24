33be947876082b56dcb5f3b786c44500bc946433
Lee Azzarello <lee@rockingtiger.com>
2012-01-23 19:41:15 -0500

    apt-get update is now in it's own resource

--- silverline/recipes/addrepo.rb
+++ silverline/recipes/addrepo.rb
@@ -1,8 +1,13 @@
 include_recipe "apt::ppa"
 
 execute "packages_librato_key" do
-  command "curl http://apt.librato.com/packages.librato.key | apt-key add - && apt-get update"
+  command "curl http://apt.librato.com/packages.librato.key | apt-key add -"
   action :nothing
+end
+
+execute "apt_get_update" do
+  command "apt-get update"
+  action :run
 end
 
 template "/etc/apt/sources.list.d/silverline.list" do
