40cf57354446b9d141f0c4523291d993f783107f
Lee Azzarello <lee@rockingtiger.com>
2012-01-25 01:16:58 -0500

    close the quotes

--- silverline/recipes/default.rb
+++ silverline/recipes/default.rb
@@ -12,5 +12,5 @@ package "librato-silverline"
 template "/etc/load_manager/lmd.conf" do
   source "lmd.conf.erb"
   mode "0600"
-  notifies :restart, resource( :service => "silverline)
+  notifies :restart, resource( :service => "silverline")
 end
