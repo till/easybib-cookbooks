f9c26a7b8d86104f3ef7b4e0a56da1dc808de7f6
Lee Azzarello <lee@rockingtiger.com>
2012-01-24 23:30:34 -0500

    run this always

--- silverline/recipes/addrepo.rb
+++ silverline/recipes/addrepo.rb
@@ -1,16 +1,13 @@
 include_recipe "apt::ppa"
 
-execute "packages_librato_key" do
+execute "librato_add_key" do
   command "curl http://apt.librato.com/packages.librato.key | apt-key add -"
-  action :nothing
 end
 
 template "/etc/apt/sources.list.d/silverline.list" do
   source "silverline.list.erb"
-  notifies :run, "execute[packages_librato_key]", :immediately
 end
 
 execute "apt_get_update" do
   command "apt-get update"
-  action :run
 end
