9755d9081616c27d1316c727f636b7c6e5175fb3
Lee Azzarello <lee@rockingtiger.com>
2012-01-25 01:11:22 -0500

    start the silverline service when installed

--- silverline/recipes/default.rb
+++ silverline/recipes/default.rb
@@ -1,8 +1,16 @@
 include_recipe "silverline::addrepo"
 
+service "silverline" do
+  start_command "start silverline"
+  stop_command "stop silverline"
+  restart_command "restart silverline"
+  supports :restart, :start, :stop
+end
+
 package "librato-silverline"
 
 template "/etc/load_manager/lmd.conf" do
   source "lmd.conf.erb"
   mode "0600"
+  notifies :restart, resource( :service => "silverline)
 end
