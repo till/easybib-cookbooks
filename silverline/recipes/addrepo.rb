8f77ad47c8ba85c822569ae4d23f8737e01c999e
Lee Azzarello <lee@rockingtiger.com>
2012-01-24 23:04:48 -0500

    oops, update the package database after adding the key

--- silverline/recipes/addrepo.rb
+++ silverline/recipes/addrepo.rb
@@ -5,12 +5,12 @@ execute "packages_librato_key" do
   action :nothing
 end
 
-execute "apt_get_update" do
-  command "apt-get update"
-  action :run
-end
-
 template "/etc/apt/sources.list.d/silverline.list" do
   source "silverline.list.erb"
   notifies :run, "execute[packages_librato_key]", :immediately
+end
+
+execute "apt_get_update" do
+  command "apt-get update"
+  action :run
 end
