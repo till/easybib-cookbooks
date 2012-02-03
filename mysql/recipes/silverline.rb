bea581b4c37548b9ecf1901ce71af03b9b0e2e0e
Lee Azzarello <lee@rockingtiger.com>
2012-02-03 17:25:11 -0500

    gross, add SL_NAME to init script and recipe

--- mysql/recipes/silverline.rb
+++ mysql/recipes/silverline.rb
@@ -3,5 +3,10 @@ include_recipe "mysql::service"
 template "/etc/mysql/debian-start" do
   source "debian-start.erb"
   mode "0755"
+end
+
+template "/etc/init.d/mysql" do
+  source "mysql.init.erb"
+  mode "0755"
   notifies :restart, resources( :service => "mysql")
 end
