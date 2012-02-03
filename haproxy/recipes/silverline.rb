5fb8e9b4abaf8dd6ff39f700e1dc583e9a88ddc8
Lee Azzarello <lee@rockingtiger.com>
2012-02-03 13:11:51 -0500

    silverline's configuration is pretty dumb. it's not a sustainable of process monitoring.

--- haproxy/recipes/silverline.rb
+++ haproxy/recipes/silverline.rb
@@ -3,5 +3,10 @@ include_recipe "haproxy"
 template "/etc/default/haproxy" do
   source "default.erb"
   mode "0644"
+end
+
+template "/etc/init.d/haproxy" do
+  source "haproxy.init.erb"
+  mode "0755"
   notifies :restart, "service[haproxy]"
 end
