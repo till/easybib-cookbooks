c07bfdd5a2cfaf345d5d3b4b64f925021c133725
till <till@php.net>
2012-02-06 19:36:32 +0100

    Cleaning up this recipe.

--- deploy/recipes/easybib.rb
+++ deploy/recipes/easybib.rb
@@ -1,30 +1,36 @@
 # custom recipe because of: http://support.scalarium.com/discussions/problems/78-app-is-not-deploying
 
-deployUser = "www-data"
-
-instanceRoles = node[:scalarium][:instance][:roles]
-
-Chef::Log.debug("deploy::easybib - entered.");
+deploy_user    = "www-data"
+instance_roles = node[:scalarium][:instance][:roles]
+cluster_name   = node[:scalarium][:cluster][:name]
 
 node[:deploy].each do |application, deploy|
 
-  Chef::Log.debug("deploy::easybib - app: #{application}, role: #{instanceRoles}")
+  Chef::Log.debug("deploy::easybib - app: #{application}, role: #{instances_roles}")
 
   case application
   when 'easybib'
-    if instanceRoles.include?('nginxphpapp')
+    if cluster_name != 'EasyBib' && cluster_name != 'EasyBib Playground'
+      next
+    end
 
-    elsif instanceRoles.include?('testapp')
+    if instances_roles.include?('nginxphpapp')
+
+    elsif instances_roles.include?('testapp')
 
     else
       next
     end
+
   when 'easybib_api'
-    next unless instanceRoles.include?('bibapi')
+    next unless instances_roles.include?('bibapi')
 
   when 'easybib_solr_research_importers'
-    # not sure on which roles you want to have this app
-    next unless instanceRoles.include?('easybibsolr')
+    if cluster_name != 'Research Cloud'
+      next
+    end
+
+    next unless instances_roles.include?('easybibsolr')
 
     Chef::Log.debug('deploy::easybib - Setting deploy for RESEARCH IMPORTERS')
 
@@ -32,39 +38,52 @@ node[:deploy].each do |application, deploy|
     deploy[:restart_command] = ""
 
   when 'easybib_solr_server'
-    # not sure on which roles you want to have this app
-    next unless instanceRoles.include?('easybibsolr')
+    if cluster_name != 'Research Cloud'
+      next
+    end
+
+    next unless instances_roles.include?('easybibsolr')
 
     Chef::Log.debug('deploy::easybib - Setting deploy for SOLR SERVER')
 
     deploy[:deploy_to]       = "/solr/apache-solr-1.4.1-compiled"
     deploy[:restart_command] = ""
 
-    deployUser = "root"
+    deploy_user = "root"
 
   when 'citation_anlytics'
-    next unless instanceRoles.include?('elasticsearch')
+    if cluster_name != 'Citation Analytics'
+      next
+    end
+
+    next unless instances_roles.include?('elasticsearch')
 
     Chef::Log.debug('deploy.easybib - Prepare for git checkout')
     prepare_git_checkouts(
-      :user    => deployUser,
-      :group   => deployUser,
+      :user    => deploy_user,
+      :group   => deploy_user,
       :home    => "/var/www",
       :ssh_key => deploy[:scm][:ssh_key]
     )
+
   when 'gearmanworker'
-    next unless instanceRoles.include?('gearman-worker')
+    next unless instances_roles.include?('gearman-worker')
 
   when 'sitescraper'
-    next unless instanceRoles.include?('sitescraper')
+    next unless instances_roles.include?('sitescraper')
+
+  when 'research'
+    if cluster_name != 'Research Cloud'
+      next
+    end
+
+    next unless instances_roles.include?('nginxphpapp')
 
   else
-    Chef::Log.debug("deploy::easybib - #{application} skipped")
+    Chef::Log.debug("deploy::easybib - #{application} (in #{cluster_name}) skipped")
     next
 
   end
-
-  Chef::Log.debug("deploy::easybib - CREATE DEPLOY DIR: #{deploy[:deploy_to]}")
 
   directory deploy[:deploy_to] do
     owner "root"
@@ -75,10 +94,7 @@ node[:deploy].each do |application, deploy|
     not_if "test -d #{deploy[:deploy_to]}"
   end
 
-  Chef::Log.debug("deploy::easybib - ABOUT TO DEPLOY FOR REALZ")
-
-  # we survived until here - so we are good to actually checkout and deploy
-  # done for every app
+  Chef::Log.debug("deploy::easybib - Deployment started.")
 
   scalarium_deploy_dir do
     user deploy[:user]
@@ -120,7 +136,7 @@ node[:deploy].each do |application, deploy|
     end
 
     repository deploy[:scm][:repository]
-    user deployUser
+    user deploy_user
 
     if !deploy[:scm][:revision].nil?
       revision deploy[:scm][:revision]
@@ -128,10 +144,6 @@ node[:deploy].each do |application, deploy|
 
     symlink_before_migrate({})
     migrate false
-    #if !instanceRoles.include?('easybibsolr')
-    #  migrate deploy[:migrate]
-    #  migration_command deploy[:migrate_command]
-    #end
 
     action deploy[:action]
 
