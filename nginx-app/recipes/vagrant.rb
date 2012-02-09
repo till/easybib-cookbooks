2bf130262a47a3ce1b2f3eb59a52290d5648c633
till <till@php.net>
2012-02-09 19:07:34 +0100

    base recipe for vagrant boxes

---
+++ nginx-app/recipes/vagrant.rb
@@ -0,0 +1,33 @@
+if !node[:deploy]
+  node[:deploy] = {}
+  node[:deploy][:deploy_to] = '/var/www/citationalysis'
+end
+
+if !node[:docroot]
+  node[:docroot] = 'www'
+end
+
+vagrant_dir = "/vagrant_data"
+
+directory "#{node[:deploy][:deploy_to]}" do
+  mode      "0755"
+  action    :create
+  recursive true
+end
+
+link "#{node[:deploy][:deploy_to]}/current" do
+  to "#{vagrant_dir}"
+end
+
+template "/etc/nginx/sites-enabled/easybib.com.conf" do
+  source "easybib.com.conf.erb"
+  mode   "0755"
+  owner  node["nginx-app"][:user]
+  group  node["nginx-app"][:group]
+  variables(
+    :deploy      => node[:deploy],
+    :application => "easybib",
+    :access_log  => 'off'
+  )
+  notifies :restart, resources(:service => "nginx"), :delayed
+end
