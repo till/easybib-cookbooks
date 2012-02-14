990966038fc247bed2c1915bcb235553da46f232
till <till@php.net>
2012-02-14 13:58:34 +0100

    updated recipe to use nginx-app::vagrant

--- nginx-app/recipes/research.rb
+++ nginx-app/recipes/research.rb
@@ -5,29 +5,4 @@ if !node[:deploy]
   node[:deploy][:deploy_to] = '/var/www/easybib'
 end
 
-node[:docroot] = 'www'
-
-vagrant_dir = "/vagrant_data"
-
-directory "#{node[:deploy][:deploy_to]}" do
-  mode      "0755"
-  action    :create
-  recursive true
-end
-
-link "#{node[:deploy][:deploy_to]}/current" do
-  to "#{vagrant_dir}"
-end
-
-template "/etc/nginx/sites-enabled/easybib.com.conf" do
-  source "easybib.com.conf.erb"
-  mode "0755"
-  owner node["nginx-app"][:user]
-  group node["nginx-app"][:group]
-  variables(
-    :deploy      => node[:deploy],
-    :application => "easybib",
-    :nginx_extra => "sendfile off;"
-  )
-  notifies :restart, resources(:service => "nginx"), :delayed
-end
+include_recipe "nginx-app::vagrant"
