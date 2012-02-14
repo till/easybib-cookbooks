2c2c713bcf28a79fd13ba4d88749250a7ca473ea
till <till@php.net>
2012-02-14 16:30:26 +0100

    monit recipe to setup our redis subscriber

---
+++ monit/recipes/citationbackup.rb
@@ -0,0 +1,16 @@
+# installs the monitrc for our backup process
+include_recipe "monit::service"
+
+template "/etc/monit/conf.d/citationbackup-redis.monitrc" do
+  source "citationbackup.monit.erb"
+  mode   "0644"
+  owner  "root"
+  group  "root"
+  variables(
+    'pid_file' => '/var/run/citation-backup/redis-importer.pid',
+    'app_name' => 'citationbackup-redis',
+    'group'    => 'www-data',
+    'init'     => '/etc/init.d/redis-importer'
+  )
+  notifies :restart, resources(:service => "monit")
+end
