86874910fc4111196bd82feb836a4a061c2937ce
till <till@php.net>
2012-02-14 17:03:05 +0100

    * moved 'configure' into php-phar::configure  * bugfix: "tar -zxf"

---
+++ php-phar/recipes/configure.rb
@@ -0,0 +1,3 @@
+execute "add .ini to load ext/phar" do
+  command "echo 'extension=phar.so' > /usr/local/etc/php/phar.ini"
+end
--- php-phar/recipes/default.rb
+++ php-phar/recipes/default.rb
@@ -2,14 +2,14 @@ php_version = node[:php_phar][:version]
 php_ext_dir = `php -r 'echo ini_get("extension_dir");'`.strip
 
 execute "download PHP" do
-  command "wget http://de2.php.net/get/php-#{php_version}.tar.gz/from/us.php.net/mirror -O /tmp/php-#{php_version}.tar.gz"
+  command "wget http://us.php.net/get/php-#{php_version}.tar.gz/from/us.php.net/mirror -O /tmp/php-#{php_version}.tar.gz"
   not_if do
     File.directory?("/tmp/php-#{php_version}")
   end
 end
 
 execute "extract tar" do
-  command "tar zxf php-#{php_version}.tar.gz"
+  command "tar -zxf php-#{php_version}.tar.gz"
   cwd     "/tmp"
   only_if do
     File.exists?("/tmp/php-#{php_version}.tar.gz")
@@ -32,6 +32,4 @@ execute "copy phar" do
   end
 end
 
-execute "add .ini to load ext/phar" do
-  command "echo 'extension=phar.so' > /usr/local/etc/php/phar.ini"
-end
+include_recipe "php-phar::configure"
