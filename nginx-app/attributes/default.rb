6b6a9cea532a4256890f5bc0e534be313cfae09b
till <till@php.net>
2012-02-14 15:53:30 +0100

    set in recipe

--- nginx-app/attributes/default.rb
+++ nginx-app/attributes/default.rb
@@ -3,9 +3,6 @@ default["nginx-app"][:user]               = "www-data"
 default["nginx-app"][:group]              = "www-data"
 default["nginx-app"][:static_directories] = ["js", "css", "images", "raw"]
 
-# for localhost, vagrant
-#set_unless[:deploy][:deploy_to] = "/vagrant"
-
 # Silverline attribs
 default[:silverline][:name] = "nginx"
 
