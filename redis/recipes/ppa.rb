39af56d7357fd86f23a2ea3ac4291c24c6ae5b2f
till <till@php.net>
2012-02-15 16:24:26 +0100

    update recipe: does not work with karmic (currently)

--- redis/recipes/ppa.rb
+++ redis/recipes/ppa.rb
@@ -1,15 +1,25 @@
 # install redis from a ppa to avoid compiling, etc.
 include_recipe "apt::ppa"
 
-execute "add Chris Lea's PPA" do
-  command "add-apt-repository ppa:chris-lea/redis-server"
+case node[:lsb][:codename]
+when "karmic"
+  Chef::Log.error("Recipe does not support karmic.")
+when "lucid"
+when "maverick"
+when "natty"
+when "oneiric"
+  execute "add Chris Lea's PPA" do
+    command "add-apt-repository ppa:chris-lea/redis-server"
+  end
+
+  execute "update sources" do
+    command "apt-get -y -f -m update"
+  end
+
+  package "redis-server"
+
+  include_recipe "redis::user"
+  include_recipe "redis::configure"
+else
+  Chef::Log.error("Unsupported platform: #{node[:lsb][:codename]}")
 end
-
-execute "update sources" do
-  command "apt-get -y -f -m update"
-end
-
-package "redis-server"
-
-include_recipe "redis::user"
-include_recipe "redis::configure"
