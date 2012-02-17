36bfa713aff0438e6ce5bf8f086d461a813a8c71
Lee Azzarello <lee@rockingtiger.com>
2012-02-17 08:10:30 -0500

    export silverling environment variables for couchdb

--- couchdb/attributes/default.rb
+++ couchdb/attributes/default.rb
@@ -14,3 +14,11 @@ default[:couchbase]      = {}
 default[:couchbase][:dl] = "http://packages.couchbase.com/releases/couch/1.1.2/couchbase-single-server-community_x86_64_1.1.2.deb"
 
 set_unless[:couchdb_admins] = {}
+
+default[:silverline][:couchdb_name] = "couchdb"
+
+if attribute?(:scalarium)
+  default[:silverline][:environment] = node[:scalarium][:cluster][:name].gsub!(/\s/,'')
+else
+  default[:silverline][:environment] = "production"
+end
