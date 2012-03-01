a82f3d48e927cd5f8128c73289ea09fff24edf27
till <till@php.net>
2012-03-01 17:40:45 +0100

    * delete old gearman setup (was broken, thanks launchpad, thanks drizzle developers)

--- gearman/attributes/default.rb
+++ gearman/attributes/default.rb
@@ -1,13 +1,2 @@
 default[:gearman]         = {}
-default[:gearman][:port]  = '31337'
-default[:gearman][:debug] = "1"
 default[:gearman][:pecl]  = '0.8.0'
-default[:gearman][:http]  = 80
-
-default[:silverline][:gearman_name] = "gearman-job-server"
-
-if attribute?(:scalarium)
-  default[:silverline][:environment] = node[:scalarium][:cluster][:name].gsub(/\s/,'')
-else
-  default[:silverline][:environment] = "production"
-end
