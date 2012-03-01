2a5645778a35900d8a3a166915d210e8b083e1b0
till <till@php.net>
2012-03-01 17:28:38 +0100

    * new recipe for gearmand (0.28)    * installs all dependencies    * compiles gearman from source (no persistence!)    * installs custom init.d script    * installs customized default file based on attributes

---
+++ gearmand/attributes/default.rb
@@ -0,0 +1,19 @@
+default[:gearmand]          = {}
+default[:gearmand][:user]   = "gearman"
+default[:gearmand][:port]   = 31337
+default[:gearmand][:log]    = "--syslog --verbose INFO" #or --logfile=/path/to/whatever
+default[:gearmand][:prefix] = "/opt/easybib"
+
+# these are relevant for when we compile from source (current default)
+default[:gearmand][:source]           = {}
+default[:gearmand][:source][:version] = "0.28"
+default[:gearmand][:source][:flags]   = "--disable-libpq --disable-libmemcached --without-sqlite3 --disable-libdrizzle"
+
+# these are for silverline
+default[:silverline][:gearmand_name] = "gearmand"
+
+if attribute?(:scalarium)
+  default[:silverline][:environment] = node[:scalarium][:cluster][:name].gsub(/\s/,'')
+else
+  default[:silverline][:environment] = "production"
+end
