e80f43279df69ffc5dfd0871224063dd93cca83d
till <till@php.net>
2012-03-01 17:38:29 +0100

    remove worker fu

--- easybib-gearman/metadata.rb
+++
@@ -1,8 +0,0 @@
-maintainer        "Till Klampaeckel"
-maintainer_email  "till@php.net"
-license           "BSD License"
-description       "Adds the gearman worker to crontab, sets up logfiles and logrotate."
-version           "0.1"
-recipe            "easybib-gearman::setup", "Installs everything."
-
-supports 'ubuntu'
