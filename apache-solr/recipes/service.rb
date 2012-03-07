db17e00a67787bd0d7346c40db5a102f03b00d03
till <till@php.net>
2012-03-07 01:43:19 +0100

    * apache-solr::service: installs upstart config/script

---
+++ apache-solr/recipes/service.rb
@@ -0,0 +1,13 @@
+template "/etc/init/apache-solr.conf" do
+  mode   "0755"
+  source "upstart.erb"
+  variables(
+    :solr_install => "#{node[:apache_solr][:base_dir]}/apache-solr/#{node[:apache_solr][:app]}",
+    :solr_version => node[:apache_solr][:version]
+  )
+end
+
+service "apache-solr" do
+  provider Chef::Provider::Service::Upstart
+  action   :nothing
+end
