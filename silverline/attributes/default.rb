287a2639e96c78caf6b65fe20989661c2597333f
Lee Azzarello <lee@rockingtiger.com>
2012-03-13 16:37:33 -0400

    that last commit message should have been this one. the last one deletes an unused template  :(

---
+++ silverline/attributes/default.rb
@@ -0,0 +1,15 @@
+if attribute?(:scalarium)
+  default[:silverline][:environment] = node[:scalarium][:cluster][:name].gsub(/\s/,'')
+  default[:silverline][:roles] = node[:scalarium][:instance][:roles]
+else
+  default[:silverline][:environment] = "production"
+  default[:silverline][:roles] = ["default"]
+end
+# set first 4 bytes of sl_tag_names value and terminate with a colon
+sl_tag_names = "env-#{default[:silverline][:environment]}:"
+# loop over roles and append characters into sl_tag_names to complete string with all tags
+default[:silverline][:roles].each do |role|
+  sl_tag_names = sl_tag_names + "role-" + role + ":"
+end
+# set a real attribute and chop off that last delimiter
+default[:silverline][:sl_tag_names] = sl_tag_names.chop!
