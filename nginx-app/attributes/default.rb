0a161c109f632080b5b112c44dc4fe7d6624eb5a
till <till@php.net>
2012-04-17 12:09:07 +0200

    add knowledgebase

--- nginx-app/attributes/default.rb
+++ nginx-app/attributes/default.rb
@@ -4,9 +4,9 @@ default["nginx-app"][:group]              = "www-data"
 default["nginx-app"][:static_directories] = ["js", "css", "images", "raw"]
 
 # module specific configuration for assets
-default["nginx-app"][:js_modules]  = {"debugger" => "debugger", "notes" => "notebook", "cms" => "cms", "bibanalytics" => "bibanalytics", "sharing" => "sharing"}
+default["nginx-app"][:js_modules]  = {"debugger" => "debugger", "notes" => "notebook", "cms" => "cms", "bibanalytics" => "bibanalytics", "sharing" => "sharing", "kb" => "kb"}
 default["nginx-app"][:img_modules] = {"notes" => "notebook", "outline" => "notebook", "paperlink" => "paperlink"}
-default["nginx-app"][:css_modules] = {"debugger" => "debugger", "notes" => "notebook", "cms" => "cms", "bibanalytics" => "bibanalytics", "sharing" => "sharing"}
+default["nginx-app"][:css_modules] = {"debugger" => "debugger", "notes" => "notebook", "cms" => "cms", "bibanalytics" => "bibanalytics", "sharing" => "sharing", "kb" => "kb"}
 
 # Silverline attribs
 default[:silverline][:nginx_name] = "nginx"
