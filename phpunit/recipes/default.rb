13219e2b2f09812d4e6e7a6ebf2829d2e9e53a39
till <till@php.net>
2012-04-25 14:47:13 +0200

    * check if php was detected yet

--- phpunit/recipes/default.rb
+++ phpunit/recipes/default.rb
@@ -3,38 +3,41 @@
 version = node[:phpunit][:version].split('.')
 major   = "#{version[0]}#{version[1]}"
 
-ohai "reload_php_easybib" do
+ohai "reload_php" do
   action :reload
-  plugin "php_easybib"
+  plugin "php"
 end
 
 phpunit_location = "/usr/local/phpunit#{major}"
 
-Chef::Log.debug("HELLO: #{node[:php_easybib][:php_bin]}")
+Chef::Log.debug("HELLO: #{node[:languages][:php][:php_bin]}")
 
-pear     = node[:php_easybib][:pear_bin]
-pear_bin = node[:php_easybib][:pear][:bin_dir]
-php_dir  = node[:php_easybib][:pear][:php_dir]
+if not node[:languages][:php].empty?
 
-execute "enable auto_discover" do
-  command "#{pear} config-set auto_discover 1"
-end
+  pear     = node[:languages][:php][:pear_bin]
+  pear_bin = node[:languages][:php][:pear][:bin_dir]
+  php_dir  = node[:languages][:php][:pear][:php_dir]
 
-execute "install PHPUnit #{node[:phpunit][:version]}" do
-  command "#{pear} install -o --installroot #{phpunit_location} pear.phpunit.de/PHPUnit-#{node[:phpunit][:version]}"
-  not_if  do
-    File.exist?("#{phpunit_location}#{pear_bin}/phpunit")
+  execute "enable auto_discover" do
+    command "#{pear} config-set auto_discover 1"
   end
-end
 
-template "#{phpunit_location}#{pear_bin}/phpunit" do
-  mode   "0755"
-  source "phpunit.erb"
-  variables({
-    :complete_path => "#{phpunit_location}#{php_dir}"
-  })
-end
+  execute "install PHPUnit #{node[:phpunit][:version]}" do
+    command "#{pear} install -o --installroot #{phpunit_location} pear.phpunit.de/PHPUnit-#{node[:phpunit][:version]}"
+    not_if  do
+      File.exist?("#{phpunit_location}#{pear_bin}/phpunit")
+    end
+  end
 
-link "#{pear_bin}/phpunit#{major}" do
-  to "#{phpunit_location}#{pear_bin}/phpunit"
+  template "#{phpunit_location}#{pear_bin}/phpunit" do
+    mode   "0755"
+    source "phpunit.erb"
+    variables({
+      :complete_path => "#{phpunit_location}#{php_dir}"
+    })
+  end
+
+  link "#{pear_bin}/phpunit#{major}" do
+    to "#{phpunit_location}#{pear_bin}/phpunit"
+  end
 end
