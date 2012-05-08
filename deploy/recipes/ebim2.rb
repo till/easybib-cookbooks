31822d5f4c41643d5bd7e538524e53eaf6455bc6
Leander <mail@leander-damme.com>
2012-05-08 13:51:27 +0200

    added ebim2 cron deploy recipe (#142)

---
+++ deploy/recipes/ebim2.rb
@@ -0,0 +1,11 @@
+cron "run collect stats every hour" do
+    minute  "23"
+    user "www-data"
+    command "/usr/local/bin/php /srv/www/ebim2/current/bin/ebim2 cron-collect-stats >> /var/log/ebim-stats.log 2>&1"
+end
+
+cron "run collect work every two hours" do
+    hour  "*/2"
+    user "www-data"
+    command "/usr/local/bin/php /srv/www/ebim2/current/bin/ebim2 cron-collect-work >> /var/log/ebim-work.log 2>&1"
+end
