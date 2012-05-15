410e9346464e45f8a8605b9f5394561e560be946
till <till@php.net>
2012-05-15 15:30:23 +0200

    move code into deploy::easybib (TO MAKE IT WORK)

--- deploy/recipes/easybib.rb
+++ deploy/recipes/easybib.rb
@@ -91,7 +91,21 @@ node[:deploy].each do |application, deploy|
     include_recipe "deploy::ebim2"
 
     base_dir = deploy[:deploy_to]
-    include_recipe "deploy::gearmanmanager"
+    app_dir  = "#{base_dir}/vendor/GearmanManager"
+    etc_dir  = "#{base_dir}/etc/gearman"
+
+    link "/usr/local/bin/gearman-manager" do
+      to "#{app_dir}/#{node[:gearman-manager][:type]}-manager.php"
+    end
+
+    link "/etc/gearman-manager" do
+      to etc_dir
+    end
+
+    link "/etc/init.d/gearman-manager" do
+      to "#{base_dir}/bin/gearman-manager.initd"
+    end
+
   end
 
   #if application == 'research_app'
