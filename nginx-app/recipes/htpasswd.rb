b235c31c02527908f9bde34e0afda0d05175d038
till <till@php.net>
2012-06-01 18:29:11 +0200

    * bugfix: cluster_name not set  * refined checks

--- nginx-app/recipes/htpasswd.rb
+++ nginx-app/recipes/htpasswd.rb
@@ -1,12 +1,18 @@
-if node[:scalarium][:cluster][:name] && node[:scalarium][:cluster][:name] == 'Fruitkid'
-  template "#{node["nginx-app"][:config_dir]}/htpasswd" do
-    source "htpasswd.erb"
-    mode   "0640"
-    owner  "root"
-    group  node["nginx-app"][:group]
-    variables(
-      :user => node["nginx-app"][:user],
-      :pass => node[:mysql][:server_root_password].crypt(cluster_name)
-    )
+if node[:scalarium]
+
+  cluster_name = node[:scalarium][:cluster][:name]
+
+  if cluster_name == 'Fruitkid'
+    template "#{node["nginx-app"][:config_dir]}/htpasswd" do
+      source "htpasswd.erb"
+      mode   "0640"
+      owner  "root"
+      group  node["nginx-app"][:group]
+      variables(
+        :user => node["nginx-app"][:user],
+        :pass => node[:mysql][:server_root_password].crypt(cluster_name)
+      )
+    end
   end
+
 end
