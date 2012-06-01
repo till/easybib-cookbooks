58baced2f08e5193c9300ef2c256be1a470ffedb
till <till@php.net>
2012-06-01 22:15:18 +0200

    use check_target

--- php/providers/composer.rb
+++ php/providers/composer.rb
@@ -4,9 +4,8 @@ end
 
 action :install do
   deploy_to = new_resource.name
-  if !::File.directory?(deploy_to)
-    raise "#{deploy_to} is not a directory"
-  end
+  check_target(deploy_to)
+
   if !::File.exists?("#{deploy_to}/composer.phar")
     Chef::Log.info("Could not find 'composer.phar' in #{deploy_to}: silently skipping.")
   else
