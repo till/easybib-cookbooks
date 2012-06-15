85a62b3f6349ca2757ab6e8e82db74b70b59b8b3
till <till@php.net>
2012-06-15 15:32:59 +0200

    * add name for name of package (and references to init.d and default file)

--- gearmand/attributes/default.rb
+++ gearmand/attributes/default.rb
@@ -1,10 +1,11 @@
-default[:gearmand]          = {}
-default[:gearmand][:user]   = "gearman"
-default[:gearmand][:port]   = 31337
-default[:gearmand][:log]    = "--syslog --verbose INFO" #or --logfile=/path/to/whatever
-default[:gearmand][:prefix] = "/opt/easybib"
+default[:gearmand]        = {}
+default[:gearmand][:user] = "gearman"
+default[:gearmand][:port] = 31337
+default[:gearmand][:log]  = "--syslog --verbose INFO" #or --logfile=/path/to/whatever
+default[:gearmand][:name] = "gearmand-easybib" # name of our package and init.d, default
 
 # these are relevant for when we compile from source (current default)
+default[:gearmand][:prefix]           = "/opt/easybib"
 default[:gearmand][:source]           = {}
 default[:gearmand][:source][:version] = "0.28"
 default[:gearmand][:source][:flags]   = "--disable-libpq --disable-libmemcached --without-sqlite3 --disable-libdrizzle"
