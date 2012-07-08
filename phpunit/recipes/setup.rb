92a1f645312fd3c473ed8ef5764c30e99591847e
till <till@php.net>
2012-07-08 18:57:06 +0200

    Add recipe to install the latest PHPUnit. Yay, your tests might break. ;)

---
+++ phpunit/recipes/setup.rb
@@ -0,0 +1,10 @@
+packages = ["phpunit", "DbUnit"]
+
+packages.each do |p|
+  php_pear p do
+    action  :install_if_missing
+    channel "pear.phpunit.de"
+    force   true
+    version "#{version}"
+  end
+end
