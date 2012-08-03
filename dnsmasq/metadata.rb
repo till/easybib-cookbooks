43c7a3e4f6bc8cd31a669a24cf981a25c9b501c0
till <till@php.net>
2012-08-03 15:37:50 +0200

    EVEN MOAR.

---
+++ dnsmasq/metadata.rb
@@ -0,0 +1,8 @@
+maintainer        "Till Klampaeckel"
+maintainer_email  "till@php.net"
+license           "BSD License"
+description       "Install a local DNS cache."
+version           "0.1"
+recipe            "dnsmasq::default", "Install and configure the DNS cache."
+
+supports 'ubuntu'
