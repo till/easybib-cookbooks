a06afa80f46e31ee5241dc9832f73772bd51e63e
till <till@php.net>
2012-08-03 15:04:05 +0200

    please librarian

---
+++ cron/metadata.rb
@@ -0,0 +1,8 @@
+maintainer        "Till Klampaeckel"
+maintainer_email  "till@php.net"
+license           "BSD License"
+description       "Install cronjobs"
+version           "0.1"
+recipe            "cron::api", "Setup the cronjob for API tracking!"
+
+supports 'ubuntu'
