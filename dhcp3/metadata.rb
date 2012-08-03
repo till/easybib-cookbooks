725be531ca2555374b12371a1e0979b5c26b21aa
till <till@php.net>
2012-08-03 15:33:19 +0200

    MOAR metadata.

---
+++ dhcp3/metadata.rb
@@ -0,0 +1,9 @@
+maintainer        "Till Klampaeckel"
+maintainer_email  "till@php.net"
+license           "BSD License"
+description       "DHCP3 configuration - this is to 'inject' a DNScache into the dhcp config on AWS instances"
+version           "0.1"
+recipe            "dhcp3::configure", "Setup a custom DNS server (currently hardcoded to 127.0.0.1)"
+recipe            "dhcp3::service", "Wrap the init.d script."
+
+supports 'ubuntu'
