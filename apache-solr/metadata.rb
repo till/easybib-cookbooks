3769470d1d8b69296d2b2a28f809c18ab80fa500
till <till@php.net>
2012-08-03 14:56:42 +0200

    add metadata

---
+++ apache-solr/metadata.rb
@@ -0,0 +1,8 @@
+maintainer        "Till Klampaeckel"
+maintainer_email  "till@php.net"
+license           "BSD License"
+description       "Setup Apache Solr"
+version           "0.1"
+recipe            "apache-solr::default", "Installs Apache Solr"
+
+supports 'ubuntu'
