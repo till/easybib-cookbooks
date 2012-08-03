b6dc1aa66706fcdb7dfccd43cff6ff7b263f9648
till <till@php.net>
2012-08-03 16:12:44 +0200

    Metadata for ElasticSearch.

---
+++ elasticsearch/metadata.rb
@@ -0,0 +1,7 @@
+maintainer        "Till Klampaeckel"
+maintainer_email  "till@php.net"
+license           "BSD License"
+description       "Install and configure ElasticSearch (including proxy, etc.)."
+version           "0.1"
+
+supports 'ubuntu'
