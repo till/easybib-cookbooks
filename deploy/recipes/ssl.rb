89bef5fba726c34861cb81d82b9e5281106f88ee
Ulf Harnhammar <ulfharn@gmail.com>
2012-08-21 13:55:21 +0200

    retrieve the certificate + key

--- deploy/recipes/ssl.rb
+++ deploy/recipes/ssl.rb
@@ -2,17 +2,17 @@ instance_roles = node[:scalarium][:instance][:roles]
 cluster_name   = node[:scalarium][:cluster][:name]
 
 node[:deploy].each do |application, deploy|
-  Chef::Log.debug("deploy::ssl - app: #{application}, role: #{instance_roles}, clustername: #{cluster_name} -- debug3!")
+  Chef::Log.debug("deploy::ssl - app: #{application}, role: #{instance_roles}, clustername: #{cluster_name} -- debug4!")
 
   if instance_roles.to_s == 'lb' and cluster_name.to_s == 'EasyBib Playground'
-    Chef::Log.debug("found it again! debug3!")
+    Chef::Log.debug("found it again! debug4!")
 
-    application.each do |appkey,appval|
-      Chef::Log.debug("application  #{appkey} = #{appval}")
-    end
-
-    deploy.each do |depkey,depval|
-      Chef::Log.debug("deploy  #{depkey} = #{depval}")
+    if deploy.has_key?('ssl_certificate') and deploy.has_key?('ssl_certificate_key')
+      ssl_certificate = deploy['ssl_certificate']
+      ssl_certificate_key = deploy['ssl_certificate_key']
+      Chef::Log.debug("debug4  cert=#{ssl_certificate}  certkey=#{ssl_certificate_key}")
+    else
+      Chef::Log.debug("debug4  don't have any keys stored here")
     end
   end
 end
