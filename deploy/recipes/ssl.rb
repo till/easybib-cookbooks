1d9595a8b5818c670bb8067415879d3d588d7bf2
Ulf Harnhammar <ulfharn@gmail.com>
2012-08-21 12:59:53 +0200

    more debugging

--- deploy/recipes/ssl.rb
+++ deploy/recipes/ssl.rb
@@ -2,7 +2,7 @@ instance_roles = node[:scalarium][:instance][:roles]
 cluster_name   = node[:scalarium][:cluster][:name]
 
 node[:deploy].each do |application, deploy|
-  Chef::Log.debug("deploy::ssl - app: #{application}, role: #{instance_roles}")
+  Chef::Log.debug("deploy::ssl - app: #{application}, role: #{instance_roles}, clustername: #{cluster_name}")
 
   if instance_roles == 'lb' and cluster_name == 'EasyBib Playground'
     Chef::Log.debug("found it!")
