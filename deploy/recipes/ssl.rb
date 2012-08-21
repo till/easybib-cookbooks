445dc32923a48edc3e2c920be8d747b91e3c8c1c
Ulf Harnhammar <ulfharn@gmail.com>
2012-08-21 13:05:32 +0200

    even more debugging

--- deploy/recipes/ssl.rb
+++ deploy/recipes/ssl.rb
@@ -5,6 +5,6 @@ node[:deploy].each do |application, deploy|
   Chef::Log.debug("deploy::ssl - app: #{application}, role: #{instance_roles}, clustername: #{cluster_name}")
 
   if instance_roles == 'lb' and cluster_name == 'EasyBib Playground'
-    Chef::Log.debug("found it!")
+    Chef::Log.debug("found it again!")
   end
 end
