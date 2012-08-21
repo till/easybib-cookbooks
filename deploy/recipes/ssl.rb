1224034afb6ce62f2d074ce1bf4390aae1266749
Ulf Harnhammar <ulfharn@gmail.com>
2012-08-21 12:47:06 +0200

    check the role instead of the app

--- deploy/recipes/ssl.rb
+++ deploy/recipes/ssl.rb
@@ -2,15 +2,9 @@ instance_roles = node[:scalarium][:instance][:roles]
 cluster_name   = node[:scalarium][:cluster][:name]
 
 node[:deploy].each do |application, deploy|
-
   Chef::Log.debug("deploy::ssl - app: #{application}, role: #{instance_roles}")
 
-  case application
-  when 'lb'
-    if cluster_name != 'EasyBib Playground'
-      next
-    end
+  if instance_roles == 'lb' and cluster_name == 'EasyBib Playground'
     Chef::Log.debug("found it!")
   end
-
 end
