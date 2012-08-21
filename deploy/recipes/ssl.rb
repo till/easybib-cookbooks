befc4f9d79721ade7e959fac8b694e4e40cba42c
Ulf Harnhammar <ulfharn@gmail.com>
2012-08-21 13:15:29 +0200

    array to string

--- deploy/recipes/ssl.rb
+++ deploy/recipes/ssl.rb
@@ -2,9 +2,9 @@ instance_roles = node[:scalarium][:instance][:roles]
 cluster_name   = node[:scalarium][:cluster][:name]
 
 node[:deploy].each do |application, deploy|
-  Chef::Log.debug("deploy::ssl - app: #{application}, role: #{instance_roles}, clustername: #{cluster_name} -- debug!")
+  Chef::Log.debug("deploy::ssl - app: #{application}, role: #{instance_roles}, clustername: #{cluster_name} -- debug2!")
 
-  if instance_roles.chomp == 'lb' and cluster_name.chomp == 'EasyBib Playground'
-    Chef::Log.debug("found it again! debug!")
+  if instance_roles.to_s.chomp == 'lb' and cluster_name.to_s.chomp == 'EasyBib Playground'
+    Chef::Log.debug("found it again! debug2!")
   end
 end
