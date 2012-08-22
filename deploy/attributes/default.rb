ab8add20af56b1157f7f17d74193e96bc336defb
Ulf Harnhammar <ulfharn@gmail.com>
2012-08-22 16:16:25 +0200

    moving it elsewhere

--- deploy/attributes/default.rb
+++ deploy/attributes/default.rb
@@ -6,8 +6,3 @@ default["deploy"]["gearmanmanager"]["real_daemon"]  = "/srv/www/ebim2/current/ve
 default["deploy"]["gearmanmanager"]["pid_dir"]      = "/var/run/gearman-manager"
 default["deploy"]["gearmanmanager"]["config_file"]  = "/srv/www/ebim2/current/etc/gearman/jango-fett.manager.ini"
 default["deploy"]["gearmanmanager"]["worker_class"] = "Ebim2\\Gearman\\Worker\\"
-
-default["deploy"]["ssl"]["role"]    = "lb"
-default["deploy"]["ssl"]["cluster"] = "EasyBib Playground"
-default["deploy"]["ssl"]["dir"]     = "/etc/nginx"
-default["deploy"]["ssl"]["int_ip"]  = "127.0.0.1"
