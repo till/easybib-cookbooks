62bbcadcf7110752dc97f9d844356d60b1daecdd
Ulf Harnhammar <ulfharn@gmail.com>
2012-08-23 13:12:41 +0200

    checking if certificate or key is empty, moving the nginx configuration into inner loop to avoid potential problems

--- nginx-lb/recipes/default.rb
+++ nginx-lb/recipes/default.rb
@@ -20,49 +20,63 @@ node[:deploy].each do |application, deploy|
     next
   end
 
-  if deploy.has_key?("ssl_certificate") and deploy.has_key?("ssl_certificate_key")
-
-    ssl_certificate = deploy["ssl_certificate"].chomp
-    ssl_certificate_key = deploy["ssl_certificate_key"].chomp
-
-    template ssl_dir + "/cert.pem" do
-      source "ssl_key.erb"
-      mode   "0640"
-      owner  "root"
-      group  "www-data"
-      variables(
-        "ssl_key" => ssl_certificate
-      )
-      notifies :restart, resources(:service => "nginx")
-    end
-
-    template ssl_dir + "/cert.key" do
-      source "ssl_key.erb"
-      mode   "0640"
-      owner  "root"
-      group  "www-data"
-      variables(
-        "ssl_key" => ssl_certificate_key
-      )
-      notifies :restart, resources(:service => "nginx")
-    end
-
+  if !deploy.has_key?("ssl_certificate")
+    next
   end
 
-end
+  if !deploy.has_key?("ssl_certificate_key")
+    next
+  end
 
-template ssl_dir + "/sites-enabled/easybib-ssl.conf" do
-  source "nginx.conf.erb"
-  mode   "0644"
-  owner  "root"
-  group  "root"
-  variables(
-    "ssl_dir" => ssl_dir,
-    "int_ip"  => int_ip
-  )
-  notifies :restart, resources(:service => "nginx")
-end
+  if deploy["ssl_certificate"].empty?
+    Chef::Log.debug("ssl_certificate is empty")
+    next
+  end
 
-file ssl_dir + "/sites-enabled/default" do
-  action :delete
+  if deploy["ssl_certificate_key"].empty?
+    Chef::Log.debug("ssl_certificate_key is empty")
+    next
+  end
+
+  ssl_certificate = deploy["ssl_certificate"].chomp
+  ssl_certificate_key = deploy["ssl_certificate_key"].chomp
+
+  template ssl_dir + "/cert.pem" do
+    source "ssl_key.erb"
+    mode   "0640"
+    owner  "root"
+    group  "www-data"
+    variables(
+      "ssl_key" => ssl_certificate
+    )
+    notifies :restart, resources(:service => "nginx")
+  end
+
+  template ssl_dir + "/cert.key" do
+    source "ssl_key.erb"
+    mode   "0640"
+    owner  "root"
+    group  "www-data"
+    variables(
+      "ssl_key" => ssl_certificate_key
+    )
+    notifies :restart, resources(:service => "nginx")
+  end
+
+  template ssl_dir + "/sites-enabled/easybib-ssl.conf" do
+    source "nginx.conf.erb"
+    mode   "0644"
+    owner  "root"
+    group  "root"
+    variables(
+      "ssl_dir" => ssl_dir,
+      "int_ip"  => int_ip
+    )
+    notifies :restart, resources(:service => "nginx")
+  end
+
+  file ssl_dir + "/sites-enabled/default" do
+    action :delete
+  end
+
 end
