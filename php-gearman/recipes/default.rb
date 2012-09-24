5675792863befb840d416af344b1e03d9fa7415c
till <till@php.net>
2012-09-24 18:19:57 +0200

    Attempt to fix restart issue. Cleanups.

--- php-gearman/recipes/default.rb
+++ php-gearman/recipes/default.rb
@@ -1,7 +1,9 @@
 include_recipe "apt::ppa"
 include_recipe "apt::easybib"
 
-package "php5-easybib-gearman"
+package "php5-easybib-gearman" do
+  notifies :restart, resources(:service => "php-fpm"), :delayed
+end
 
 include_recipe "php-fpm::service"
 
--- php-suhosin/recipes/default.rb
+++ php-suhosin/recipes/default.rb
@@ -3,3 +3,5 @@ include_recipe "apt::easybib"
 
 package "php5-easybib-suhosin"
 
+include_recipe "php-suhosin::configure"
+
