825c21eab08c31852fc98401f322773f0f612665
till <till@php.net>
2012-11-09 16:12:04 +0100

    Replace cluster_name with node[:foo].

--- deploy/recipes/citationanalytics.rb
+++ deploy/recipes/citationanalytics.rb
@@ -1,20 +1,19 @@
 # custom recipe because of: http://support.scalarium.com/discussions/problems/78-app-is-not-deploying
 
 instance_roles = node[:scalarium][:instance][:roles]
-cluster_name   = node[:scalarium][:cluster][:name]
 
 node[:deploy].each do |application, deploy|
 
   Chef::Log.debug("deploy::easybib - app: #{application}, role: #{instance_roles}")
   Chef::Log.debug("Deploying as user: #{deploy[:user]}")
 
-  next unless cluster_name == 'Citation Analytics'
+  next unless node[:scalarium][:cluster][:name] == 'Citation Analytics'
 
   case application
   when 'citation_anlytics'
     next unless instance_roles.include?('elasticsearch')
   else
-    Chef::Log.debug("deploy::analytics - #{application} (in #{cluster_name}) skipped")
+    Chef::Log.debug("deploy::analytics - #{application} (in #{node[:scalarium][:cluster][:name]}) skipped")
     next
   end
 
