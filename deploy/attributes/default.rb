8aac3c5ff8e9c8f033b5f77ab238c4a0fd731f02
till <till@php.net>
2012-11-23 18:20:31 +0100

    Don't write into 'deploy' seems to screw up the 'apps' send to the instance.
    Not sure why that surfaced now (cc @ulfharn)

--- deploy/attributes/default.rb
+++ deploy/attributes/default.rb
@@ -1,8 +1,8 @@
 #default[:gearman_manager]        = {}
 #default[:gearman_manager][:type] = "pecl"
 
-default["deploy"]["gearmanmanager"]["daemon"]       = "/usr/local/bin/gearman-manager"
-default["deploy"]["gearmanmanager"]["real_daemon"]  = "/srv/www/ebim2/current/vendor/GearmanManager/pecl-manager.php"
-default["deploy"]["gearmanmanager"]["pid_dir"]      = "/var/run/gearman-manager"
-default["deploy"]["gearmanmanager"]["config_file"]  = "/srv/www/ebim2/current/etc/gearman/jango-fett.manager.ini"
-default["deploy"]["gearmanmanager"]["worker_class"] = "Ebim2\\Gearman\\Worker\\"
+default["gearmanmanager"]["daemon"]       = "/usr/local/bin/gearman-manager"
+default["gearmanmanager"]["real_daemon"]  = "/srv/www/ebim2/current/vendor/GearmanManager/pecl-manager.php"
+default["gearmanmanager"]["pid_dir"]      = "/var/run/gearman-manager"
+default["gearmanmanager"]["config_file"]  = "/srv/www/ebim2/current/etc/gearman/jango-fett.manager.ini"
+default["gearmanmanager"]["worker_class"] = "Ebim2\\Gearman\\Worker\\"
