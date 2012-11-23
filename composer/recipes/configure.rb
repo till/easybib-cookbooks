7da223d8e11c7b8311aec373c167cdf87f92a33a
till <till@php.net>
2012-11-23 18:02:45 +0100

    disable this, deploy::default runs before this

--- composer/recipes/configure.rb
+++ composer/recipes/configure.rb
@@ -4,22 +4,22 @@ if node[:scalarium]
   deploy_group    = node[:scalarium][:deploy_user][:group]
 
   # this is an assumption, so sue me
-  home_dir = "/home/#{deploy_username}"
+  #home_dir = "/home/#{deploy_username}"
 
   # copied this from scalarium's deploy/definition/scalarium_deploy_user.rb
-  user deploy_username do
-    action   :create
-    comment  "deploy user"
-    gid      deploy_group
-    supports :manage_home => true
-    shell    node[:scalarium][:deploy_user][:shell]
-    home     home_dir
-    not_if do
-      existing_usernames = []
-      Etc.passwd {|user| existing_usernames << user['name']}
-      existing_usernames.include?(deploy_username)
-    end
-  end
+  #user deploy_username do
+  #  action   :create
+  #  comment  "deploy user"
+  #  gid      deploy_group
+  #  supports :manage_home => true
+  #  shell    node[:scalarium][:deploy_user][:shell]
+  #  home     home_dir
+  #  not_if do
+  #    existing_usernames = []
+  #    Etc.passwd {|user| existing_usernames << user['name']}
+  #    existing_usernames.include?(deploy_username)
+  #  end
+  #end
 
   directory "#{home_dir}/.composer" do
     owner     deploy_username
