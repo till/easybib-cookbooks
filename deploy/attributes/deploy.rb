7799470a463732ecf6c51a318e38211b31c8ff52
till <till@php.net>
2012-11-25 22:54:49 +0100

    mind boggling

--- deploy/attributes/deploy.rb
+++ deploy/attributes/deploy.rb
@@ -3,10 +3,7 @@ unless node[:deploy].nil?
   deploy.each do |application, deploy|
 
     # if there's no scalarium
-    next unless !node[:scalarium]
-
-    # if there is no override
-    next unless !node[:scalarium][:deploy_user][:user].nil?
+    next unless node[:scalarium]
 
     # there's absolutely no need for zsh
     node[:deploy][application][:shell] = '/bin/sh'
