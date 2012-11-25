c99a92df73999f66ddc4a93c58ba433a4ffd45c7
till <till@php.net>
2012-11-25 22:17:06 +0100

    Set home directory and shell.

--- deploy/attributes/deploy.rb
+++ deploy/attributes/deploy.rb
@@ -1,9 +1,26 @@
-#unless node[:deploy].nil?
-  #deploy.each do |application, deploy|
-    #node[:deploy][application][:user]            = 'www-data'
-    #node[:deploy][application][:group]           = 'www-data'
-    #node[:deploy][application][:shell]           = '/bin/sh'
-    #node[:deploy][application][:home]            = '/var/www'
-    #node[:deploy][application][:restart_command] = '/etc/init.d/php-fpm reload'
-  #end
-#end
+unless node[:deploy].nil?
+
+  deploy.each do |application, deploy|
+
+    # if there's no scalarium
+    next unless !node[:scalarium]
+
+    # if there is no override
+    next unless !node[:scalarium][:deploy_user][:user].nil?
+
+    # there's absolutely no need for zsh
+    node[:deploy][application][:shell] = '/bin/sh'
+
+    case node[:scalarium][:deploy_user][:user]
+    when 'www-data'
+      node[:deploy][application][:home] = '/var/www'
+    when 'root'
+      node[:deploy][application][:home] = '/root'
+    else
+      node[:deploy][application][:home] = "/home/#{node[:scalarium][:deploy_user][:user]}"
+    end
+
+    Chef::Log.info("User: #{node[:scalarium][:deploy_user][:user]}, Home: #{node[:deploy][application][:home]}")
+
+  end
+end
