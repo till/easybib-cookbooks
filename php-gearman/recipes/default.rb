55713d26ed424fb9e66579b05202f0ff1a5ade74
till <till@php.net>
2013-02-25 22:54:32 +0100

    Skip ext/gearman on precise for now.

--- php-gearman/recipes/default.rb
+++ php-gearman/recipes/default.rb
@@ -1,7 +1,13 @@
-include_recipe "apt::ppa"
-include_recipe "apt::easybib"
 include_recipe "php-fpm::service"
 
-package "php5-easybib-gearman" do
-  notifies :reload, resources(:service => "php-fpm"), :delayed
+case node[:lsb][:codename]
+when 'lucid'
+  include_recipe "apt::ppa"
+  include_recipe "apt::easybib"
+
+  package "php5-easybib-gearman" do
+    notifies :reload, resources(:service => "php-fpm"), :delayed
+  end
+when 'precise'
+  Chef::Log.info("Skipping this for now.")
 end
