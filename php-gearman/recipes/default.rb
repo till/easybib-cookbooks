160b72d35424c6e77ba0ed1bd29cb127d13eb20e
till <till@php.net>
2013-02-26 01:39:38 +0100

    Download libgearman4 from squeeze and install it using dpkg.

--- php-gearman/recipes/default.rb
+++ php-gearman/recipes/default.rb
@@ -7,6 +7,16 @@ case node[:lsb][:codename]
 when 'lucid'
   p = "php5-easybib-gearman"
 when 'precise'
+  remote_file "/tmp/libgearman4_0.13-1_amd64.deb" do
+    source "http://ftp.de.debian.org/debian/pool/main/g/gearmand/libgearman4_0.13-1_amd64.deb"
+  end
+
+  package "libgearman4" do
+    action   :install
+    source   "/tmp/libgearman4_0.13-1_amd64.deb"
+    provider Chef::Provider::Package::Dpkg
+  end
+
   p = "php5-gearman"
 end
 
