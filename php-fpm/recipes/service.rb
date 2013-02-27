ec3deae72a74f7891ad2ef0457023a9c318e5b6d
till <till@php.net>
2013-02-27 17:38:20 +0100

    Adjust name of the service for precise.

--- php-fpm/recipes/service.rb
+++ php-fpm/recipes/service.rb
@@ -1,4 +1,11 @@
+case node[:lsb][:codename]
+when 'lucid'
+  service_name = "php-fpm"
+when 'precise'
+  service_name = "php5-fpm"
+end
+
 service "php-fpm" do
-  service_name "php-fpm"
+  service_name service_name
   supports     [:start, :stop, :reload, :restart]
 end
