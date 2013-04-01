f1bc8e24d1563c9a2645abe6f11042f4c5405b28
till <till@php.net>
2013-04-01 18:43:25 +0200

    install ssl on appservers

---
+++ deploy/recipes/ssl-infolit.rb
@@ -0,0 +1,82 @@
+right_role    = "nginxphpapp"
+right_cluster = "InfoLit"
+nginx_dir     = "/etc/nginx"
+ssl_dir       = nginx_dir + "/ssl"
+
+if node.attribute?(:scalarium)
+  instance_roles = node[:scalarium][:instance][:roles]
+  cluster_name   = node[:scalarium][:cluster][:name]
+else
+  Chef::Log.debug("Not running in scalarium, setting defaults.")
+  instance_roles = ""
+  cluster_name   = ""
+end
+
+stored_certificate = false
+
+node[:deploy].each do |application, deploy|
+
+  if application != "ssl"
+    next
+  end
+
+  if !instance_roles.include?(right_role)
+    next
+  end
+
+  if !right_cluster.include?(cluster_name.to_s)
+    Chef::Log.info("Will not deploy to: " + cluster_name.to_s)
+    next
+  end
+
+  if !deploy.has_key?("ssl_certificate")
+    Chef::Log.info("No ssl_certificate 'key'")
+    next
+  end
+
+  if !deploy.has_key?("ssl_certificate_key")
+    Chef::Log.info("No ssl_certificate_key 'key'")
+    next
+  end
+
+  if deploy["ssl_certificate"].empty?
+    Chef::Log.error("ssl_certificate is empty")
+    next
+  end
+
+  if deploy["ssl_certificate_key"].empty?
+    Chef::Log.error("ssl_certificate_key is empty")
+    next
+  end
+
+  ssl_certificate     = deploy["ssl_certificate"].chomp
+  ssl_certificate_key = deploy["ssl_certificate_key"].chomp
+
+  directory ssl_dir do
+    mode      "0750"
+    owner     "root"
+    group     "www-data"
+    recursive true
+  end
+
+  template ssl_dir + "/cert.pem" do
+    source "ssl_key.erb"
+    mode   "0640"
+    owner  "root"
+    group  "www-data"
+    variables(
+      "ssl_key" => ssl_certificate
+    )
+  end
+
+  template ssl_dir + "/cert.key" do
+    source "ssl_key.erb"
+    mode   "0640"
+    owner  "root"
+    group  "www-data"
+    variables(
+      "ssl_key" => ssl_certificate_key
+    )
+  end
+
+end
