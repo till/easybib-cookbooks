8958fc59a70e921a04c5ab9f21ea91eae6758186
dazz <annejulia@gmail.com>
2013-04-25 15:05:25 +0200

    add xdebug ide remote connection params

--- php-xdebug/attributes/default.rb
+++ php-xdebug/attributes/default.rb
@@ -3,5 +3,8 @@ default[:xdebug][:version] = "latest"
 
 default[:xdebug][:config] = {
     "scream"               => 1,
-    "show_exception_trace" => 1
+    "remote_enable"        => 1,
+    "remote_handler"       => "dbgp",
+    "remote_connect_back"  => 1,
+    "idekey"               => "XDEBUG_PHPSTORM"
 }
