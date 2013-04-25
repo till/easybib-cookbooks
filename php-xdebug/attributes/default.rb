2f997e96352823accf02766a8a6976d196ac830c
dazz <annejulia@gmail.com>
2013-04-25 15:11:03 +0200

    re-add show_exception_trace

--- php-xdebug/attributes/default.rb
+++ php-xdebug/attributes/default.rb
@@ -3,6 +3,7 @@ default[:xdebug][:version] = "latest"
 
 default[:xdebug][:config] = {
     "scream"               => 1,
+    "show_exception_trace" => 0,
     "remote_enable"        => 1,
     "remote_handler"       => "dbgp",
     "remote_connect_back"  => 1,
