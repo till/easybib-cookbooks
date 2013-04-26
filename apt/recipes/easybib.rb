fb3778bda618071abb2451a8dcb825789f09ab5a
till <till@php.net>
2013-04-26 21:02:21 +0200

    bleeding edge in vagrant

--- apt/recipes/easybib.rb
+++ apt/recipes/easybib.rb
@@ -1,10 +1,16 @@
-ppa="easybib/ppa"
+ppa=["easybib/ppa"]
 if node.attribute?(:scalarium) && node[:scalarium][:cluster][:name] == "EasyBib Playground"
-  ppa="easybib/test"
+  ppa=["easybib/test", "easybib/multipackagetest"]
 end
 
-execute "add #{ppa}" do
-  command "add-apt-repository ppa:#{ppa}"
+if node.attribute?(:vagrant)
+  ppa=["easybib/test", "easybib/multipackagetest"]
+end
+
+ppa.each do |p|
+  execute "add #{ppa}" do
+    command "add-apt-repository ppa:#{ppa}"
+  end
 end
 
 execute "update sources" do
