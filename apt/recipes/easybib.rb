d3e071bca68079844f1967592c355469db5b1216
till <till@php.net>
2013-05-02 14:16:24 +0200

    Bugfix: use test repos locally (fixes the add-apt-repository command).

--- apt/recipes/easybib.rb
+++ apt/recipes/easybib.rb
@@ -3,16 +3,18 @@ if node.attribute?(:scalarium) && node[:scalarium][:cluster][:name] == "EasyBib 
   ppa=["easybib/test", "easybib/multipackagetest"]
 end
 
-if node.attribute?(:vagrant)
+if !node.attribute?(:scalarium)
   ppa=["easybib/test", "easybib/multipackagetest"]
 end
 
 ppa.each do |p|
-  execute "add #{ppa}" do
-    command "add-apt-repository ppa:#{ppa}"
+  Chef::Log.debug("Adding #{p}")
+  execute "add #{p}" do
+    command "add-apt-repository ppa:#{p}"
   end
 end
 
+Chef::Log.debug("Updating sources.")
 execute "update sources" do
   command "apt-get -y -f -q update"
 end
