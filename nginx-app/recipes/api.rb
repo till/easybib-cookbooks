66239007e3cbc25303c84dcb067164c5eda2b389
till <till@php.net>
2013-05-17 20:13:07 +0200

    Determine where we are and set variables accordingly!

--- nginx-app/recipes/api.rb
+++ nginx-app/recipes/api.rb
@@ -1,4 +1,10 @@
-vagrant_dir = "/vagrant_data/web/"
+if is_aws()
+  deploy_dir = "/srv/api/current"
+  nginx_extras = ""
+else
+ deploy_dir = "/vagrant_data/web/"
+ nginx_extras = "sendfile off;"
+end
 
 template "/etc/nginx/sites-enabled/silex.conf" do
   source "silex.conf.erb"
@@ -7,9 +13,9 @@ template "/etc/nginx/sites-enabled/silex.conf" do
   group  node["nginx-app"][:group]
   variables(
     :php_user    => node["php-fpm"][:user],
-    :doc_root    => vagrant_dir,
+    :doc_root    => deploy_dir,
     :access_log  => 'off',
-    :nginx_extra => 'sendfile  off;'
+    :nginx_extra => nginx_extras
   )
   notifies :restart, resources(:service => "nginx"), :delayed
 end
