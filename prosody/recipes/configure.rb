1911997ef0430ab5c81e98e9492a920c01a754b6
till <till@php.net>
2013-05-18 18:35:18 +0200

    finish automating db setup

--- prosody/recipes/configure.rb
+++ prosody/recipes/configure.rb
@@ -8,6 +8,21 @@ else
   Chef::Log.error("Not supported: #{node[:lsb][:name]}")
 end
 
+if node["prosody"]["storage"] == "sql"
+
+  db_conf = node["prosody"]["db"]
+
+  mysql_command = "mysql -u %s -h %s" % [ db_conf["username"], db_conf["hostname"] ]
+  if !db_conf["password"].empty?
+    mysql_command += " -p #{db_conf["password"]}"
+  end
+
+  execute "create database '#{db_conf["database"]} for prosody" do
+    command "#{mysql_command} -e \"CREATE DATABASE IF NOT EXISTS #{db_conf["database"]}\""
+  end
+
+end
+
 template "#{cfg_dir}/prosody.cfg.lua" do
   owner "prosody"
   group "prosody"
