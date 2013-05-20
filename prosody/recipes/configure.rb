769eb373a87d51e74279f69f7829ca2e454d5247
till <till@php.net>
2013-05-20 17:24:12 +0200

    Move storage related into its own recipe.

--- prosody/recipes/configure.rb
+++ prosody/recipes/configure.rb
@@ -8,36 +8,7 @@ else
   Chef::Log.error("Not supported: #{node[:lsb][:name]}")
 end
 
-if node["prosody"]["storage"] == "sql"
-
-  db_conf = node["prosody"]["db"]
-
-  driver = db_conf["driver"].downcase
-
-  package "Installing DBI bindings for '#{driver}'" do
-    package_name "lua-dbi-#{driver}"
-  end
-
-  case driver
-  when "mysql"
-    mysql_command = "mysql -u %s -h %s" % [ db_conf["username"], db_conf["hostname"] ]
-    if !db_conf["password"].empty?
-      mysql_command += " -p #{db_conf["password"]}"
-    end
-
-    execute "create database '#{db_conf["database"]} for prosody" do
-      command "#{mysql_command} -e \"CREATE DATABASE IF NOT EXISTS #{db_conf["database"]}\""
-    end
-  when "postgresql"
-    # this needs to be done
-    Chef::Log.info("Haven't do this yet.")
-  when "sqlite3"
-    # this should just work :)
-  else
-    Chef.Log::Error("The '#{driver}' is not yet supported.")
-  end
-
-end
+include_recipe "prosody::storage"
 
 template "#{cfg_dir}/prosody.cfg.lua" do
   owner "prosody"
---
+++ prosody/recipes/storage.rb
@@ -0,0 +1,30 @@
+if node["prosody"]["storage"] == "sql"
+
+  db_conf = node["prosody"]["db"]
+
+  driver = db_conf["driver"].downcase
+
+  package "Installing DBI bindings for '#{driver}'" do
+    package_name "lua-dbi-#{driver}"
+  end
+
+  case driver
+  when "mysql"
+    mysql_command = "mysql -u %s -h %s" % [ db_conf["username"], db_conf["hostname"] ]
+    if !db_conf["password"].empty?
+      mysql_command += " -p #{db_conf["password"]}"
+    end
+
+    execute "create database '#{db_conf["database"]} for prosody" do
+      command "#{mysql_command} -e \"CREATE DATABASE IF NOT EXISTS #{db_conf["database"]}\""
+    end
+  when "postgresql"
+    # this needs to be done
+    Chef::Log.info("Haven't do this yet.")
+  when "sqlite3"
+    # this should just work :)
+  else
+    Chef.Log::Error("The '#{driver}' is not yet supported.")
+  end
+
+end
