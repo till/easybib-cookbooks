946c6b8f6f169343355b24fc1bb95b9c7be23965
till <till@php.net>
2013-05-27 16:20:57 +0200

    fix path to app

--- nginx-app/recipes/api.rb
+++ nginx-app/recipes/api.rb
@@ -1,5 +1,5 @@
 if is_aws()
-  deploy_dir = "/srv/api/current"
+  deploy_dir = "/srv/www/api/current"
   nginx_extras = ""
   if get_cluster_name() == "API Staging"
     default_router = "index_staging.php"
