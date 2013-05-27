f74c786f1995557df5d1a2e16c401d0c8813a4b4
till <till@php.net>
2013-05-27 16:38:32 +0200

    no trailing slash

--- xhprof.io/attributes/default.rb
+++ xhprof.io/attributes/default.rb
@@ -4,5 +4,5 @@ default["xhprof.io"]["dsn"] = 'mysql:dbname=xhprof;host=33.33.33.130;charset=utf
 default["xhprof.io"]["username"] = "root"
 default["xhprof.io"]["password"] = ""
 default["xhprof.io"]["dbname"]   = "xhprof"
-default["xhprof.io"]["root"]     = "/var/www/xhprof.io/"
+default["xhprof.io"]["root"]     = "/var/www/xhprof.io"
 default["xhprof.io"]["host"]     = "33.33.33.130"
