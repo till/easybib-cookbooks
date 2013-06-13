efae001d3d05800e32cb9aa9dd7cd1309e040646
fh <fh-github@fholzhauer.de>
2013-06-13 14:07:06 +0200

    inital cb for satis

---
+++ easybib/recipes/role-satis.rb
@@ -0,0 +1,8 @@
+include_recipe "easybib::setup"
+include_recipe "loggly::setup"
+include_recipe "php-fpm"
+include_recipe "php-fpm::pear"
+include_recipe "unfuddle-ssl-fix::install"
+include_recipe "php-phar"
+include_recipe "php-posix"
+include_recipe "silverline"
