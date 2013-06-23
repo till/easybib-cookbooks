d2e0789c7e2bb5cd3e673a5d6b5b078e45b9c401
till <till@php.net>
2013-06-23 22:57:00 +0200

    Clean-Up: delete old cookbook

--- php-doctrine/attributes/default.rb
+++
@@ -1,5 +0,0 @@
-node[:php_doctrine]           = {}
-node[:php_doctrine][:channel] = "pear.doctrine-project.org"
-node[:php_doctrine][:common]  = "2.1.4"
-node[:php_doctrine][:dbal]    = "2.1.6"
-node[:php_doctrine][:orm]     = "2.1.6"
