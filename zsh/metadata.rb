a8ee18e7c38c3f8d6b5c24f1a2aea3aeade5811d
Leander <mail@leander-damme.com>
2013-06-25 17:29:45 +0200

    copy and paste artifacts

--- zsh/metadata.rb
+++ zsh/metadata.rb
@@ -3,6 +3,6 @@ maintainer_email  "leander@wesrc.com"
 license           "BSD License"
 description       "configures/exports zsh env variables."
 version           "0.1"
-recipe            "nginx::configure", "sets env variables in /etc/zsh/zprofile"
+recipe            "zsh::configure", "sets env variables in /etc/zsh/zprofile"
 
 supports 'ubuntu'
