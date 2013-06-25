6f12b10ea1ffd40517d9584ed87740181628cf74
Leander <mail@leander-damme.com>
2013-06-25 17:28:39 +0200

    export app-config vars via zprofile

---
+++ zsh/metadata.rb
@@ -0,0 +1,8 @@
+maintainer        "Leander Damme"
+maintainer_email  "leander@wesrc.com"
+license           "BSD License"
+description       "configures/exports zsh env variables."
+version           "0.1"
+recipe            "nginx::configure", "sets env variables in /etc/zsh/zprofile"
+
+supports 'ubuntu'
