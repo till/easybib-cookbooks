4c89a118ecd3a0e4123471dd1bd162c2cdd92fb2
till <till@php.net>
2013-06-26 11:51:41 +0200

    Bugfix: for effhaa

--- deploy/attributes/default.rb
+++ deploy/attributes/default.rb
@@ -11,5 +11,4 @@ default["ssl-deploy"] = {}
 default["ssl-deploy"]["directory"] = "/etc/nginx/ssl"
 
 default["s3-syncer"]["path"] = "/mnt/srv/www/s3-syncer/bin/syncer"
-default["s3-syncer"]["source"] = "https://github.com/easybiblabs/s3-syncer/archive/master.tar.gz""
-
+default["s3-syncer"]["source"] = "https://github.com/easybiblabs/s3-syncer/archive/master.tar.gz"
