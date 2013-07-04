6f22290df52fda225942999c9e43c0eed2a05c09
till <till@php.net>
2013-07-04 17:26:45 +0200

    Enhancement: pdo_sqlite recipe

---
+++ php-pdo_sqlite/recipes/default.rb
@@ -0,0 +1,28 @@
+php_source = "/tmp/php-#{node["php-pdo_sqlite"]["version"]}"
+php_archive = "#{php_source}.tar.gz"
+
+remote_file "#{php_archive}" do
+ checksum node["php-pdo_sqlite"]["checksum"]
+ source "http://de3.php.net/get/php-#{node["php-pdo_sqlite"]["version"]}.tar.gz/from/de1.php.net/mirror"
+end
+
+execute "tar -zxf #{php_archive}" do
+  cwd "/tmp"
+end
+
+module_source = "#{php_source}/ext/pdo_sqlite"
+ext_dir = `php -r 'echo ini_get("extension_dir");'`.strip
+
+commands = [
+  "phpsize",
+  "./configure",
+  "make",
+  "cp modules/pdo_sqlite.so #{ext_dir}",
+  "echo 'extension=pdo_sqlite.so' >> /usr/local/etc/php/pdo_sqlite.ini"
+]
+
+commands.each do |command|
+  execute "#{command}" do
+    cwd module_source
+  end
+end
