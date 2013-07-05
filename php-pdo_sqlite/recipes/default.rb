630bd0d0cd39de7df2e1db7f4aa3b484ed97df2f
till <till@php.net>
2013-07-05 17:18:02 +0200

    Bugfix: use the different ohai plugin for php

--- php-pdo_sqlite/recipes/default.rb
+++ php-pdo_sqlite/recipes/default.rb
@@ -1,6 +1,11 @@
 package "libsqlite3-dev"
 package "autoconf"
 
+ohai "reload_php" do
+  action :reload
+  plugin "php_fpm"
+end
+
 php_source = "/tmp/php-#{node["php-pdo_sqlite"]["version"]}"
 php_archive = "#{php_source}.tar.gz"
 
@@ -14,14 +19,13 @@ execute "tar -zxf #{php_archive}" do
 end
 
 module_source = "#{php_source}/ext/pdo_sqlite"
-ext_dir = `php -r 'echo ini_get("extension_dir");'`.strip
 
 commands = [
   "phpize",
   "./configure",
   "make",
-  "cp modules/pdo_sqlite.so #{ext_dir}",
-  "echo 'extension=pdo_sqlite.so' >> /usr/local/etc/php/pdo_sqlite.ini"
+  "cp modules/pdo_sqlite.so #{node[:languages][:php_fpm][:extension_dir]}",
+  "echo 'extension=pdo_sqlite.so' >> #{node["php-fpm"][:prefix]}/etc/php/pdo_sqlite.ini"
 ]
 
 commands.each do |command|
