20b9f32b3e4ee568fe77709d93992466cc17c91e
till <till@php.net>
2013-07-09 22:50:50 +0200

    Enhancement: spin off repo discovery

--- newrelic/recipes/app-monitor.rb
+++ newrelic/recipes/app-monitor.rb
@@ -1,9 +1,6 @@
 include_recipe "php-fpm::service"
 
 commands = [
-  "wget -O - http://download.newrelic.com/548C16BF.gpg | apt-key add -",
-  "echo 'deb http://apt.newrelic.com/debian/ newrelic non-free' > /etc/apt/sources.list.d/newrelic.list",
-  "apt-get update",
   "apt-get install -y newrelic-php5",
   "newrelic-install install",
   "echo 'newrelic.license=\"#{node["newrelic"]["license"]}\"' > #{node["php-fpm"][:prefix]}/etc/php/newrelic.ini"
