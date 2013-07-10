09993d6c7a3d713105c4471fbb910a735b422b53
till <till@php.net>
2013-07-10 14:10:52 +0200

    Enhancement: refactor silverline::default
    Spin off service definition and re-use.

--- silverline/recipes/default.rb
+++ silverline/recipes/default.rb
@@ -1,16 +1,11 @@
 include_recipe "silverline::addrepo"
+include_recipe "silverline::service"
 
 package "librato-silverline"
 
+# hack, apparently we need to start it first
 service "silverline" do
-  case node[:platform]
-  when "ubuntu"
-    if node[:platform_version].to_f >= 9.10
-      provider Chef::Provider::Service::Upstart
-    end
-  end
-  supports :restart => true, :start => true
-  action [:start]
+  action :start
 end
 
 template "/etc/load_manager/lmd.conf" do
