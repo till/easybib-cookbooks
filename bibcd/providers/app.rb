56c75b0fe22490d8094e3a056012e1d3c2850a7a
fh <fh-github@fholzhauer.de>
2013-07-24 10:37:19 +0200

    fixing template path in provider

--- bibcd/providers/app.rb
+++ bibcd/providers/app.rb
@@ -5,6 +5,7 @@ action :add do
     Chef::Log.info "Adding #{new_resource.app_name} config to #{node['bibcd_path']}/config/apps/#{new_resource.app_name}.yml"
     
     template "#{node['bibcd_path']}/config/apps/#{new_resource.app_name}.yml" do
+      cookbook "bibcd" #if we dont set this, the template cmd would search in the calling cookbook
       mode   0644
       variables :content => YAML::dump(new_resource.config.to_hash)
       source "app.yml.erb"
