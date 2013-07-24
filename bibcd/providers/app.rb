17aea328f9a624b2acc8a6bcc42ff8f0d69d4a2c
fh <fh-github@fholzhauer.de>
2013-07-24 10:32:28 +0200

    fixing path

--- bibcd/providers/app.rb
+++ bibcd/providers/app.rb
@@ -1,10 +1,10 @@
 require "yaml"
 
 action :add do
-  unless ::File.exists?("#{bicd_path}/config/apps/#{new_resource.app_name}.yml")
-    Chef::Log.info "Adding #{new_resource.app_name} config to #{bicd_path}/config/apps/#{new_resource.app_name}.yml"
+  unless ::File.exists?("#{node['bibcd_path']}/config/apps/#{new_resource.app_name}.yml")
+    Chef::Log.info "Adding #{new_resource.app_name} config to #{node['bibcd_path']}/config/apps/#{new_resource.app_name}.yml"
     
-    template "#{bicd_path}/config/apps/#{new_resource.app_name}.yml" do
+    template "#{node['bibcd_path']}/config/apps/#{new_resource.app_name}.yml" do
       mode   0644
       variables :content => YAML::dump(new_resource.config.to_hash)
       source "app.yml.erb"
