17314911b975b7833e61a39b619ccb53012d6e89
till <till@php.net>
2013-07-25 14:33:25 +0200

    Remove old recipe.

--- vagrant-fix/metadata.rb
+++
@@ -1,8 +0,0 @@
-maintainer        "Till Klampaeckel"
-maintainer_email  "till@php.net"
-license           "New BSD License"
-description       "Runs aptitude update and gem update prior to all other recipes."
-version           "0.1"
-recipe            "vagrant-fix::default", "See description!"
-
-supports 'ubuntu'
