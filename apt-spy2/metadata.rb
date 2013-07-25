e9187d759c141410d1d83a8fcf9ce55a68cb913a
till <till@php.net>
2013-07-25 14:16:14 +0200

    Enhancement: commit all artifacts to make foodcritic happy

---
+++ apt-spy2/metadata.rb
@@ -0,0 +1,9 @@
+name             'apt-spy2'
+maintainer       'Till Klampaeckel'
+maintainer_email 'till@php.net'
+license          'New BSD license'
+description      'Installs apt-spy2'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+
+supports 'ubuntu'
+depends 'ruby'
---
+++ ruby/README.md
@@ -0,0 +1,8 @@
+# ruby
+
+Installs Ruby 1.8 or 1.9.1 from Brightbox's PPA.
+
+See `attributes/default.rb` for all overrides:
+
+ * node["ruby"]["version"]
+ * node["ruby"]["ppa"]
