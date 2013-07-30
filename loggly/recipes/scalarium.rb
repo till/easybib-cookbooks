1d6379caf3e28f8370a6c61518585d07e57470f1
till <till@php.net>
2013-07-30 11:54:33 +0200

    WIP: fix more foodcritic

--- loggly/recipes/scalarium.rb
+++ loggly/recipes/scalarium.rb
@@ -1,21 +1,20 @@
-# this is specific to our setup and is only triggered on scalarium/opsworks
-if !get_cluster_name().empty?
-
-  if get_instance_roles().include?('loadbalancer')
-
-    template "/etc/rsyslog.d/10-haproxy.conf" do
-      source "10-haproxy.conf.erb"
-      mode "0644"
-    end
-
-    directory "#{node[:syslog][:haproxy][:log_dir]}" do
-      recursive true
-      mode "0755"
-    end
-
-    service "rsyslog" do
-      supports :status => true, :restart => true, :reload => true
-      action [ :restart ]
-    end
+template "/etc/rsyslog.d/10-haproxy.conf" do
+  source "10-haproxy.conf.erb"
+  mode "0644"
+  only_if do
+    get_instance_roles().include?('loadbalancer')
   end
+end
+
+directory node["syslog"]["haproxy"]["log_dir"] do
+  recursive true
+  mode "0755"
+  only_if do
+    get_instance_roles().include?('loadbalancer')
+  end
+end
+
+service "rsyslog" do
+  supports :status => true, :restart => true, :reload => true
+  action [ :restart ]
 end
