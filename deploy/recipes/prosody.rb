e663fae28aa41951bdf7c9b45b4aea2e5cf6efb9
fh <fh-github@fholzhauer.de>
2013-07-30 09:02:57 +0200

    added deploy env for prosody

---
+++ deploy/recipes/prosody.rb
@@ -0,0 +1,22 @@
+instance_roles = get_instance_roles()
+cluster_name   = get_cluster_name()
+
+node[:deploy].each do |application, deploy|
+
+  Chef::Log.info("deploy::jabber - request to deploy app: #{application}, role: #{instance_roles} in #{cluster_name}")
+
+  next unless cluster_name == node["easybib"]["cluster_name"]
+
+  case application
+  when 'jabber'
+    next unless instance_roles.include?('jabber')
+  else
+    Chef::Log.info("deploy::jabber - #{application} (in #{cluster_name}) skipped")
+    next
+  end
+
+  Chef::Log.info("deploy::jabber - Deployment started.")
+
+  include_recipe "prosody::default"
+  
+end
---
+++ easybib/recipes/role-prosody.rb
@@ -0,0 +1,4 @@
+include_recipe "easybib::setup"
+include_recipe "loggly::setup"
+include_recipe "percona::server"
+include_recipe "deploy::prosody"
