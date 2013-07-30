4d46d40e90a5b28680e81a7587173180ea6806cd
till <till@php.net>
2013-07-30 13:26:49 +0200

    Bugfix: namespaces and ruby

--- loggly/recipes/scalarium.rb
+++ loggly/recipes/scalarium.rb
@@ -1,20 +1,23 @@
+roles = get_instance_roles()
+
+service "rsyslog" do
+  supports :status => true, :restart => true, :reload => true
+  action :nothing
+end
+
 template "/etc/rsyslog.d/10-haproxy.conf" do
   source "10-haproxy.conf.erb"
   mode "0644"
   only_if do
-    get_instance_roles().include?('loadbalancer')
+    roles.include?('loadbalancer')
   end
+  notifies :restart, resources( :service => "rsyslog" )
 end
 
 directory node["syslog"]["haproxy"]["log_dir"] do
   recursive true
   mode "0755"
   only_if do
-    get_instance_roles().include?('loadbalancer')
+    roles().include?('loadbalancer')
   end
-end
-
-service "rsyslog" do
-  supports :status => true, :restart => true, :reload => true
-  action [ :restart ]
 end
