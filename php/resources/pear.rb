4a4bc16224cbf3286650ceff983a9c8c232eb0b2
till <till@php.net>
2013-07-31 19:55:46 +0200

    Bugfix: FC016

--- php/resources/pear.rb
+++ php/resources/pear.rb
@@ -1,5 +1,7 @@
 actions :install, :uninstall, :upgrade, :install_if_missing
- 
+
+default_action :install
+
 attribute :package, :kind_of => String, :name_attribute => true
 attribute :channel, :kind_of => String, :default => 'pear.php.net'
 attribute :version, :kind_of => String, :default => nil
