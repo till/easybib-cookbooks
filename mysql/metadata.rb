0892306aae87c57d2ddcffdf5dc3a605f7e4a846
till <till@php.net>
2013-07-31 12:50:33 +0200

    Change: delete old recipe

--- mysql/metadata.rb
+++
@@ -1,3 +0,0 @@
-name              "mysql"
-maintainer        "Till Klampaeckel"
-maintainer_email  "till@php.net"
--- mysql/recipes/silverline.rb
+++
@@ -1,7 +0,0 @@
-include_recipe "mysql::service"
-
-template "/etc/init.d/mysql" do
-  source "mysql.init.erb"
-  mode "0755"
-  notifies :restart, resources( :service => "mysql")
-end
