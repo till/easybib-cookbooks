1b2b7f14dc7651bfb678951c9c85ee5db45aa969
till <till@php.net>
2013-08-01 19:12:21 +0200

    Enhancement: configuration options
    Added more configuration options.
    Some sensible production defaults.

---
+++ php-apc/attributes/default.rb
@@ -0,0 +1,5 @@
+default["php-apc"] = {}
+default["php-apc"]["stat"] = 0
+default["php-apc"]["slam_defense"] = 1
+default["php-apc"]["shm_size"] = "70M"
+default["php-apc"]["max_file_size"] = "2M"
--- php-apc/recipes/configure.rb
+++ php-apc/recipes/configure.rb
@@ -3,10 +3,14 @@ include_recipe "php-fpm::service"
 is_cloud = is_aws()
 
 template "#{node["php-fpm"][:prefix]}/etc/php/apc.ini" do
-  source   "apc.ini.erb"
-  mode     "0644"
+  source "apc.ini.erb"
+  mode   "0644"
   variables({
-    :is_cloud => is_cloud
+    :is_cloud      => is_cloud,
+    :stat          => node["php-apc"]["stat"],
+    :slam_defense  => node["php-apc"]["slam_defense"],
+    :shm_size      => node["php-apc"]["shm_size"],
+    :max_file_size => node["php-apc"]["max_file_size"]
   })
   notifies :reload, resources(:service => "php-fpm"), :delayed
 end
