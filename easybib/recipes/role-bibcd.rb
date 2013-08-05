4c30c33e6e088085110e3028bee3570d65ec9fce
till <till@php.net>
2013-08-05 17:14:41 +0200

    Change: pear recipe
    Remove placeholder (php-fpm::pear) and include php-pear directly.
    
    Removed php-pear from role-satis, role-bibcd and role-gearmanw.

--- easybib/recipes/role-bibcd.rb
+++ easybib/recipes/role-bibcd.rb
@@ -1,7 +1,7 @@
 include_recipe "easybib::setup"
 include_recipe "loggly::setup"
 include_recipe "php-fpm"
-include_recipe "php-fpm::pear"
+include_recipe "php-pear"
 include_recipe "php-phar"
 include_recipe "php-posix"
 include_recipe "composer::configure"
