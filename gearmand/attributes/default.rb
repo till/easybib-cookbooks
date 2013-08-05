20da43104dea80a86dca641fdb8fe94af9b01317
till <till@php.net>
2013-08-05 16:23:23 +0200

    Bugfix: Chef11 and FC

--- gearmand/attributes/default.rb
+++ gearmand/attributes/default.rb
@@ -1,13 +1,13 @@
-default[:gearmand]        = {}
-default[:gearmand][:user] = "gearman"
-default[:gearmand][:port] = 31337
-default[:gearmand][:log]  = "--syslog --verbose INFO" #or --logfile=/path/to/whatever
+default['gearmand']         = {}
+default['gearmand']['user'] = 'gearman'
+default['gearmand']['port'] = 31337
+default['gearmand']['log']  = '--syslog --verbose INFO' #or --logfile=/path/to/whatever
 
-# these are relevant for when we compile from source (current default)
-default[:gearmand][:prefix]           = "/opt/easybib"
-default[:gearmand][:source]           = {}
-default[:gearmand][:source][:version] = "0.28"
-default[:gearmand][:source][:flags]   = "--disable-libpq --disable-libmemcached --without-sqlite3 --disable-libdrizzle"
+# these are relevant for when we compile from source
+default['gearmand']['prefix']            = '/opt/easybib'
+default['gearmand']['source']            = {}
+default['gearmand']['source']['version'] = '0.28'
+default['gearmand']['source']['flags']   = '--disable-libpq --disable-libmemcached --without-sqlite3 --disable-libdrizzle'
 
 # these are for silverline
-default[:silverline][:gearmand_name] = "gearmand"
+default['silverline']['gearmand_name'] = 'gearmand'
--- gearmand/recipes/default.rb
+++ gearmand/recipes/default.rb
@@ -1,12 +1,12 @@
-case node[:lsb][:codename]
+case node['lsb']['codename']
 when 'lucid'
   include_recipe "apt::ppa"
   include_recipe "apt::easybib"
-  node[:gearmand][:name] = 'gearmand-easybib'
+  node.set['gearmand']['name'] = 'gearmand-easybib'
 when 'precise'
-  node[:gearmand][:name] = 'gearman-job-server'
+  node.set["gearmand"]["name"] = 'gearman-job-server'
 end
 
-package node[:gearmand][:name]
+package node['gearmand']['name']
 
 include_recipe "gearmand::configure"
