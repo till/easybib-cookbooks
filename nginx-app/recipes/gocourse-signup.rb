a1deba38e4cb2eb62cf11980d080a6883fe30b82
Mario Scheliga <marsch.js@gmail.com>
2013-08-14 15:02:57 +0200

    Update signup to use /build dir

--- nginx-app/recipes/gocourse-signup.rb
+++ nginx-app/recipes/gocourse-signup.rb
@@ -1,9 +1,9 @@
 if is_aws()
-  deploy_dir = "/srv/www/signup/current/src/"
+  deploy_dir = "/srv/www/signup/current/build/"
   nginx_extras = ""
   domain_name = node["gocourse"]["domain"]["signup"]
 else
-  deploy_dir = "/vagrant_data/src/"
+  deploy_dir = "/vagrant_data/build/"
   domain_name = ""
   nginx_extras = "sendfile off;"
 end
