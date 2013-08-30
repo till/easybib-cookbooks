396b1ba5bf4cce15e9f9277c2159d2b1e44a02d3
till <till@php.net>
2013-08-30 14:58:10 +0200

    Enhancement: support groups
    Allows us to automatically populate the buddylist with certain groups.

---
+++ prosody/recipes/groups.rb
@@ -0,0 +1,27 @@
+include_recipe "prosody::service"
+
+directory "/var/prosody" do
+  owner "prosody"
+  group "prosody"
+  mode "0755"
+end
+
+groups_cfg = "/etc/prosody/conf.d/prosody_groups.lua"
+
+execute "load sharedgroups" do
+  command "echo 'groups_file = \"/var/prosody/sharedgroups.txt\"' >> #{groups_cfg}"
+  not_if do
+    File.exists?(groups_cfg)
+  end
+end
+
+template "/var/prosody/sharedgroups.txt" do
+  source "sharedgroups.txt.erb"
+  owner "prosody"
+  group "prosody"
+  mode "0644"
+  variables({
+    :groups => node['prosody']['groups'],
+  })
+  notifies :restart, resources( :service => "prosody")
+end
