bb234353145ef26b1e8c5c1571a82dcb5ece02d6
till <till@php.net>
2013-09-16 18:25:20 +0200

    Bugfix: ruby code

--- postfix/attributes/default.rb
+++ postfix/attributes/default.rb
@@ -1,4 +1,4 @@
-set_unless["postfix]             = {}
+set_unless["postfix"]            = {}
 set_unless["postfix"]["relay"]   = [{"user" => "foo@example.org", "pass" => "test123", "host" => "example.org"}]
 set_unless["postfix"]["aliases"] = ['postmaster', 'root']
 set_unless["sysop_email"]        = 'foo@example.org'
