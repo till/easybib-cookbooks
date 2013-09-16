1a97ede88ff10b44cdabc00b26d72f143e7f4756
till <till@php.net>
2013-09-16 23:06:23 +0200

    Enhancement: finish php_pecl provider

--- php/providers/pecl.rb
+++ php/providers/pecl.rb
@@ -1,9 +1,12 @@
 action :install do
 
   extension = new_resource.name
-  so_file   = "#{node["php-fpm"]["prefix"]}/lib/php/extensions/no-debug-non-zts-20090626/#{extension}.so"
+  so_file   = "#{new_resource.ext_dir}/#{extension}.so"
 
-  # TODO: new_resource.version
+  if !new_resource.version.empty?
+    extension = "#{extension}-#{new_resource.version}"
+  end
+
   execute "pecl install #{extension}" do
     not_if do
       ::File.exists?(so_file)
@@ -14,22 +17,60 @@ action :install do
 
 end
 
-action :setup do
+action :compile do
 
-  package  = new_resource.name
-  ini_file = "#{node["php-fpm"]["prefix"]}/etc/php/#{new_resource.name}.ini"
+  if new_resource.source_dir.empty?
+    raise "Missing 'source_dir'."
+  end
 
-  if package == "xdebug"
-    ::Chef::Log.debug("Need to use zend_extension")
-  else
-    execute "enable PHP extension #{package}" do
-      command "echo 'extension=#{package}.so' >> #{ini_file}"
-      not_if do
-        ::File.exists?(ini_file)
-      end
+  source_dir = new_resource.source_dir
+  if !::File.exists?(source_dir)
+    raise "The 'source_dir' does not exist: #{source_dir}"
+  end
+
+  extension = new_resource.name
+
+  configure = "./configure"
+
+  cflags = new_resource.cflags
+  if !cflags.nil? && !cflags.empty?
+    configure = "CFLAGS=\"#{cflags}\" #{configure}"
+  end
+
+  commands = [
+      "phpize",
+      configure,
+      "make",
+      "cp modules/#{extension}.so #{new_resource.ext_dir}",
+    ]
+
+    commands.each do |command|
+      Chef::Log.debug("Running #{command} in #{source_dir}")
+      cmd = ::Mixlib::ShellOut.new(command, :cwd => source_dir)
+      cmd.run_command
+      cmd.error!
     end
 
     new_resource.updated_by_last_action(true)
 
+end
+
+action :setup do
+
+  package  = new_resource.name
+  ini_file = "#{new_resource.prefix}/etc/php/#{new_resource.name}.ini"
+
+  if package == "xdebug"
+    ::Chef::Log.debug("Need to use zend_extension")
+    return
   end
+
+  execute "enable PHP extension #{package}" do
+    command "echo 'extension=#{package}.so' >> #{ini_file}"
+    not_if do
+      ::File.exists?(ini_file)
+    end
+  end
+
+  new_resource.updated_by_last_action(true)
 end
