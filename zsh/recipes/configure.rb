8aaaaf1fa007f38cb2d00f51dbbe62773338f2e0
till <till@php.net>
2013-09-27 15:26:23 +0200

    Enhancement: write everything in `env` into the zprofile

--- zsh/recipes/configure.rb
+++ zsh/recipes/configure.rb
@@ -1,15 +1,15 @@
 # this is for vagrant
 
-database_credentials = []
-if node["gocourse"]["database"]
-  database_credentials = node["gocourse"]["database"]
-end
+#database_credentials = []
+#if node["gocourse"]["database"]
+#  database_credentials = node["gocourse"]["database"]
+#end
 
 template "/etc/zsh/zprofile" do
   source "zprofile.erb"
   mode   "0755"
-  variables :vars => database_credentials
+  variables :vars => node["gocourse"]["env"]
   not_if do
-    database_credentials.empty?
+    node["gocourse"]["env"].nil? || node["gocourse"]["env"].empty?
   end
 end
