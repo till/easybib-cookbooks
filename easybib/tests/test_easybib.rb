a6d26b633ed0f6de84d9e6d7c4c64390495cd0f6
till <till@php.net>
2013-09-27 19:18:51 +0200

    Bugfix: bugfix my bugfix

--- easybib/tests/test_easybib.rb
+++ easybib/tests/test_easybib.rb
@@ -20,7 +20,7 @@ class TestEasyBib < Test::Unit::TestCase
     fake_node["env"]["database"]["app"].each do |k,v|
       assert_equal(
         build_nginx_config("app_#{k}", v),
-        "fastcgi_param APP_#{k} \"#{v}\";\n"
+        "fastcgi_param app_#{k} \"#{v}\";\n"
       )
     end
   end
