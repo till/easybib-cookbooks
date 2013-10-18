f5f0d247c079c1c7c45a6d92317e47ea6a5812a8
fh <fh-github@fholzhauer.de>
2013-10-18 13:21:42 +0200

    this did not work

--- easybib/recipes/opsworks-fixes.rb
+++ easybib/recipes/opsworks-fixes.rb
@@ -1,6 +1,6 @@
 node[:deploy].each do |application, deploy|
   if deploy[:home].nil? || deploy[:home] == "/home/www-data"
     Chef::Log.debug("home was empty or /home/www, changing it to /var/www")
-    default[:deploy][application][:home] = '/var/www'
+    node.default[:deploy][application][:home] = '/var/www'
   end
 end
