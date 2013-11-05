d73106f07f23487faf3603d3b83acf81e401b6fb
till <till@php.net>
2013-11-05 23:00:31 +0100

    Update: probe for available packages
     * use `dpkg -s` or `apt-cache showpgk` :S

--- php-apc/recipes/default.rb
+++ php-apc/recipes/default.rb
@@ -1,10 +1,16 @@
-find_package = Mixlib::ShellOut.new("dpkg -s php5-easybib-apcu")
-find_package.run_command
+include_recipe "apt::ppa"
+include_recipe "apt::easybib"
 
-if find_package.exitstatus == 0
-  package "php5-easybib-apcu"
-else
-  package "php5-easybib-apc"
+["php5-easybib-apcu", "php5-easybib-apc"].each do |package_name|
+  Chef::Log.debug("Trying #{package_name}")
+  package package_name do
+    action :install
+    only_if do
+      find_package = Mixlib::ShellOut.new("apt-cache showpkg #{package_name}")
+      find_package.run_command
+      find_package.exitstatus == 0 && !find_package.stdout.empty?
+    end
+  end
 end
 
 include_recipe "php-apc::configure"
--- php-suhosin/recipes/default.rb
+++ php-suhosin/recipes/default.rb
@@ -1,11 +1,10 @@
 include_recipe "apt::ppa"
 include_recipe "apt::easybib"
 
-check_cmd = Mixlib::ShellOut.new("dpkg -s php5-easybib-suhosin")
-check_cmd.run_command
-
 package "php5-easybib-suhosin" do
   only_if do
+    check_cmd = Mixlib::ShellOut.new("dpkg -s php5-easybib-suhosin")
+    check_cmd.run_command
     check_cmd.exitstatus == 0
   end
 end
