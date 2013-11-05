5bf3881a1fe60cc24d8104b2df89f0da335e0ff7
till <till@php.net>
2013-11-05 17:09:45 +0100

    Update: WIP only install available packages and fail over

--- php-apc/recipes/default.rb
+++ php-apc/recipes/default.rb
@@ -1,3 +1,10 @@
-package "php5-easybib-apc"
+find_package = Mixlib::ShellOut.new("dpkg -s php5-easybib-apcu")
+find_package.run_command
+
+if find_package.exitstatus == 0
+  package "php5-easybib-apcu"
+else
+  package "php5-easybib-apc"
+end
 
 include_recipe "php-apc::configure"
--- php-suhosin/recipes/default.rb
+++ php-suhosin/recipes/default.rb
@@ -1,6 +1,13 @@
 include_recipe "apt::ppa"
 include_recipe "apt::easybib"
 
-package "php5-easybib-suhosin"
+check_cmd = Mixlib::ShellOut.new("dpkg -s php5-easybib-suhosin")
+check_cmd.run_command
+
+package "php5-easybib-suhosin" do
+  only_if do
+    check_cmd.exitstatus == 0
+  end
+end
 
 include_recipe "php-suhosin::configure"
