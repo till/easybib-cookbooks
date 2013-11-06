cd6ce0e40591e29d08cd49a99e04843d5a274b12
fh <fh-github@fholzhauer.de>
2013-11-06 16:11:26 +0100

    debugging, this did not work

--- loggly/recipes/setup.rb
+++ loggly/recipes/setup.rb
@@ -7,12 +7,15 @@ if node["loggly"] && (node["loggly"]["domain"] != 'example')
   if is_aws()
     cluster_name   = get_cluster_name()
     logglydata << " tag=\"#{cluster_name}\""
+    Chef::Log.debug("Logglydata is: #{logglydata}")
     
     get_instance_roles().each do |layer|
       logglydata << " tag=\"#{cluster_name}\""
+      Chef::Log.debug("Logglydata is: #{logglydata}")
     end
   end
 
+  Chef::Log.debug("Logglydata is: #{logglydata}")
   template "/etc/rsyslog.d/10-loggly.conf" do
     source "10-loggly.conf.erb"
     variables(
