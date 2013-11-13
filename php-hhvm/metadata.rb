8f5c350eea5042caa90527964d2f2e882dd8a046
till <till@php.net>
2013-11-13 14:47:08 +0100

    Bugfix: kopierpaste

--- php-hhvm/metadata.rb
+++ php-hhvm/metadata.rb
@@ -1,8 +1,8 @@
-name              "php-apc"
+name              "php-hhvm"
 maintainer        "Till Klampaeckel"
 maintainer_email  "till@php.net"
 license           "BSD License"
-description       "Installs ext/apc for PHP"
+description       "Installs Facebook's HHVM"
 version           "0.1"
 
 supports 'ubuntu'
