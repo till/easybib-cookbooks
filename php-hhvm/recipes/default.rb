b578594e59991d1036b2caa43ac54a59c052458a
till <till@php.net>
2013-11-13 16:56:17 +0100

    Bugfix: fix random `apt-get update`-issues

--- php-hhvm/recipes/default.rb
+++ php-hhvm/recipes/default.rb
@@ -1,14 +1,12 @@
-execute "update_hhvm_apt" do
-  command "apt-get update -y"
-  action :nothing
-end
-
 execute "discover apt-repository" do
   command "echo deb #{node["php-hhvm"]["apt"]["repo"]} #{node["lsb"]["codename"]} main > #{node["php-hhvm"]["apt"]["file"]}"
   not_if do
     File.exists?(node["php-hhvm"]["apt"]["file"])
   end
-  notifies :run, "execute[update_hhvm_apt]"
+end
+
+execute "update_hhvm_apt" do
+  command "apt-get update -y --allow-unauthenticated"
 end
 
 apt_package "install hhvm" do
