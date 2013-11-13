04a8af570c5de8511117d0a5c9c70409c844fb29
till <till@php.net>
2013-11-13 14:35:38 +0100

    Bugfix: FC011, FC031, FC045

---
+++ php-hhvm/README.md
@@ -0,0 +1,7 @@
+# php-hhvm
+
+A recipe to install Facebook's HHVM on Ubuntu Precise (12.04).
+
+Currently not included:
+
+ * any kind of web-server setup
---
+++ php-hhvm/metadata.rb
@@ -0,0 +1,8 @@
+name              "php-apc"
+maintainer        "Till Klampaeckel"
+maintainer_email  "till@php.net"
+license           "BSD License"
+description       "Installs ext/apc for PHP"
+version           "0.1"
+
+supports 'ubuntu'
