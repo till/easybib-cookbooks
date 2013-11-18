47ff54a15518d97b33ca6d0e65f1b3cc9f9debf8
till <till@php.net>
2013-11-18 16:01:09 +0100

    Bugfix: syntax error (no idea how this fixes it, but it's gone)

--- php-phar/attributes/default.rb
+++ php-phar/attributes/default.rb
@@ -1,2 +1,7 @@
 default[:php_phar]           = {}
 default[:php_phar][:version] = "5.3.8"
+default[:php_phar][:config]  = {
+  "detect_unicode" => "Off",
+  "phar.readonly" => "Off",
+  "phar.require_hash" => "Off"
+}
