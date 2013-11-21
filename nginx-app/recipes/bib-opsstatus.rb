9f4edf87121c0069e284bac59e3184c1591634fc
fh <fh-github@fholzhauer.de>
2013-11-21 15:56:24 +0100

    setup for bib-opsstatus

---
+++ nginx-app/recipes/bib-opsstatus.rb
@@ -0,0 +1,25 @@
+if is_aws()
+  deploy_dir = "/srv/www/bib-opsstatus/current/web"
+else
+  deploy_dir = "/vagrant_data/web/"
+end
+
+config = "bib-opsstatus"
+
+template "/etc/nginx/sites-enabled/#{config}.conf" do
+  source "silex.conf.erb"
+  mode   "0755"
+  owner  node["nginx-app"]["user"]
+  group  node["nginx-app"]["group"]
+  variables(
+    :php_user    => node["php-fpm"]["user"],
+    :doc_root    => deploy_dir,
+    :access_log  => 'off',
+    :nginx_extra => node["nginx-app"]["extras"],
+    :default_router => node["nginx-app"]["default_router"],
+    :upstream => config,
+    :db_conf => "",
+    :domain_conf => ""
+  )
+  notifies :restart, "service[nginx]", :delayed
+end
