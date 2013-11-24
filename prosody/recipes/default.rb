0cfe16b7c6794e661d9811fff77de8e2f83c8c53
fh <fh-github@fholzhauer.de>
2013-11-24 14:46:38 +0100

    prosody 0.9.1 with better ciphers and syslog

--- prosody/recipes/default.rb
+++ prosody/recipes/default.rb
@@ -2,7 +2,7 @@ case node["platform"]
 when "debian", "ubuntu"
   include_recipe "prosody::apt"
 
-  ["lua-event", "liblua5.1-sec1", "lua-zlib"].each do |package_dep|
+  ["lua-event", "luasec-prosody", "lua-zlib"].each do |package_dep|
     package "installing #{package_dep} for prosody" do
       package_name package_dep
       action :install
@@ -10,12 +10,6 @@ when "debian", "ubuntu"
   end
 
   package_name = "prosody"
-when "freebsd", "gentoo"
-  package_name = "net-im/prosody"
-when "netbsd"
-  package_name = "chat/prosody"
-when "openbsd"
-  package_name = "net/prosody"
 default
   Chef::Log.error("Unsupported: #{node["platform"]}")
 end
@@ -23,6 +17,12 @@ end
 package "installing prosody" do
   package_name package_name
   action :install
+end
+
+#if it already is installed, do upgrade:
+package "updating prosody" do
+  package_name package_name
+  action :upgrade
 end
 
 include_recipe "prosody::configure"
