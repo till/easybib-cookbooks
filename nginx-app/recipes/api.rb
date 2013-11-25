e34c324993260cc5d0f083e04d12f3239634adf1
till <till@php.net>
2013-11-25 15:15:04 +0100

    Enhancement: support discover_api app

--- nginx-app/recipes/api.rb
+++ nginx-app/recipes/api.rb
@@ -1,5 +1,7 @@
-easybib_nginx "api" do
-  config_template "silex.conf.erb"
-  doc_root 'web'
-  notifies :restart, "service[nginx]", :delayed
+["api", "discover_api"].each do |app|
+  easybib_nginx "api" do
+    config_template "silex.conf.erb"
+    doc_root 'web'
+    notifies :restart, "service[nginx]", :delayed
+  end
 end
