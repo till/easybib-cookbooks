8d3823f6d1ffea9b457a37a3af12eccbcaf0ad69
fh <fh-github@fholzhauer.de>
2013-11-27 12:46:10 +0100

    lets test if we can override opsworks script

---
+++ opsworks_nodejs/libraries/nodejs_configuration.rb
@@ -0,0 +1,10 @@
+module OpsWorks
+  module NodejsConfiguration
+    def self.npm_install(app_name, app_config, app_root_path)
+      if File.exists?("#{app_root_path}/package.json")
+        Chef::Log.info("package.json detected. Running npm install.")
+        Chef::Log.info(`sudo su #{app_config[:user]} -c 'cd #{app_root_path} && npm install' 2>&1`)
+      end
+    end
+  end
+end
