c0684722138624312df3fcd1018a553195e600e7
fh <fh-github@fholzhauer.de>
2013-11-27 14:20:59 +0100

    reverting last try, did not work

--- opsworks_nodejs/libraries/nodejs_configuration.rb
+++ opsworks_nodejs/libraries/nodejs_configuration.rb
@@ -1,16 +1,7 @@
 module OpsWorks
   module NodejsConfiguration
     def self.npm_install(app_name, app_config, app_root_path)
-      
       if File.exists?("#{app_root_path}/package.json")
-       
-        directory "~#{app_config[:user]}" do
-          owner app_config[:user]
-          mode "0755"
-          action :create
-          recursive true
-        end
-        
         Chef::Log.info("package.json detected. Running npm install.")
         Chef::Log.info(`sudo su #{app_config[:user]} -c 'cd #{app_root_path} && npm install' 2>&1`)
       end
