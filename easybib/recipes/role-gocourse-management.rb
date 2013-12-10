7064b070fa8f237a4521bdbff867960201993488
fh <fh-github@fholzhauer.de>
2013-12-10 12:06:29 +0100

    Adding env support for non login shells

--- easybib/recipes/role-gocourse-management.rb
+++ easybib/recipes/role-gocourse-management.rb
@@ -2,6 +2,7 @@ include_recipe "easybib::role-phpapp"
 
 include_recipe "php-intl"
 
+include_recipe "bash::configure"
+include_recipe "bash::bashrc"
 include_recipe "deploy::gocourse-management"
 include_recipe "nginx-app::gocourse-management"
-include_recipe "bash::configure"
