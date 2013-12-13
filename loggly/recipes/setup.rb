0007c5023e9c246a433d1a2cd5446fe0bd6e3fe0
fh <fh-github@fholzhauer.de>
2013-12-13 09:03:03 +0100

    Added rsyslogd gnutls requirement

--- loggly/recipes/setup.rb
+++ loggly/recipes/setup.rb
@@ -1,12 +1,11 @@
 gem_package "json"
 
-require "easybib"
 include_recipe "rsyslogd"
 
 if node["loggly"] && (node["loggly"]["token"] != 'example')
 
   logglydata = node["loggly"]["token"]
-  
+
   if is_aws()
     cluster_name   = get_cluster_name().gsub(/\W/,'_')
     logglydata << " tag=\\\"stack.#{cluster_name}\\\""
@@ -24,19 +23,21 @@ if node["loggly"] && (node["loggly"]["token"] != 'example')
     mode "0644"
     notifies :restart, "service[rsyslog]", :delayed
   end
-  
+
   #clean up old location
   file "/etc/rsyslog.d/10-loggly.conf" do
     action :delete
     only_if { File.exists?('/etc/rsyslog.d/10-loggly.conf') }
   end
-  
+
   template "/etc/rsyslog.d/11-filewatcher.conf" do
     source "11-filewatcher.conf.erb"
     mode "0644"
     notifies :restart, "service[rsyslog]", :delayed
   end
-  
+
+  package "rsyslogd-gnutls"
+
   cookbook_file "/etc/ssl/certs/loggly.full.pem" do
     source "loggly.full.pem"
     owner "root"
