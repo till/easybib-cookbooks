0e094bc5c553d956d17969d383a5635e21d47f54
fh <fh-github@fholzhauer.de>
2013-12-13 12:06:18 +0100

    Debugging wrong checkout

--- haproxy/attributes/default.rb
+++ haproxy/attributes/default.rb
@@ -19,6 +19,6 @@ default["haproxy"]["ctl"] = {}
 default["haproxy"]["ctl"]["version"] = "1.1.0"
 default["haproxy"]["ctl"]["base_path"] = "/usr/local/share"
 default["haproxy"]["hatop"] = {}
-default["haproxy"]["hatop"]["version"] = "v0.7.7"
+default["haproxy"]["hatop"]["version"] = "tags/v0.7.7"
 default["haproxy"]["hatop"]["base_path"] = "/usr/local/share"
 default["haproxy"]["log_dir"] = "/mnt/var/log/haproxy"
