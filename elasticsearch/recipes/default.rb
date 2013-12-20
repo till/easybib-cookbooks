89f8b1115cb3ac8ce0feb197c59bb78990c7c9b8
fh <fh-github@fholzhauer.de>
2013-12-20 12:28:14 +0100

    Cleaned Recipe

--- elasticsearch/recipes/default.rb
+++ elasticsearch/recipes/default.rb
@@ -1,16 +1,20 @@
 package "openjdk-7-jre-headless"
 
-remote_file "#{Chef::Config[:file_cache_path]}/elasticsearch-#{node[:elasticsearch][:version]}.deb" do
-  source "#{node[:elasticsearch][:mirror]}/elasticsearch-#{node[:elasticsearch][:version]}.deb"
+deb_name = "elasticsearch-" + node[:elasticsearch][:version] + ".deb"
+deb_url  = node[:elasticsearch][:mirror] + "/" + deb_name
+deb_path = Chef::Config[:file_cache_path] + "/" + deb_name
+
+remote_file deb_path do
+  source deb_url
   not_if do
-    File.exists?("#{Chef::Config[:file_cache_path]}/elasticsearch-#{node[:elasticsearch][:version]}.deb")
+    File.exists?(deb_path)
   end
 end
 
 dpkg_package "elasticsearch" do
-  source "#{Chef::Config[:file_cache_path]}/elasticsearch-#{node[:elasticsearch][:version]}.deb"
+  source  deb_path
   version node[:elasticsearch][:version]
-  action :install
+  action  :install
 end
 
 include_recipe "elasticsearch::service"
