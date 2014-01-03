8ace5872f7aeca56fc39b7d1d1ca321ddaed3f5c
fh <fh-github@fholzhauer.de>
2014-01-03 15:52:22 +0100

    First shot at a cronjob

--- deploy/recipes/crossref-collector.rb
+++ deploy/recipes/crossref-collector.rb
@@ -12,5 +12,15 @@ node['deploy'].each do |application, deploy|
     deploy_data deploy
     app application
   end
-
+  
+  cron "trigger crossref collector cronjob " do
+    minute "0"
+    user deploy["user"]
+    home node["deploy"][application]["home"]
+    mailto node["sysop_email"]
+    command "cd /srv/www/#{application}/current && ./bin/crossref"
+    only_if do
+      File.exists?("/srv/www/#{application}/current/bin/crossref")
+    end
+  end
 end
