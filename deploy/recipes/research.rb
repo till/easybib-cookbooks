89f7dccc20617fcee325b2bd5001aa836bace24c
fh <fh-github@fholzhauer.de>
2014-01-06 15:12:27 +0100

    First shot at research role

--- deploy/recipes/research.rb
+++ deploy/recipes/research.rb
@@ -1,52 +1,31 @@
 include_recipe "php-fpm::service"
-
-instance_roles = get_instance_roles()
-cluster_name   = get_cluster_name()
+include_recipe "nginx-app::service"
 
 node["deploy"].each do |application, deploy|
 
   Chef::Log.info("deploy::research - app: #{application}, role: #{instance_roles}")
-
   Chef::Log.info("Deploying as user: #{deploy["user"]} and #{deploy["group"]}")
 
   case application
   when 'easybib_solr_server'
-    next unless cluster_name == 'Research Cloud'
-    next unless instance_roles.include?('easybibsolr')
-
+    next unless allow_deploy(application, 'easybib_solr_server', 'easybibsolr')
     Chef::Log.debug('deploy::research - Setting deploy for SOLR SERVER')
-
     # fix this: deploy to instance storage
     deploy["deploy_to"]       = "/solr/apache-solr-1.4.1-compiled"
     deploy["restart_command"] = ""
-
-    deploy["user"] = "root"
-
+    deploy["user"]            = "root"
   when 'ebim2'
-    next unless cluster_name == 'Research Cloud'
-    if !instance_roles.include?('easybibsolr') && !instance_roles.include?('ebim2')
-      next
-    end
+    next unless ( allow_deploy(application, 'ebim2') || allow_deploy(application, 'easybibsolr') )
     deploy["restart_command"] = ""
-
   when 'ebim2_research_importer'
-    next unless cluster_name == 'Research Cloud'
-    if !instance_roles.include?('ebim2') && !instance_roles.include?('easybibsolr')
-      next
-    end
+    next unless ( allow_deploy( application, 'ebim2_research_importer', 'ebim2') || allow_deploy(application, 'ebim2_research_importer', 'easybibsolr') )
     deploy["restart_command"] = ""
-
   when 'research_app'
-    next unless cluster_name == 'Research Cloud'
-    next unless instance_roles.include?('nginxphpapp')
-
+    next unless allow_deploy(application, 'research_app', 'nginxphpapp')
   when 'citationbackup'
-    next unless cluster_name == 'Research Cloud'
-    next unless instance_roles.include?('backup')
-
+    next unless allow_deploy(application, 'citationbackup', 'backup')
   when 'gearmanworker'
-    next unless instance_roles.include?('gearman-worker')
-
+    next unless allow_deploy(application, 'gearmanworker', 'gearman-worker')
   else
     Chef::Log.info("deploy::research - #{application} (in #{cluster_name}) skipped")
     next
@@ -60,18 +39,9 @@ node["deploy"].each do |application, deploy|
     path  deploy["deploy_to"]
   end
 
-  opsworks_deploy do
+  easybib_deploy do
     deploy_data deploy
     app application
-  end
-
-  service "php-fpm-#{application}" do
-    service_name "php-fpm"
-    supports :reload => true
-    action :reload
-    only_if do
-      application == 'research_app'
-    end
   end
 
   next unless application == 'ebim2'
