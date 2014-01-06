2077127bf5fa71068c4ef6f42ee7e604b2a35085
fh <fh-github@fholzhauer.de>
2014-01-06 18:26:04 +0100

    We will first only care about solr and research app

--- deploy/recipes/research.rb
+++ deploy/recipes/research.rb
@@ -14,18 +14,8 @@ node["deploy"].each do |application, deploy|
     deploy["deploy_to"]       = "/solr/apache-solr-1.4.1-compiled"
     deploy["restart_command"] = ""
     deploy["user"]            = "root"
-  when 'ebim2'
-    next unless ( allow_deploy(application, 'ebim2') || allow_deploy(application, 'easybibsolr') )
-    deploy["restart_command"] = ""
-  when 'ebim2_research_importer'
-    next unless ( allow_deploy( application, 'ebim2_research_importer', 'ebim2') || allow_deploy(application, 'ebim2_research_importer', 'easybibsolr') )
-    deploy["restart_command"] = ""
   when 'research_app'
     next unless allow_deploy(application, 'research_app', 'nginxphpapp')
-  when 'citationbackup'
-    next unless allow_deploy(application, 'citationbackup', 'backup')
-  when 'gearmanworker'
-    next unless allow_deploy(application, 'gearmanworker', 'gearman-worker')
   else
     Chef::Log.info("deploy::research - #{application} (in #{cluster_name}) skipped")
     next
@@ -43,25 +33,4 @@ node["deploy"].each do |application, deploy|
     deploy_data deploy
     app application
   end
-
-  next unless application == 'ebim2'
-
-  include_recipe "deploy::ebim2"
-
-  base_dir = deploy["deploy_to"]
-  app_dir  = "#{base_dir}/vendor/GearmanManager"
-  etc_dir  = "#{base_dir}/etc/gearman"
-
-  link "/usr/local/bin/gearman-manager" do
-    to "#{app_dir}/pecl-manager.php"
-  end
-
-  link "/etc/gearman-manager" do
-    to etc_dir
-  end
-
-  link "/etc/init.d/gearman-manager" do
-    to "#{base_dir}/bin/gearman-manager.initd"
-  end
-
 end
