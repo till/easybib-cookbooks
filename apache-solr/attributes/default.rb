10d7ee4aa5fc77428e872ce0438972db3821022c
fh <fh-github@fholzhauer.de>
2014-01-08 10:55:16 +0100

    3.5.0 is no longer available. Since we dont use solr anywhere currently, I updated to the most recent version

--- apache-solr/attributes/default.rb
+++ apache-solr/attributes/default.rb
@@ -1,11 +1,11 @@
 default[:apache_solr]            = {}
-default[:apache_solr][:version]  = "3.5.0"
+default[:apache_solr][:version]  = "4.6.0"
 default[:apache_solr][:user]     = "root"
 default[:apache_solr][:group]    = "root"
 default[:apache_solr][:base_dir] = "/opt/apache"
 default[:apache_solr][:log_file] = "/var/log/apache-solr.log"
 
-set_unless[:apache_solr][:mirror] = "http://apache.imsam.info/lucene/solr"
+set_unless[:apache_solr][:mirror] = "http://www.carfab.com/apachesoftware/lucene/solr/"
 set_unless[:apache_solr][:app]    = "easybib-test"
 
 set_unless[:apache_solr][:memory]       = {}
