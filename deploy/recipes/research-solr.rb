0a17582189c32e9ff984307c5b6416f72477dcdf
fh <fh-github@fholzhauer.de>
2014-01-12 23:20:52 +0100

    Forgot git add

---
+++ deploy/recipes/research-solr.rb
@@ -0,0 +1,32 @@
+include_recipe "php-fpm::service"
+include_recipe "nginx-app::service"
+
+node["deploy"].each do |application, deploy|
+
+  Chef::Log.info("deploy::research-solr - app: #{application}")
+  Chef::Log.info("Deploying as user: #{deploy["user"]} and #{deploy["group"]}")
+
+  next unless allow_deploy(application, 'research_solr', 'easybib_solr_server')
+
+  Chef::Log.info("deploy::research-solr - Deployment started.")
+
+  opsworks_deploy_dir do
+    user  deploy["user"]
+    group deploy["group"]
+    path  deploy["deploy_to"]
+  end
+
+  easybib_deploy application do
+    deploy_data deploy
+    app application
+  end
+
+  execute "copy config from git to solr basedir" do
+    cwd     node["apache_solr"]["base_dir"]
+    command "cp -R #{node["apache_solr"]["config_source_dir"]}/* #{node["apache_solr"]["base_dir"]}/solr/"
+    only_if do
+      application == 'research_solr'
+    end
+    notifies :restart, "service[apache-solr]"
+  end
+end
