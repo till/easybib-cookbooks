61ce45c4dc056249aa4802f6b210d03c23bab414
fh <fh-github@fholzhauer.de>
2014-01-14 16:38:16 +0100

    Cleanups, ebim2 is now longer around

--- deploy/recipes/ebim2.rb
+++
@@ -1,5 +0,0 @@
-cron "run collect stats every hour" do
-  minute  "23"
-  user "www-data"
-  command "/usr/local/bin/php /srv/www/ebim2/current/bin/ebim2 cron-collect-stats >> /var/log/ebim-stats.log 2>&1"
-end
