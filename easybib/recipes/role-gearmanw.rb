a305f9a27168e2a6e00b75f66fa7ccc391f7fbf8
fh <fh-github@fholzhauer.de>
2014-01-16 09:34:52 +0100

    Simplify Gearmanw Role, fixes easybib/issues#446

--- easybib/recipes/role-gearmanw.rb
+++ easybib/recipes/role-gearmanw.rb
@@ -1,15 +1,7 @@
-include_recipe "easybib::setup"
-include_recipe "loggly::setup"
-include_recipe "nginx-app::server"
-include_recipe "php-fpm"
+include_recipe "easybib::role-phpapp"
+
 include_recipe "php-mysqli::configure"
 include_recipe "unfuddle-ssl-fix::install"
 include_recipe "php-gearman"
-include_recipe "php-phar"
 include_recipe "php-posix"
-include_recipe "composer::configure"
 include_recipe "deploy::easybib"
-
-if is_aws()
-  include_recipe "newrelic"
-end
