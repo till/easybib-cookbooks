0581e46a9c9718ff03c8cb56102817b01f024059
Richard Wossal <richard@r-wos.org>
2014-01-29 16:32:01 +0100

    fix: gearman manager config file location on vagrant

--- easybib/recipes/role-gearmanw.rb
+++ easybib/recipes/role-gearmanw.rb
@@ -6,3 +6,15 @@ include_recipe "php-gearman"
 include_recipe "php-posix"
 include_recipe "deploy::easybib"
 include_recipe "pecl-manager::vagrant"
+
+unless is_aws
+  gearmanconf_root_dir = ::File.expand_path(node['easybib_deploy']['gearman_root'])
+  import_file_path = "#{gearmanconf_root_dir}/deploy/#{node['easybib_deploy']['gearman_file']}"
+
+  pecl_manager_script "Setting up Pecl Manager Script for vagrant" do
+    dir gearmanconf_root_dir
+    envvar_file import_file_path
+    envvar_json_source node['easybib_deploy']['env_source']
+    only_if { ::File.exists?(import_file_path) }
+  end
+end
