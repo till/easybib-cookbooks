399e49ecd41f712d672df4ff8a5343332a52ee81
fh <fh-github@fholzhauer.de>
2014-01-29 09:42:46 +0100

    Renamed envvars to envvar_json for better variable naming, style fixes

--- pecl-manager/providers/script.rb
+++ pecl-manager/providers/script.rb
@@ -1,22 +1,26 @@
 action :create do
   root_dir = new_resource.dir
 
-  # TODO we should add a "if file exists" check here
+  # TODO: we should add a "if file exists" check here
 
-  unless new_resource.envvar_json_source.nil
-    global_envvars = ::EasyBib.get_env_for_shell(new_resource.envvar_json_source, node)
+  Chef::Log.debug("Pecl-Manager: Importing file #{new_resource.envvar_file} in Startscript")
+
+  if new_resource.envvar_json_source.nil
+    Chef::Log.debug("Pecl-Manager: No source for json env found")
+  else
+    envvar_json = ::EasyBib.get_env_for_shell(new_resource.envvar_json_source, node)
   end
-  
+
   template "/etc/init.d/pecl-manager" do
     cookbook "pecl-manager"
     source "init.d.erb"
-    mode "0700"
+    mode  "0700"
     owner "root"
     group "root"
     variables(
       :dir => root_dir,
       :envvar_file => new_resource.envvar_file,
-      :envvars => global_envvars
+      :envvar_json => envvar_json
     )
   end
 
