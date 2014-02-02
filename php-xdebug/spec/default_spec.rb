663acc45b49cff4e354a72f8385db36aeaed2bd5
till <till@php.net>
2014-02-03 00:58:32 +0100

    Enhancement: add a test to confirm this works
     * checks for php5-easybib-xdebug (with correct node attribute)
     * asserts xdebug-settings.ini is installed
    
    Related: getcourse/issues#3

---
+++ php-xdebug/spec/default_spec.rb
@@ -0,0 +1,21 @@
+require 'chefspec'
+
+describe 'php-apc::configure' do
+
+  let (:chef_run) do
+    ChefSpec::Runner.new do |node|
+      node.default["apt"] = {}
+      node.default.apt["easybib"] = {}
+      node.default.apt.easybib["ppa"] = "ppa:easybib/php55"
+    end.converge(described_recipe)
+  end
+
+  it "creates xdebug-settings.ini" do
+    expect(chef_run).to render_file("/opt/easybib/etc/php/xdebug-settings.ini")
+  end
+
+  it "installs php5-easybib-xdebug" do
+    expect(chef_run).to install_package("php5-easybib-xdebug")
+  end
+
+end
