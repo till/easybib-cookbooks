a2df484c42b3f0f5dc9b221383b9dea490b49f3d
fh <fh-github@fholzhauer.de>
2014-02-18 12:06:49 +0100

    Preparations for private/public satis separation

--- deploy/attributes/default.rb
+++ deploy/attributes/default.rb
@@ -1,7 +1,4 @@
 default["ssl-deploy"] = {}
 default["ssl-deploy"]["directory"] = "/etc/nginx/ssl"
 
-default["s3-syncer"]["path"] = "/mnt/srv/www/s3-syncer/"
-default["s3-syncer"]["source"] = "https://github.com/easybiblabs/s3-syncer/archive/master.tar.gz"
-
 default["bibcd"]["apps"] = {}
