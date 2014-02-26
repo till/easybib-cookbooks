fcba7c1a0227369c3a69b5dbd73e603195e7b872
Richard Wossal <richard@r-wos.org>
2014-02-26 16:57:17 +0100

    updated gearman worker script setup on vagrant VM

--- pecl-manager/recipes/vagrant.rb
+++ pecl-manager/recipes/vagrant.rb
@@ -1,8 +1,8 @@
 unless is_aws
-  import_file_path = "/vagrant_data/deploy/#{node['easybib_deploy']['gearman_file']}"
+  import_file_path = "#{node['easybib_deploy']['gearman_file']}/deploy/#{node['easybib_deploy']['gearman_file']}"
 
   pecl_manager_script "Setting up Pecl Manager Script for vagrant" do
-    dir "/vagrant_data"
+    dir "/vagrant_www"
     envvar_file import_file_path
     envvar_json_source node['easybib_deploy']['env_source']
     only_if { ::File.exists?(import_file_path) }
