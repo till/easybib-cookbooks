b328d90b3b1077214125b2e9a8133947c722e469
fh <fh-github@fholzhauer.de>
2014-03-10 08:33:50 +0100

    Do not use handlers in vagrant

--- chef_handler/recipes/dummy.rb
+++ chef_handler/recipes/dummy.rb
@@ -1,13 +1,13 @@
 include_recipe "chef_handler"
 
-Chef::Log.info("#{node['chef_handler']['handler_path']}/dummy_handler.rb")
+if is_aws
+  chef_handler "Chef::Handler::DummyHandler" do
+    source "#{node['chef_handler']['handler_path']}/dummy_handler.rb"
+    action :nothing
+  end.run_action(:enable)
 
-chef_handler "Chef::Handler::DummyHandler" do
-  source "#{node['chef_handler']['handler_path']}/dummy_handler.rb"
-  action :nothing
-end.run_action(:enable)
-
-chef_handler "Chef::Handler::Dummy2Handler" do
-  source "#{node['chef_handler']['handler_path']}/dummy2_handler.rb"
-  action :enable
+  chef_handler "Chef::Handler::Dummy2Handler" do
+    source "#{node['chef_handler']['handler_path']}/dummy2_handler.rb"
+    action :enable
+  end
 end
