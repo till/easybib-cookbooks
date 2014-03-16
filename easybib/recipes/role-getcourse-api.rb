939296b743f437850e2ded8ef4a6792d5598fc3f
till <till@php.net>
2014-03-16 20:01:52 +0100

    Enhancement: make snooze available

--- easybib/recipes/role-getcourse-api.rb
+++ easybib/recipes/role-getcourse-api.rb
@@ -6,6 +6,7 @@ include_recipe "php-intl"
 include_recipe "php-gearman"
 include_recipe "php-mysqli::configure"
 
+include_recipe "snooze"
 include_recipe "bash::bashrc"
 include_recipe "bash::configure"
 include_recipe "deploy::getcourse-api"
