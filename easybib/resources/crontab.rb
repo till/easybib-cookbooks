970f0229d2feb4b3d2adbd53a5a0297dc007c0ff
Florian Holzhauer <fh-github@fholzhauer.de>
2014-03-30 14:35:23 +0200

    Pleasing rubocop

--- easybib/resources/crontab.rb
+++ easybib/resources/crontab.rb
@@ -3,4 +3,4 @@ actions :create
 default_action :create
 
 attribute :app, :default => ""
-attribute :crontab_file, :kind_of => String, :default => "" 
+attribute :crontab_file, :kind_of => String, :default => ""
