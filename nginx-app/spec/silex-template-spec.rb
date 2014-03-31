6cea051d2aa23b49fd56ba62c31b993adc282654
Florian Holzhauer <fh-github@fholzhauer.de>
2014-03-31 14:25:37 +0200

    Tests & Refactoring, preparation for easybib/issues#994

---
+++ nginx-app/spec/silex-template-spec.rb
@@ -0,0 +1,41 @@
+require_relative "spec_helper"
+
+describe 'silex-config-template' do
+
+  let(:cookbook_paths) do
+    [
+      File.expand_path("#{File.dirname(__FILE__)}/../../"),
+      File.expand_path("#{File.dirname(__FILE__)}/")
+    ]
+  end
+
+  let(:runner)   { ChefSpec::Runner.new(:cookbook_path => cookbook_paths) }
+  let(:chef_run) { runner.converge("fixtures::silex-template") }
+  let(:node)     { runner.node }
+
+  describe "enabled access log" do
+    before do
+      node.set["testdata"]["access_log"] = "/tmp.log"
+    end
+
+    it "sets correct access log path" do
+      expect(chef_run).to render_file('/tmp/config.conf').with_content(/access_log \/tmp.log;/)
+    end
+  end
+
+  describe "no routes enabled or disabled" do
+    before do
+      node.set["testdata"]["routes_enabled"] = nil
+      node.set["testdata"]["routes_denied"]  = nil
+    end
+
+    it "does not deny any routes" do
+      expect(chef_run).not_to render_file('/tmp/config.conf').with_content(/deny all;/)
+    end
+
+    it "does enable all routes" do
+      expect(chef_run).not_to render_file('/tmp/config.conf').with_content(%r[location / {\n\w*try_files $uri $uri/ @site;\n\w*}])
+    end
+  end
+
+end
---
+++ nginx-app/spec/spec_helper.rb
@@ -0,0 +1,7 @@
+require "chefspec"
+
+RSpec.configure do |c|
+  c.platform = 'ubuntu'
+  c.version = '12.04'
+#  c.log_level = :debug
+end
