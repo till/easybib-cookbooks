4a56526416624e0806b110cbefbd7db651ca1f9f
Florian Holzhauer <fh-github@fholzhauer.de>
2014-04-01 11:53:01 +0200

    routes_enabled for silex

--- nginx-app/spec/silex-template-spec.rb
+++ nginx-app/spec/silex-template-spec.rb
@@ -12,6 +12,7 @@ describe 'silex-config-template' do
   let(:runner)   { ChefSpec::Runner.new(:cookbook_path => cookbook_paths) }
   let(:chef_run) { runner.converge("fixtures::silex-template") }
   let(:node)     { runner.node }
+  let(:config_filename) { '/tmp/config.conf' }
 
   describe "enabled access log" do
     before do
@@ -19,7 +20,7 @@ describe 'silex-config-template' do
     end
 
     it "sets correct access log path" do
-      expect(chef_run).to render_file('/tmp/config.conf').with_content(/access_log \/tmp.log;/)
+      expect(chef_run).to render_file(config_filename).with_content(/access_log \/tmp.log;/)
     end
   end
 
@@ -30,12 +31,35 @@ describe 'silex-config-template' do
     end
 
     it "does not deny any routes" do
-      expect(chef_run).not_to render_file('/tmp/config.conf').with_content(/deny all;/)
+      expect(chef_run).not_to render_file(config_filename).with_content(some_routes_denied)
     end
-
-    it "does enable all routes" do
-      expect(chef_run).to render_file('/tmp/config.conf').with_content(%r!location / {\n.*try_files \$uri \$uri/ @site;\n.*}!)
+    it "does route / to php" do
+      expect(chef_run).to render_file(config_filename).with_content(slash_is_enabled)
     end
   end
+
+  describe "some routes enabled" do
+    before do
+      node.set["testdata"]["routes_enabled"] = ['/some/route']
+      node.set["testdata"]["routes_denied"]  = nil
+    end
+
+    it "does not deny any routes" do
+      expect(chef_run).not_to render_file(config_filename).with_content(some_routes_denied)
+    end
+
+    it "does not route / to php" do
+      expect(chef_run).not_to render_file(config_filename).with_content(slash_is_enabled)
+    end
+
+    it "does redirect / to another location" do
+      expect(chef_run).to render_file(config_filename).with_content(slash_is_redirected)
+    end
+  end
+
+  let(:slash_is_redirected)          { %r!location = / {\s* return 301 http://easybib.com/company/contact;\s*}! }
+  let(:default_route_for_some_route) { %r!location ~ /(.*) {\s*location / {\s*try_files $uri $uri/ @site;\s*}! }
+  let(:slash_is_enabled)             { %r!location = / {\s*try_files @site @site;\s*}! }
+  let(:some_routes_denied)           { %r!location ~ /(.*) {\s*deny all;\s*}! }
 
 end
