11636c3725b793db9e01afbcf5ba236f77a8bbcd
Florian Holzhauer <fh-github@fholzhauer.de>
2014-04-04 12:17:09 +0200

    Bugfix: Function in a recipe is not available to a template statmenet

--- php-suhosin/recipes/configure.rb
+++ php-suhosin/recipes/configure.rb
@@ -6,11 +6,9 @@ template "#{etc_dir}/suhosin.ini" do
   source   "suhosin.ini.erb"
   mode     "0644"
   notifies :reload, "service[php-fpm]", :delayed
-  only_if is_suhosin_available?
-end
-
-def is_suhosin_available
-  check_cmd = Mixlib::ShellOut.new("dpkg -s php5-easybib-suhosin")
-  check_cmd.run_command
-  check_cmd.exitstatus == 0
+  only_if do
+    check_cmd = Mixlib::ShellOut.new("dpkg -s php5-easybib-suhosin")
+    check_cmd.run_command
+    check_cmd.exitstatus == 0
+  end
 end
