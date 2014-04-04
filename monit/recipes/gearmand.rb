5679787b6f327172f558e3ba757d6c1dfe85f4f7
Florian Holzhauer <fh-github@fholzhauer.de>
2014-04-04 14:31:29 +0200

    Feature: Monitor gearmand with monit

---
+++ monit/recipes/gearmand.rb
@@ -0,0 +1,15 @@
+include_recipe "monit::service"
+
+template "/etc/monit/conf.d/gearmand.monitrc" do
+  source "gearmand.monit.erb"
+  mode   "0644"
+  owner  "root"
+  group  "root"
+  variables(
+    'pid_file' => '/var/run/gearmand-easybib/gearmand-easybib.pid',
+    'app_name' => 'gearmand',
+    'group'    => 'gearman',
+    'init'     => '/etc/init.d/gearmand-easybib'
+  )
+  notifies :restart, "service[monit]"
+end
