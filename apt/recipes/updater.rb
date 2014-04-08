d4987609f880e51e4c6f8cdca4a09126b6847227
Florian Holzhauer <fh-github@fholzhauer.de>
2014-04-08 08:46:40 +0200

    Adding an update-mechanism for a package

---
+++ apt/recipes/updater.rb
@@ -0,0 +1,8 @@
+execute 'apt-get update' do
+  command 'apt-get update'
+end
+
+execute 'apt-get install package' do
+  command 'apt-get install -y #{node["apt"]["upgrade-package"]}'
+  not_if node["apt"]["upgrade-package"].nil?
+end
