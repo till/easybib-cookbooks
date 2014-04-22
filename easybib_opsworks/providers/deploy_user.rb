ce64633857a704d9fc988a225d10a5803cd7d280
Florian Holzhauer <fh-github@fholzhauer.de>
2014-04-22 08:34:05 +0200

    Feature: Adding debug logging

--- easybib_opsworks/providers/deploy_user.rb
+++ easybib_opsworks/providers/deploy_user.rb
@@ -1,5 +1,7 @@
 action :run do
 
+  Chef::Log.debug("I am in easybib_opsworks_deploy_user. Deploy_data is #{new_resource.deploy_data}.")
+
   d = opsworks_deploy_user do
     deploy_data new_resource.deploy_data
   end
