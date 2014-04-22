7dacb7e668122f563f806fcd6ee07857bb6896bb
Florian Holzhauer <fh-github@fholzhauer.de>
2014-04-22 08:44:13 +0200

    Further debugging

--- easybib_opsworks/providers/deploy_user.rb
+++ easybib_opsworks/providers/deploy_user.rb
@@ -1,7 +1,5 @@
 action :run do
 
-  Chef::Log.debug("I am in easybib_opsworks_deploy_user. Deploy_data is #{new_resource.deploy_data}.")
-
   d = opsworks_deploy_user do
     deploy_data new_resource.deploy_data
   end
--- easybib_opsworks/resources/deploy_user.rb
+++ easybib_opsworks/resources/deploy_user.rb
@@ -2,4 +2,4 @@ actions :run
 
 default_action :run
 
-attribute :deploy_data
+attribute :deploy_data, :kind_of => Array, :required => true
