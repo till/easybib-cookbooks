6771ebaeb16109dcf58273e3545e68afd1511e79
Florian Holzhauer <fh-github@fholzhauer.de>
2014-05-06 17:49:54 +0200

    Bugfix: Use our own config file
    fixes easybib/issues#1332

--- newrelic/recipes/server-monitor.rb
+++ newrelic/recipes/server-monitor.rb
@@ -3,15 +3,27 @@ include_recipe "newrelic::service"
 execute "newrelic-license" do
   command "nrsysmond-config --set license_key=#{node["newrelic"]["license"]}"
   action :nothing
-  notifies :create, "template[/etc/newrelic/nrsysmond.cfg]", :immediately
+  notifies :create, "template[/etc/newrelic/nrsysmond.easybib.cfg]", :immediately
   not_if do
     node["newrelic"]["license"].empty?
   end
 end
 
+template "/etc/default/newrelic-sysmond" do
+  source "nrsysmond.default.erb"
+  owner "root"
+  group "newrelic"
+  mode "0640"
+end
+
+#easybib/issues#1332
+file "/etc/newrelic/nrsysmond.cfg" do
+  action :delete
+end
+
 host_name = "#{node["hostname"]}.#{get_cluster_name.gsub(/\s+/, "-").strip.downcase}"
 
-template "/etc/newrelic/nrsysmond.cfg" do
+template "/etc/newrelic/nrsysmond.easybib.cfg" do
   source "nrsysmond.cfg.erb"
   owner "root"
   group "newrelic"
