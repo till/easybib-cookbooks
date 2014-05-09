f5e3a6725c76228d9060865bfbaabedb9756461f
till <till@php.net>
2014-05-09 17:12:02 +0200

    Enhancement: add a few more assertions

--- php/tests/test_config.rb
+++ php/tests/test_config.rb
@@ -3,16 +3,23 @@ require 'chef'
 require File.join(File.dirname(__FILE__), '../libraries', 'config.rb')
 
 class TestConfig < Test::Unit::TestCase
+
+  # ensures a leading extension name is stripped
   def test_get_directives
 
     directives = {
-      "opcache.memory_consumption" => 10
+      "opcache.memory_consumption" => 10,
+      "opcache.super_duper" => true,
+      "no_prefix" => 1
     }
 
     config = ::Php::Config.new("opcache", directives)
 
     config_directives = config.get_directives
     assert_equal(false, config_directives.empty?)
-    assert_equal(1, config_directives.length)
+    assert_equal(directives.length, config_directives.length)
+    assert_equal(config_directives["memory_consumption"], 10)
+    assert_equal(config_directives["super_duper"], true)
+    assert_equal(config_directives["no_prefix"], 1)
   end
 end
