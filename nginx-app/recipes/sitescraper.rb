4322efac077b99355ed79c9fc98ba19a9aa493cd
till <till@php.net>
2014-05-12 15:20:51 +0200

    Enhancement: inject access_log
    Related: easybib/issues#1083

--- nginx-app/recipes/sitescraper.rb
+++ nginx-app/recipes/sitescraper.rb
@@ -1,11 +1,11 @@
 config = "sitescraper"
 
+access_log = node["nginx-app"]["access_log"]
+
 easybib_nginx config do
   config_template "internal-api.conf.erb"
   doc_root        "www"
-  access_log      "off"
-  nginx_extras = "sendfile off;"
-  routes_enabled  node["nginx-app"][config]["routes_enabled"]
-  routes_denied   node["nginx-app"][config]["routes_denied"]
+  access_log      access_log
+  nginx_extras    "sendfile off;"
   notifies :restart, "service[nginx]", :delayed
 end
