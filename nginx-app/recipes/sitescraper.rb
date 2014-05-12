a022d77eb80fde48e1d62cdb041f2269c75f6090
till <till@php.net>
2014-05-12 15:21:38 +0200

    Fix: set app_name because routes_denied and routes_enabled moved into the LWRP
    Related: easybib/issues#1217

--- nginx-app/recipes/sitescraper.rb
+++ nginx-app/recipes/sitescraper.rb
@@ -3,6 +3,7 @@ config = "sitescraper"
 access_log = node["nginx-app"]["access_log"]
 
 easybib_nginx config do
+  app_name        "sitescraper"
   config_template "internal-api.conf.erb"
   doc_root        "www"
   access_log      access_log
