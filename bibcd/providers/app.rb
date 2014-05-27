9dd8d1e66e8d49cbff67fa09dabeb1a7c307bd62
Florian Holzhauer <fh-github@fholzhauer.de>
2014-05-27 17:24:03 +0200

    Fixing broken yaml with ruby 2.0

--- bibcd/providers/app.rb
+++ bibcd/providers/app.rb
@@ -4,12 +4,15 @@ action :add do
   unless ::File.exists?("#{new_resource.path}/config/apps/#{new_resource.app_name}.yml")
     Chef::Log.info "Adding #{new_resource.app_name} config to #{new_resource.path}/config/apps/#{new_resource.app_name}.yml"
 
+    # This is an ugly quick hack: Ruby Yaml adds !map:Chef::Node::ImmutableMash which the Symfony Yaml
+    # parser doesnt like. So lets remove it. First Chef 11.4/Ruby 1.8, then Chef 11.10/Ruby 2.0
+    content = YAML.dump(new_resource.config.to_hash).gsub('!map:Chef::Node::ImmutableMash', '')
+    content = content.gsub('!ruby/hash:Chef::Node::ImmutableMash', '')
+
     template "#{new_resource.path}/config/apps/#{new_resource.app_name}.yml" do
       cookbook "bibcd" # if we dont set this, the template cmd would search in the calling cookbook
       mode   0644
-      # This is an ugly quick hack: Ruby Yaml adds !map:Chef::Node::ImmutableMash which the Symfony Yaml
-      # parser doesnt like. So lets remove it.
-      variables :content => YAML.dump(new_resource.config.to_hash).gsub('!map:Chef::Node::ImmutableMash', '')
+      variables :content => content
       source "app.yml.erb"
     end
 
