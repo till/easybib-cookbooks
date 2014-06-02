5e50bb182fdfb23f8a951897b7c320942e0a3c41
Florian Holzhauer <fh-github@fholzhauer.de>
2014-06-02 20:56:04 +0200

    Refactoring: Relocting yaml converting in lib
    Added tests

--- easybib/libraries/easybib.rb
+++ easybib/libraries/easybib.rb
@@ -200,6 +200,15 @@ module EasyBib
     false
   end
 
+  def to_php_yaml(obj)
+    # This is an ugly quick hack: Ruby Yaml adds object info !map:Chef::Node::ImmutableMash which
+    # the Symfony Yaml parser doesnt like. So lets remove it. First Chef 11.4/Ruby 1.8,
+    # then Chef 11.10/Ruby 2.0
+    yaml    = YAML.dump(obj)
+    content = yaml.gsub('!map:Chef::Node::ImmutableMash', '')
+    content.gsub('!ruby/hash:Chef::Node::ImmutableMash', '')
+  end
+
   extend self
 end
 
