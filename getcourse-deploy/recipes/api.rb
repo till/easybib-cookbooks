f8cd5658b0f81b47effda60900bd7de7de7b446e
till <till@php.net>
2014-06-17 14:54:15 +0200

    Enhancement: deploy feature flags with the same recipe (and on the same role)

--- getcourse-deploy/recipes/api.rb
+++ getcourse-deploy/recipes/api.rb
@@ -2,7 +2,10 @@ include_recipe "php-fpm::service"
 
 node['deploy'].each do |application, deploy|
 
-  next unless allow_deploy(application, 'api', 'api-server')
+  apps = ['api', 'feature_flags']
+  next unless apps.include?(application)
+
+  #next unless allow_deploy(application, 'api', 'api-server')
 
   opsworks_deploy_dir do
     user  deploy["user"]
@@ -10,13 +13,13 @@ node['deploy'].each do |application, deploy|
     path  deploy["deploy_to"]
   end
 
-  easybib_deploy "getcourse-api" do
+  easybib_deploy "getcourse-#{application}" do
     deploy_data deploy
     app application
     envvar_json_source "getcourse"
   end
 
-  include_recipe "monit::pecl-manager"
+  include_recipe "monit::pecl-manager" if application == 'api'
 
   service "php-fpm" do
     action :reload
