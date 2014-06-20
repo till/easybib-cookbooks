c71b1880ba9bc667e615b1006eb40217e67c3e38
Florian Holzhauer <fh-github@fholzhauer.de>
2014-06-20 11:08:42 +0200

    Bugfix: Added mailsender-variable for monit template

--- monit/recipes/mailnotify.rb
+++ monit/recipes/mailnotify.rb
@@ -7,6 +7,7 @@ template "/etc/monit/conf.d/mailnotify.monitrc" do
     'host'     => node['monit']['mailhost'],
     'mailuser' => node['monit']['mailuser'],
     'mailpass' => node['monit']['mailpass'],
+    'mailsender' => node['monit']['mailsender'],
     'recipients' => node['monit']['notification_recipients']
   )
   notifies :restart, "service[monit]"
