47a269d020f4ec0794ed7ade24d80174ce377177
till <till@php.net>
2014-06-20 16:46:14 +0200

    Update: enable xhprof when newrelic is disabled

--- easybib/recipes/role-phpapp.rb
+++ easybib/recipes/role-phpapp.rb
@@ -12,4 +12,5 @@ if is_aws
   include_recipe "php-opcache::configure"
   include_recipe "newrelic" if node["easybib_deploy"]["use_newrelic"] == "yes"
   include_recipe "qafoo-profiler" if node["easybib_deploy"]["use_newrelic"] != "yes"
+  include_recipe "php-xhprof" if node["easybib_deploy"]["use_newrelic"] != "yes"
 end
