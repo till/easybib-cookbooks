9a47272c51bf743923475250fb86c6e83d5e06d2
Florian Holzhauer <fh-github@fholzhauer.de>
2014-06-30 20:38:20 +0200

    Adding username for correct permissions

--- easybib/recipes/ruby.rb
+++ easybib/recipes/ruby.rb
@@ -1,12 +1,16 @@
-gemrc_paths = ["/root", "/var/www-data"]
+gemrc_paths = {
+  "root" => "/root",
+  "www-data" => "/var/www"
+}
 
 gemrc_paths << Dir.entries("/home").select do |f|
   !File.directory? f
 end
 
-gemrc_paths.each do |home_directory|
+gemrc_paths.each do |username, home_directory|
   cookbook_file "#{home_directory}/.gemrc" do
     mode 0644
+    user username
     source "gemrc"
     only_if do
       File.exists?(home_directory)
