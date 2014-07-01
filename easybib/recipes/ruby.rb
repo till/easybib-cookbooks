35f7c5aefa8f930eb66b0846affd7bc93bc8e331
till <till@php.net>
2014-07-01 15:05:51 +0200

    Enhancement: dumb it down - use /etc/gemrc

--- easybib/recipes/ruby.rb
+++ easybib/recipes/ruby.rb
@@ -1,23 +1,4 @@
-gemrc_paths = {
-  "root" => "/root",
-  "www-data" => "/var/www"
-}
-
-home_dir = "/home"
-
-Dir.entries(home_dir).each do |file_name|
-  next if file_name =~ /^\.\.?$/
-  next unless File.directory?(file_name)
-  gemrc_paths[file_name] = "#{home_dir}/#{file_name}"
-end
-
-gemrc_paths.each do |username, home_directory|
-  cookbook_file "#{home_directory}/.gemrc" do
-    mode 0644
-    user username
-    source "gemrc"
-    only_if do
-      File.exists?(home_directory)
-    end
-  end
+cookbook_file "/etc/gemrc" do
+  mode 0644
+  source "gemrc"
 end
