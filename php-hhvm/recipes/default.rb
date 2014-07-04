29edc24006424308337cadb908591dc064ea2859
Richard Wossal <richard@r-wos.org>
2014-07-04 14:50:45 +0200

    added libboost ppa to hhvm install recipe
    Won't install, otherwise. Instructions taken from
    https://github.com/facebook/hhvm/wiki/Prebuilt-Packages-on-Ubuntu-12.04

--- php-hhvm/recipes/default.rb
+++ php-hhvm/recipes/default.rb
@@ -1,17 +1,25 @@
+apt_package "install add-apt-repository" do
+  package_name "python-software-properties"
+end
+
+execute "add libboost ppa" do
+  command <<-EOF
+    add-apt-repository -y ppa:mapnik/boost
+    wget -O - http://dl.hhvm.com/conf/hhvm.gpg.key | apt-key add -
+  EOF
+end
+
 execute "discover apt-repository" do
   command "echo deb #{node["php-hhvm"]["apt"]["repo"]} #{node["lsb"]["codename"]} main > #{node["php-hhvm"]["apt"]["file"]}"
-  not_if do
-    File.exists?(node["php-hhvm"]["apt"]["file"])
-  end
+  creates "#{node["php-hhvm"]["apt"]["file"]}"
 end
 
 execute "update_hhvm_apt" do
-  command "apt-get update -y --allow-unauthenticated"
+  command "apt-get update -y"
 end
 
 apt_package "install hhvm" do
   package_name "hhvm"
-  options "--allow-unauthenticated"
 end
 
 include_recipe "php-hhvm::configure"
