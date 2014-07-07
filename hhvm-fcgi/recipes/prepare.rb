72d691357dbddf369ae2e8937665279e047a34a5
Richard Wossal <richard@r-wos.org>
2014-07-07 16:24:32 +0200

    add initial init.d for hhvm
    taken directly from ubuntu package, no changes (in this commit)

--- hhvm-fcgi/recipes/prepare.rb
+++ hhvm-fcgi/recipes/prepare.rb
@@ -15,3 +15,10 @@ file node["hhvm-fcgi"]["logfile"] do
   owner node["hhvm-fcgi"]["user"]
   group node["hhvm-fcgi"]["group"]
 end
+
+template "/etc/init.d/hhvm" do
+  mode   "0755"
+  source "init.d.hhvm.erb"
+  owner  node["hhvm-fcgi"]["user"]
+  group  node["hhvm-fcgi"]["group"]
+end
