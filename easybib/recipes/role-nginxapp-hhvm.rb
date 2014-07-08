b1178d7355fa55a92d55d8afd1f019c5b288cfb2
Richard Wossal <richard@r-wos.org>
2014-07-08 14:13:20 +0200

    fix CI errors
    + first try of a chefspec test for hhvm-fcgi

--- easybib/recipes/role-nginxapp-hhvm.rb
+++ easybib/recipes/role-nginxapp-hhvm.rb
@@ -2,7 +2,7 @@ include_recipe "hhvm-fcgi"
 include_recipe "easybib::role-generic"
 
 if is_aws
-  ##include_recipe "easybib-deploy::api" XXX TODO
+  # include_recipe "easybib-deploy::api" XXX TODO
 else
   include_recipe "nginx-app::hhvm"
 end
--- hhvm-fcgi/spec/default_spec.rb
+++ hhvm-fcgi/spec/default_spec.rb
@@ -1,29 +1,25 @@
-#require 'chefspec'
-#
-#describe 'php-fpm::default' do
-#  before do
-#    stub_command("apt-get install -s php5-easybib-apcu").and_return(1)
-#    stub_command("apt-get install -s php5-easybib-apc").and_return(0)
-#    @shellout = double('shellout')
-#    @shellout.stub(:live_stream=).with(STDOUT)
-#    @shellout.stub(:run_command)
-#    @shellout.stub(:exitstatus)
-#    @shellout.stub(:error!)
-#  end
-#
-#  let (:chef_run) do
-#    ChefSpec::Runner.new.converge('php-fpm::default')
-#  end
-#
-#  it "installs php5-easybib" do
-#    Mixlib::ShellOut.stub(:new).and_return(@shellout)
-#    expect(chef_run).to install_package('php5-easybib')
-#  end
-#
-#  # it "creates symlinks to all the binaries" do
-#  #  Mixlib::ShellOut.stub(:new).and_return(@shellout)
-#  #  ["pear", "peardev", "pecl", "phar", "phar.phar", "php-config", "phpize"].each do |bin|
-#  #    expect(chef_run).to create_link("/usr/local/bin/#{bin}")
-#  #  end
-#  # end
-#end
+require 'chefspec'
+
+describe 'hhvm-fcgi::default' do
+  before do
+    @shellout = double('shellout')
+    @shellout.stub(:live_stream=).with(STDOUT)
+    @shellout.stub(:run_command)
+    @shellout.stub(:exitstatus)
+    @shellout.stub(:error!)
+  end
+
+  let (:chef_run) do
+    ChefSpec::Runner.new.converge('hhvm-fcgi::default')
+  end
+
+  it "creates the config files" do
+    Mixlib::ShellOut.stub(:new).and_return(@shellout)
+    expect(chef_run).to create_template('/etc/init.d/hhvm')
+    expect(chef_run).to create_template('/etc/logrotate.d/hhvm')
+    expect(chef_run).to create_template('/etc/hhvm/php.ini')
+    expect(chef_run).to create_template('/etc/hhvm/php-fcgi.ini')
+    expect(chef_run).to create_template('/etc/hhvm/config.hdf')
+  end
+
+end
