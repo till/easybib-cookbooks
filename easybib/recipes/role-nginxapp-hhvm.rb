a0753c2d754178bca893adec64855075cce986c3
Richard Wossal <richard@r-wos.org>
2014-07-09 16:13:02 +0200

    raise TODO for missing deploy recipe

--- easybib/recipes/role-nginxapp-hhvm.rb
+++ easybib/recipes/role-nginxapp-hhvm.rb
@@ -2,7 +2,7 @@ include_recipe "hhvm-fcgi"
 include_recipe "easybib::role-generic"
 
 if is_aws
-  # include_recipe "easybib-deploy::api" XXX TODO
+  raise "TODO: deployment recipe for nginx/hhvm"
 else
   include_recipe "nginx-app::hhvm"
 end
