e90a4d254468340e3c40de88f23482479ed625dc
Richard Wossal <richard@r-wos.org>
2014-07-10 19:39:56 +0200

    using fail instead of raise

--- easybib/recipes/role-nginxapp-hhvm.rb
+++ easybib/recipes/role-nginxapp-hhvm.rb
@@ -2,7 +2,7 @@ include_recipe "hhvm-fcgi"
 include_recipe "easybib::role-generic"
 
 if is_aws
-  raise "TODO: deployment recipe for nginx/hhvm"
+  fail "TODO: deployment recipe for nginx/hhvm"
 else
   include_recipe "nginx-app::hhvm"
 end
