e633457c444409ef89a9ed84168c67d29dca73be
Richard Wossal <richard@r-wos.org>
2014-07-11 15:17:32 +0200

    fix confusion between libbpoost ppa and hhvm repo

--- php-hhvm/recipes/default.rb
+++ php-hhvm/recipes/default.rb
@@ -1,8 +1,20 @@
-apt_repository "liboost" do
-  uri node["php-hhvm"]["apt"]["ppa"]
+apt_package "install add-apt-repository" do
+  package_name "python-software-properties"
+end
+
+execute "add libboost ppa" do
+  command "add-apt-repository -y #{node["php-hhvm"]["boost"]["ppa"]}"
+end
+
+apt_repository "hhvm" do
+  uri node["php-hhvm"]["apt"]["repo"]
   distribution node["lsb"]["codename"]
   components ["main"]
   key node["php-hhvm"]["apt"]["key"]
+end
+
+execute "apt-get update" do
+  command "apt-get update"
 end
 
 apt_package "install hhvm" do
