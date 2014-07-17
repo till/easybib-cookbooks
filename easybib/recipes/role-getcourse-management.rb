9d260980a0ae9a2a7f9861eabff3ee79b4b6f5e6
till <till@php.net>
2014-07-17 18:27:23 +0200

    Fix: syntax copy'n'paste error

--- easybib/recipes/role-getcourse-management.rb
+++ easybib/recipes/role-getcourse-management.rb
@@ -1,4 +1,3 @@
-easybib/recipes/role-getcourse-signup.rb                                                                                      [16:45:31]
 include_recipe "easybib::role-generic"
 
 include_recipe "getcourse-deploy::static"
