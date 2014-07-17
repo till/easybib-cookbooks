c2cb565d0d7b2754a806dfe6158f134e5c68011e
till <till@php.net>
2014-07-17 18:15:29 +0200

    Update: switch management to static app

--- easybib/recipes/role-getcourse-management.rb
+++ easybib/recipes/role-getcourse-management.rb
@@ -1,8 +1,5 @@
-include_recipe "easybib::role-phpapp"
+easybib/recipes/role-getcourse-signup.rb                                                                                      [16:45:31]
+include_recipe "easybib::role-generic"
 
-include_recipe "php-intl"
-
-include_recipe "bash::configure"
-include_recipe "bash::bashrc"
-include_recipe "getcourse-deploy::management"
+include_recipe "getcourse-deploy::static"
 include_recipe "nginx-app::getcourse-management"
--- nginx-app/recipes/getcourse-management.rb
+++ nginx-app/recipes/getcourse-management.rb
@@ -1,7 +1,7 @@
 config = "management"
 
 if is_aws
-  deploy_dir = "/srv/www/#{config}/current/public/"
+  deploy_dir = "/srv/www/#{config}/current/dist/"
 else
   if node["vagrant"]["combined"] == true
     deploy_dir = node["vagrant"]["deploy_to"][config]
@@ -10,28 +10,20 @@ else
   end
 end
 
-domain_name = node["getcourse"]["domain"]["management"]
-domain_conf = get_domain_conf("getcourse")
-
-env_conf = ""
-if has_env?("getcourse")
-  env_conf = get_env_for_nginx("getcourse")
-end
+domain_name = node["getcourse"]["domain"]["signup"]
+default_router = "index.html"
 
 template "/etc/nginx/sites-enabled/#{config}.conf" do
-  source "silex.conf.erb"
+  source "static.conf.erb"
   mode   "0755"
   owner  node["nginx-app"]["user"]
   group  node["nginx-app"]["group"]
   variables(
-    :php_user => node["php-fpm"]["user"],
-    :doc_root => deploy_dir,
+    :doc_root    => deploy_dir,
     :domain_name => domain_name,
-    :access_log => 'off',
+    :access_log  => 'off',
     :nginx_extra => node["nginx-app"]["extras"],
-    :default_router => node["nginx-app"]["default_router"],
-    :upstream => config,
-    :env_conf => env_conf
+    :default_router => default_router
   )
   notifies :restart, "service[nginx]", :delayed
 end
