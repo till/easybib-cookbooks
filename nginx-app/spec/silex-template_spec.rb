602ce86e8b24ae2019792483525ffc0ffeb91730
Florian Holzhauer <fh-github@fholzhauer.de>
2014-07-21 16:29:25 +0200

    Bugfix: Remove warnings

--- nginx-app/spec/silex-template_spec.rb
+++ nginx-app/spec/silex-template_spec.rb
@@ -44,6 +44,7 @@ describe 'silex-config-template' do
     before do
       node.set["testdata"]["routes_enabled"] = ['/some/route', '/other/route']
       node.set["testdata"]["routes_denied"]  = nil
+      node.set["opsworks"]["stack"]["name"]  = "rspec"
     end
 
     it "does set routes for enabled routes" do
@@ -91,6 +92,7 @@ describe 'silex-config-template' do
     before do
       node.set["testdata"]["routes_enabled"] = ['/some/route', '/other/route']
       node.set["testdata"]["routes_denied"]  = ['/some/droute', '/other/droute']
+      node.set["opsworks"]["stack"]["name"]  = "rspec"
     end
 
     it "does set routes for enabled routes" do
