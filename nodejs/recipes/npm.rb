37bb3ec55fbc310c0f5ab4ec43ebca3577c66065
Richard Wossal <richard@r-wos.org>
2014-07-22 16:32:34 +0200

    fix npm install recipe
    - changed URL (got a 301 there before)
    - added "clean=no" env so that the install works
      (see http://stackoverflow.com/a/20177638/1070069)

--- nodejs/recipes/npm.rb
+++ nodejs/recipes/npm.rb
@@ -2,7 +2,7 @@ package "curl"
 package "nodejs"
 
 remote_file "#{Chef::Config[:file_cache_path]}/install-npm.sh" do
-  source "http://npmjs.org/install.sh"
+  source "https://www.npmjs.org/install.sh"
   mode "0755"
   not_if do
     File.exists?("/usr/bin/npm")
@@ -11,6 +11,7 @@ end
 
 execute "Install the latest npm" do
   command "#{Chef::Config[:file_cache_path]}/install-npm.sh"
+  environment ({"clean" => "no"})
   not_if do
     File.exist?("/usr/bin/npm")
   end
