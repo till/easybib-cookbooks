a3466d153da065144fab0f70e2190f9381869628
Florian Holzhauer <fh-github@fholzhauer.de>
2014-07-26 14:55:24 +0200

    Bugfix: Using apt_repository for percona
    apt-get -f is not known in ubuntu 14.04

--- percona/recipes/repository.rb
+++ percona/recipes/repository.rb
@@ -1,19 +1,8 @@
-cookbook_file "/tmp/percona.pub" do
-  source "percona.pub"
-  mode   "0644"
-end
-
-execute "import key" do
-  command "apt-key add /tmp/percona.pub"
-end
-
-execute "update-percona-apt" do
-  command "apt-get -y -f -q update"
-  action :nothing
-end
-
-template "/etc/apt/sources.list.d/percona.list" do
-  source "percona.list.erb"
-  mode 0644
-  notifies :run, "execute[update-percona-apt]", :immediately
+apt_repository 'percona' do
+  uri          'http://repo.percona.com/apt'
+  arch         'amd64'
+  distribution node['lsb']['codename']
+  components   ['main']
+  keyserver    'keys.gnupg.net'
+  key          '1C4CBDCDCD2EFD2A'
 end
