f25f75ecf7146cd319a1314da47af5680447307f
Florian Holzhauer <fh-github@fholzhauer.de>
2014-07-26 15:19:40 +0200

    Bugfix: libboost is in default repo on 14.04

--- php-hhvm/recipes/default.rb
+++ php-hhvm/recipes/default.rb
@@ -1,24 +1,29 @@
-apt_package "install add-apt-repository" do
-  package_name "python-software-properties"
-end
+case node['platform']
+when 'ubuntu'
 
-execute "add libboost ppa" do
-  command "add-apt-repository -y #{node["php-hhvm"]["boost"]["ppa"]}"
-end
+  apt_package "install add-apt-repository" do
+    package_name "python-software-properties"
+    only_if { node['lsb']['release'].to_f < 14.04 }
+  end
 
-apt_repository "hhvm" do
-  uri node["php-hhvm"]["apt"]["repo"]
-  distribution node["lsb"]["codename"]
-  components ["main"]
-  key node["php-hhvm"]["apt"]["key"]
-end
+  execute "add libboost ppa" do
+    command "add-apt-repository -y #{node["php-hhvm"]["boost"]["ppa"]}"
+    only_if { node['lsb']['release'].to_f < 14.04 }
+  end
 
-execute "apt-get update" do
-  command "apt-get update"
-end
+  apt_repository "hhvm" do
+    uri node["php-hhvm"]["apt"]["repo"]
+    distribution node["lsb"]["codename"]
+    components ["main"]
+    key node["php-hhvm"]["apt"]["key"]
+  end
 
-apt_package "install hhvm" do
-  package_name "hhvm"
-end
+  execute "apt-get update" do
+    command "apt-get update"
+  end
 
+  apt_package "install hhvm" do
+    package_name "hhvm"
+  end
+end
 include_recipe "php-hhvm::configure"
