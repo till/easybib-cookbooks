d5c89e54fb62390ffbcb575c4fe8dde872dad81d
till <till@php.net>
2014-08-01 16:53:35 +0200

    Fix: CS

--- php-hhvm/recipes/configure.rb
+++ php-hhvm/recipes/configure.rb
@@ -1,11 +1,11 @@
 template "/etc/hhvm/php.ini" do
   cookbook "php-fpm"
-  source   "php.ini.erb"
+  source "php.ini.erb"
   variables(
     :enable_dl      => 'Off',
     :memory_limit   => '-1',
     :display_errors => 'On'
   )
-  owner    node["php-fpm"]["user"]
-  group    node["php-fpm"]["group"]
+  owner node["php-fpm"]["user"]
+  group node["php-fpm"]["group"]
 end
