513ee670900947785a3e400136088f50dbeeed9f
till <till@php.net>
2014-08-18 19:59:12 +0200

    Fix: remove call to haproxy::down

--- easybib/recipes/role-lb.rb
+++ easybib/recipes/role-lb.rb
@@ -1,6 +1,5 @@
 include_recipe "easybib::setup"
 include_recipe "loggly::setup"
-include_recipe "haproxy::down"
 include_recipe "haproxy::ctl"
 include_recipe "haproxy::hatop"
 include_recipe "haproxy::logs"
