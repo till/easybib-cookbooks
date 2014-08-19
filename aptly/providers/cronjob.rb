7a099f47fdff9108fc4a19dcd76faa6344978f4d
Florian Holzhauer <fh-github@fholzhauer.de>
2014-08-19 16:55:18 +0200

    Various fixes (permissions, template)

--- aptly/providers/cronjob.rb
+++ aptly/providers/cronjob.rb
@@ -3,16 +3,12 @@ action :create do
 
   mirror_name = node["aptly"]["mirror_name"]
   s3_mirror = node["aptly"]["s3_mirror"]
-  cron_command = "MIRROR_NAME=#{mirror_name} S3_APT_MIRROR=s3://#{s3_mirror} #{path}/mirror-it.rb"
-
-  directory "/var/backups" do
-    user "backup"
-  end
+  cron_command = "MIRROR_NAME=#{mirror_name} S3_APT_MIRROR=s3:#{s3_mirror}: #{path}/mirror-it.rb"
 
   cron_d "aptly_sync_launchpad" do
     action :create
     minute "*/30"
-    user "backup"
+    user "root"
     command cron_command
     path node["easybib_deploy"]["cron_path"]
   end
--- aptly/recipes/setup.rb
+++ aptly/recipes/setup.rb
@@ -9,3 +9,15 @@ end
 package "aptly" do
   action :install
 end
+
+template "/etc/aptly.conf" do
+  source 'aptly.conf.erb'
+  owner 'root'
+  group 'root'
+  mode 00644
+  variables(
+    :root_dir => node["aptly"]["mirror_dir"],
+    :endpoint => node["aptly"]["s3_mirror"]
+    )
+  action :create
+end
