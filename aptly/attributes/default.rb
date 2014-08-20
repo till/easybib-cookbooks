f7c4105e5b394030f38289f81450173fbc827407
Florian Holzhauer <fh-github@fholzhauer.de>
2014-08-20 08:37:07 +0200

    Adding support for our ppa mirror

--- aptly/attributes/default.rb
+++ aptly/attributes/default.rb
@@ -2,3 +2,13 @@ default["aptly"]                 = {}
 default["aptly"]["mirror_name"]  = "easybib_ppa"
 default["aptly"]["s3_mirror"]    = "easybib_ppa"
 default["aptly"]["mirror_dir"]   = "/tmp/ppa"
+default["aptly"]["gpgkeys"]      = [
+  "66E3A9B7",
+  "63561DC6",
+  "5D50B6BA",
+  "C7917B12",
+  "06ED541C",
+  "C3173AA6",
+  "B6AC7E8C",
+  "0737E5F5"
+]
