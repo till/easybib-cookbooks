a2a5e3a002a66d91ffc277ad94b96192a8651d47
Florian Holzhauer <fh-github@fholzhauer.de>
2014-08-20 14:35:27 +0200

    Final touches for aptly setup

--- aptly/attributes/default.rb
+++ aptly/attributes/default.rb
@@ -1,4 +1,5 @@
 default["aptly"]                 = {}
 default["aptly"]["mirror_name"]  = "easybib_ppa"
-default["aptly"]["s3_mirror"]    = "easybib_ppa"
+default["aptly"]["s3_mirror"]    = "easybib_ppa:prefix"
 default["aptly"]["mirror_dir"]   = "/tmp/ppa"
+default["aptly"]["sign_pass"]    = "heythere"
