b48791f04e0be34a0cf5675fbf1961f080d2ce3b
Till Klampaeckel <till@php.net>
2014-09-11 22:48:41 +0200

    Refactor: try to be more officient

--- composer/recipes/configure.rb
+++ composer/recipes/configure.rb
@@ -1,40 +1,36 @@
-if is_aws
+deploy_user = get_deploy_user
 
-  deploy_user     = get_deploy_user
-  deploy_username = deploy_user[:user]
-  deploy_group    = deploy_user[:group]
-
-  # this is an assumption, so sue me
-  case deploy_username
+# this is an assumption, so sue me
+if deploy_user.is_a?(Hash)
+  case deploy_user["user"]
   when 'root'
     home_dir = '/root'
   when 'www-data'
     home_dir = '/var/www'
   else
-    home_dir = "/home/#{deploy_username}"
+    home_dir = "/home/#{deploy_user["user"]}"
   end
+end
 
-  directory "#{home_dir}/.composer" do
-    owner     deploy_username
-    group     deploy_group
-    mode      "0750"
-    recursive true
+directory "#{home_dir}/.composer" do
+  owner     deploy_user[:user]
+  group     deploy_user[:group]
+  mode      "0750"
+  recursive true
+  only_if do
+    ::EasyBib.is_aws(node)
   end
+end
 
-  if node["composer"]
-    template "#{home_dir}/.composer/config.json" do
-      owner  deploy_username
-      group  deploy_group
-      source "composer.config.json.erb"
-      mode   "0640"
-      variables(
-        :oauth_key => node["composer"]["oauth_key"]
-      )
-    end
-  else
-    Chef::Log.error('Configuration error, missing node["composer"]["oauth_key"]')
+template "#{home_dir}/.composer/config.json" do
+  owner  deploy_user[:user]
+  group  deploy_user[:group]
+  source "composer.config.json.erb"
+  mode   "0640"
+  variables(
+    :oauth_key => node["composer"]["oauth_key"]
+  )
+  only_if do
+    ::EasyBib.is_aws(node) && !node["composer"]["oauth_key"].nil?
   end
-
-else
-  Chef::Log.debug("Recipe requires `aws` to be present.")
 end
