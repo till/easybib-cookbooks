74e4b4c452f25eeacafee7a35cf195411d2ad119
Till Klampaeckel <till@php.net>
2014-09-15 02:52:01 +0200

    Enhancement: add spec for nginx-app::configure

---
+++ nginx-app/spec/configure_spec.rb
@@ -0,0 +1,72 @@
+require 'chefspec'
+
+describe 'nginx-app::configure' do
+
+  let(:runner)   { ChefSpec::Runner.new(:version => 12.04) }
+  let(:chef_run) { runner.converge(described_recipe) }
+  let(:node)     { runner.node }
+
+  let(:vhost) { "/etc/nginx/sites-enabled/easybib.com.conf" }
+  let(:access_log) { "/some/drive/access.log" }
+  let(:php_user) { "some_user_account" }
+
+  describe "deployment" do
+    before do
+      stub_command('rm -f /etc/nginx/sites-enabled/default').and_return(true)
+
+      node.set["deploy"] = {}
+
+      node.set["easybib"]["cluster_name"] = "Stack Name"
+
+      node.set["opsworks"]["instance"]["layers"] = ["nginxphpapp"]
+      node.set["opsworks"]["stack"]["name"] = "Stack Name"
+    end
+
+    it "includes necessary recipes" do
+      expect(chef_run).to include_recipe "nginx-app::server"
+      expect(chef_run).to include_recipe "php-fpm::service"
+    end
+
+    describe "virtualhost" do
+      before do
+        node.set["deploy"]["easybib"] = {
+          "deploy_to" => "/srv/www/easybib"
+        }
+        node.set["nginx-app"]["access_log"] = access_log
+        node.set["php-fpm"]["user"] = php_user
+      end
+
+      it "writes virtualhost for app 'easybib'" do
+        expect(chef_run).to create_template(vhost)
+      end
+
+      it "sets the correct upstream" do
+        expect(chef_run).to render_file(vhost)
+          .with_content(
+            "unix:/var/run/php-fpm/#{php_user}"
+          )
+      end
+
+      it "sets the correct BIB_ENV" do
+        expect(chef_run).to render_file(vhost)
+          .with_content(
+            include('fastcgi_param BIB_ENV "Stack Name";')
+          )
+      end
+
+      it "sets the correct SCRIPT_FILENAME" do
+        expect(chef_run).to render_file(vhost)
+          .with_content(
+            include('fastcgi_param SCRIPT_FILENAME /srv/www/easybib/current/www/index.php;')
+          )
+      end
+
+      it "sets the correct access_log" do
+        expect(chef_run).to render_file(vhost)
+          .with_content(
+            include("access_log #{access_log};")
+          )
+      end
+    end
+  end
+end
