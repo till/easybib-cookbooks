86f945aef72d2e356f969ef758a461b9307ee9ad
Till Klampaeckel <till@php.net>
2014-09-16 11:31:39 +0200

    Fix: rubocop offenses

--- easybib/spec/easybib_deploy_spec.rb
+++ easybib/spec/easybib_deploy_spec.rb
@@ -11,8 +11,8 @@ describe 'easybib_deploy' do
 
   let(:runner) do
     ChefSpec::Runner.new(
-      cookbook_path: cookbook_paths,
-      step_into: ["easybib_deploy"]
+      :cookbook_path => cookbook_paths,
+      :step_into => ["easybib_deploy"]
     )
   end
 
