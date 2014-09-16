a718cd006fb03813e25e92d00682183b11125470
Till Klampaeckel <till@php.net>
2014-09-16 16:04:01 +0200

    Enhancement: add group attribute for consistency

--- nginx-lb/attributes/default.rb
+++ nginx-lb/attributes/default.rb
@@ -3,3 +3,4 @@ default["nginx-lb"]["cluster"] = ["Fruitkid", "Fruitkid Playground", "EasyBib Pl
 default["nginx-lb"]["dir"]     = "/etc/nginx"
 default["nginx-lb"]["int_ip"]  = "127.0.0.1"
 default["nginx-lb"]["user"]    = "www-data"
+default["nginx-lb"]["group"]   = "www-data"
