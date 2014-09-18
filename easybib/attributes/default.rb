54d8dd82dda13c82bb88f026ade397e8ffc70ac9
Florian Holzhauer <fh-github@fholzhauer.de>
2014-09-18 10:43:46 +0200

    Cleanup: Indentation

--- easybib/attributes/default.rb
+++ easybib/attributes/default.rb
@@ -1,8 +1,8 @@
-default['easybib_deploy']                 = {}
-default['easybib_deploy']['gearman_file'] = 'pecl_manager_env'
-default['easybib_deploy']['env_source']   = nil
-default['easybib_deploy']['provide_pear'] = false
-default['easybib_deploy']['cron_path']    = '/usr/local/bin:/usr/bin:/bin'
+default['easybib_deploy']                  = {}
+default['easybib_deploy']['gearman_file']  = 'pecl_manager_env'
+default['easybib_deploy']['env_source']    = nil
+default['easybib_deploy']['provide_pear']  = false
+default['easybib_deploy']['cron_path']     = '/usr/local/bin:/usr/bin:/bin'
 default['easybib_deploy']['deploy_action'] = :reload
-default['easybib_deploy']['use_newrelic'] = 'yes'
-default['easybib_deploy']['envtype'] = 'playground'
+default['easybib_deploy']['use_newrelic']  = 'yes'
+default['easybib_deploy']['envtype']       = 'playground'
