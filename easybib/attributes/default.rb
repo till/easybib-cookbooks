e9bc0f25d091808bfad79ee0c20e69a643a29079
Florian Holzhauer <fh-github@fholzhauer.de>
2014-09-18 14:29:18 +0200

    Disabling Newrelic by default

--- easybib/attributes/default.rb
+++ easybib/attributes/default.rb
@@ -4,5 +4,5 @@ default['easybib_deploy']['env_source']    = nil
 default['easybib_deploy']['provide_pear']  = false
 default['easybib_deploy']['cron_path']     = '/usr/local/bin:/usr/bin:/bin'
 default['easybib_deploy']['deploy_action'] = :reload
-default['easybib_deploy']['use_newrelic']  = 'yes'
+default['easybib_deploy']['use_newrelic']  = 'no'
 default['easybib_deploy']['envtype']       = 'playground'
