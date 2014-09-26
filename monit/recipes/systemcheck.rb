3d32f90d4b71f816e88c8f946a92bdbc11c1cc68
Florian Holzhauer <fh-github@fholzhauer.de>
2014-09-26 13:21:37 +0200

    Bugfix: Work around non-opsworks-env

--- monit/recipes/systemcheck.rb
+++ monit/recipes/systemcheck.rb
@@ -1,6 +1,10 @@
-machine_name = node['opsworks']['instance']['hostname']
-stack_name = get_normalized_cluster_name
-hostname = "#{machine_name}.#{stack_name}"
+hostname = 'vagrant-machine'
+
+unless node.fetch('opsworks', {}).fetch('instance', {})['hostname'].nil?
+  machine_name = node['opsworks']['instance']['hostname']
+  stack_name = get_normalized_cluster_name
+  hostname = "#{machine_name}.#{stack_name}"
+end
 
 template '/etc/monit/conf.d/system.monitrc' do
   source 'system.monit.erb'
