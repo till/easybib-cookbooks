9ea486629ff822b6584ae91a6e98a625dea08634
Florian Holzhauer <fh-github@fholzhauer.de>
2014-10-01 18:52:52 +0200

    Packetbeat Agent recipe

---
+++ packetbeat/recipes/agent.rb
@@ -0,0 +1,32 @@
+hostname = "#{node['opsworks']['instance']['hostname']}.#{get_normalized_cluster_name}"
+
+filename = node['packetbeat']['agent_deb'].split('/').last
+remote_file "#{Chef::Config['file_cache_path']}/#{filename}" do
+  source node['packetbeat']['agent_deb']
+  mode 0644
+end
+
+apt_package 'packetbeat' do
+  action :upgrade
+  source "#{Chef::Config['file_cache_path']}/#{filename}"
+end
+
+service 'packetbeat' do
+  service_name  'packetbeat'
+  supports     [:start, :stop, :restart]
+end
+
+template '/etc/packetbeat.conf' do
+  source 'packetbeat.conf.erb'
+  mode '0644'
+  variables(
+    :elasticsearch => node['packetbeat']['config']['elasticsearch'],
+    :redis => node['packetbeat']['config']['redis'],
+    :protocols => node['packetbeat']['config']['protocols'],
+    :procs => node['packetbeat']['config']['procs'],
+    :hostname => hostname,
+    :ignore_outgoing => node['packetbeat']['config']['ignore_outgoint'],
+    :hide_keywords => node['packetbeat']['config']['hide_keywords']
+  )
+  notifies :restart, 'service[packetbeat]', :immediately
+end
---
+++ packetbeat/spec/agent_spec.rb
@@ -0,0 +1,42 @@
+require_relative 'spec_helper'
+
+describe 'packetbeat agent' do
+
+  let(:cookbook_paths) do
+    [
+      File.expand_path("#{File.dirname(__FILE__)}/../../")
+    ]
+  end
+  let(:runner) do
+    ChefSpec::Runner.new(
+      :cookbook_path => cookbook_paths
+    )
+  end
+  let(:chef_run) { runner.converge('packetbeat::agent') }
+  let(:node)     { runner.node }
+
+  describe 'standardrun' do
+    before do
+      node.set['packetbeat']['agent_deb'] = 'https://github.com/packetbeat/packetbeat/releases/download/v0.3.3/packetbeat_0.3.3-1_amd64.deb'
+      node.set['opsworks']['instance']['hostname'] = 'some-host'
+      node.set['opsworks']['stack']['name'] = 'some-stack'
+    end
+
+    it 'does download the packetbeat package' do
+      expect(chef_run).to create_remote_file('/var/chef/cache/packetbeat_0.3.3-1_amd64.deb')
+    end
+
+    it 'does download the packetbeat package' do
+      expect(chef_run).to upgrade_apt_package('packetbeat')
+    end
+
+    it 'does create a config' do
+      expect(chef_run).to render_file('/etc/packetbeat.conf')
+        .with_content(
+          include('procs.monitored.nginx')
+        )
+    end
+
+  end
+
+end
