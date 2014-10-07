25aeee361c2d0a238dc152afeac2969145aef1a5
sodoku <philip.d.stewart@gmail.com>
2014-10-07 15:42:11 +0200

    Add gpg keys

--- aptly/attributes/default.rb
+++ aptly/attributes/default.rb
@@ -4,3 +4,14 @@ default['aptly']['s3_mirror']    = 'easybib_ppa'
 default['aptly']['s3_mirror_prefix'] = 'prefix'
 default['aptly']['mirror_dir']   = '/tmp/ppa'
 default['aptly']['sign_pass']    = 'heythere'
+default["aptly"]["gpgkeys"]      = [
+  "66E3A9B7",
+  "63561DC6",
+  "5D50B6BA",
+  "C7917B12",
+  "06ED541C",
+  "C3173AA6",
+  "B6AC7E8C",
+  "0737E5F5",
+  "E93F5328"
+]
