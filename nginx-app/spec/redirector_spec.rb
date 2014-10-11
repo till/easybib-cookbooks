1d46069fbeda52686d17b905142e576e14bfd97e
till <till@php.net>
2014-10-11 03:56:25 +0200

    Enhancement: support for granular URL-based redirects
    Related: getcourse/issues#559

--- nginx-app/spec/redirector_spec.rb
+++ nginx-app/spec/redirector_spec.rb
@@ -11,7 +11,9 @@ describe 'nginx-app::redirector' do
   let(:chef_run) { runner.converge(described_recipe) }
   let(:node)     { runner.node }
 
-  let(:vhost) { "/etc/nginx/sites-enabled/redir-example.org.conf" }
+  let(:conf_dir) { "/etc/nginx/sites-enabled" }
+  let(:vhost_redir) { "#{conf_dir}/redir-example.org.conf" }
+  let(:vhost_urls) { "#{conf_dir}/urls-john-doe.example.org.conf" }
 
   describe 'setup domain redirect' do
     before do
@@ -21,17 +23,49 @@ describe 'nginx-app::redirector' do
     end
 
     it 'uses the template' do
-      expect(chef_run).to create_template(vhost)
+      expect(chef_run).to create_template(vhost_redir)
     end
 
     it 'it sets up the redirect' do
-      expect(chef_run).to render_file(vhost)
+      expect(chef_run).to render_file(vhost_redir)
         .with_content(
           include('server_name example.org;')
         )
         .with_content(
           include('rewrite ^ http://example.com$request_uri? permanent;')
         )
+    end
+  end
+
+  describe 'setup links to redirect' do
+    before do
+      node.set['redirector']['urls'] = {
+        'john-doe.example.org' => {
+          '/foo' => 'http://example.org/bar',
+          '/' => 'http://example.com',
+          '/bar' => 'http://example.com/bar'
+        }
+      }
+    end
+
+    it 'sets the john-doe.example.org server_name' do
+      expect(chef_run).to create_template(vhost_urls)
+    end
+
+    it 'sets the rewrite rules' do
+      node['redirector']['urls'].each do |domain_name, locations|
+        expect(chef_run).to render_file("#{conf_dir}/urls-#{domain_name}.conf")
+          .with_content(
+            include("server_name #{domain_name};")
+            )
+
+        locations.each do |from, to|
+          expect(chef_run).to render_file("#{conf_dir}/urls-#{domain_name}.conf")
+            .with_content(
+              include("#{from} #{to} permanent;")
+            )
+        end
+      end
     end
   end
 end
