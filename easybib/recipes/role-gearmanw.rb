644f09cddeb48cf39f892b6b1244b9e6f9b68fda
Florian Holzhauer <fh-github@fholzhauer.de>
2014-10-13 18:43:11 +0200

    Adding redis for gearman under vagrant

--- easybib/recipes/role-gearmanw.rb
+++ easybib/recipes/role-gearmanw.rb
@@ -9,6 +9,8 @@ include_recipe 'easybib-deploy::easybib'
 include_recipe 'pecl-manager::vagrant'
 
 unless is_aws
+  package 'redis'
+
   gearmanconf_root_dir = ::File.expand_path(node['easybib_deploy']['gearman_root'])
   import_file_path = "#{gearmanconf_root_dir}/deploy/#{node['easybib_deploy']['gearman_file']}"
 
