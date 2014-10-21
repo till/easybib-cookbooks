b683cf01ea41078409ba729da6853cf69ed2bda1
Florian Holzhauer <fh-github@fholzhauer.de>
2014-10-21 09:39:22 +0200

    Bugfix: Do not use su - www-data
    su - www-data fails with ubuntu 14.04, it just echoes "This account is currently not available.",
    since www-data has a nologin shell.

---
+++ opsworks_nodejs/libraries/nodejs_configuration.rb
@@ -0,0 +1,10 @@
+module OpsWorks
+  module NodejsConfiguration
+    def self.npm_install(app_name, app_config, app_root_path, npm_install_options)
+      if File.exists?("#{app_root_path}/package.json")
+        Chef::Log.info("package.json detected. Running npm #{npm_install_options}.")
+        Chef::Log.info(OpsWorks::ShellOut.shellout("cd #{app_root_path} && sudo -Hu #{app_config[:user]} 'npm #{npm_install_options}' 2>&1"))
+      end
+    end
+  end
+end
