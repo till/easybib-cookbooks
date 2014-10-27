f937c3ba5faa759e8b275a550d5d57c91846c278
till <till@php.net>
2014-10-27 11:33:09 -0400

    Fix: set default to empty string so .empty? works

--- php/resources/pecl.rb
+++ php/resources/pecl.rb
@@ -10,7 +10,7 @@ attribute :prefix, :kind_of => String
 attribute :package, :kind_of => String, :name_attribute => true
 attribute :version, :kind_of => String, :default => nil
 attribute :config,  :kind_of => Hash
-attribute :source_dir, :kind_of => String
+attribute :source_dir, :kind_of => String, :default => ''
 attribute :cflags, :kind_of => String
 attribute :config_directives, :kind_of => Hash, :default => {}
 attribute :zend_extensions, :kind_of => Array, :default => []
