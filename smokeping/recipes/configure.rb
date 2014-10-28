667199011ff9be095e305511429b1bcfd6a38f55
till <till@php.net>
2014-10-27 22:41:27 -0400

    Enhancement: configuration for probes and targets

--- smokeping/recipes/configure.rb
+++ smokeping/recipes/configure.rb
@@ -1,3 +1,4 @@
+include_recipe 'smokeping::service'
 include_recipe 'nginx-app::service'
 
 cookbook_file "#{node['nginx-app']['config_dir']}/conf.d/smokeping.conf" do
@@ -15,4 +16,27 @@ end
 
 link "#{smokeping_dir}/smokeping.cgi" do
   to '/usr/lib/cgi-bin/smokeping.cgi'
+end
+
+config = node['smokeping']['config']
+
+chef_gem "aws-sdk" unless config['aws']['access-key-id'].empty?
+
+template "/etc/smokeping/config.d/Probes" do
+  mode 0644
+  source "probes.erb"
+  variables(
+    :probes => config['probes']
+  )
+  notifies :restart, 'service[smokeping]'
+end
+
+template "/etc/smokeping/config.d/Targets" do
+  mode 0644
+  source "targets.erb"
+  variables(
+    :menu => config['menu'],
+    :targets => config['targets']
+  )
+  notifies :restart, 'service[smokeping]'
 end
