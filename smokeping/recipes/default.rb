a6dd75a0faf8b29e9e56d629f63ce2c9453f2cef
sodoku <philip.d.stewart@gmail.com>
2014-11-11 17:00:20 +0100

    Enhancement: Add TCPPing support for smokeping

--- smokeping/recipes/default.rb
+++ smokeping/recipes/default.rb
@@ -10,10 +10,15 @@ end
   fping curl libauthen-radius-perl libnet-ldap-perl
   libnet-dns-perl libio-socket-ssl-perl libnet-telnet-perl
   libsocket6-perl libio-socket-inet6-perl rrdtool sendmail
+  tcptraceroute
 ).each do |pkg|
   package pkg do
     action :install
   end
+end
+remote_file "/usr/bin/tcpping" do
+    source "http://www.vdberg.org/~richard/tcpping"
+    mode   "0755"
 end
 
 # for nginx
