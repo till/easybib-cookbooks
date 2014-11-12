014a06cf5f95a1b4e3bc61e80d6949c085e15e6a
Florian Holzhauer <fh-github@fholzhauer.de>
2014-11-12 15:32:45 +0100

    Adding rr-webeval

--- easybib-deploy/recipes/infolit.rb
+++ easybib-deploy/recipes/infolit.rb
@@ -2,7 +2,18 @@ include_recipe 'php-fpm::service'
 
 node['deploy'].each do |application, deploy|
 
-  next unless allow_deploy(application, 'infolit', 'nginxphpapp')
+  case application
+
+  when 'infolit'
+    next unless allow_deploy(application, 'infolit', 'nginxphpapp')
+
+  when 'rr-webeval'
+    next unless allow_deploy(application, 'rr-webeval', 'nginxphpapp')
+
+  else
+    Chef::Log.info("deploy::infolit - #{application} (in #{cluster_name}) skipped")
+    next
+  end
 
   Chef::Log.info('deploy::infolit - Deployment started.')
   Chef::Log.info("deploy::infolit - Deploying as user: #{deploy['user']} and #{deploy['group']}")
