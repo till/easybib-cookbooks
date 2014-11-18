fad2285756335df4fabf9c86865a08b27ba87862
Florian Holzhauer <fh-github@fholzhauer.de>
2014-11-18 18:52:56 +0100

    quickfix: empty recipe

--- php-fpm/recipes/monit.rb
+++ php-fpm/recipes/monit.rb
@@ -1,15 +1,1 @@
-include_recipe 'monit::service'
 
-template '/etc/monit/conf.d/php-fpm.monitrc' do
-  source 'php-fpm.monit.erb'
-  mode   '0644'
-  owner  'root'
-  group  'root'
-  variables(
-    'pid_file' => '#{node["php-fpm"]["prefix"]}/var/run/php-fpm.pid',
-    'socket_file' => '#{node["php-fpm"]["prefix"]}/var/run/php-fpm.pid',
-    'app_name' => 'php-fpm',
-    'init'     => '/etc/init.d/php-fpm'
-  )
-  notifies :restart, 'service[monit]'
-end
