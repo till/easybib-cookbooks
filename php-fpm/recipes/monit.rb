cdc12f4be1892166ed0a483d9421f98ef24d88c0
Florian Holzhauer <fh-github@fholzhauer.de>
2014-11-18 12:34:36 +0100

    Feature: Monit for php-fpm

--- php-fpm/recipes/monit.rb
+++ php-fpm/recipes/monit.rb
@@ -1,10 +1,12 @@
+include_recipe 'monit::service'
+
 template '/etc/monit/conf.d/php-fpm.monitrc' do
   source 'php-fpm.monit.erb'
   mode   '0644'
   owner  'root'
   group  'root'
   variables(
-    'pid_file' => "#{node["php-fpm"]["prefix"]}/var/run/php-fpm.pid",
+    'pid_file' => "#{node['php-fpm']['prefix']}/var/run/php-fpm.pid",
     'app_name' => 'php-fpm',
     'init'     => '/etc/init.d/php-fpm'
   )
