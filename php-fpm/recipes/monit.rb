aaf53a2357f1647c3a2bbd8702ab3ec0c9aa4c54
Florian Holzhauer <fh-github@fholzhauer.de>
2014-11-19 11:28:05 +0100

    Bugfix: Use double quotes to have variable replacement Fixes conflict with master

--- php-fpm/recipes/monit.rb
+++ php-fpm/recipes/monit.rb
@@ -1,1 +1,12 @@
-
+template '/etc/monit/conf.d/php-fpm.monitrc' do
+  source 'php-fpm.monit.erb'
+  mode   '0644'
+  owner  'root'
+  group  'root'
+  variables(
+    'pid_file' => "#{node["php-fpm"]["prefix"]}/var/run/php-fpm.pid",
+    'app_name' => 'php-fpm',
+    'init'     => '/etc/init.d/php-fpm'
+  )
+  notifies :restart, 'service[monit]'
+end
