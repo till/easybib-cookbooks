e652829b6b93dddb3275b2e693d79d68821b0a2b
till <till@php.net>
2014-11-28 19:24:31 +0100

    Update: delete guest user

--- service/recipes/role-rabbitmq.rb
+++ service/recipes/role-rabbitmq.rb
@@ -1,7 +1,10 @@
 include_recipe 'easybib::setup'
 
 node.set['rabbitmq']['job_control'] = 'upstart' if platform?('ubuntu')
-node.set['rabbitmq']['disabled_users'] = ['guest'] if is_aws
 
 include_recipe 'rabbitmq'
 include_recipe 'rabbitmq::mgmt_console'
+
+rabbitmq_user "guest" do
+  action :delete
+end
