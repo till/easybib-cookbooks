82ff2bf76601dd2e86f7b19ea7ada4e904614690
Florian Holzhauer <fh-github@fholzhauer.de>
2014-11-28 16:16:54 +0100

    Feature: fetch domain_name from opsworks if none provided

--- easybib/resources/nginx.rb
+++ easybib/resources/nginx.rb
@@ -7,7 +7,7 @@ attribute :config_template, :kind_of => String, :required => true
 attribute :config_name, :kind_of => String, :default => ''
 attribute :doc_root, :kind_of => String, :default => 'www'
 attribute :access_log, :kind_of => String, :default => 'off'
-attribute :domain_name, :kind_of => String, :default => ''
+attribute :domain_name, :kind_of => String, :default => nil
 attribute :env_config, :kind_of => String, :default => ''
 attribute :nginx_extras, :kind_of => String, :default => nil
 attribute :nginx_local_conf, :kind_of => String, :default => nil
