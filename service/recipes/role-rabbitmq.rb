9a71a8a307e35ad6cd9d10b042afa9de73ae8adc
till <till@php.net>
2014-11-28 01:54:48 +0100

    Enhancement: delete default guest account on prod/stage

--- service/recipes/role-rabbitmq.rb
+++ service/recipes/role-rabbitmq.rb
@@ -1,6 +1,7 @@
 include_recipe 'easybib::setup'
 
 node.set['rabbitmq']['job_control'] = 'upstart' if platform?('ubuntu')
+node.set['rabbitmq']['disabled_users'] = ['guest'] if is_aws
 
 include_recipe 'rabbitmq'
 include_recipe 'rabbitmq::mgmt_console'
