bae6001352dac2ed9ab0ed1f600e9c5b5221c288
till <till@php.net>
2014-12-05 16:51:27 +0100

    Fix: env config for sitescraper

--- nginx-app/recipes/sitescraper.rb
+++ nginx-app/recipes/sitescraper.rb
@@ -3,10 +3,12 @@ config = 'sitescraper'
 access_log = node['nginx-app']['access_log']
 
 easybib_nginx config do
-  app_name        'sitescraper'
+  app_name        config
   config_template 'internal-api.conf.erb'
   doc_root        'www'
   access_log      access_log
   nginx_extras    'sendfile off;'
   notifies :restart, 'service[nginx]', :delayed
 end
+
+easybib_envconfig config
