db18321da7ee0ef6a8424de745466286d8a702d0
Florian Holzhauer <fh-github@fholzhauer.de>
2015-01-06 17:33:01 +0100

    Add ssl support in vagrant env
    references https://github.com/easybib/issues/issues/3014

---
+++ easybib-deploy/recipes/ssl-vagrant.rb
@@ -0,0 +1,52 @@
+include_recipe 'nginx-lb::default'
+include_recipe 'nginx-lb::service'
+nginx_dir     = node['nginx-lb']['dir']
+ssl_dir       = node['ssl-deploy']['directory']
+int_ip        = node['nginx-lb']['int_ip']
+
+unless node.fetch('vagrant', {})['ssl'].nil?
+  deploy = node['vagrant']['ssl']
+  ssl_certificate     = deploy['ssl_certificate'].chomp
+  ssl_certificate_key = deploy['ssl_certificate_key'].chomp
+
+  directory ssl_dir do
+    mode      '0750'
+    owner     'root'
+    group  node['nginx-app']['group']
+    recursive true
+  end
+
+  template ssl_dir + '/cert.pem' do
+    source 'ssl_key.erb'
+    mode   '0640'
+    owner  'root'
+    group  node['nginx-app']['group']
+    variables(
+      'ssl_key' => ssl_certificate
+    )
+    notifies :restart, 'service[nginx]'
+  end
+
+  template ssl_dir + '/cert.key' do
+    source 'ssl_key.erb'
+    mode   '0640'
+    owner  'root'
+    group  node['nginx-app']['group']
+    variables(
+      'ssl_key' => ssl_certificate_key
+    )
+    notifies :restart, 'service[nginx]'
+  end
+
+  template nginx_dir + '/sites-enabled/easybib-ssl.conf' do
+    source 'nginx-ssl.conf.erb'
+    mode   '0644'
+    owner  'root'
+    group  'root'
+    variables(
+      'ssl_dir' => ssl_dir,
+      'int_ip'  => int_ip,
+    )
+    notifies :restart, 'service[nginx]'
+  end
+end
