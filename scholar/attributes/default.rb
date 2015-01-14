dd8fc1987acc2da818c3cbbfbb0425368b9d93c6
Florian Holzhauer <fh-github@fholzhauer.de>
2015-01-14 09:12:14 +0100

    Adding scholar roles, added haproxy role

---
+++ scholar/attributes/default.rb
@@ -0,0 +1,3 @@
+default['haproxy']['ssl']         = 'on'
+default['haproxy']['type']        = '1.5'
+default['ssl-deploy']['ssl-role'] = 'lb'
---
+++ scholar/recipes/role-lb.rb
@@ -0,0 +1,3 @@
+include_recipe 'easybib::role-generic'
+include_recipe 'easybib-deploy::ssl-certificates'
+include_recipe 'haproxy'
