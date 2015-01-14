28909453cbc17f060e8668b2430920966bab276c
Florian Holzhauer <fh-github@fholzhauer.de>
2015-01-14 12:17:21 +0100

    Use force_default to override default from haproxy cb

--- scholar/attributes/default.rb
+++ scholar/attributes/default.rb
@@ -1,3 +1,3 @@
-default['haproxy']['ssl']         = 'on'
-default['haproxy']['type']        = '1.5'
-default['ssl-deploy']['ssl-role'] = 'lb'
+force_default['haproxy']['ssl']         = 'on'
+force_default['haproxy']['type']        = '1.5'
+force_default['ssl-deploy']['ssl-role'] = 'lb'
