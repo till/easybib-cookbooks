c8edf8a5c04f53c9dcdf5b7f6cc5cf8a0d16350b
Florian Holzhauer <fh-github@fholzhauer.de>
2015-03-05 08:31:15 +0100

    Relocating scholar deployment in own recipe

--- easybib-deploy/recipes/scholar.rb
+++ easybib-deploy/recipes/scholar.rb
@@ -4,17 +4,11 @@ include_recipe 'nginx-app::service'
 node['deploy'].each do |application, deploy|
   listen_opts = nil
   case application
-  when 'api'
-    next unless allow_deploy(application, 'api', 'nginxphpapp')
-  when 'discover_api'
+  when 'scholar'
     listen_opts = 'default_server'
-    next unless allow_deploy(application, 'discover_api', 'nginxphpapp')
-  when 'featureflags'
-    next unless allow_deploy(application, 'featureflags', 'nginxphpapp')
-  when 'id'
-    next unless allow_deploy(application, 'id', 'nginxphpapp')
+    next unless allow_deploy(application, 'scholar', 'nginxphpapp')
   else
-    Chef::Log.info("deploy::silex - #{application} skipped")
+    Chef::Log.info("deploy::scholar - #{application} skipped")
     next
   end
 
@@ -27,7 +21,7 @@ node['deploy'].each do |application, deploy|
   end
 
   easybib_nginx application do
-    config_template 'silex.conf.erb'
+    config_template 'scholar.conf.erb'
     domain_name deploy['domains'].join(' ')
     doc_root deploy['document_root']
     htpasswd "#{deploy['deploy_to']}/current/htpasswd"
