2c5d4b217574195efd58d618b0831d2cdcd6dde5
Florian Holzhauer <fh-github@fholzhauer.de>
2015-03-30 11:28:04 +0200

    Add php-intl to housekeeping for new easybib-citation dep

--- easybib/recipes/role-easybib-housekeeping.rb
+++ easybib/recipes/role-easybib-housekeeping.rb
@@ -1,6 +1,7 @@
 include_recipe 'easybib::role-phpapp'
 include_recipe 'snooze'
 include_recipe 'postfix'
+include_recipe 'php-intl'
 
 if is_aws
   include_recipe 'easybib-deploy::easybib-housekeeping'
