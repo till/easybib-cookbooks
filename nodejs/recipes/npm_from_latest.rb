e6febad788521a67ba97e837b32bf412e9861ebb
till <till@php.net>
2015-05-06 16:48:06 +0200

    Fix: monkey patch helpers to the resources for lazy execution

--- nodejs/recipes/npm_from_latest.rb
+++ nodejs/recipes/npm_from_latest.rb
@@ -1,4 +1,5 @@
-Chef::Recipe.send(:include, NodeJs::Helper)
+Chef::Resource::RemoteFile.send(:include, NodeJs::Helper)
+Chef::Resource::Execute.send(:include, NodeJs::Helper)
 node.force_override['nodejs']['npm']['install_method'] = 'from_latest'
 
 npm_bin = '/usr/bin/npm'
@@ -7,13 +8,11 @@ local_latest = "#{Chef::Config[:file_cache_path]}/install-npm.sh"
 
 npm_version = node['nodejs']['npm']['version']
 
-already_installed = npm_package_installed?('npm', npm_version)
-
 remote_file local_latest do
   source 'https://www.npmjs.org/install.sh'
   mode '0755'
   not_if do
-    File.exist?(local_latest) || already_installed
+    File.exist?(local_latest) || npm_package_installed?('npm', node['nodejs']['npm']['version'])
   end
 end
 
@@ -31,6 +30,6 @@ execute 'Install npm' do
     'npm_install' => npm_version
   })
   not_if do
-    already_installed
+    npm_package_installed?('npm', node['nodejs']['npm']['version'])
   end
 end
