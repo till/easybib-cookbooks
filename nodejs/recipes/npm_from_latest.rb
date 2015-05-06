b7487e08f2046e4af27c6480db1851fda91bd7b0
till <till@php.net>
2015-05-06 15:35:00 +0200

    Fix: remove the lambda magic and use the provided helper function

--- nodejs/recipes/npm_from_latest.rb
+++ nodejs/recipes/npm_from_latest.rb
@@ -1,18 +1,19 @@
+Chef::Recipe.send(:include, NodeJs::Helper)
+node.force_override['nodejs']['npm']['install_method'] = 'from_latest'
+
 npm_bin = '/usr/bin/npm'
 
 local_latest = "#{Chef::Config[:file_cache_path]}/install-npm.sh"
 
 npm_version = node['nodejs']['npm']['version']
 
-is_correct_version = ->(version) {
-  `npm -v`.strip! == version
-}
+already_installed = npm_package_installed?('npm', npm_version)
 
 remote_file local_latest do
   source 'https://www.npmjs.org/install.sh'
   mode '0755'
   not_if do
-    File.exist?(local_latest) || is_correct_version[npm_version]
+    File.exist?(local_latest) || already_installed
   end
 end
 
@@ -30,6 +31,6 @@ execute 'Install npm' do
     'npm_install' => npm_version
   })
   not_if do
-    is_correct_version[npm_version]
+    already_installed
   end
 end
