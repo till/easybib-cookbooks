ed5dd2238c416eeed34cbbd0afd63093a9899299
till <till@php.net>
2015-05-19 17:23:27 +0200

    Update: configure bib-vagrant

--- stack-qa/attributes/default.rb
+++ stack-qa/attributes/default.rb
@@ -1,2 +1,11 @@
 default['stack-qa']['vagrant-ci']['apps'] = []
 default['stack-qa']['vagrant-ci']['plugins'] = ['bib-vagrant', 'vagrant-cachier']
+
+default['stack-qa']['vagrant-ci']['plugin_config'] = {
+  'bib-vagrant' => {
+    'chef_log_level' => 'error',
+    'cookbook_path' => '/opt/aws/opsworks/current/site-cookbooks',
+    'use_nfs' => false,
+    'virtualbox_gui' => false
+  }
+}
--- stack-qa/recipes/role-vagrant-ci.rb
+++ stack-qa/recipes/role-vagrant-ci.rb
@@ -16,6 +16,15 @@ node['deploy'].each do |app, deploy|
 
   next unless allow_deploy(app, deployable_apps, deploy_role)
 
+  # this has to be in this loop so we can access deploy and only do this
+  # when apps are actually deployed ;)
+  template "Create: #{deploy['home']}/.config/easybib/vagrantdefault.yml" do
+    source 'vagrantdefault.yml.erb'
+    variables(
+      :config => node['stack-qa'][deploy_role]['plugin_config']['bib-vagrant']
+    )
+  end
+
   easybib_deploy app do
     deploy_data deploy
     app app
