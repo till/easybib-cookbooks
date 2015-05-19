b8c3d5c9b4fe258c0c5094ec4825e8b7a32a5292
till <till@php.net>
2015-05-19 14:06:47 +0200

    Enhancement: setup to deploy apps

--- stack-qa/recipes/role-vagrant-ci.rb
+++ stack-qa/recipes/role-vagrant-ci.rb
@@ -1,3 +1,17 @@
 include_recipe 'easybib::role-generic'
 include_recipe 'virtualbox'
 include_recipe 'vagrant'
+
+deploy_role = 'vagrant-ci'
+deployable_apps = node['stack-qa'][deploy_role]['apps']
+
+node['deploy'].each do |app, deploy|
+
+  next unless allow_deploy(app, deployable_apps, deploy_role)
+
+  easybib_deploy app do
+    deploy_data deploy
+    app app
+  end
+
+end
