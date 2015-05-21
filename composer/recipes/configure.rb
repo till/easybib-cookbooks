bfb89fb377513a8a08173b0528f56e2efdee1238
till <till@php.net>
2015-05-21 21:01:51 +0200

    Fix: consistent attribute access, remove debug code

--- composer/recipes/configure.rb
+++ composer/recipes/configure.rb
@@ -1,14 +1,12 @@
 deploy_user = node['composer']['environment']
 
-Chef::Log.info("HELLO #{deploy_user['user']}")
-
 return if deploy_user['user'].nil?
 
 home_dir = Dir.home(deploy_user['user'])
 
 directory "#{home_dir}/.composer" do
-  owner     deploy_user[:user]
-  group     deploy_user[:group]
+  owner     deploy_user['user']
+  group     deploy_user['group']
   mode      '0750'
   recursive true
   not_if do
@@ -17,8 +15,8 @@ directory "#{home_dir}/.composer" do
 end
 
 template "#{home_dir}/.composer/config.json" do
-  owner  deploy_user[:user]
-  group  deploy_user[:group]
+  owner  deploy_user['user']
+  group  deploy_user['group']
   source 'composer.config.json.erb'
   mode   '0640'
   variables(
