cdaf4c7b2561844ff205aec078534ba211fbf45a
till <till@php.net>
2015-05-21 20:43:38 +0200

    Fix: remove newrelic

--- easybib/recipes/role-crossref-collector.rb
+++ easybib/recipes/role-crossref-collector.rb
@@ -7,8 +7,5 @@ include_recipe 'php-mysqli::configure'
 
 node.set['composer']['environment'] = get_deploy_user
 include_recipe 'composer::configure'
-include_recipe 'easybib-deploy::crossref-collector'
 
-if is_aws
-  include_recipe 'newrelic' if node['easybib_deploy']['use_newrelic'] == 'yes'
-end
+include_recipe 'easybib-deploy::crossref-collector'
--- easybib/recipes/role-satis.rb
+++ easybib/recipes/role-satis.rb
@@ -9,7 +9,3 @@ include_recipe 'composer::configure'
 
 include_recipe 'nginx-app::configure'
 include_recipe 'easybib-deploy::satis'
-
-if is_aws
-  include_recipe 'newrelic' if node['easybib_deploy']['use_newrelic'] == 'yes'
-end
