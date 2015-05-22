fe75f6b187f77842dba35fc917df3d51f929efcc
till <till@php.net>
2015-05-22 14:15:00 +0200

    Refactor: home in variable + exit early

--- stack-qa/recipes/deploy-vagrant-ci.rb
+++ stack-qa/recipes/deploy-vagrant-ci.rb
@@ -2,17 +2,18 @@ deploy_role = node['stack-qa']['deploy_role']
 deployable_apps = node['stack-qa'][deploy_role]['apps']
 
 deploy_user_name = node['stack-qa'][deploy_role]['deploy_user']
+deploy_user_home = Dir.home(deploy_user_name)
 
 # override deploy-user so it's only used by this recipe
 node['deploy'].each do |app, deploy|
   next unless allow_deploy(app, deployable_apps, deploy_role)
   node.default['deploy'][app]['user'] = deploy_user_name
   node.default['deploy'][app]['group'] = deploy_user_name
-  node.default['deploy'][app]['home'] = Dir.home(deploy_user_name)
+  node.default['deploy'][app]['home'] = deploy_user_home
 end
 
 execute 'fix-home-dir' do
-  command "chown -R #{deploy_user_name}:#{deploy_user_name} /home/#{deploy_user_name}"
+  command "chown -R #{deploy_user_name}:#{deploy_user_name} #{deploy_user_home}"
   user 'root'
   action :nothing
 end
@@ -27,27 +28,27 @@ node['deploy'].each do |app, deploy|
   end
 end
 
-if get_instance_roles.include?(deploy_role)
-  node.default['easybib_vagrant']['environment'] = {
-    'user' => deploy_user_name,
-    'group' => deploy_user_name
-  }
+return unless get_instance_roles.include?(deploy_role)
 
-  include_recipe 'easybib_vagrant'
+node.default['easybib_vagrant']['environment'] = {
+  'user' => deploy_user_name,
+  'group' => deploy_user_name
+}
 
-  # install public key to please keychain
-  public_key = node['stack-qa'][deploy_role]['public_key']
-  file "#{Dir.home(deploy_user_name)}/.ssh/id_dsa.pub" do
-    content public_key
-    mode 0644
-    not_if do
-      public_key.nil?
-    end
+include_recipe 'easybib_vagrant'
+
+# install public key to please keychain
+public_key = node['stack-qa'][deploy_role]['public_key']
+file "#{deploy_user_home}/.ssh/id_dsa.pub" do
+  content public_key
+  mode 0644
+  not_if do
+    public_key.nil?
   end
-
-  node.default['bash']['environment'] = {
-    'user' => deploy_user_name,
-    'group' => deploy_user_name
-  }
-  include_recipe 'bash::profile'
 end
+
+node.default['bash']['environment'] = {
+  'user' => deploy_user_name,
+  'group' => deploy_user_name
+}
+include_recipe 'bash::profile'
