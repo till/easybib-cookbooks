3b422d6c29079f40998c4ae96bdd29dce8026fa1
till <till@php.net>
2015-05-23 02:42:05 +0200

    Enhancement: a few tests for this role

---
+++ stack-qa/spec/role-vagrant-ci_spec.rb
@@ -0,0 +1,37 @@
+require 'chefspec'
+
+describe 'stack-qa::role-vagrant-ci' do
+
+  let(:cookbook_paths) do
+    [
+      File.expand_path("#{File.dirname(__FILE__)}/../../")
+    ]
+  end
+
+  let(:runner) do
+    ChefSpec::Runner.new(
+      :cookbook_path => cookbook_paths,
+      :platform => 'Ubuntu',
+      :version => '12.04'
+    )
+  end
+
+  let(:chef_run) { runner.converge(described_recipe) }
+  let(:node)     { runner.node }
+
+  describe 'role-vagrant-ci' do
+    before do
+      Dir.stub(:home) { '/root' }
+
+      # don't deploy
+      node.set['deploy'] = {}
+    end
+
+    it 'includes the desired cookbooks' do
+      expect(chef_run).to include_recipe('easybib::role-generic')
+      expect(chef_run).to include_recipe('virtualbox')
+      expect(chef_run).to include_recipe('vagrant')
+      expect(chef_run).to include_recipe('stack-qa::deploy-vagrant-ci')
+    end
+  end
+end
