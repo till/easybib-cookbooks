80c6924c0e76f1f8e8d078d3deff4b49d6abebc3
Florian Holzhauer <fh-github@fholzhauer.de>
2015-06-05 13:30:32 +0200

    Relocate ppa function call to receipes

--- redis/attributes/default.rb
+++ redis/attributes/default.rb
@@ -19,7 +19,7 @@ default['redis']['rdb'] = {
   'keys_changed' => 0
 }
 
-default['redis']['ppa'] = ::EasyBib::Ppa.ppa_mirror(node, 'ppa:chris-lea/redis-server')
+default['redis']['ppa'] = 'ppa:chris-lea/redis-server'
 
 # master-slave configuration, allow overriding from opsworks/upstream
 set_unless['redis']['master']             = {}
