864d69fa3235658e0eb47a97a61fb93de9810415
Florian Holzhauer <fh-github@fholzhauer.de>
2015-06-16 14:54:01 +0200

    Workaround: Enable us to deploy two different php repos
    Ticket for the actual clean up later: https://github.com/easybib/issues/issues/4314

--- aptly/attributes/default.rb
+++ aptly/attributes/default.rb
@@ -1,5 +1,4 @@
 default['aptly']                 = {}
-default['aptly']['mirror_name']  = 'easybib_ppa'
 default['aptly']['s3_mirror']    = 'easybib_ppa'
 default['aptly']['s3_mirror_prefix'] = 'prefix'
 default['aptly']['mirror_dir']   = '/tmp/ppa'
