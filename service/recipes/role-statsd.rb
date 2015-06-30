8cb77a2ad862b2a676b99816841e6eb8caee0d78
till <till@php.net>
2015-06-30 17:52:53 +0200

    Fix: set correct deploy user, group and home on OpsWorks
    Related: easybib/issues#4353

--- service/recipes/role-statsd.rb
+++ service/recipes/role-statsd.rb
@@ -1,5 +1,14 @@
 if !is_aws
   include_recipe 'nodejs'
+else
+  # override deploy-user so it's only used by this recipe
+  node['deploy'].each do |app, deploy|
+    next unless allow_deploy(app, deployable_apps, deploy_role)
+    next if app == 'statsd'
+    node.default['deploy'][app]['user'] = node['statsd']['user']
+    node.default['deploy'][app]['group'] = node['statsd']['group']
+    node.default['deploy'][app]['home'] = node['statsd']['deploy_dir']
+  end
 end
 
 include_recipe 'statsd'
