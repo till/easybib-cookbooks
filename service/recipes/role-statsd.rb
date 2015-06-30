694a9a78ef235d7da6182d727c941c17e3afee5a
till <till@php.net>
2015-06-30 22:52:45 +0200

    Fix: reverse condition
    Related: easybib/issues#4353

--- service/recipes/role-statsd.rb
+++ service/recipes/role-statsd.rb
@@ -4,7 +4,7 @@ else
   # override deploy-user so it's only used by this recipe
   node['deploy'].each do |app, deploy|
     next unless allow_deploy(app, deployable_apps, deploy_role)
-    next if app == 'statsd'
+    next if app != 'statsd'
     node.default['deploy'][app]['user'] = node['statsd']['user']
     node.default['deploy'][app]['group'] = node['statsd']['group']
     node.default['deploy'][app]['home'] = node['statsd']['deploy_dir']
