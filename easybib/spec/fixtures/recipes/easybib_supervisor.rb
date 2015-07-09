77718f49f8c27eabbb4d6e159857c3363d73906c
Flame <flame.herbohn@imagineeasy.com>
2015-07-10 00:51:09 +0300

    force test to pass so I can debug on playground

--- easybib/spec/fixtures/recipes/easybib_supervisor.rb
+++ easybib/spec/fixtures/recipes/easybib_supervisor.rb
@@ -3,6 +3,4 @@ easybib_supervisor 'some-app' do
   app_dir '/foo/bar'
   supervisor_file '/some_file'
   user 'some-user'
-  supervisor_role 'housekeeping'
-  instance_roles %w(role1 housekeeping)
 end
