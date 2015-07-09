763b8115e432fad979553d1082238dee57a27e89
Flame <flame@appmo.de>
2015-07-09 15:28:32 +0300

    disable ClassLength limit for test class. It was either that or remove the tests I wrote.

--- easybib/tests/test_easybib.rb
+++ easybib/tests/test_easybib.rb
@@ -2,6 +2,7 @@ require 'test/unit'
 require 'chef'
 require File.join(File.dirname(__FILE__), '../libraries', 'easybib.rb')
 
+# rubocop:disable ClassLength
 class TestEasyBib < Test::Unit::TestCase
   include EasyBib
 
