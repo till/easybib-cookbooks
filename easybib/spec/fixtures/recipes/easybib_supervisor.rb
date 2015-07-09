6401570ae8dd6636bbead8dd8e9b7810a9c80c62
Flame <flame.herbohn@imagineeasy.com>
2015-07-09 23:59:14 +0300

    add roles to supervisor fixture

--- easybib/spec/fixtures/recipes/easybib_supervisor.rb
+++ easybib/spec/fixtures/recipes/easybib_supervisor.rb
@@ -3,4 +3,6 @@ easybib_supervisor 'some-app' do
   app_dir '/foo/bar'
   supervisor_file '/some_file'
   user 'some-user'
+  supervisor_role 'housekeeping'
+  instance_roles ["role1", "housekeeping"]
 end
