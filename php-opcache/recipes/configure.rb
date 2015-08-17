fdd367e8313f09c0b8143e9749e1fe32f40a4ae6
till <till@php.net>
2015-08-17 16:25:02 +0200

    Fix: fc023

--- php-opcache/recipes/configure.rb
+++ php-opcache/recipes/configure.rb
@@ -4,11 +4,10 @@ php_pecl 'opcache' do
   action :setup
 end
 
-if node['php-opcache']['settings'].attribute?('error_log')
-  file node['php-opcache']['settings']['error_log'] do
-    mode 0755
-    owner node['php-fpm']['user']
-    group node['php-fpm']['group']
-    notifies :restart, 'service[php-fpm]', :delayed
-  end
+file node['php-opcache']['settings']['error_log'] do
+  mode 0755
+  owner node['php-fpm']['user']
+  group node['php-fpm']['group']
+  notifies :restart, 'service[php-fpm]', :delayed
+  only_if node['php-opcache']['settings'].attribute?('error_log')
 end
