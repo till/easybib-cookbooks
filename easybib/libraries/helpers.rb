606a869f29536b997c91279b8647295b1a8bd37c
Flame <flame.herbohn@imagineeasy.com>
2015-08-21 16:43:05 +0300

    add get_www_redirect_name method to helpers

--- easybib/libraries/helpers.rb
+++ easybib/libraries/helpers.rb
@@ -7,5 +7,9 @@ module EasyBib
     def fetch_node(node, app, *args)
       ::EasyBib::Config.node(node, app, args)
     end
+    
+    def get_www_redirect_name(domain_name)
+      return domain_name.split(' ').select { |d| d.start_with?('www.') }.map { |d| d[4..-1] }.join(' ')
+    end
   end
 end
