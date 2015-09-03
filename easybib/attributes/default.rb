45733607dab8e9cbab63b846a56134288596b057
till <till@php.net>
2015-09-03 15:56:05 +0200

    Fix: cs (cc @strmpnk)

--- easybib/attributes/default.rb
+++ easybib/attributes/default.rb
@@ -1,11 +1,11 @@
 default['easybib_deploy'] = {
-	'gearman_file' => 'pecl_manager_env',
-	'env_source' => nil,
-	'provide_pear' => false,
-	'cron_path' => '/usr/local/bin:/usr/bin:/bin',
-	'use_newrelic' => 'no',
-	'envtype' => 'playground',
-	'cronjob_role' => nil
+  'gearman_file' => 'pecl_manager_env',
+  'env_source' => nil,
+  'provide_pear' => false,
+  'cron_path' => '/usr/local/bin:/usr/bin:/bin',
+  'use_newrelic' => 'no',
+  'envtype' => 'playground',
+  'cronjob_role' => nil
 }
 
 default['easybib']['enable_ppa_mirror']  = false
