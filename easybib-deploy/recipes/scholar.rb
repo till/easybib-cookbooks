d64a67858a1144ad4ba5663900a233ecc7d94d9e
till <till@php.net>
2015-09-03 15:57:18 +0200

    WIP: deployment for erlang releases

--- easybib-deploy/recipes/scholar.rb
+++ easybib-deploy/recipes/scholar.rb
@@ -18,9 +18,45 @@ node['deploy'].each do |application, deploy|
   Chef::Log.info("deploy::#{application} - Deployment started.")
   Chef::Log.info("deploy::#{application} - Deploying as user: #{deploy[:user]} and #{deploy[:group]}")
 
+  scm_options = node[:opsworks_custom_cookbooks][:scm] if node[:opsworks_custom_cookbooks][:scm][:type].to_s == 's3'
+
+  template '/root/.s3curl' do
+    cookbook 'scm_helper'
+    source 's3curl.erb'
+    mode '0600'
+    variables(
+      :access_key => scm_options[:user],
+      :secret_key => scm_options[:password]
+    )
+    only_if application == 'notebook' && scm_options
+  end
+
+  tmpdir = Dir.mktmpdir('opsworks')
+  directory tmpdir do
+    mode 0755
+    only_if application == 'notebook' && scm_options
+  end
+
+  local_path = "#{tmpdir}/#{File.basename(scm_options[:repository])}"
+
+  execute "Download application from S3: #{scm_options[:repository]}" do
+    command "#{node[:opsworks_agent][:current_dir]}/bin/s3curl.pl --id opsworks -- -o #{local_path} #{scm_options[:repository]}"
+    retries 2
+    retry_delay 10
+    only_if application == 'notebook' && scm_options
+  end
+
+  erlang_otp_release do
+    destination "/srv/www/#{application}/releases"
+    artifact "#{local_path}"
+    reboot true
+    only_if application == 'notebook' && scm_options
+  end
+
   easybib_deploy application do
     deploy_data deploy
     app application
+    only_if application == 'scholar'
   end
 
   easybib_nginx application do
