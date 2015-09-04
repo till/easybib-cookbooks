a901876783fdaafc6151cf763abd7ac947f896a1
Florian Holzhauer <fh-github@fholzhauer.de>
2015-09-04 09:59:32 +0200

    Checkstyle fixes

--- nginx-app/tests/test_helpers.rb
+++ nginx-app/tests/test_helpers.rb
@@ -16,10 +16,10 @@ class TestHelpers < Test::Unit::TestCase
 
     result = ::NginxApp::Helpers.uncached_static_extensions(nil)
     assert_equal(%w(jpg jpeg gif png css js ico woff ttf eot), result)
-    
+
     result = ::NginxApp::Helpers.uncached_static_extensions(cache_config)
     assert_equal(%w(jpg jpeg gif png css js ico woff ttf eot), result)
-    
+
     cache_config['enabled'] = true
     result = ::NginxApp::Helpers.uncached_static_extensions(cache_config)
     assert_equal(%w(jpg jpeg gif png css ico), result)
