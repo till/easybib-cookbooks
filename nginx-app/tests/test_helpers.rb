ef857c6c9eb84bd39601c414eee756399ba7e79c
Florian Holzhauer <fh-github@fholzhauer.de>
2015-09-06 10:07:18 +0200

    Relocate map merging in helper function
    for better testability

--- nginx-app/tests/test_helpers.rb
+++ nginx-app/tests/test_helpers.rb
@@ -3,6 +3,37 @@ require 'chef'
 require File.join(File.dirname(__FILE__), '../libraries', 'helpers.rb')
 
 class TestHelpers < Test::Unit::TestCase
+  def test_merge_alias_config
+    js_alias = {
+      'debugger' => 'debugger',
+      'notes'    => 'notebook'
+    }
+    path = '/some/path'
+    expected = {
+      'js/debugger' => '/some/path/app/modules/debugger',
+      'js/notes' => '/some/path/app/modules/notebook'
+    }
+    result = ::NginxApp::Helpers.merge_alias_config({}, js_alias, 'js', path)
+    assert_equal(expected, result)
+
+    css_alias_config = {
+      'notes' => 'notebook'
+    }
+    alias_config = {
+      'js/debugger' => '/some/path/app/modules/debugger',
+      'js/notes' => '/some/path/app/modules/notebook'
+    }
+    # note: ending slash is intentionally only here, to test if autodetect+add works
+    path = '/some/path/'
+    expected = {
+      'js/debugger' => '/some/path/app/modules/debugger',
+      'js/notes' => '/some/path/app/modules/notebook',
+      'css/notes' => '/some/path/app/modules/notebook'
+    }
+    result = ::NginxApp::Helpers.merge_alias_config(alias_config, css_alias_config, 'css', path)
+    assert_equal(expected, result)
+  end
+
   def test_uncached_static_extensions
     cache_config = {
       'enabled' => false,
