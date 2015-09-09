b5234c5ea234621f6f4eef2ad50413830805bda1
Florian Holzhauer <fh-github@fholzhauer.de>
2015-09-09 11:23:49 +0200

    Bugfix: Wrong scope

--- easybib/libraries/sns.rb
+++ easybib/libraries/sns.rb
@@ -10,13 +10,13 @@ module EasyBib
           :credentials => node['easybib']['sns']['credentials']
         }
 
-        client = Aws::SNS::Client.new(args)
+        client = ::Aws::SNS::Client.new(args)
         resp = client.publish(
           :topic_arn => node['easybib']['sns']['topic'],
           :message => 'Hello world'
         )
         Chef::Log.info "notified sns with message id #{resp[:message_id]}"
-      rescue Aws::SNS::Errors::ServiceError => e
+      rescue ::Aws::SNS::Errors::ServiceError => e
         Chef::Log.warn "unable to send SNS notification: #{e}"
       end
     end
