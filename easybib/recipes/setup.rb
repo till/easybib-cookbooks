3f1f428536011d308d3fc5373bf3929a513498fe
Brian Wiborg <baccenfutter@c-base.org>
2015-09-10 12:27:44 +0200

    implement load-based spin-up notifications

--- easybib/recipes/setup.rb
+++ easybib/recipes/setup.rb
@@ -32,7 +32,45 @@ include_recipe 'easybib::cron'
 include_recipe 'easybib::ruby'
 include_recipe 'easybib::profile'
 
+def send_spinup_notification
+  if !get_cluster_name.empty?
+    instance    = get_instance
+    my_hostname = instance['hostname']
+  else
+    # node.json
+    if node['server_name']
+      my_hostname = node['server_name']
+    # from 'ohai'
+    else
+      my_hostname = node['hostname']
+    end
+  end
+
+  if my_hostname.nil?
+    Chef::Log.error "can not determine hostname of this node!"
+    return
+  end
+
+  if my_hostname.include?('-load')
+    sns_message = "subject: SPIN-UP notification of #{my_hostname}
+
+    The node #{my_hostname} has gone into deployment phase and will be booted shortly after.
+
+    Sincerely yours,
+    EasyBib SNS Library
+    "
+
+    begin
+      ::EasyBib::SNS.sns_notify(my_hostname, sns_message)
+    rescue ::AWS::SNS::Errors::ServiceError => e
+      Chef::Log.warn "unable to send SNS notification: #{e}"
+    end
+  end
+end
+
 if is_aws
+  send_spinup_notification
+
   include_recipe 'fail2ban'
   if node.attribute?('chef_handler_sns') &&
      node['chef_handler_sns'].attribute?('topic_arn') &&
