894f13663bb86eaa6808412fe312670c9b89998e
Brian Wiborg <baccenfutter@c-base.org>
2015-09-14 16:22:38 +0200

    re-introduce call to sns_notify_spinup during setup

--- easybib/recipes/setup.rb
+++ easybib/recipes/setup.rb
@@ -33,6 +33,7 @@ include_recipe 'easybib::ruby'
 include_recipe 'easybib::profile'
 
 if is_aws
+  ::EasyBib::SNS.sns_notify_spinup
   include_recipe 'fail2ban'
   if node.attribute?('chef_handler_sns') &&
      node['chef_handler_sns'].attribute?('topic_arn') &&
