5ddac206335133910e3658089dabcf1bade87de4
Brian Wiborg <baccenfutter@c-base.org>
2015-09-15 15:07:39 +0200

    it is false to imply valid nil arguments here!

--- easybib/libraries/sns.rb
+++ easybib/libraries/sns.rb
@@ -6,7 +6,7 @@ module EasyBib
     #
     # @return [nil]
     extend self
-    def sns_notify_spinup(node = self.node)
+    def sns_notify_spinup(node)
       my_hostname = 'test-bacce-load'
 
       if my_hostname.include?('-load')
