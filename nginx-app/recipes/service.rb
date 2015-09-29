d416248e910f7cab1d5141ee278052eda2741e03
Brian Wiborg <baccenfutter@c-base.org>
2015-09-29 09:50:18 +0200

    moving change to nginx-app::service

--- nginx-app/recipes/service.rb
+++ nginx-app/recipes/service.rb
@@ -1,3 +1,9 @@
+# Nginx installs an Upstart configuration by default. If this file
+# doesn't exist, Ubuntu will fall-back to prehistoric init-system.
+file '/etc/init/nginx.conf' do
+  action :delete
+end
+
 service 'nginx' do
   supports :status => true, :restart => true, :reload => true
   action :nothing
