ac81a01c542106f7359a987bf725b13519d2f8a3
Brian Wiborg <baccenfutter@c-base.org>
2015-10-05 13:31:26 +0200

    order matters; `action :start` is bad here

--- nginx-app/recipes/service.rb
+++ nginx-app/recipes/service.rb
@@ -1,3 +1,8 @@
+service 'nginx' do
+  provider Chef::Provider::Service::Init::Debian
+  supports :status => true, :restart => true, :reload => true
+end
+
 # Nginx installs an Upstart configuration by default. If this file
 # doesn't exist, Ubuntu will fall-back to prehistoric init-system.
 service 'nginx-upstart' do
@@ -13,10 +18,4 @@ end
 file '/etc/init/nginx.conf' do
   action :delete
   ignore_failure true
-end
-
-service 'nginx' do
-  provider Chef::Provider::Service::Init::Debian
-  supports :status => true, :restart => true, :reload => true
-  action :start
 end
