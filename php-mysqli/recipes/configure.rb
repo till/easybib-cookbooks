2a7e0fea1b4c81e5bb64aa3382274c6663559cef
Florian Holzhauer <fh-github@fholzhauer.de>
2015-10-11 13:52:50 +0200

    Refactoring: Use a standard k=v template for mysqli settings

--- php-mysqli/recipes/configure.rb
+++ php-mysqli/recipes/configure.rb
@@ -2,6 +2,9 @@ template "#{node['php-fpm']['prefix']}/etc/php/mysqli-settings.ini" do
   mode '0644'
   source 'mysqli.ini.erb'
   variables(
-    :reconnect => node['php-mysqli']['reconnect']
+    'name' => 'mysqli',
+    'directives' => {
+      'reconnect' => node['php-mysqli']['reconnect']
+    }
   )
 end
