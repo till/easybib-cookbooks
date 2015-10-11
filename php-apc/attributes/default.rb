982a96648f0c98584c3f58b1b40588fdffe7fc7d
Florian Holzhauer <fh-github@fholzhauer.de>
2015-10-11 13:44:52 +0200

    Refactoring: Use a standard k=v template for apc settings

--- php-apc/attributes/default.rb
+++ php-apc/attributes/default.rb
@@ -1,5 +1,4 @@
 default['php-apc'] = {}
-default['php-apc']['stat'] = 0
-default['php-apc']['slam_defense'] = 1
+default['php-apc']['ttl'] = 0
+default['php-apc']['mmap_file_mask'] = '/dev/zero'
 default['php-apc']['shm_size'] = '70M'
-default['php-apc']['max_file_size'] = '2M'
