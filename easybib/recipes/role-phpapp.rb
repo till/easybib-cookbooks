b415e99b1e5933ea17f2e69bb32cb48bf8696429
Brian Wiborg <baccenfutter@c-base.org>
2015-10-13 22:01:00 +0200

    move location from where supervisor is pulled in

--- easybib/recipes/role-phpapp.rb
+++ easybib/recipes/role-phpapp.rb
@@ -9,8 +9,6 @@ include_recipe 'php-intl'
 node.set['composer']['environment'] = get_deploy_user if is_aws
 include_recipe 'composer::configure'
 
-include_recipe 'supervisor'
-
 if node['easybib_deploy']['provide_pear']
   include_recipe 'php-pear'
 end
--- scholar/recipes/role-scholar.rb
+++ scholar/recipes/role-scholar.rb
@@ -1,2 +1,3 @@
+include_recipe 'supervisor'
 include_recipe 'easybib::role-phpapp'
 include_recipe 'easybib-deploy::scholar'
