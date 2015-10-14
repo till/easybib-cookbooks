7455c4c7481861805feb8824db5fe7845af27c9b
Florian Holzhauer <fh-github@fholzhauer.de>
2015-10-14 18:47:15 +0200

    Create academy vagrant recipe

---
+++ academy/metadata.rb
@@ -0,0 +1,12 @@
+name              'academy'
+maintainer        'Till Klampaeckel'
+maintainer_email  'till@php.net'
+license           'BSD License'
+description       'academy roles'
+version           '0.1'
+
+supports 'ubuntu'
+
+depends 'easybib'
+depends 'nginx-app'
+depends 'ies'
