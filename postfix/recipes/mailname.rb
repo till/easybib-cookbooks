cefc9bbde26c717b1fd2a52308e8a35b2a466713
Florian Holzhauer <fh-github@fholzhauer.de>
2015-10-15 08:11:38 +0200

    Refactoring/Cleanup: Use central hostname function

--- postfix/recipes/mailname.rb
+++ postfix/recipes/mailname.rb
@@ -1,21 +1,7 @@
-# fix this for Ubuntu
-if !get_cluster_name.empty?
-  instance    = get_instance
-  my_hostname = instance['hostname']
-else
-  # node.json
-  if node['server_name']
-    my_hostname = node['server_name']
-  # from 'ohai'
-  else
-    my_hostname = node['hostname']
-  end
-end
-
 template '/etc/mailname' do
   mode   '0644'
   source 'mailname.erb'
   variables(
-    :my_hostname => my_hostname
+    :my_hostname => get_hostname(node)
   )
 end
