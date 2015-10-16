3f2e8f42d8d2ec29b53131961e3b5de009975400
Florian Holzhauer <fh-github@fholzhauer.de>
2015-10-16 14:56:04 +0200

    Add rewrite address support

--- postfix/attributes/default.rb
+++ postfix/attributes/default.rb
@@ -1,3 +1,6 @@
 set_unless['postfix']            = {}
-set_unless['postfix']['relay']   = { 'user' => 'foo@example.org', 'pass' => 'test123', 'host' => nil }
-set_unless['postfix']['aliases'] = %w(postmaster root mailer-daemon www-data)
+# host is just hostname, full_host is used as relayhost setting, so e.g. "[foo.com]:578"
+set_unless['postfix']['relay']   = { 'user' => 'foo@example.org', 'pass' => 'test123', 'host' => nil, 'full_host' => nil }
+set_unless['postfix']['aliases'] = %w(postmaster mailer-daemon www-data)
+#if true: rewrites all from addresses to sysadmin_mail
+set_unless['postfix']['rewrite_address'] = false
