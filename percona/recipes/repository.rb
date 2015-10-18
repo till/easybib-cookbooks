5231baef74c30cfbef768db58e53a3ad370a6773
till <till@php.net>
2015-10-18 20:54:43 +0200

    Update: delete percona recipe :tada:

--- percona/recipes/repository.rb
+++
@@ -1,8 +0,0 @@
-apt_repository 'percona' do
-  uri          'http://repo.percona.com/apt'
-  arch         'amd64'
-  distribution node['lsb']['codename']
-  components   ['main']
-  keyserver    node['percona']['keyserver']
-  key          node['percona']['key']
-end
