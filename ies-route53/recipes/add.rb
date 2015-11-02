b79a8194c72647cc402985d43e6c9635e1e70b91
Brian Wiborg <baccenfutter@c-base.org>
2015-11-02 15:24:49 +0100

    really fix bug in node attribute handling

--- ies-route53/recipes/add.rb
+++ ies-route53/recipes/add.rb
@@ -6,7 +6,7 @@ include_recipe 'route53'
 route53_record 'create a record' do
   host_name = get_hostname(node, true)
   stack_name = get_normalized_cluster_name(node)
-  zone_name = node['ies-route53']['name']
+  zone_name = node['ies-route53']['zone']['name']
   name "#{host_name}.#{stack_name}.#{zone_name}"
   value Net::HTTP.get(URI.parse('http://169.254.169.254/latest/meta-data/public-ipv4'))
   type  'A'
