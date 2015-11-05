e9e6d64ca986a3e3e0045c2f3cffb4ea455f3100
Joel Gilley <joel.gilley@ImagineEasy.com>
2015-11-05 11:48:00 -0500

    Changes to implement udp loopback logs as per https://github.com/easybib/issues/issues/4152

--- haproxy/recipes/logs.rb
+++ haproxy/recipes/logs.rb
@@ -20,3 +20,25 @@ if (node.fetch('lsb', {})['codename'] == 'precise')
     group 'adm'
   end
 end
+if (node.fetch('lsb', {})['codename'] == 'trusty')
+  # trusty has different log routing, it wont work there
+  include_recipe 'rsyslogd'
+
+  template '/etc/rsyslog.d/10-haproxy.conf' do
+    source 'haproxy-logs-trusty.erb'
+    mode '0644'
+    notifies :restart, 'service[rsyslog]'
+  end
+
+  directory node['haproxy']['log_dir'] do
+    recursive true
+    mode '0755'
+  end
+
+  template '/etc/logrotate.d/haproxy' do
+    source 'logrotate.erb'
+    mode '0644'
+    owner 'syslog'
+    group 'adm'
+  end
+end
