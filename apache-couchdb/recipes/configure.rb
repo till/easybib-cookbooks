952dd154e1c46a254a88aa56f7e498a35f61a8c2
Florian Holzhauer <fh-github@fholzhauer.de>
2015-11-06 09:50:42 +0100

    Fix: Expect passwords to be pre-hashed to prevent couchdb restarts on each deploy
    https://github.com/easybib/issues/issues/5081

--- apache-couchdb/recipes/configure.rb
+++ apache-couchdb/recipes/configure.rb
@@ -14,6 +14,11 @@ node['apache-couchdb']['config'].each do |section, config|
 
   if section == 'admins'
     ini_file = "#{local_dir}/local.ini"
+    config.each do |user, pw|
+      unless pw.start_with?('-pbkdf2-')
+        Chef::Log.error('Unencrypted passwords in node attr will lead to couch restart on each deploy!')
+      end
+    end
   else
     ini_file = "#{local_dir}/#{section}.ini"
   end
@@ -25,23 +30,5 @@ node['apache-couchdb']['config'].each do |section, config|
       :config => config
     )
     notifies :restart, 'service[couchdb]', :delayed
-    not_if do
-      section == 'admins'
-    end
-  end
-
-  # write admins
-  # this is a hack because 'local.ini' is always the last
-  # .ini file read in the chain, so we add the admins here!
-  template ini_file do # ~FC022
-    source 'local.ini.erb'
-    variables(
-      :section => section,
-      :config => config
-    )
-    notifies :restart, 'service[couchdb]', :delayed
-    only_if do
-      section == 'admins' && !::File.exist?(ini_file)
-    end
   end
 end
