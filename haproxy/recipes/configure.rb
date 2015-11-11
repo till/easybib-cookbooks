545e83886702f349c214be64a9afb713b7b0110e
Joel Gilley <gilleyj@gmail.com>
2015-11-11 17:01:04 -0500

    I don't seem to need that switch

--- haproxy/recipes/configure.rb
+++ haproxy/recipes/configure.rb
@@ -1,9 +1,5 @@
 include_recipe 'haproxy::service'
 
-if node['haproxy']['logging']['enabled'] == true
-  include_recipe 'haproxy::logs'
-end
-
 directory '/etc/haproxy/errors/' do
   recursive true
   mode '0755'
