e1b6a944792aa5e97502dbe74fe99ac04742e7bb
Brian Wiborg <baccenfutter@c-base.org>
2015-11-11 09:18:25 +0100

    check nginx config after changes

--- nginx-app/recipes/service.rb
+++ nginx-app/recipes/service.rb
@@ -2,3 +2,8 @@ service 'nginx' do
   provider Chef::Provider::Service::Upstart
   supports :status => true, :restart => true, :reload => true
 end
+
+execute 'configtest' do
+  command '/usr/sbin/nginx -t'
+  action :nothing
+end
