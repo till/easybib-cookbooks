8968a68e60ce935aa399ba12bfbffac8959e32bc
Florian Holzhauer <fh-github@fholzhauer.de>
2015-11-23 12:24:02 +0100

    Cleanup: Remove now obsolete php-mysqli cookbook

--- php-mysqli/recipes/configure.rb
+++
@@ -1,8 +0,0 @@
-template "#{node['php-fpm']['prefix']}/etc/php/mysqli-settings.ini" do
-  mode '0644'
-  source 'mysqli.ini.erb'
-  variables(
-    'name' => 'mysqli',
-    'directives' => node['php-mysqli']['settings']
-  )
-end
