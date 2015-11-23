d8c4fa8150735008de4721910beb2c5a66e5f3c0
Florian Holzhauer <fh-github@fholzhauer.de>
2015-11-23 12:14:54 +0100

    Relocate php-mysqli in php cookbook
    DEVOPS-18

--- php/attributes/default.rb
+++ php/attributes/default.rb
@@ -4,3 +4,5 @@ default['php-apc'] = {}
 default['php-apc']['settings']['ttl'] = 0
 default['php-apc']['settings']['mmap_file_mask'] = '/dev/zero'
 default['php-apc']['settings']['shm_size'] = '70M'
+
+default['php-mysqli']['settings']['reconnect'] = 1
