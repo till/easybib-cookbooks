7a308866b45338a3b5b9f0caa2d55bb49376b9a4
Brian Wiborg <baccenfutter@c-base.org>
2015-11-25 15:30:02 +0100

    move php-poppler-pdf to php::module-poppler-pdf

---
+++ php/spec/module-poppler-pdf_spec.rb
@@ -0,0 +1,17 @@
+require_relative 'spec_helper.rb'
+
+describe 'php::module-poppler-pdf' do
+  let(:chef_run) do
+    ChefSpec::Runner.new.converge(described_recipe)
+  end
+
+  it 'adds ppa mirror configuration' do
+    expect(chef_run).to include_recipe('apt::ppa')
+    expect(chef_run).to include_recipe('apt::easybib')
+  end
+
+  it 'installs and configures the extension' do
+    expect(chef_run).to install_php_ppa_package('poppler-pdf')
+  end
+
+end
