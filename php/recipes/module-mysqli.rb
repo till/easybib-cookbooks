51ee9a6493e3abfbe8a812479cdda895d133fa8c
Florian Holzhauer <fh-github@fholzhauer.de>
2015-11-27 13:10:57 +0100

    Bugfix: php-mysqli is part of core, needs only config
    DEVOPS-18

--- php/recipes/module-mysqli.rb
+++ php/recipes/module-mysqli.rb
@@ -1,7 +1,14 @@
 include_recipe 'php::dependencies-ppa'
 
 module_config = node['php-mysqli']['settings']
+package_name = node['php']['ppa']['package_prefix']
 
-php_ppa_package 'mysqli' do
+package package_name do
+  action :install
+  notifies :reload, 'service[php-fpm]', :delayed
+end
+
+php_config 'mysqli' do
   config module_config
+  notifies :reload, 'service[php-fpm]', :delayed
 end
