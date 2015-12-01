46dea8f42705688ec4e47d6431b324bab11cdc69
Brian Wiborg <baccenfutter@c-base.org>
2015-12-01 11:35:39 +0100

    introducing stack-research

---
+++ stack-research/metadata.rb
@@ -0,0 +1,14 @@
+name              'stack-research'
+maintainer        'Till Klampaeckel'
+maintainer_email  'till@php.net'
+license           'BSD License'
+description       'research roles'
+version           '0.1'
+
+supports 'ubuntu'
+
+depends 'easybib'
+depends 'easybib-deploy'
+depends 'php'
+depends 'nginx-app'
+depends 'memcache'
