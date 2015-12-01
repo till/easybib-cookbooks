d05d48b4f66e44cb46cc21c9ec2ab406da223465
Brian Wiborg <baccenfutter@c-base.org>
2015-12-01 11:46:38 +0100

    introducing stack-api cookbook

---
+++ stack-api/metadata.rb
@@ -0,0 +1,12 @@
+name              'stack-api'
+maintainer        'Till Klampaeckel'
+maintainer_email  'till@php.net'
+license           'BSD License'
+description       'API roles'
+version           '0.1'
+
+supports 'ubuntu'
+
+depends 'easybib'
+depends 'easybib-deploy'
+depends 'nginx-app'
