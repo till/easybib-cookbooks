82843a3b1888fde6b7fe3b6f4f1f3bd45642cb5a
Florian Holzhauer <fh-github@fholzhauer.de>
2015-12-01 11:35:03 +0100

    Attempt to override deploy user and coordinates

--- stack-qa/recipes/deploy-vagrant-ci.rb
+++ stack-qa/recipes/deploy-vagrant-ci.rb
@@ -4,6 +4,15 @@ deployable_apps = node['stack-qa'][deploy_role]['apps']
 node['deploy'].each do |app, deploy|
   next unless allow_deploy(app, deployable_apps, deploy_role)
 
+  deploy.set['user'] = 'vagrant-ci'
+  deploy.set['group'] = 'vagrant-ci'
+  deploy.set['home'] = '/home/vagrant-ci'
+  deploy.set['shell'] = '/bin/bash'
+
+  opsworks_deploy_user do
+    deploy_data deploy
+  end
+
   easybib_deploy app do
     deploy_data deploy
     app app
