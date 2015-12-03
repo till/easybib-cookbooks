70cafaf9acd646c43b665b77f4113ae928bb5ed6
Brian Wiborg <baccenfutter@c-base.org>
2015-12-03 12:31:52 +0100

    adjust tests for this change

--- nginx-app/spec/redirector_spec.rb
+++ nginx-app/spec/redirector_spec.rb
@@ -66,10 +66,9 @@ describe 'nginx-app::redirector' do
           )
 
         locations.each do |from, to|
-          from = 'default' if from == '/'
           expect(chef_run).to render_file("#{conf_dir}/urls-#{domain_name}.conf")
             .with_content(
-              include("#{from} #{to};")
+              include("#{from} " + '"' + "#{to}" + '";')
             )
         end
       end
