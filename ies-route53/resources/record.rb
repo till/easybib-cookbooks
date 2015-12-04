0c85b7d61ee56b94e4ee62e3234052ed9d887d70
Brian Wiborg <baccenfutter@c-base.org>
2015-12-04 17:53:03 +0100

    fix copy+paste bug

--- ies-route53/resources/record.rb
+++ ies-route53/resources/record.rb
@@ -3,7 +3,7 @@ actions :create
 default_action :create
 
 attribute :name,                  :kind_of => String, :required => true, :name_attribute => true
-attribute :value,                 :kind_of => [String, Array]
+attribute :value,                 :kind_of => String, :required => true
 attribute :type,                  :kind_of => String, :required => true
 attribute :ttl,                   :kind_of => Integer, :default => 3600
 attribute :zone_id,               :kind_of => String
