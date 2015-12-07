3921f9f8c81ec8ce6c4b2bd8304e37534066afbf
Brian Wiborg <baccenfutter@c-base.org>
2015-12-07 16:28:48 +0100

    add tests

--- nginx-app/spec/redirector_spec.rb
+++ nginx-app/spec/redirector_spec.rb
@@ -36,4 +36,30 @@ describe 'nginx-app::redirector' do
         )
     end
   end
+
+  describe 'setup single url redirect' do
+    before do
+      node.set['redirector']['urls'] = {
+        'john-doe.example.org' => {
+          '/' => 'http://example.com'
+        }
+      }
+    end
+
+    it 'creates the configuration file' do
+      expect(chef_run).to create_template(vhost_urls)
+    end
+
+    it 'creates the rewrite rule' do
+      node['redirector']['urls'].each do |domain_name, rewrites|
+        expect(chef_run).to render_file("#{conf_dir}/urls-#{domain_name}.conf")
+          .with_content(include("server_name #{domain_name};"))
+
+        rewrites.each do |location, target|
+          expect(chef_run).to render_file("#{conf_dir}/urls-#{domain_name}.conf")
+            .with_content(include("rewrite #{location} http://#{target} permanent;"))
+        end
+      end
+    end
+  end
 end
