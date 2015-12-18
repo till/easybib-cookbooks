b8b83d9f0d3b7354d6e8d6e65580163f87fac350
Brian Wiborg <baccenfutter@c-base.org>
2015-12-18 16:05:57 +0100

    entirely new approach

--- ies-route53/providers/record.rb
+++ ies-route53/providers/record.rb
@@ -1,82 +1,46 @@
-begin
-  require 'aws-sdk-v1'
-rescue
-  Chef::Application.fatal!('aws-sdk-v1 missing!')
-end
+require 'aws-sdk-v1'
 
-def credentials
-  {
-    :aws_access_key_id => new_resource.aws_access_key_id,
-    :aws_secret_access_key => new_resource.aws_secret_access_key
-  }
-end
-
-def name
-  @name ||= begin
-    return new_resource.name + '.' if new_resource.name !~ /\.$/
-    new_resource.name
-  end
-end
-
-def value
-  @value ||= new_resource.value
-end
-
-def type
-  @type ||= new_resource.type
-end
-
-def ttl
-  @ttl ||= new_resource.ttl
-end
-
-def overwrite
-  @overwrite ||= new_resource.overwrite
-end
-
-def zone
-  r53 = AWS::Route53.new(credentials)
-  r53.hosted_zones.each do |zone|
-    if zone.name == name
-      return zone
-    end
-  end
-  Chef::Application.fatal!("Zone does not exist: #{name}")
-end
-
-def record
-  zone.rrsets[name, type]
+def same_record?(record)
+  record.resource_records[:value] == @value && record.ttl == @ttl && record.type == @type
 end
 
 action :create do
-  def same_record?(record)
-    record.resource_records[0][:value] == value && record.ttl == ttl
-  end
+  Chef::Log.info('Calling IES-Route53 Record-Update...')
 
-  batch = AWS::Route53::ChangeBatch.new(zone.hosted_zone_id)
+  r53 = AWS::Route53.new(
+    :aws_access_key_id => new_resource.aws_access_key_id,
+    :aws_secret_access_key => new_resource.aws_secret_access_key
+  )
 
-  if record.exists?
+  zone_id = new_resource.zone_id
+  overwrite = new_resource.overwrite
+  @name = new_resource.name
+  @ttl = new_resource.ttl
+  @type = new_resource.type
+  @value = new_resource.value
+
+  zone = r53.hosted_zones.find { |z| z.id == zone_id }
+
+  fqdn = @name + '.' + zone
+  record = zone.rrsets[fqdn, @type]
+  Chef::Log.info("Selected record for update: #{fqdn}")
+
+  if rrset.exists?
     if same_record?(record)
-      Chef::Log.info("Not touching this record as nothing seems to have changed: #{name}")
+      Chef::Log.info("Unchanged, not updating: #{fqdn}")
     else
       if overwrite
-        Chef::Log.info("Update record: #{name}")
-        batch << AWS::Route53::DeleteRequest.new(name, type)
-        batch << AWS::Route53::CreateRequest.new(name, type, :ttl => ttl, :resource_records => [{ :value => value }])
+        Chef::Log.info("Overwriting already existing record: #{fqdn}")
+        record.resource_records = [{ :value => @value }]
+        record.update
         new_resource.updated_by_last_action(true)
       else
-        Chef.Log.warn("Update required, but override not set: #{name}")
+        Chef::Log.warn("Skipping update! OVERWRITE is not set: #{fqdn}")
       end
     end
   else
-    Chef::Log.info("Creating new record: #{name}")
-    batch << AWS::Route53::CreateRequest.new(name, type, :ttl => ttl, :resource_records => [{ :value => value }])
+    Chef::Log.info("Creating new record for: #{fqdn}")
+    record.create
     new_resource.updated_by_last_action(true)
-  end
-
-  begin
-    batch.call
-  rescue
-    Chef::Application.fatal!('Change batch failed!')
   end
 end
