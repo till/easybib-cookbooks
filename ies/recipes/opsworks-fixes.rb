23a7786723efc2ca489e45c5c19e671ea038ace0
Brian Wiborg <baccenfutter@c-base.org>
2016-01-05 16:12:14 +0100

    ensure correct formatting and mounting of all devs

--- ies/recipes/opsworks-fixes.rb
+++ ies/recipes/opsworks-fixes.rb
@@ -12,3 +12,39 @@ execute 'Generate missing ssh host keys' do
   command 'ssh-keygen -A'
   ignore_failure true
 end
+
+# ensures mounting of all devices:
+# - https://imagineeasy.atlassian.net/browse/DEVOPS-77
+if is_aws
+  directory '/mnt2' do
+    owner 'root'
+    group 'root'
+    mode '0755'
+    action :create
+    only_if do
+      File.exist?('/dev/xvdc')
+    end
+  end
+  execute 'format xvdc if required' do
+    # AWS auto-formats its volumes with ext3. We want ext4 for performance
+    # reasons. If we find an ext3 formatted xvdc device, we assume that the
+    # volume is "new" (as in: has no data yet) and format it with ext4.
+    command 'file -sL /dev/xvdc | grep "\<ext3\>" && mkfs.ext4 /dev/xvdc'
+    ignore_failure true
+    only_if do
+      File.exist?('/dev/xvdc')
+    end
+  end
+  template '/etc/fstab' do
+    source 'fstab'
+    owner 'root'
+    group 'root'
+    mode '0644'
+    only_if do
+      File.exist?('/dev/xvdc')
+    end
+  end
+  execute 'mount all' do
+    command 'mount -a'
+  end
+end
