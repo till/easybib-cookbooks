0c18c5ff1a881a08a23cad2016cf3a3d282c25b4
till <till@php.net>
2016-01-25 16:53:33 +0100

    Enhancement: clean-up from loggly::setup

--- ies/recipes/setup-syslog.rb
+++ ies/recipes/setup-syslog.rb
@@ -4,4 +4,16 @@ else
   Chef::Log.info("Setting up papertrail: #{node['papertrail']['remote_host']}:#{node['papertrail']['remote_port']}")
   node.set['papertrail']['hostname_name'] = get_record_name
   include_recipe 'papertrail'
+
+  # this can be delete later, previously setup in loggly::setup
+  old_files = ['/etc/rsyslog.d/49-loggly.conf', '/etc/rsyslog.d/11-filewatcher.conf']
+
+  old_files.each do |old_file|
+    file old_file do
+      action :delete
+      only_if do
+        File.exist?(old_file)
+      end
+    end
+  end
 end
