f06b2114112cb4eeaa3e3e40f0aaa35e27cd9bd9
fh <fh-gitcommit@fholzhauer.de>
2016-02-09 13:04:42 +0100

    Fix permissions for nginx config

--- stack-qa/recipes/deploy-sinopia-nginx.rb
+++ stack-qa/recipes/deploy-sinopia-nginx.rb
@@ -3,7 +3,7 @@ node['deploy'].each do |application, deploy|
   next unless allow_deploy(application, 'sinopia', 'npm-proxy')
 
   if node.fetch('sinopia', {})['listen'].nil?
-    sinopia_listener = '127.0.0.1:4873'  
+    sinopia_listener = '127.0.0.1:4873'
   else
     sinopia_listener = node['sinopia']['listen']
   end
@@ -11,8 +11,6 @@ node['deploy'].each do |application, deploy|
   template '/etc/nginx/sites-enabled/sinopia-forward.conf' do
     source 'nginx-sinopia.conf.erb'
     mode   '0644'
-    owner  'root'
-    group  'root'
     variables(
       'sinopia' => sinopia_listener,
       'domains' => deploy['domains']
