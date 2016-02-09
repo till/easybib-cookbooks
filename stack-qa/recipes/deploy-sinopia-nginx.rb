9cec1c52783fcc5b6876bf6f577ce577af40b481
fh <fh-gitcommit@fholzhauer.de>
2016-02-09 11:22:00 +0100

    Add deployment recipes

---
+++ stack-qa/recipes/deploy-sinopia-nginx.rb
@@ -0,0 +1,22 @@
+node['deploy'].each do |application, deploy|
+
+  next unless allow_deploy(application, 'sinopia', 'npm-proxy')
+
+  if node.fetch('sinopia', {}).['listen'].nil?
+    sinopia_listener = '127.0.0.1:4873'  
+  else
+    sinopia_listener = node['sinopia']['listen']
+  end
+
+  template nginx_dir + '/sites-enabled/sinopia-forward.conf' do
+    source 'nginx-sinopia.conf.erb'
+    mode   '0644'
+    owner  'root'
+    group  'root'
+    variables(
+      'sinopia' => sinopia_listener,
+      'domains' => deploy['domains']
+    )
+    notifies :reload, 'service[nginx]', :delayed
+  end
+end
