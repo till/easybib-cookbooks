1d5d64a42e01220bfa2738588068965d0626579b
Brian Wiborg <baccenfutter@c-base.org>
2016-02-23 16:05:56 +0100

    use deploy::public-api
    ... rather than nginx-app::configure

---
+++ easybib-deploy/recipes/public-api.rb
@@ -0,0 +1,41 @@
+include_recipe 'nginx-app::server'
+
+cluster_name = get_cluster_name
+
+node['deploy'].each do |application, deploy|
+  Chef::Log.info("nginx-app::configure - app: #{application}")
+
+  case application
+  when 'easybib'
+    nginxphpapp_allowed = allow_deploy(application, 'easybib', 'nginxphpapp')
+    testapp_allowed     = allow_deploy(application, 'easybib', 'testapp')
+    if !nginxphpapp_allowed && !testapp_allowed
+      Chef::Log.info('nginx-app::configure - skipping easybib, allow_deploy mismatch')
+      next
+    end
+
+  when 'easybib_api'
+    next unless allow_deploy(application, 'easybib_api', 'bibapi')
+
+  else
+    Chef::Log.info("deploy::#{application} (in #{cluster_name}) skipped.")
+    next
+  end
+
+  Chef::Log.info("deploy::#{application} - Deployment started.")
+  Chef::Log.info("deploy::#{application} - Deploying as user: #{deploy[:user]} and #{deploy[:group]}")
+
+  easybib_deploy application do
+    deploy_data deploy
+    app application
+  end
+
+  easybib_nginx application do
+    config_template 'internal-api.conf.erb'
+    domain_name deploy['domains'].join(' ')
+    doc_root deploy['document_root']
+    access_log      'off'
+    notifies :reload, 'service[nginx]', :delayed
+    notifies node['easybib-deploy']['php-fpm']['restart-action'], 'service[php-fpm]', :delayed
+  end
+end
