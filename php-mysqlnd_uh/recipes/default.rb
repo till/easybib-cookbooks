9ed9d3b23199603768fad05fb587dada8c68afe0
fh <fh-gitcommit@fholzhauer.de>
2016-02-24 14:17:26 +0100

    Remove deprecated mysqlnd-uh extension
    https://github.com/easybib/www/pull/1706

--- php-mysqlnd_uh/recipes/default.rb
+++
@@ -1,7 +0,0 @@
-include_recipe 'php-mysqlnd_uh::compile'
-include_recipe 'php-fpm::service'
-
-execute 'ensure extension is loaded' do
-  command  "cat 'extension=mysqlnd_uh.so' > /usr/local/etc/php/mysqlnd_uh.ini"
-  notifies :reload, 'service[php-fpm]', :delayed
-end
