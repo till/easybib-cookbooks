41c37a6c3837fb4ce9b2f84255d54c1cd994db8b
Brian Wiborg <baccenfutter@c-base.org>
2016-02-29 14:34:02 +0100

    add stack-fruitkid

---
+++ stack-fruitkid/metadata.rb
@@ -0,0 +1,16 @@
+name              'stack-citationapi'
+maintainer        'Till Klampaeckel'
+maintainer_email  'till@php.net'
+license           'BSD License'
+description       'CitationAPI roles'
+version           '0.1'
+
+supports 'ubuntu'
+
+depends 'stack-easybib'
+depends 'easybib-deploy'
+depends 'ies'
+depends 'memcache'
+depends 'nginx-app'
+depends 'php'
+depends 'postfix'
