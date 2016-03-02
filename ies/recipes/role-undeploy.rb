91654eedb44c520168362428b352756d12068ba8
Joel Gilley <gilleyj@gmail.com>
2016-03-02 11:57:40 -0500

    hmm rubocop didn't complain/travis rubocop did

--- ies/recipes/role-undeploy.rb
+++ ies/recipes/role-undeploy.rb
@@ -1,12 +1,10 @@
-unless is_aws
-  unless node.fetch('nginx-app', {})['user'].nil?
-    crontab_user = node.fetch('nginx-app', {})['user']
-    node['deploy'].each do |application, deploy|
-      easybib_crontab application do
-        crontab_user crontab_user
-        app application
-        action :delete
-      end
+unless is_aws && node.fetch('nginx-app', {})['user'].nil?
+  crontab_user = node.fetch('nginx-app', {})['user']
+  node['deploy'].each do |application, deploy|
+    easybib_crontab application do
+      crontab_user crontab_user
+      app application
+      action :delete
     end
   end
 end
