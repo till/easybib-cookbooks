2660c655e2a290759fc47776be6f723a75192f63
Joel Gilley <gilleyj@gmail.com>
2016-03-03 15:03:24 -0500

    file declaration removed from role-undeploy

--- easybib/resources/supervisor.rb
+++ easybib/resources/supervisor.rb
@@ -3,6 +3,7 @@ actions :create, :delete
 default_action :create
 
 attribute :app, :kind_of => String, :default => ''
+attribute :deploy_data, :default => {}
 attribute :app_dir, :kind_of => String, :default => ''
 attribute :supervisor_file, :kind_of => String, :default => ''
 attribute :user, :kind_of => String, :default => ''
