1e7e28a835bb6ee6cb63f1384fcf4dc78665c18b
Joel Gilley <gilleyj@gmail.com>
2016-03-03 16:07:50 -0500

    Rubo

--- ies/recipes/role-undeploy.rb
+++ ies/recipes/role-undeploy.rb
@@ -8,7 +8,7 @@ node['deploy'].each do |application, deploy|
   easybib_crontab application do
     app application
     crontab_user node['easybib_deploy']['crontab_user']
-    crontab_file = "#{application_root_dir}/deploy/crontab"
+    crontab_file "#{application_root_dir}/deploy/crontab"
     action :delete
   end
 end
