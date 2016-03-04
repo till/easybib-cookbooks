8bde61e2014738b71b0323232e7754d740eced5d
till <till@php.net>
2016-03-04 18:18:14 +0100

    Fix: force binary (instead of package)
    Related: DEVOPS-116

--- stack-scholar/recipes/role-consumer.rb
+++ stack-scholar/recipes/role-consumer.rb
@@ -4,6 +4,7 @@ include_recipe 'stack-easybib::role-phpapp'
 unless node['haproxy']['websocket_layers'].nil?
   if has_role?(get_instance_roles, node['haproxy']['websocket_layers'].keys.first)
     node.set['nodejs']['version'] = '4.2.6'
+    node.set['nodejs']['install_method'] = 'binary'
     include_recipe 'nodejs::install'
   end
 end
