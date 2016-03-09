2a20fecab74f477c0564a48ca5fdf67063a3082a
till <till@php.net>
2016-03-10 00:44:13 +0100

    Fix: update include from db184ba
    Related: DEVOPS-42

--- stack-academy/recipes/role-nginxphpapp.rb
+++ stack-academy/recipes/role-nginxphpapp.rb
@@ -4,7 +4,7 @@ include_recipe 'supervisor'
 if is_aws
   include_recipe 'easybib-deploy::ssl-certificates'
   include_recipe 'nodejs::npm_from_source' # for frontend assets build in before_symlink.rb
-  include_recipe 'easybib-deploy::infolit'
+  include_recipe 'stack-academy::deploy'
 else
   include_recipe 'nginx-app::vagrant'
 end
