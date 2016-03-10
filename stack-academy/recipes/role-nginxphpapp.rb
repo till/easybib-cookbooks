b2e6edfa7cf6b754e367e5d853a4236dc5d15629
till <till@php.net>
2016-03-10 13:07:14 +0100

    Update: remove nodejs (for playground/production)
    cc @dazz @sodoku
    
    Related: DEVOPS-42

--- stack-academy/recipes/role-nginxphpapp.rb
+++ stack-academy/recipes/role-nginxphpapp.rb
@@ -3,7 +3,6 @@ include_recipe 'supervisor'
 
 if is_aws
   include_recipe 'easybib-deploy::ssl-certificates'
-  include_recipe 'nodejs::npm_from_source' # for frontend assets build in before_symlink.rb
   include_recipe 'stack-academy::deploy'
 else
   include_recipe 'nginx-app::vagrant'
