cad802eb2ecf23f22a309ddee5a2709db66c90c8
till <till@php.net>
2016-03-10 13:09:30 +0100

    Refactor: remove vagrant specifics
    Related: DEVOPS-42

--- stack-academy/recipes/role-nginxphpapp.rb
+++ stack-academy/recipes/role-nginxphpapp.rb
@@ -1,9 +1,5 @@
 include_recipe 'stack-easybib::role-phpapp'
 include_recipe 'supervisor'
 
-if is_aws
-  include_recipe 'easybib-deploy::ssl-certificates'
-  include_recipe 'stack-academy::deploy'
-else
-  include_recipe 'nginx-app::vagrant'
-end
+include_recipe 'easybib-deploy::ssl-certificates'
+include_recipe 'stack-academy::deploy'
