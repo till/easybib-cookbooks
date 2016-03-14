3793b7740db8209d6c831fb7e667f10049509ceb
till <till@php.net>
2016-03-14 14:07:39 +0100

    Fix: removed attribute (see #947)
    Related: DEVOPS-119

--- easybib/providers/deploy_manager.rb
+++ easybib/providers/deploy_manager.rb
@@ -41,7 +41,6 @@ action :deploy do
         domain_name deploy['domains'].join(' ')
         doc_root deploy['document_root']
         htpasswd "#{deploy['deploy_to']}/current/htpasswd"
-        nginx_local_conf "#{app_dir}/deploy/nginx.conf"
         notifies :reload, 'service[nginx]', :delayed
         notifies node['easybib-deploy']['php-fpm']['restart-action'], 'service[php-fpm]', :delayed
       end
