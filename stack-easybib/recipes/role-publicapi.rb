348531848cee370c0e760504b374a5b2cb0b7625
Brian Wiborg <baccenfutter@c-base.org>
2016-03-17 13:26:09 +0100

    split deploy-easybib and deploy-gearman-worker

--- stack-easybib/recipes/role-publicapi.rb
+++ stack-easybib/recipes/role-publicapi.rb
@@ -4,8 +4,8 @@ include_recipe 'php::module-gearman'
 include_recipe 'php::module-zip'
 include_recipe 'php::module-zlib'
 
-include_recipe 'easybib-deploy::easybib'
-
+include_recipe 'stack-easybib::deploy-easybib'
+include_recipe 'stack-easybib::deploy-gearman-worker'
 include_recipe 'redis::default'
 
 if is_aws
