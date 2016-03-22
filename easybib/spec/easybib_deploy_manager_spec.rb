3bc511d04f9f1c25b9252f57ac79b1658e4aea23
till <till@php.net>
2016-03-22 15:00:54 +0100

    Enhancement: spec for easybib_deploy_manager
     * asserts on no deploys (with missing deployments)
     * asserts on deploys (when everything is set)
    
    Related: DEVOPS-119

--- easybib/spec/easybib_deploy_manager_spec.rb
+++ easybib/spec/easybib_deploy_manager_spec.rb
@@ -13,7 +13,11 @@ describe 'easybib_deploy_manager' do
     ChefSpec::Runner.new(
       :cookbook_path => cookbook_paths,
       :log_level => :debug,
-      :step_into => ['easybib_deploy_manager']
+      :step_into => [
+        'easybib_deploy_manager',
+        'easybib_deploy',
+        'easybib_nginx'
+      ]
     )
   end
 
@@ -40,11 +44,64 @@ describe 'easybib_deploy_manager' do
         }
       end
 
-      it 'it logs accordingly' do
-        # expect(Chef::Log).to receive(:info).with('easybib_deploy_manager: No deployments')
-        # @chef_run = runner.converge(fixture)
-        # expect(@chef_run).to deploy_easybib_deploy_manager('fixtures')
-        pending 'need to explore how to assert on Chef::Log in a LWRP'
+      it 'does not call easybib_deploy' do
+        @chef_run = runner.converge(fixture)
+
+        %w(app_number_one app_number_two).each do |application|
+          expect(@chef_run).not_to deploy_easybib_deploy(application)
+          expect(@chef_run).not_to setup_easybib_nginx(application)
+        end
+      end
+    end
+  end
+
+  context 'deployments' do
+    before do
+      node.set['deploy'] = {
+        :app_number_one => {
+          :deploy_to => '/var/www/app1',
+          :document_root => 'www',
+          :domains => %w( example.org )
+        },
+        :app_number_two => {
+          :deploy_to => '/var/www/app2',
+          :document_root => 'htdocs',
+          :domains => %w( app2.example.org example2.org )
+        }
+      }
+
+      node.set['fixtures']['applications'] = {
+        :app_number_one => {
+          :layer => 'app-server',
+          :nginx => 'silex.erb.conf'
+        },
+        :app_number_two => {
+          :layer => 'app-server',
+          :nginx => 'silex.erb.conf'
+        }
+      }
+
+      node.set['opsworks'] = {
+        :instance => {
+          :layers => ['app-server']
+        },
+        :stack => {
+          :name => 'chefspec'
+        }
+      }
+
+      @chef_run = runner.converge('fixtures::easybib_deploy_manager')
+    end
+
+    it 'deploys an app' do
+      expect(@chef_run).to deploy_easybib_deploy_manager('fixtures')
+
+      deploy_manager = @chef_run.easybib_deploy_manager('fixtures')
+      expect(deploy_manager).to notify('execute[foo]').to(:run).delayed
+
+      %w( app_number_one app_number_two).each do |application|
+        expect(@chef_run).to deploy_easybib_deploy(application)
+        expect(@chef_run).to setup_easybib_nginx(application)
       end
     end
   end
