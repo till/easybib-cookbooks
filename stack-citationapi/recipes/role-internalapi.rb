b606f1f9b01686051382c0eb33a4bc3b94070ba1
Brian Wiborg <baccenfutter@c-base.org>
2016-03-24 10:08:00 +0100

    refactor easybib-deploy::internal-api

--- stack-citationapi/recipes/role-internalapi.rb
+++ stack-citationapi/recipes/role-internalapi.rb
@@ -1,7 +1,2 @@
 include_recipe 'stack-citationapi::role-phpapp'
-
-if is_aws
-  include_recipe 'easybib-deploy::internal-api'
-else
-  include_recipe 'nginx-app::sitescraper'
-end
+include_recipe 'stack-citationapi::deploy-citation-data-api'
