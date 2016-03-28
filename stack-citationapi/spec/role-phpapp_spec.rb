2c8b89bbd7b0c1738c08ba61655e3372f88ee72d
fh <fh-gitcommit@fholzhauer.de>
2016-03-28 14:14:01 +0200

    First rspec 3 implementation of shared_context

---
+++ stack-citationapi/spec/role-phpapp_spec.rb
@@ -0,0 +1,26 @@
+require_relative 'spec_helper.rb'
+
+describe_recipe 'stack-citationapi::role-phpapp' do
+  it 'our basic setup recipe' do
+    expect(chef_run).to include_recipe('ies::role-generic')
+  end
+  it 'installs nginx' do
+    expect(chef_run).to include_recipe('nginx-app::server')
+  end
+  it 'sets up php-fpm' do
+    expect(chef_run).to include_recipe('php-fpm')
+    expect(chef_run).to include_recipe('php-fpm::ohai')
+  end
+  %w(phar zlib intl gearman zip zlib opcache).each do |modulename|
+    it "installs the php module php::module-#{modulename}" do
+      expect(chef_run).to include_recipe("php::module-#{modulename}")
+    end
+  end
+  it 'sets up tideways profiling' do
+    expect(chef_run).to include_recipe('tideways')
+  end
+  it 'sets up composer' do
+    # node.set['composer']['environment'] = get_deploy_user if is_aws
+    expect(chef_run).to include_recipe('composer::configure')
+  end
+end
