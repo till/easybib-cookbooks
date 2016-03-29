dddd1ee88187134422acc9d2fbd29c7e9bcabd67
fh <fh-gitcommit@fholzhauer.de>
2016-03-29 07:22:41 +0200

    Add a check for nginx config generation

--- stack-citationapi/spec/role-citation-formatting-api_spec.rb
+++ stack-citationapi/spec/role-citation-formatting-api_spec.rb
@@ -2,14 +2,12 @@ require_relative 'spec_helper.rb'
 
 describe 'stack-citationapi::role-formatting-api' do
 
-  let(:runner) do
-    ChefSpec::Runner.new
-  end
+  let(:runner)   { ChefSpec::Runner.new }
   let(:chef_run) { runner.converge(described_recipe) }
   let(:node)     { runner.node }
   before do
     node.set['opsworks']['stack']['name'] = 'Stack'
-    node.set['opsworks']['instance']['layers'] = ['easybib_api']
+    node.set['opsworks']['instance']['layers'] = ['bibapi']
     node.set['opsworks']['instance']['hostname'] = 'host'
     node.set['opsworks']['instance']['ip'] = '127.0.0.1'
     node.set['deploy']['easybib_api'] = {
@@ -24,6 +22,13 @@ describe 'stack-citationapi::role-formatting-api' do
 
   it 'starts the app deployment' do
     expect(chef_run).to include_recipe('stack-citationapi::deploy-citationapi')
+  end
+
+  it 'creates the nginx config with the correct template' do
+    expect(chef_run).to setup_easybib_nginx('easybib_api')
+      .with(
+        :config_template => 'default-web-nginx.conf.erb'
+      )
   end
 
 end
--- stack-citationapi/spec/role-sitescraper_spec.rb
+++ stack-citationapi/spec/role-sitescraper_spec.rb
@@ -2,9 +2,7 @@ require_relative 'spec_helper.rb'
 
 describe 'stack-citationapi::role-sitescraper' do
 
-  let(:runner) do
-    ChefSpec::Runner.new
-  end
+  let(:runner)   { ChefSpec::Runner.new }
   let(:chef_run) { runner.converge(described_recipe) }
   let(:node)     { runner.node }
   before do
@@ -24,6 +22,13 @@ describe 'stack-citationapi::role-sitescraper' do
 
   it 'starts the app deployment' do
     expect(chef_run).to include_recipe('stack-citationapi::deploy-citationapi')
+  end
+
+  it 'creates the nginx config with the correct template' do
+    expect(chef_run).to setup_easybib_nginx('sitescraper')
+      .with(
+        :config_template => 'default-web-nginx.conf.erb'
+      )
   end
 
 end
