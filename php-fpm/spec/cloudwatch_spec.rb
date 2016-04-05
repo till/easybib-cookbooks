275b465f6c74d3d7e44b69507231b2d92a262bf5
Joel Gilley <gilleyj@gmail.com>
2016-04-05 15:12:39 -0400

    refactor of cloudwatch_spec to check for cron_d create notification

--- php-fpm/spec/cloudwatch_spec.rb
+++ php-fpm/spec/cloudwatch_spec.rb
@@ -4,6 +4,7 @@ describe 'php-fpm::cloudwatch' do
   let(:runner) { ChefSpec::Runner.new }
   let(:chef_run) { runner.converge(described_recipe) }
   let(:node) { runner.node }
+  let(:conscript_name) { "#{node['php-fpm']['prefix']}/bin/phpfpm-cloudwatch.sh" }
 
   before do
     node.set['opsworks']['stack']['name'] = 'Stack'
@@ -12,13 +13,16 @@ describe 'php-fpm::cloudwatch' do
   end
 
   it 'creates cronjob script' do
-    expect(chef_run).to render_file("#{node['php-fpm']['prefix']}/bin/phpfpm-cloudwatch.sh")
+    expect(chef_run).to render_file(conscript_name)
       .with_content(
         include('dimensions StackId=stack,InstanceId=host_stack')
       )
   end
 
-  it 'inits cronjob' do
-    expect(chef_run).to nothing_cron_d('phpfpm-cloudwatch')
+  it 'notifies cron_d create' do
+    conscript_resource = chef_run.template(conscript_name)
+    expect(conscript_resource).to notify('cron_d[phpfpm-cloudwatch]')
+      .to(:create)
+      .immediately
   end
 end
