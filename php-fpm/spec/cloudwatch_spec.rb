6a63acdd191f6cd11504766cf47701986982d7a1
Joel Gilley <gilleyj@gmail.com>
2016-04-05 13:43:10 -0400

    Remove the unnessicary codeblock in cloudwatch_spec

--- php-fpm/spec/cloudwatch_spec.rb
+++ php-fpm/spec/cloudwatch_spec.rb
@@ -5,22 +5,20 @@ describe 'php-fpm::cloudwatch' do
   let(:chef_run) { runner.converge(described_recipe) }
   let(:node) { runner.node }
 
-  describe 'cloudwatch cron enabled' do
-    before do
-      node.set['opsworks']['stack']['name'] = 'Stack'
-      node.set['opsworks']['stack']['name'] = 'Stack'
-      node.set['php-fpm']['cloudwatch'] = true
-    end
+  before do
+    node.set['opsworks']['stack']['name'] = 'Stack'
+    node.set['opsworks']['stack']['name'] = 'Stack'
+    node.set['php-fpm']['cloudwatch'] = true
+  end
 
-    it 'creates cronjob script' do
-      expect(chef_run).to render_file("#{node['php-fpm']['prefix']}/bin/phpfpm-cloudwatch.sh")
-        .with_content(
-          include('dimensions StackId=stack,InstanceId=host_stack')
-        )
-    end
+  it 'creates cronjob script' do
+    expect(chef_run).to render_file("#{node['php-fpm']['prefix']}/bin/phpfpm-cloudwatch.sh")
+      .with_content(
+        include('dimensions StackId=stack,InstanceId=host_stack')
+      )
+  end
 
-    it 'inits cronjob' do
-      expect(chef_run).to create_cron_d('phpfpm-cloudwatch')
-    end
+  it 'inits cronjob' do
+    expect(chef_run).to create_cron_d('phpfpm-cloudwatch')
   end
 end
