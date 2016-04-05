e9dd4282e6d62fe1a2b6ba84a5a37b821e406ec2
Joel Gilley <gilleyj@gmail.com>
2016-04-05 13:36:13 -0400

    remove disabled test in cloudwatch_spec, not implemented in cloudwatch recipe directly

--- php-fpm/spec/cloudwatch_spec.rb
+++ php-fpm/spec/cloudwatch_spec.rb
@@ -5,23 +5,10 @@ describe 'php-fpm::cloudwatch' do
   let(:chef_run) { runner.converge(described_recipe) }
   let(:node) { runner.node }
 
-  before do
-    node.set['opsworks']['stack']['name'] = 'Stack'
-    node.set['opsworks']['stack']['name'] = 'Stack'
-  end
-
-  describe 'cloudwatch cron default disabled' do
-    it 'does not create cronjob script' do
-      expect(chef_run).to_not render_file("#{node['php-fpm']['prefix']}/bin/phpfpm-cloudwatch.sh")
-    end
-
-    it 'does not init cronjob' do
-      expect(chef_run).to_not create_cron_d('phpfpm-cloudwatch')
-    end
-  end
-
   describe 'cloudwatch cron enabled' do
     before do
+      node.set['opsworks']['stack']['name'] = 'Stack'
+      node.set['opsworks']['stack']['name'] = 'Stack'
       node.set['php-fpm']['cloudwatch'] = true
     end
 
