cb649a4b8abac5e62b5d3ad894777fd08c25505a
till <till@php.net>
2016-04-10 19:08:28 +0200

    Update: debug log

--- stack-scholar/recipes/deploy.rb
+++ stack-scholar/recipes/deploy.rb
@@ -10,6 +10,8 @@ unless node['haproxy']['websocket_layers'].nil?
   node.set['stack-scholar']['applications']['scholar_realtime'] = node['haproxy']['websocket_layers'].keys
 end
 
+Chef::Log.info(node['stack-scholar']['applications'])
+
 easybib_deploy_manager get_cluster_name do
   apps node['stack-scholar']['applications']
   deployments node['deploy']
