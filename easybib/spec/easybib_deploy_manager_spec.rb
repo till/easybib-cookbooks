416dbcfd8b72c40cd8bee62a3a650422df5ec00a
till <till@php.net>
2016-04-12 18:43:38 +0200

    Update: remove dependencies on spec_helper
    Related: DEVOPS-119

--- easybib/spec/easybib_deploy_manager_spec.rb
+++ easybib/spec/easybib_deploy_manager_spec.rb
@@ -1,4 +1,4 @@
-require_relative 'spec_helper'
+require 'chefspec'
 
 describe 'easybib_deploy_manager' do
 
@@ -11,6 +11,8 @@ describe 'easybib_deploy_manager' do
 
   let(:runner) do
     ChefSpec::Runner.new(
+      :platform => 'ubuntu',
+      :version => '14.04',
       :cookbook_path => cookbook_paths,
       :log_level => :error,
       :step_into => %w(
