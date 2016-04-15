2abfc17079c78a4caf73d3c9de8e9b462b713f60
Joel Gilley <gilleyj@gmail.com>
2016-04-15 10:14:18 -0400

    ppa name collision fix in ondrejphp.rb

--- ies-apt/recipes/ondrejphp.rb
+++ ies-apt/recipes/ondrejphp.rb
@@ -1,7 +1,7 @@
 include_recipe 'ies-apt::ppa'
 include_recipe 'aptly::gpg'
 
-apt_repository 'easybib-ppa' do
+apt_repository 'ondrejphp-ppa' do
   uri           'ppa:ondrej/php' # FIXME: this needs to be reverted
   distribution  node['lsb']['codename']
   components    ['main']
