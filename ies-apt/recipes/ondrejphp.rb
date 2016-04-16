a14ae45a167ac890874a7c88c84886b641f4433b
till <till@php.net>
2016-04-16 22:33:17 +0200

    Refactor: ppa discovery
    Related: DEVOPS-124

--- ies-apt/recipes/ondrejphp.rb
+++
@@ -1,8 +0,0 @@
-include_recipe 'ies-apt::ppa'
-include_recipe 'aptly::gpg'
-
-apt_repository 'ondrejphp-ppa' do
-  uri           'ppa:ondrej/php' # FIXME: this needs to be reverted
-  distribution  node['lsb']['codename']
-  components    ['main']
-end
