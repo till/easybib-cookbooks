3e3d12032c56635e2295f516e9c2b2638f240864
Joel Gilley <gilleyj@gmail.com>
2016-04-18 15:37:01 -0400

    Adding load_priority to config provider and chefspec test

--- php/providers/config.rb
+++ php/providers/config.rb
@@ -1,11 +1,23 @@
 action :generate do
-  file = format(
-    '%{prefix}/%{config_dir}/%{ext_name}%{ini_suffix}.ini',
-    :prefix => new_resource.prefix_dir,
-    :config_dir => new_resource.config_dir,
-    :ext_name => new_resource.name,
-    :ini_suffix => new_resource.suffix
-  )
+
+  if new_resource.load_priority.to_i > 0
+    file = format(
+      '%{prefix}/%{config_dir}/%{load_priority}-%{ext_name}%{ini_suffix}.ini',
+      :prefix => new_resource.prefix_dir,
+      :config_dir => new_resource.config_dir,
+      :load_priority => new_resource.load_priority,
+      :ext_name => new_resource.name,
+      :ini_suffix => new_resource.suffix
+    )
+  else
+    file = format(
+      '%{prefix}/%{config_dir}/%{ext_name}%{ini_suffix}.ini',
+      :prefix => new_resource.prefix_dir,
+      :config_dir => new_resource.config_dir,
+      :ext_name => new_resource.name,
+      :ini_suffix => new_resource.suffix
+    )
+  end
 
   config = ::Php::Config.new(new_resource.name, new_resource.config)
   extension = {}
--- php/spec/fixtures/recipes/php-config.rb
+++ php/spec/fixtures/recipes/php-config.rb
@@ -2,6 +2,7 @@ php_config node['config-spec']['name'] do
   config node['config-spec']['config']
   prefix_dir node['config-spec']['prefix_dir']
   extension_path node['config-spec']['extension_path']
+  load_priority node['config-spec']['load_priority']
   config_dir node['config-spec']['config_dir']
   suffix node['config-spec']['ini_suffix']
   load_extension node['config-spec']['load_extension']
