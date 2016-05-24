9ff465dc1d0e2ba7c7c0eedb5020aaeec156b296
till <till@php.net>
2016-05-24 14:16:50 +0200

    Fix: make vagrant provision the correct configuration
    Related: DEVOPS-147

--- stack-academy/recipes/role-vagrant.rb
+++ stack-academy/recipes/role-vagrant.rb
@@ -2,7 +2,18 @@ include_recipe 'ohai'
 include_recipe 'ies-mysql'
 include_recipe 'ies-mysql::dev'
 include_recipe 'ies::role-phpapp'
-include_recipe 'nginx-app::vagrant'
+
+
+include_recipe 'nginx-app::vagrant-silex'
+
+easybib_envconfig 'infolit'
+easybib_nginx 'infolit' do
+  cookbook 'stack-academy'
+  config_template 'infolit.conf.erb'
+  notifies :reload, 'service[nginx]', :delayed
+  notifies :restart, 'service[php-fpm]', :delayed
+end
+
 include_recipe 'php::module-pdo_sqlite'
 include_recipe 'nodejs'
 include_recipe 'avahi'
