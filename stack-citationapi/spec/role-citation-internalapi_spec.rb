7e94ca2a123a7c8ffa2a16a89e6699689be71534
Brian Wiborg <baccenfutter@c-base.org>
2016-06-20 10:59:37 +0200

    stack-citationapi specs

--- stack-citationapi/spec/role-citation-internalapi_spec.rb
+++ stack-citationapi/spec/role-citation-internalapi_spec.rb
@@ -1,6 +1,6 @@
 require_relative 'spec_helper.rb'
 
-describe 'stack-citationapi::role-citation-data-api' do
+describe 'stack-citationapi::role-internalapi' do
 
   let(:runner)   { ChefSpec::Runner.new }
   let(:chef_run) { runner.converge(described_recipe) }
@@ -8,12 +8,12 @@ describe 'stack-citationapi::role-citation-data-api' do
 
   before do
     node.set['opsworks']['stack']['name'] = 'Stack'
-    node.set['opsworks']['instance']['layers'] = ['citation-apis']
+    node.set['opsworks']['instance']['layers'] = ['bibapi']
     node.set['opsworks']['instance']['hostname'] = 'host'
     node.set['opsworks']['instance']['ip'] = '127.0.0.1'
-    node.set['deploy']['citation_apis'] = {
-      'deploy_to' => '/srv/www/citation_apis',
-      'document_root' => 'public'
+    node.set['deploy']['easybib_api'] = {
+      :deploy_to => '/srv/www/bibapi',
+      :document_root => 'public'
     }
   end
 
@@ -26,7 +26,7 @@ describe 'stack-citationapi::role-citation-data-api' do
   end
 
   it 'creates the nginx config with the correct template' do
-    expect(chef_run).to setup_easybib_nginx('citation_apis')
+    expect(chef_run).to setup_easybib_nginx('easybib_api')
       .with(
         :config_template => 'default-web-nginx.conf.erb'
       )
