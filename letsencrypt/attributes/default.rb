3e0aa6f5c2d05b3d14b353906721a783cff85aea
till <till@php.net>
2016-06-24 18:25:33 +0200

    Update: prefix cookbook with ies

--- letsencrypt/attributes/default.rb
+++
@@ -1,3 +0,0 @@
-default['letsencrypt'] = {
-  :certbot_dl => 'https://dl.eff.org/certbot-auto'
-}
--- letsencrypt/metadata.rb
+++
@@ -1,8 +0,0 @@
-name              'letsencrypt'
-maintainer        'Till Klampaeckel'
-maintainer_email  'till@php.net'
-license           'Apache-2.0'
-description       'Install letsencrypt'
-version           '0.1'
-
-supports 'ubuntu'
