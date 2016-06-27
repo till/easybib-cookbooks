cb0caab9fa4384d63b4c5d12ec80f730f0a44fb7
Brian Wiborg <baccenfutter@c-base.org>
2016-06-27 18:06:05 +0200

    review fixes
    thx: @fh

--- stack-easybib/recipes/deploy-easybib.rb
+++ stack-easybib/recipes/deploy-easybib.rb
@@ -1,7 +1,7 @@
 node['deploy'].each do |application, deploy|
   case application
   when 'easybib'
-    unless allow_deploy(application, 'easybib', 'nginxphpapp') || allow_deploy(application, 'easybib', 'housekeeping')
+    unless allow_deploy(application, 'easybib', %w(nginxphpapp housekeeping))
       Chef::Log.info("stack-easybib::deploy-easybib - #{application} skipped")
       next
     end
