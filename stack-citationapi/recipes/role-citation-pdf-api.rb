469733db8d7fc8dfb145ae7430a534695d444dd7
fh <fh-gitcommit@fholzhauer.de>
2016-07-04 20:03:25 +0200

    add redis to pdf-vagrantbox

--- stack-citationapi/recipes/role-citation-pdf-api.rb
+++ stack-citationapi/recipes/role-citation-pdf-api.rb
@@ -5,5 +5,6 @@ include_recipe 'php::module-poppler-pdf'
 if is_aws
   include_recipe 'stack-citationapi::deploy-citationapi'
 else
+  include_recipe 'redis'
   include_recipe 'stack-citationapi::role-vagrant'
 end
