e494c572c3f478abbd7289f795712913ca9da61b
fh <fh-gitcommit@fholzhauer.de>
2016-07-06 09:58:42 +0200

    add php-posix for gearman

--- stack-citationapi/recipes/role-citation-pdf-api.rb
+++ stack-citationapi/recipes/role-citation-pdf-api.rb
@@ -1,6 +1,7 @@
 include_recipe 'stack-citationapi::role-phpapp'
 include_recipe 'gearmand'
 include_recipe 'php::module-poppler-pdf'
+include_recipe 'php::module-posix'
 
 if is_aws
   include_recipe 'stack-citationapi::deploy-citationapi'
