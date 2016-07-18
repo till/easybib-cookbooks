7bd5897a841e2ba4bd1f9950969018fe9c5fddc8
Till! <till@users.noreply.github.com>
2016-07-18 22:55:22 +0200

    Update: allow empty credentials (#1051)
    This can be discovered using the instance role when required
    and saves us from having yet another IAM user that is setup
    in order to do something in Chef.

--- ies-route53/resources/record.rb
+++ ies-route53/resources/record.rb
@@ -7,6 +7,6 @@ attribute :name,                  :kind_of => String, :required => true, :name_a
 attribute :ttl,                   :kind_of => Integer, :default => 3600
 attribute :type,                  :kind_of => String, :required => true
 attribute :value,                 :kind_of => String, :required => true
-attribute :aws_access_key_id,     :kind_of => String
-attribute :aws_secret_access_key, :kind_of => String
+attribute :aws_access_key_id,     :kind_of => [String, NilClass]
+attribute :aws_secret_access_key, :kind_of => [String, NilClass]
 attribute :overwrite,             :kind_of => [TrueClass, FalseClass], :default => true
