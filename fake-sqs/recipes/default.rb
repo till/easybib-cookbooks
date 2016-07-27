463b9ec67d2954f2cd91e2bd5d28091f25479f8c
till <till@php.net>
2016-07-27 14:17:22 +0200

    Fix: ensure these are linked into /usr/local/bin
    cc @bluemaex

--- fake-sqs/recipes/default.rb
+++ fake-sqs/recipes/default.rb
@@ -8,7 +8,13 @@ unless is_aws
   gem_package 'fake_sqs' do
     version conf['version']
   end
-  ::Chef::Log.info('SQS - configuring supervisor...')
+
+  # installed chef inside vagrant box demands this
+  link sqs_bin do
+    to '/opt/chef/embedded/bin/fake_sqs'
+    only_if { File.exist?('/opt/chef/embedded/bin/fake_sqs') }
+  end
+
   supervisor_service 'fake_sqs' do
     action :enable
     autostart true
--- fake-sqs/spec/default_spec.rb
+++ fake-sqs/spec/default_spec.rb
@@ -6,6 +6,17 @@ describe 'fake-sqs::default' do
   end
 
   let(:chef_run) { runner.converge(described_recipe) }
+  let(:chef_sqs) { '/opt/chef/embedded/bin/fake_sqs' }
+
+  before do
+    File.stub(:exist?).and_call_original
+    File.stub(:exist?).with(chef_sqs).and_return(true)
+  end
+
+  it 'links fakesqs to /usr/local/bin' do
+    link = chef_run.link('/usr/local/bin/fake_sqs')
+    expect(link).to link_to(chef_sqs)
+  end
 
   it 'it starts the fakesqs service via supervisord' do
     expect(chef_run).to enable_supervisor_service('fake_sqs')
