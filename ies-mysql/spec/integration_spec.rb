66547e3c838265c05617f1007dbe37de12594dad
till <till@php.net>
2016-07-27 11:01:02 +0200

    Update: add an integration test for cookbook mysql

---
+++ ies-mysql/spec/integration_spec.rb
@@ -0,0 +1,29 @@
+require 'chefspec'
+
+# This is an integration test to ensure that future updates to
+# the mysql cookbook which we wrap with ies-mysql, do not break
+# our expectations.
+
+describe 'ies-mysql::default' do
+  let(:runner) do
+    ChefSpec::Runner.new(
+      :log_level => :error,
+      :platform => 'ubuntu',
+      :version => '14.04',
+      :step_into => ['mysql_service', 'mysql_client']
+    )
+  end
+
+  let(:chef_run) { runner.converge(described_recipe) }
+
+  describe 'mysql 5.6' do
+    before do
+      stub_command("/usr/bin/test -f /var/lib/mysql-vagrant/mysql/user.frm").and_return(false)
+    end
+
+    it 'installs version 5.6 of the mysql-server and -client package' do
+      expect(chef_run).to install_package('mysql-server-5.6')
+      expect(chef_run).to install_package('mysql-client-5.6')
+    end
+  end
+end
