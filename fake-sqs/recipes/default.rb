001107a42089ce8319fefd1a2850340f309207b6
till <till@php.net>
2016-07-27 13:57:52 +0200

    Update: variables

--- fake-sqs/recipes/default.rb
+++ fake-sqs/recipes/default.rb
@@ -1,7 +1,10 @@
 include_recipe 'supervisor'
 
 unless is_aws
-  ::Chef::Log.info('SQS - installing fake_sqs...')
+  conf = node['fake-sqs']
+
+  sqs_bin = '/usr/local/bin/fake_sqs'
+
   gem_package 'fake_sqs' do
     version conf['version']
   end
@@ -9,7 +12,7 @@ unless is_aws
   supervisor_service 'fake_sqs' do
     action :enable
     autostart true
-    command '/usr/local/bin/fake_sqs'
+    command sqs_bin
     user 'nobody'
   end
 end
