35d261073fec64f31fb7b9ac0ef3accf6443d3b7
Brian Wiborg <baccenfutter@c-base.org>
2016-08-01 16:15:06 +0200

    Clean up obsolete role-internalapi (#1074)
    * rm obsolete stack-citation-api::role-internalapi
    
    * also delete tests for internalapi

--- stack-citationapi/recipes/role-internalapi.rb
+++
@@ -1,4 +0,0 @@
-Chef::Log.warn('This stack still uses the old stack-citationapi::role-internalapi recipe!!!')
-
-include_recipe 'stack-citationapi::role-citation-data-api'
-include_recipe 'stack-citationapi::role-sitescraper'
