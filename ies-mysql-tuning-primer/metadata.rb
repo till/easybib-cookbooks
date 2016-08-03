44900ab93b4a0bd9212bad4b682a152e8368468d
Brian Wiborg <baccenfutter@c-base.org>
2016-08-03 16:57:14 +0200

    introducing cookbook ies-mysql-tuning-primer

---
+++ ies-mysql-tuning-primer/metadata.rb
@@ -0,0 +1,8 @@
+name              'ies-mysql-tuning-primer'
+maintainer        'Brian Wiborg'
+maintainer_email  'bwiborg@chegg.com'
+license           'BSD/2'
+description       'Install customized MySQL Tuning-Primer'
+version           '0.1'
+
+supports 'ubuntu'
