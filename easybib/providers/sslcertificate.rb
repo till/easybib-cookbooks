53ab7e45065724fbf979dbe94c20db7991a442b5
Joel Gilley <gilleyj@gmail.com>
2016-08-16 11:20:25 -0400

    Rake tests says this logic works, testing on instance

--- easybib/providers/sslcertificate.rb
+++ easybib/providers/sslcertificate.rb
@@ -71,8 +71,10 @@ action :create do
     )
   end
 
-  new_resource.notifies :restart, run_context.resource_collection.find(runit_service: 'haproxy')
-  new_resource.updated_by_last_action(d.updated_by_last_action? || t1.updated_by_last_action? || t2.updated_by_last_action? || t3.updated_by_last_action?)
+  new_resource.updated_by_last_action(true) if d.updated_by_last_action?
+  new_resource.updated_by_last_action(true) if t1.updated_by_last_action?
+  new_resource.updated_by_last_action(true) if t2.updated_by_last_action?
+  new_resource.updated_by_last_action(true) if t3.updated_by_last_action?
 
 end
 
