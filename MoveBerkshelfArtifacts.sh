b12e3a9c4a08f8dd4304e8cc34e4ae54a12b2f1c
Joel Gilley <gilleyj@gmail.com>
2016-08-17 17:45:24 -0400

    move berkshelf artifacts to build

---
+++ MoveBerkshelfArtifacts.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+TARGET_PATTERN="*/cookbooks-*.tar.gz"
+
+for FILE in `ls */cookbooks-*.tar.gz`; do
+	BASE=`basename $FILE`
+	DIR=`dirname $FILE`
+	mv $FILE build/$DIR.tar.gz
+done
+
+exit 0
+
