10eefcc35b6797c78ed24881dfae3ff6255adc33
Till! <till@users.noreply.github.com>
2016-08-23 20:38:06 +0200

    T/wpt zip (#1094)
    * Refactor: move php config here
    
     * just set the node attribute in stack-wpt to change it (!!!)
     * moved here from easybib/wpt#62
    
    * Enhancement: add zip extension
    
    Related: easybib/issues#5688

--- stack-wpt/recipes/role-vagrant.rb
+++ stack-wpt/recipes/role-vagrant.rb
@@ -15,7 +15,44 @@ include_recipe 'fake-s3'
 # package 'build-essential'
 # package 'g++'
 
+php_version = node['stack-wpt']['php_version']
+
+# php setup
+node.set['php']['extensions']['config_dir'] = "etc/php/#{php_version}/mods-available"
+node.set['php']['ppa'] = {
+  'name' => 'ondrejphp',
+  'uri' => 'ppa:ondrej/php',
+  'package_prefix' => "php#{php_version}"
+}
+
+node.set['php-apc']['package_prefix'] = 'php'
+
+php_core_deps = %W(
+  php#{php_version}-fpm
+  php#{php_version}-cli
+  php#{php_version}-mbstring
+  php#{php_version}-mysql
+  php#{php_version}-curl
+  php#{php_version}-bcmath
+  php#{php_version}-dom
+  php-memcached
+)
+
+node.set['php-fpm'] = {
+  'prefix' => '',
+  'exec_prefix' => '/usr',
+  'fpm_config' => "etc/php/#{php_version}/fpm/php.ini",
+  'cli_config' => "etc/php/#{php_version}/cli/php.ini",
+  'pool_dir' => "etc/php/#{php_version}/fpm/pool.d",
+  'socketdir' => '/var/run/php',
+  'pid' => "/var/run/php/php#{php_version}-fpm.pid",
+  'packages' => php_core_deps.join(','),
+  'user' => 'vagrant',
+  'group' => 'vagrant'
+}
+
 include_recipe 'stack-scholar::role-scholar'
+include_recipe 'php::module-zip'
 include_recipe 'php::module-pdo_sqlite'
 include_recipe 'nginx-app::vagrant-silex'
 
