69b540ddcfea2227e477752ecb722b778bf51495
Joel Gilley <gilleyj@gmail.com>
2016-08-24 18:40:52 -0400

    move install and tests back into parent cookbook

--- poppler/README.md
+++
@@ -1,3 +0,0 @@
-# poppler cookbook
-
- * `poppler` installs poppler libraries from https://packagecloud.io/till/poppler-backport
--- poppler/metadata.rb
+++
@@ -1,9 +0,0 @@
-name              'poppler'
-maintainer        'Joel Gilley'
-maintainer_email  'jgilley@chegg.com'
-version           '0.1'
-
-supports 'ubuntu'
-
-depends 'apt'
-license           'BSD License'
