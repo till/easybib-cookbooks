ece028f680f3b8c99cfb6b76e5efc92fc844547f
Joel Gilley <gilleyj@gmail.com>
2016-08-24 14:40:17 -0400

    add packagecloud chef add packagecloud do poppler debs

--- php/recipes/module-poppler-pdf.rb
+++ php/recipes/module-poppler-pdf.rb
@@ -1,3 +1,10 @@
-include_recipe 'php::dependencies-ppa'
+# include_recipe 'php::dependencies-ppa'
+# php_ppa_package 'poppler-pdf'
 
-php_ppa_package 'poppler-pdf'
+packagecloud_repo 'till/poppler-backport' do
+  type 'deb'
+end
+
+package 'poppler'
+package 'poppler-glib'
+package 'php-poppler-pdf'
