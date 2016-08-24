5c73c69060e003defb76de347437a9544ba9aca6
till <till@php.net>
2016-08-24 13:46:01 +0200

    Enhancement: install language tool
    Fixes: easybib/issues#5690

--- stack-wpt/recipes/role-vagrant.rb
+++ stack-wpt/recipes/role-vagrant.rb
@@ -67,3 +67,15 @@ apt_package 'libreoffice' do
   action :install
   options '--no-install-recommends --no-install-suggests'
 end
+
+include_recipe 'java'
+lt_conf = node['stack-wpt']['languagetool']
+
+ark 'languagetool' do
+  url  "https://languagetool.org/download/LanguageTool-#{lt_conf['version']}.zip"
+  version lt_conf['version']
+  home_dir lt_conf['path']
+  owner node['nginx-app']['user']
+  group node['nginx-app']['group']
+  checksum lt_conf['checksum']
+end
