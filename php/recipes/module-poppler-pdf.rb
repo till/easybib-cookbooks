ff97f658df371e141dc68b1fd939c3911ce87db4
Joel Gilley <gilleyj@gmail.com>
2016-08-25 02:25:47 -0400

    Refactor poppler library * use php-poppler from original * attempt to install gpg key (fail) * rework recipe and debs for no arch * rewrite test

--- php/recipes/module-poppler-pdf.rb
+++ php/recipes/module-poppler-pdf.rb
@@ -1,3 +1,15 @@
 include_recipe 'php::dependencies-ppa'
 
+apt_repository 'poppler' do
+  uri          node['poppler']['package_uri']
+  distribution node['poppler']['package_distro']
+  components   node['poppler']['package_components']
+  key          node['poppler']['package_key_uri']
+end
+
+package_list = node['poppler']['package_list']
+package_list.each do |package_name|
+  package package_name
+end
+
 php_ppa_package 'poppler-pdf'
