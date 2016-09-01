abf9ccdfd4c363dfb69c58eda3e0d8f464fe29c8
till <till@php.net>
2016-09-01 15:28:11 +0200

    Fix: sometimes things are not installed :trollface:
     - re-order setup for java, libreoffice and lt
    
    Related: easybib/issues#5721

---
+++ stack-wpt/recipes/role-languagetool.rb
@@ -0,0 +1,11 @@
+include_recipe 'java'
+lt_conf = node['stack-wpt']['languagetool']
+
+ark 'languagetool' do
+  url  "https://languagetool.org/download/LanguageTool-#{lt_conf['version']}.zip"
+  version lt_conf['version']
+  home_dir lt_conf['path']
+  owner node['nginx-app']['user']
+  group node['nginx-app']['group']
+  checksum lt_conf['checksum']
+end
