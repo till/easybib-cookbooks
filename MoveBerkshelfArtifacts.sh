884b47bfaf62ea9b2d2c5c80c52fbf4be8e8cd1c
Brian Wiborg <baccenfutter@c-base.org>
2016-09-14 20:43:05 +0200

    improve MoveBerkshelfArtifacts.sh (#1096)
    - don't use sh syntax in Bash
    - create missing directories
    - never use the output of ls in for loops
    - always quote path variables

--- MoveBerkshelfArtifacts.sh
+++ MoveBerkshelfArtifacts.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
+set -e
+shopt -s nullglob
+
 TARGET_PATTERN="*/cookbooks-*.tar.gz"
 
-for FILE in `ls */cookbooks-*.tar.gz`; do
-	BASE=`basename $FILE`
-	DIR=`dirname $FILE`
-	mv $FILE build/artifact-$DIR.tar.gz
+if [[ ! -r ./MoveBerkshelfArtifacts.sh ]]; then
+    echo "Please make sure you are in the cookbook root directory!"
+    exit 1
+fi
+
+mkdir -p build
+
+for FILE in ${TARGET_PATTERN}; do
+    BASE=$(basename $FILE)
+    DIR=$(dirname $FILE)
+    printf "%-50s => %s\n" "$FILE"  "build/artifact-${DIR}.tar.gz'"
+    mv "$FILE" "build/artifact-${DIR}.tar.gz"
 done
-
-exit 0
-
