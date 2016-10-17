f12f1c1f6436f272fbe603468c3dce72c4fcc2c0
till <till@php.net>
2016-10-17 17:39:06 +0200

    Update: use the init wrapper provided by package

--- php-fpm/recipes/service.rb
+++ php-fpm/recipes/service.rb
@@ -1,6 +1,10 @@
-service_name = 'php-fpm'
+if node['php']['ppa']['package_prefix'].include?('easybib')
+  service_name = 'php-fpm'
+else
+  service_name = "#{node['php']['ppa']['package_prefix']}-fpm"
+end
 
-service service_name do
+service 'php-fpm' do
   action :nothing
   service_name service_name
   supports [:start, :stop, :status, :reload, :restart]
