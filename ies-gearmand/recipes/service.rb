6e00dde983a18824a3a9e61745a90f1af9aa0e92
till <till@php.net>
2016-10-20 15:50:30 +0200

    Update: bugs in upstart and systemd configs
     * provided configs by package are faulty (ignore defaults file)
     * this adds the configs and reloads services, etc.
     * added a few tests to nail this

--- ies-gearmand/recipes/service.rb
+++ ies-gearmand/recipes/service.rb
@@ -1,3 +1,34 @@
+service_provider = if Chef::VersionConstraint.new('>= 15.04').include?(node['platform_version'])
+                     Chef::Provider::Service::Systemd
+                   elsif Chef::VersionConstraint.new('>= 12.04').include?(node['platform_version'])
+                     Chef::Provider::Service::Upstart
+                   else
+                     service_provider = nil
+                   end
+
 service 'gearman-job-server' do
   action :nothing
+  provider service_provider
+end
+
+execute 'systemctl daemon-reload' do
+  action :nothing
+  only_if do
+    service_provider == Chef::Provider::Service::Systemd
+  end
+end
+
+cookbook_file '/etc/init/gearman-job-server.conf' do
+  source 'upstart.conf'
+  owner 'root'
+  group 'root'
+  mode 00644
+end
+
+cookbook_file '/lib/systemd/system/gearman-job-server.service' do
+  source 'systemd.service'
+  owner 'root'
+  group 'root'
+  mode 00644
+  notifies :run, 'execute[systemctl daemon-reload]', :immediate
 end
