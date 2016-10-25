752b1ad0ddca2b5c8efb3d5a5a51b69587d314e3
till <till@php.net>
2016-10-25 14:40:05 +0200

    Update: berkshelf

--- stack-service/metadata.rb
+++ stack-service/metadata.rb
@@ -5,7 +5,8 @@ license 'BSD-2-Clause'
 description 'Role recipes for the service stack'
 source_url        'https://github.com/till/easybib-cookbooks' if respond_to?(:source_url)
 issues_url        'https://github.com/till/easybib-cookbooks/issues' if respond_to?(:issues_url)
-version           '0.1'
+version '0.2'
+
 depends 'gearmand'
 depends 'ies-gearmand'
 depends 'logrotate'
