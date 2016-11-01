a25a7efd76f2323be99301a825e6fa3da7291016
fh <fh-gitcommit@fholzhauer.de>
2016-11-01 17:14:43 +0100

    Cleanup: Relocate housekeeping deployment

--- stack-easybib/recipes/role-housekeeping.rb
+++ stack-easybib/recipes/role-housekeeping.rb
@@ -3,7 +3,7 @@ include_recipe 'supervisor'
 
 if is_aws
   include_recipe 'stack-easybib::deploy-easybib'
-  include_recipe 'easybib-deploy::easybib-housekeeping'
+  include_recipe 'stack-easybib::deploy-housekeeping'
 else
   include_recipe 'stack-easybib::deploy-easybib-vagrant'
 end
