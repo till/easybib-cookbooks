f24b2345d6afd42123c36f19b367f39f4c6542be
Till! <till@users.noreply.github.com>
2016-11-02 16:28:20 +0100

    Fix: this overwrote global PHP (#1146)
    * Fix: this overwrote global PHP
    
    * Update: make test pending

--- ies-gearmand/attributes/default.rb
+++ ies-gearmand/attributes/default.rb
@@ -1,8 +1,8 @@
 default['ies-gearmand']['port'] = 31_337
 default['ies-gearmand']['log']  = '--syslog -l stderr' # or --logfile=/path/to/whatever
 
-normal['php']['ppa'] = {
-  'name' => 'ondrejphp',
-  'package_prefix' => 'php5.6',
-  'uri' => 'ppa:ondrej/php'
-}
+# normal['php']['ppa'] = {
+#  'name' => 'ondrejphp',
+#  'package_prefix' => 'php5.6',
+#  'uri' => 'ppa:ondrej/php'
+# }
