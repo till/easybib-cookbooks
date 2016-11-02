1bbb4fe895597c10beb605283b058bcab509e59a
fh <fh-gitcommit@fholzhauer.de>
2016-11-02 15:23:05 +0100

    Moving node[travis-asset-browser] to stack-qa

--- easybib-deploy/attributes/default.rb
+++ easybib-deploy/attributes/default.rb
@@ -1,12 +1,2 @@
 default['easybib_deploy']['php-fpm']['restart-action'] = :reload
 
-default['travis-asset-browser']['config'] = {
-  'theme' => 'plain',
-  'page-header' => 'My Amazon S3 files',
-  's3-access-key' => '',
-  's3-secret-key' => '',
-  'cache-time' => 600,
-  'cache-dir' => '/tmp',
-  'bucket-name' => '',
-  'bucket-url-prefix' => 'http://bucket-name.s3.amazonaws.com'
-}
--- stack-qa/attributes/default.rb
+++ stack-qa/attributes/default.rb
@@ -7,3 +7,14 @@ default['stack-qa']['vagrant-ci']['deploy']['group'] = 'vagrant-ci'
 default['stack-qa']['vagrant-ci']['deploy']['home'] = '/home/vagrant-ci'
 
 default['stack-qa']['vagrant-ci']['apps'] = []
+
+default['travis-asset-browser']['config'] = {
+  'theme' => 'plain',
+  'page-header' => 'My Amazon S3 files',
+  's3-access-key' => '',
+  's3-secret-key' => '',
+  'cache-time' => 600,
+  'cache-dir' => '/tmp',
+  'bucket-name' => '',
+  'bucket-url-prefix' => 'http://bucket-name.s3.amazonaws.com'
+}
