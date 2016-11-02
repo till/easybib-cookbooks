f4f01fa741643643b9fe4f952bfd7c46643e3189
fh <fh-gitcommit@fholzhauer.de>
2016-11-02 10:31:56 +0100

    Cleanup: Reomve deprecated roles and broken template

--- easybib/recipes/role-aptcache.rb
+++
@@ -1,5 +0,0 @@
-include_recipe 'ies::role-generic'
-
-if get_instance_roles.include?('aptcache')
-  include_recipe 'apt::cacher-ng'
-end
--- easybib/recipes/role-generic.rb
+++
@@ -1,3 +0,0 @@
-Chef::Log.warn('Using deprecated easybib::role-generic')
-include_recipe 'ies::role-generic'
-include_recipe 'nginx-app::server'
