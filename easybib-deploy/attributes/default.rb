d92ea2df89ecb252951fb142f225517d0daf2187
fh <fh-gitcommit@fholzhauer.de>
2016-11-02 15:00:58 +0100

    Relocate node[ssl-deploy] to ies-ssl

--- easybib-deploy/attributes/default.rb
+++ easybib-deploy/attributes/default.rb
@@ -1,9 +1,5 @@
 default['easybib_deploy']['php-fpm']['restart-action'] = :reload
 
-default['ssl-deploy'] = {}
-default['ssl-deploy']['directory'] = '/etc/nginx/ssl'
-default['ssl-deploy']['ssl-role'] = 'nginxphpapp'
-
 default['bibcd']['apps'] = {}
 
 default['travis-asset-browser']['config'] = {
---
+++ ies-ssl/attributes/metadata.rb
@@ -0,0 +1,4 @@
+default['ssl-deploy'] = {}
+default['ssl-deploy']['directory'] = '/etc/nginx/ssl'
+default['ssl-deploy']['ssl-role'] = 'nginxphpapp'
+
