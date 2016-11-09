988ea33dd27eebc8f5900c0d265aaf6e9544d845
fh <fh-gitcommit@fholzhauer.de>
2016-11-09 12:36:02 +0100

    Cleanup: Email Adresses

--- bibcd/metadata.rb
+++ bibcd/metadata.rb
@@ -1,10 +1,11 @@
 name              'bibcd'
 maintainer        'Florian Holzhauer'
-maintainer_email  'fh-cookbookzgalorealsocats@holzhauer.it'
-version          '0.1'
+maintainer_email  'florian@chegg.com'
+version           '0.1'
 source_url        'https://github.com/till/easybib-cookbooks' if respond_to?(:source_url)
 issues_url        'https://github.com/till/easybib-cookbooks/issues' if respond_to?(:issues_url)
 
-depends 'easybib'
-supports 'ubuntu'
-license           'BSD License'
+supports          'ubuntu'
+license           'Apache 2 License'
+
+depends           'easybib'
