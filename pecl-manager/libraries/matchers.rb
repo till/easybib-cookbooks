01fa47f9fc0178e1116163a47e23545c84779ee1
fh <fh-gitcommit@fholzhauer.de>
2016-11-11 13:22:09 +0100

    Fixes: Rspec3

--- pecl-manager/libraries/matchers.rb
+++ pecl-manager/libraries/matchers.rb
@@ -1,5 +1,10 @@
 if defined?(ChefSpec)
   def create_pecl_manager_script(resource_name)
-    ChefSpec::Matchers::ResourceMatcher.new('pecl-manager_script', :create, resource_name)
+    rspec_version = Gem::Version.new(::RSpec::Core::Version::STRING)
+    if rspec_version >= Gem::Version.new('3.0.0')
+      ChefSpec::Matchers::ResourceMatcher.new('pecl_manager_script', :create, resource_name)
+    else
+      ChefSpec::Matchers::ResourceMatcher.new('pecl-manager_script', :create, resource_name)
+    end
   end
 end
