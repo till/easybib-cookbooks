0ee71f5ceb3c741069b036afff38eb16796a1ddf
fh <fh-gitcommit@fholzhauer.de>
2016-11-17 14:59:10 +0100

    Cleanup: Carve out the scholar-silex setup variant to simplify the setup

--- stack-scholar/recipes/deploy-silex-vagrant.rb
+++ stack-scholar/recipes/deploy-silex-vagrant.rb
@@ -4,15 +4,11 @@ include_recipe 'nginx-app::service'
 
 node['vagrant']['applications'].each do |app_name, app_config|
 
-  next if app_name == 'www'
-
   default_router = if app_config.attribute?('default_router')
                      app_config['default_router']
                    else
                      'index.php'
                    end
-
-  template = 'silex.conf.erb'
 
   domain_name        = app_config['domain_name']
   doc_root_location  = app_config['doc_root_location']
@@ -20,7 +16,7 @@ node['vagrant']['applications'].each do |app_name, app_config|
   app_dir = app_config['app_root_location']
 
   easybib_nginx app_name do
-    config_template template
+    config_template 'scholar.conf.erb'
     deploy_dir doc_root_location
     default_router default_router
     domain_name domain_name
@@ -28,11 +24,9 @@ node['vagrant']['applications'].each do |app_name, app_config|
   end
 
   easybib_envconfig app_name do
-    # we are using stackname easybib since both is served from www-vagrant
-    stackname 'easybib'
+    stackname 'scholar'
   end
 
-  next if app_name == 'edu'
   easybib_supervisor "#{app_name}_supervisor" do
     supervisor_file "#{app_dir}/deploy/supervisor.json"
     app_dir app_dir
