1652972217f9a41b8a4dac5ef15d26e7a40b994c
fh <fh-gitcommit@fholzhauer.de>
2016-11-18 13:39:00 +0100

    Cleanup: Remove unused and broken cronjob user param

--- easybib/resources/crontab.rb
+++ easybib/resources/crontab.rb
@@ -5,4 +5,3 @@ default_action :create
 attribute :app, :default => ''
 attribute :crontab_file, :kind_of => String, :default => ''
 attribute :instance_roles, :default => []
-attribute :crontab_user, :kind_of => String, :default => 'www-data'
--- ies/recipes/role-undeploy.rb
+++ ies/recipes/role-undeploy.rb
@@ -6,7 +6,6 @@ node['deploy'].each do |application, deploy|
 
   easybib_crontab application do
     app application
-    crontab_user node['easybib_deploy']['crontab_user']
     action :delete
   end
 
