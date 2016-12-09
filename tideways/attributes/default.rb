de3faf2b415c056a7f3b524991d4847db147bbae
RomanCRS <RomanCRS@users.noreply.github.com>
2016-12-09 19:12:34 +0200

    Fix: set an explicit version for tideways packages (#1194)
    * Fix: set an explicit version for tideways packages
    
    Related: easybib/ops#251
    
    * Update: only set version for php ext
    
    Related: easybib/ops#251
    
    * Update: set daemon version
    
    Related: easybib/ops#251
    
    * Fix: style

--- tideways/attributes/default.rb
+++ tideways/attributes/default.rb
@@ -1,5 +1,7 @@
 default['tideways'] = {
   'key' => 'EEB5E8F4.asc',
   'keyserver' => nil,
-  'repository' => 'http://s3-eu-west-1.amazonaws.com/qafoo-profiler/packages'
+  'repository' => 'http://s3-eu-west-1.amazonaws.com/qafoo-profiler/packages',
+  'php_version' => '4.0.6',
+  'daemon_version' => '1.3.9'
 }
