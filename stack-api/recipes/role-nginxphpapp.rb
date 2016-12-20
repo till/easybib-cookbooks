aaccbce63c44abc81aedecd336daf8d2b1eb1d42
Brian Wiborg <baccenfutter@c-base.org>
2016-12-20 16:23:54 +0100

    add test for all php modules from production (#1201)

--- stack-api/recipes/role-nginxphpapp.rb
+++ stack-api/recipes/role-nginxphpapp.rb
@@ -12,16 +12,32 @@ if node['easybib']['cluster_name'] == 'API Staging'
   node.normal['php-apc']['package_prefix'] = 'php'
 
   php_core_deps = %W(
-    php#{php_version}-fpm
-    php#{php_version}-cli
-    php#{php_version}-mbstring
-    php#{php_version}-mysql
-    php#{php_version}-curl
+    php#{php_version}-apcu
     php#{php_version}-bcmath
+    php#{php_version}-cli
+    php#{php_version}-ctype
+    php#{php_version}-curl
     php#{php_version}-dom
+    php#{php_version}-fileinfo
+    php#{php_version}-fpm
+    php#{php_version}-iconv
+    php#{php_version}-intl
+    php#{php_version}-json
+    php#{php_version}-mbstring
+    php#{php_version}-memcache
+    php#{php_version}-pdo-mysql
+    php#{php_version}-pdo-pgsql
+    php#{php_version}-phar
+    php#{php_version}-simplexml
     php#{php_version}-soap
+    php#{php_version}-sockets
+    php#{php_version}-tidy
+    php#{php_version}-tokenizer
+    php#{php_version}-xml
+    php#{php_version}-xmlreader
+    php#{php_version}-xmlwriter
+    php#{php_version}-opcache
     php#{php_version}-zip
-    php-memcache
   )
 
   node.normal['php-fpm'] = {
