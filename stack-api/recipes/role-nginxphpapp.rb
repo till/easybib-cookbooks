0e12caed4cc0075e7459e84aa813cd901f4b22ac
Till! <till@users.noreply.github.com>
2017-01-02 17:46:46 +0100

    Update Stable (#1199)
    * Update: switch staging PHP to ondreij ppa (#1198)
    
     * same PHP version (5.6)
     * also adds sybase
     * add symlink: /usr/local/bin/php => /usr/bin/php
     * Fix: update cli immediately
    
    Related: easybib/sbm-issues#841
    
    * install php-zip (#1200)
    
    * add test for all php modules from production (#1201)

--- stack-api/recipes/role-nginxphpapp.rb
+++ stack-api/recipes/role-nginxphpapp.rb
@@ -1,6 +1,68 @@
+if node['easybib']['cluster_name'] == 'API Staging'
+  # TODO: refactor/generalize this with stack-easybib::role-vagrant
+  php_version = '5.6'
+
+  node.normal['php']['extensions']['config_dir'] = "etc/php/#{php_version}/mods-available"
+  node.normal['php']['ppa'] = {
+    'name' => 'ondrejphp',
+    'uri' => 'ppa:ondrej/php',
+    'package_prefix' => "php#{php_version}"
+  }
+
+  node.normal['php-apc']['package_prefix'] = 'php'
+
+  php_core_deps = %W(
+    php#{php_version}-apcu
+    php#{php_version}-bcmath
+    php#{php_version}-cli
+    php#{php_version}-ctype
+    php#{php_version}-curl
+    php#{php_version}-dom
+    php#{php_version}-fileinfo
+    php#{php_version}-fpm
+    php#{php_version}-iconv
+    php#{php_version}-intl
+    php#{php_version}-json
+    php#{php_version}-mbstring
+    php#{php_version}-memcache
+    php#{php_version}-pdo-mysql
+    php#{php_version}-pdo-pgsql
+    php#{php_version}-phar
+    php#{php_version}-simplexml
+    php#{php_version}-soap
+    php#{php_version}-sockets
+    php#{php_version}-tidy
+    php#{php_version}-tokenizer
+    php#{php_version}-xml
+    php#{php_version}-xmlreader
+    php#{php_version}-xmlwriter
+    php#{php_version}-opcache
+    php#{php_version}-zip
+  )
+
+  node.normal['php-fpm'] = {
+    'prefix' => '',
+    'exec_prefix' => '/usr',
+    'fpm_config' => "etc/php/#{php_version}/fpm/php.ini",
+    'cli_config' => "etc/php/#{php_version}/cli/php.ini",
+    'pool_dir' => "etc/php/#{php_version}/fpm/pool.d",
+    'socketdir' => '/var/run/php',
+    'pid' => "/var/run/php/php#{php_version}-fpm.pid",
+    'packages' => php_core_deps.join(','),
+    'user' => 'www-data',
+    'group' => 'www-data'
+  }
+end
+
+link '/usr/local/bin/php' do
+  to '/usr/bin/php'
+end
+
 include_recipe 'ies::role-phpapp'
 include_recipe 'php::module-soap'
 include_recipe 'php::module-tidy'
+
+include_recipe 'php::module-sybase' if node['easybib']['cluster_name'] == 'API Staging'
 
 if is_aws
   include_recipe 'stack-api::deploy-silex'
--- stack-api/spec/role-nginxphpapp_spec.rb
+++ stack-api/spec/role-nginxphpapp_spec.rb
@@ -1,9 +1,40 @@
 require_relative 'spec_helper'
 
 describe 'stack-api::role-nginxphpapp' do
-  let(:runner)       { ChefSpec::Runner.new }
+  let(:runner)       { ChefSpec::Runner.new(:step_into => ['php-fpm']).converge('ies::role-phpapp') }
   let(:chef_run)     { runner.converge(described_recipe) }
   let(:node)         { runner.node }
+  let(:php_version)  { '5.6' }
+  let(:php_deps)     do
+    %W(
+      php#{php_version}-apcu
+      php#{php_version}-bcmath
+      php#{php_version}-cli
+      php#{php_version}-ctype
+      php#{php_version}-curl
+      php#{php_version}-dom
+      php#{php_version}-fileinfo
+      php#{php_version}-fpm
+      php#{php_version}-iconv
+      php#{php_version}-intl
+      php#{php_version}-json
+      php#{php_version}-mbstring
+      php#{php_version}-memcache
+      php#{php_version}-pdo-mysql
+      php#{php_version}-pdo-pgsql
+      php#{php_version}-phar
+      php#{php_version}-simplexml
+      php#{php_version}-soap
+      php#{php_version}-sockets
+      php#{php_version}-tidy
+      php#{php_version}-tokenizer
+      php#{php_version}-xml
+      php#{php_version}-xmlreader
+      php#{php_version}-xmlwriter
+      php#{php_version}-opcache
+      php#{php_version}-zip
+    ).join(',')
+  end
 
   before do
     node.override['opsworks']['stack']['name'] = 'Stack'
@@ -14,6 +45,7 @@ describe 'stack-api::role-nginxphpapp' do
       :deploy_to => '/srv/www/silex',
       :document_root => 'public'
     }
+    node.override['php-fpm']['packages'] = php_deps
   end
 
   it 'pulls in ies::role-phpapp' do
@@ -22,5 +54,11 @@ describe 'stack-api::role-nginxphpapp' do
 
   it 'deploys silex' do
     expect(chef_run).to include_recipe('stack-api::deploy-silex')
+  end
+
+  it 'installs all php-module deps' do
+    php_deps.split(',').each do |dep|
+      expect(chef_run).to install_package(dep)
+    end
   end
 end
