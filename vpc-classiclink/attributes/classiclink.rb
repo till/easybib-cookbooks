15d264db6e9e075e0f2506d362ff930255b0c2e8
till <till@php.net>
2017-05-19 16:02:13 +0200

    Update: remove defaults

--- vpc-classiclink/attributes/classiclink.rb
+++ vpc-classiclink/attributes/classiclink.rb
@@ -1,7 +1,3 @@
-############
-###
-###This is the place to set ID of VPC and security group for VPC ClassicLink
-###
-############
-default['vpc-classiclink']['classiclink_vpc_id'] = 'vpc-9bb465fd'
-default['vpc-classiclink']['classiclink_security_group_id'] = 'sg-7e335900'
+# This is the place to set ID of VPC and security group for VPC ClassicLink
+default['vpc-classiclink']['classiclink_vpc_id'] = ''
+default['vpc-classiclink']['classiclink_security_group_id'] = ''
