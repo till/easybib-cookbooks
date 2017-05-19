17df2ab2803f2d5cecd9cdca52bdb7613270879f
till <till@php.net>
2017-05-19 16:05:08 +0200

    Refactor: vpc-classlink call
     * move into execute
     * be defensive about how we get values
     * skip it, if something is empty

--- vpc-classiclink/README.md
+++ vpc-classiclink/README.md
@@ -1,2 +1,22 @@
-##This cookbook enables VPC ClassicLink
-##Please create proper VPC, security group and if necessary peering connection between VPCs
+# This cookbook enables VPC ClassicLink
+
+Please create proper VPC, security group and if necessary peering connection between VPCs.
+
+## Configuration
+
+Please ensure you have an instance policy like the following setup:
+
+```
+
+```
+
+Add the following to your OpsWorks Stack custom json:
+
+```
+{
+  "vpc-classiclink": {
+    "classiclink_vpc_id": "YOUR VPC ID",
+    "classiclink_security_group_id": "YOUR SECURITY GROUP TO LINK TO"
+  }
+}
+```
--- vpc-classiclink/recipes/classiclink.rb
+++ vpc-classiclink/recipes/classiclink.rb
@@ -3,6 +3,15 @@
 # Recipe:: classiclink
 #
 
-bash "attach-classiclink-to" do
-  code "aws ec2 attach-classic-link-vpc --instance-id #{node["opsworks"]["instance"]["aws_instance_id"]} --vpc-id #{node["vpc-classiclink"]["classiclink_vpc_id"]} --groups #{node["vpc-classiclink"]["classiclink_security_group_id"]}"
+instance = get_instance
+
+instance_id = instance['aws_instance_id']
+vpc_id = node.fetch('vpc-classiclink', {}).fetch('classiclink_vpc_id', '')
+sg = node.fetch('vpc-classiclink', {}).fetch('classiclink_security_group_id', '')
+
+execute 'attach-classiclink-to' do
+  command "aws ec2 attach-classic-link-vpc --instance-id #{instance_id} --vpc-id #{vpc_id} --groups #{sg}"
+  not_if do
+    vpc_id.empty? || sg.empty?
+  end
 end
