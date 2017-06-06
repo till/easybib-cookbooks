af57f96604f046c98b469c5fd08d920f862864a7
till <till@php.net>
2017-06-06 12:46:47 +0200

    Update: berkshelf

--- stack-api/metadata.rb
+++ stack-api/metadata.rb
@@ -3,7 +3,7 @@ maintainer        'Till Klampaeckel'
 maintainer_email  'till@php.net'
 license           'BSD License'
 description       'API roles'
-version           '0.1'
+version           '0.2'
 source_url        'https://github.com/till/easybib-cookbooks' if respond_to?(:source_url)
 issues_url        'https://github.com/till/easybib-cookbooks/issues' if respond_to?(:issues_url)
 
