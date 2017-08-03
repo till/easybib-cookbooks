6be0200b764be9d492847214564098d28040320c
till <till@php.net>
2017-08-03 12:16:54 +0200

    Fix: add cronjob for /tmp overflowing
    Related: OPS-13425, easybib/sbm-issues#1512

--- stack-easybib/recipes/role-gearmanw.rb
+++ stack-easybib/recipes/role-gearmanw.rb
@@ -6,6 +6,19 @@ include_recipe 'php::module-poppler-pdf'
 
 if is_aws
   include_recipe 'stack-easybib::deploy-gearman-worker'
+
+  # bugfix: OPS-13425
+  cron_d 'pdf_autocite_cleanup' do
+    action :create
+    minute '15'
+    hour '*'
+    day '*'
+    month '*'
+    weekday '*'
+    user 'www-data'
+    command "find /tmp/ -daystart -maxdepth 1 -mmin +240 -type f -name 'pdf-autocite*' -execdir rm -- {} \;"
+  end
+
 else
   # we use our recipe instead of the default package, because
   # our recipe writes data to disk instead of memory, so it survives
