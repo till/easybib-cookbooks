6daf2ca190dfbbc9851e25894fa86438655f2d86
till <till@php.net>
2017-09-07 19:23:04 +0200

    Enhancement: enable tagging
     * add environment
     * add role loadbalancer

--- stack-easybib/recipes/role-lb.rb
+++ stack-easybib/recipes/role-lb.rb
@@ -7,6 +7,8 @@ include_recipe 'haproxy::logs'
 # haproxy::default and haproxy::configure are being executed/specified in the opsworks
 # recipe list, no need to have them here
 
+node.normal['scout']['environment'] = get_cluster_name
+node.normal['scout']['roles'] = [:loadbalancer]
 include_recipe 'ies-scout::default'
 
 package 'ngrep'
