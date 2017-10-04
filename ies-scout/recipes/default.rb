090077c51ab34aa8b80af163fafa0aa396922527
Yuriy Onuk <yuriy.onuk@gmail.com>
2017-10-04 08:38:16 +0300

    Added action to cookbook

--- ies-scout/recipes/default.rb
+++ ies-scout/recipes/default.rb
@@ -2,6 +2,7 @@ apt_repository 'scout' do
   key 'scout-archive.key'
   uri 'http://archive.scoutapp.com'
   components %w(ubuntu main)
+  action :add
 end
 
 node.normal['scout']['hostname'] = get_hostname(node)
