d1857358ffc33849220701193d335a4d58e0972f
gilleyj <gilleyj@gmail.com>
2017-10-20 14:00:55 -0400

    added new gearman ppa fixed issue with upstart.conf not being made (override only didn't cut it?)

--- ies-gearmand/recipes/service.rb
+++ ies-gearmand/recipes/service.rb
@@ -20,7 +20,7 @@ execute 'systemctl daemon-reload' do
   end
 end
 
-cookbook_file "/etc/init/#{name}.override" do
+cookbook_file "/etc/init/#{name}.conf" do
   source 'upstart.conf'
   owner 'root'
   group 'root'
--- php/recipes/module-gearman.rb
+++ php/recipes/module-gearman.rb
@@ -1,5 +1,10 @@
 include_recipe 'php::dependencies-ppa'
 
+apt_repository 'pkg-gearman' do
+  uri          node['php-gearman']['package_uri']
+  distribution node['php-gearman']['package_distro']
+end
+
 php_ppa_package 'gearman' do
   package_prefix node['php-gearman']['package_prefix']
 end
