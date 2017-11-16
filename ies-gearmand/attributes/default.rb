2c352b5ea6bc72deeaa29a8024d93df543b92eba
gilleyj <gilleyj@gmail.com>
2017-11-16 02:06:27 -0500

    stop changing ies-gearmand

--- ies-gearmand/attributes/default.rb
+++ ies-gearmand/attributes/default.rb
@@ -2,8 +2,6 @@ default['ies-gearmand']['port'] = 31_337
 default['ies-gearmand']['log']  = '--syslog -l stderr' # or --logfile=/path/to/whatever
 default['ies-gearmand']['name'] = 'gearman'
 default['ies-gearmand']['group'] = 'gearman'
-default['ies-gearmand']['package_uri'] = 'ppa:ondrej/pkg-gearman'
-default['ies-gearmand']['package_distro'] = 'trusty'
 
 # normal['php']['ppa'] = {
 #  'name' => 'ondrejphp',
