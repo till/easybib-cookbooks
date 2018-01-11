46a7f5e32826467dfdfda7ae25fb13b63288a14e
Roman Chyr <rchyr@chegg.com>
2018-01-11 12:08:15 +0200

    OPS-15568. Added source_url and issues_url

--- php-redis/metadata.rb
+++ php-redis/metadata.rb
@@ -5,5 +5,7 @@ license          'MIT'
 description      'Installs/Configures php-redis'
 long_description 'Installs/Configures php-redis'
 version          '0.1.1'
+source_url        'https://github.com/till/easybib-cookbooks' if respond_to?(:source_url)
+issues_url        'https://github.com/till/easybib-cookbooks/issues' if respond_to?(:issues_url)
 
 depends 'php'
